49f33c04f44049e183fb43d9772ec5d95de2c73b
Robert Parkinson <robert.parkinson@hmcts.net>
2017-09-12 10:13:37 +0100

    add optional code for method

--- src/test/java/uk/gov/hmcts/cmc/claimstore/models/SerializationTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/models/SerializationTest.java
@@ -37,7 +37,7 @@ public class SerializationTest {
             .withPersonalInjury(null)
             .withStatementOfTruth(null)
             .withClaimant(SampleParty.builder().withRepresentative(null).individual())
-            .withDefendant(SampleTheirDetails.builder().withRepresentative(null).individualDetails())
+            .withDefendant(SampleTheirDetails.builder().withServiceAddress(null).withRepresentative(null).individualDetails())
             .build();
 
         assertThat(claimData).isEqualTo(other);
