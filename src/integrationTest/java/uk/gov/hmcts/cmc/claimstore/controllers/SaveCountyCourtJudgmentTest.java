6310d096402764556772c1f6de9d2a9c82ae2edc
Patryk Jar <jar.patryk@gmail.com>
2017-09-15 10:47:21 +0100

    ROC-2046: remove unused imports

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/controllers/SaveCountyCourtJudgmentTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/controllers/SaveCountyCourtJudgmentTest.java
@@ -3,16 +3,11 @@ package uk.gov.hmcts.cmc.claimstore.controllers;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.HttpHeaders;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import uk.gov.hmcts.cmc.claimstore.BaseTest;
 import uk.gov.hmcts.cmc.claimstore.controllers.utils.sampledata.SampleClaim;
-import uk.gov.hmcts.cmc.claimstore.controllers.utils.sampledata.SampleClaimData;
 import uk.gov.hmcts.cmc.claimstore.idam.models.UserDetails;
 import uk.gov.hmcts.cmc.claimstore.models.Claim;
-import uk.gov.hmcts.cmc.claimstore.models.ClaimData;
-import uk.gov.hmcts.cmc.email.EmailData;
-import uk.gov.service.notify.NotificationClientException;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -20,14 +15,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,12 +24,6 @@ public class SaveCountyCourtJudgmentTest extends BaseTest {
 
     private static final long CLAIM_ID = 1L;
     private static final long CLAIMANT_ID = 123L;
-    private static final Long LETTER_HOLDER_ID = 1L;
-    private static final Long DEFENDANT_ID = 2L;
-    private static final String REFERENCE_NUMBER = "000MC001";
-    private static final String PIN = "my-pin";
-    private static final String EXTERNAL_ID = "external-id";
-    private static final boolean DEADLINE_NOT_UPDATED = false;
 
     @Before
     public void setup() {
