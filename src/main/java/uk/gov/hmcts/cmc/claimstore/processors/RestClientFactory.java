475dba18d98c528268a71dd9682e742442084e82
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-09-19 17:31:43 +0100

    reverted jsonproperty annotation for user details

--- src/main/java/uk/gov/hmcts/cmc/claimstore/processors/RestClientFactory.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/processors/RestClientFactory.java
@@ -2,16 +2,14 @@ package uk.gov.hmcts.cmc.claimstore.processors;
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.claimstore.clients.RestClient;
+import uk.gov.hmcts.cmc.claimstore.repositories.mapping.JsonMapperFactory;
 
 @Component
 public class RestClientFactory {
-    private JsonMapper jsonMapper;
-
-    public RestClientFactory(final JsonMapper jsonMapper) {
-        this.jsonMapper = jsonMapper;
-    }
 
     public RestClient create(final String url) {
-        return RestClient.Builder.of().apiDomain(url).jsonMapper(jsonMapper).build();
+        return RestClient.Builder.of().apiDomain(url)
+            .jsonMapper(JsonMapperFactory.create())
+            .build();
     }
 }
