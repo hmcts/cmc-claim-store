805c9eaf31ce22e10ac014a6eb99c22a5a59d2c2
Anirudha Deshpande <anirudha.deshpande@hmcts.net>
2017-09-21 16:45:58 +0100

    remove unused imports

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/staff/content/CCJContentProvider.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/staff/content/CCJContentProvider.java
@@ -3,14 +3,12 @@ package uk.gov.hmcts.cmc.claimstore.services.staff.content;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.claimstore.models.Claim;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class CCJContentProvider {
-    //TODO Data for default judgmenrt PDF
-
+    
     public Map<String,Object> createContent(Claim claim) {
         Map<String, Object> map = new HashMap<>();
         map.put("claim",claim);
