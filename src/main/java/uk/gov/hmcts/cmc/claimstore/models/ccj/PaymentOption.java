db5f69b0c533e0fece93da995341ca57ed5ccb7d
Anirudha Deshpande <anirudha.deshpande@hmcts.net>
2017-09-21 14:19:04 +0100

    payment option

--- src/main/java/uk/gov/hmcts/cmc/claimstore/models/ccj/PaymentOption.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/models/ccj/PaymentOption.java
@@ -1,7 +1,17 @@
 package uk.gov.hmcts.cmc.claimstore.models.ccj;
 
 public enum PaymentOption {
-    IMMEDIATELY,
-    FULL,
-    INSTALMENTS
+    IMMEDIATELY("immediately"),
+    FULL("on or before a set date"),
+    INSTALMENTS("by instalments");
+
+    String description;
+
+    PaymentOption(String description) {
+        this.description = description;
+    }
+
+    public String description() {
+        return description;
+    }
 }
