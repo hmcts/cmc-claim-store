fc2af90c598ff0c86d08f451c5b3e104d9a8f33c
jacmurphy <30896346+jacmurphy@users.noreply.github.com>
2017-10-09 18:14:10 +0100

    ROC-2062: Added stricter template binding / fail on missing values (#53)

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/TemplateServiceTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/TemplateServiceTest.java
@@ -1,35 +1,27 @@
 package uk.gov.hmcts.cmc.claimstore.services;
 
 import com.mitchellbosecke.pebble.PebbleEngine;
-import com.mitchellbosecke.pebble.error.PebbleException;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.cmc.claimstore.config.PebbleConfiguration;
 import uk.gov.hmcts.cmc.claimstore.exceptions.TemplateException;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.singletonMap;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
 public class TemplateServiceTest {
 
-    @Mock
-    private PebbleEngine mockPebble;
-    private PebbleEngine realPebble = new PebbleConfiguration().pebbleEngine();
+
+    private PebbleEngine pebble = new PebbleConfiguration().pebbleEngine();
 
     private TemplateService service;
 
     @Test
     public void shouldCorrectlyProcessTemplateWithVariables() {
-        service = new TemplateService(realPebble);
+        service = new TemplateService(pebble);
         Map<String, Object> variables = singletonMap("key", "Hello, World!");
 
         String processed = service.evaluate("{{ key }}", variables);
@@ -38,23 +30,30 @@ public class TemplateServiceTest {
     }
 
     @Test(expected = TemplateException.class)
-    public void shouldThrowTemplateExceptionWhenPebbleExceptionIsThrown() throws Exception {
-        when(mockPebble.getTemplate(anyString())).thenThrow(mock(PebbleException.class));
-        service = new TemplateService(mockPebble);
+    public void shouldThrowTemplateExceptionOnEmptyObject() {
+        service = new TemplateService(pebble);
+        Map<String, Object> variables = new HashMap<>();
 
-        service.evaluate("content doesn't matter", emptyMap());
+        service.evaluate("{{ person }}", variables);
+    }
+
+    @Test(expected = TemplateException.class)
+    public void shouldThrowTemplateExceptionWhenPebbleExceptionIsThrown() throws Exception {
+        service = new TemplateService(pebble);
+
+        service.evaluate("block {{", emptyMap());
     }
 
     @Test(expected = NullPointerException.class)
     public void shouldThrowNullPointerWhenGivenNullTemplateToMapEvaluate() {
-        service = new TemplateService(mockPebble);
+        service = new TemplateService(pebble);
 
         service.evaluate(null, emptyMap());
     }
 
     @Test(expected = NullPointerException.class)
     public void shouldThrowNullPointerWhenGivenNullValuesToMapEvaluate() {
-        service = new TemplateService(mockPebble);
+        service = new TemplateService(pebble);
 
         service.evaluate("content doesn't matter",null);
     }
