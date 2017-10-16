44593d6213b6a55b20793feb29429b1ee446f6fd
Damian Dunajski <damian.dunajski@hmcts.net>
2017-10-16 20:04:03 +0100

    Migrate defendant response copy test to use DB

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/controllers/GenerateDefendantResponseCopyTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/controllers/GenerateDefendantResponseCopyTest.java
@@ -1,43 +1,34 @@
 package uk.gov.hmcts.cmc.claimstore.controllers;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.cmc.claimstore.BaseTest;
+import uk.gov.hmcts.cmc.claimstore.controllers.utils.sampledata.SampleClaimData;
 import uk.gov.hmcts.cmc.claimstore.documents.DefendantResponseCopyService;
 import uk.gov.hmcts.cmc.claimstore.models.Claim;
 import uk.gov.hmcts.reform.cmc.pdf.service.client.exception.PDFServiceClientException;
 
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Ignore
-public class DocumentsControllerDefendantResponseCopyTest extends BaseTest {
-
-    private static final String EXTERNAL_ID = "1234dsfdf";
+public class GenerateDefendantResponseCopyTest extends BaseTest {
 
     private static final byte[] PDF_BYTES = new byte[]{1, 2, 3, 4};
-
-    @Mock
-    private Claim claim;
 
     @MockBean
     private DefendantResponseCopyService defendantResponseCopyService;
 
     @Test
     public void shouldReturnPdfDocumentIfEverythingIsFine() throws Exception {
-        when(claimRepository.getClaimByExternalId(eq(EXTERNAL_ID))).thenReturn(Optional.of(claim));
-        when(defendantResponseCopyService.createPdf(eq(claim))).thenReturn(PDF_BYTES);
+        Claim claim = claimStore.save(SampleClaimData.builder().build());
+
+        when(defendantResponseCopyService.createPdf(claim)).thenReturn(PDF_BYTES);
 
         MvcResult result = webClient
-            .perform(get("/documents/defendantResponseCopy/" + EXTERNAL_ID))
+            .perform(get("/documents/defendantResponseCopy/" + claim.getExternalId()))
             .andExpect(status().isOk())
             .andReturn();
 
@@ -46,24 +37,23 @@ public class DocumentsControllerDefendantResponseCopyTest extends BaseTest {
 
     @Test
     public void shouldReturnNotFoundWhenClaimIsNotFound() throws Exception {
-        when(claimRepository.getClaimByExternalId(eq(EXTERNAL_ID))).thenReturn(Optional.empty());
+        String nonExistingExternalId = "f5b92e36-fc9c-49e6-99f7-74d60aaa8da2";
 
         webClient
-            .perform(get("/documents/defendantResponseCopy/" + EXTERNAL_ID))
-            .andExpect(status().isNotFound())
-            .andReturn();
+            .perform(get("/documents/defendantResponseCopy/" + nonExistingExternalId))
+            .andExpect(status().isNotFound());
     }
 
     @Test
     public void shouldReturnServerErrorWhenPdfGenerationFails() throws Exception {
-        when(claimRepository.getClaimByExternalId(eq(EXTERNAL_ID))).thenReturn(Optional.of(claim));
-        when(defendantResponseCopyService.createPdf(eq(claim)))
+        Claim claim = claimStore.save(SampleClaimData.builder().build());
+
+        when(defendantResponseCopyService.createPdf(claim))
             .thenThrow(new PDFServiceClientException(new RuntimeException("Something bad happened!")));
 
         webClient
-            .perform(get("/documents/defendantResponseCopy/" + EXTERNAL_ID))
-            .andExpect(status().isInternalServerError())
-            .andReturn();
+            .perform(get("/documents/defendantResponseCopy/" + claim.getExternalId()))
+            .andExpect(status().isInternalServerError());
     }
 
 }
