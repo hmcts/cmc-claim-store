435cd964fb30507cbb9a7db878a2f492813ca8bd
Sabah Irfan <Sabah.Irfan@hmcts.net>
2017-10-16 13:58:13 +0100

    Correct regex for claim reference no

--- src/main/java/uk/gov/hmcts/cmc/claimstore/controllers/ClaimController.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/controllers/ClaimController.java
@@ -33,7 +33,7 @@ public class ClaimController {
     public static final String UUID_PATTERN = "\\p{XDigit}{8}-\\p{XDigit}"
         + "{4}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{12}";
 
-    public static final String CLAIM_REFERENCE_PATTERN = "^\\d{3}(LR|MC)\\d{3}$";
+    public static final String CLAIM_REFERENCE_PATTERN = "^\\d{3}(?:LR|MC)\\d{3}$";
 
     private final ClaimService claimService;
 
