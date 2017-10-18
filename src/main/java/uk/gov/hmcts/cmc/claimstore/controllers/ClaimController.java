b20f19dbb5c2669b7125a41c4f18d778f43ab530
Sabah Irfan <Sabah.Irfan@hmcts.net>
2017-10-18 14:19:56 +0100

    Fix merge issue

--- src/main/java/uk/gov/hmcts/cmc/claimstore/controllers/ClaimController.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/controllers/ClaimController.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.cmc.claimstore.controllers;
 
-import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
