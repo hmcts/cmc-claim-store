7a3c661c52340eceae532e8f6b0f425a0e483a12
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-10-18 11:31:52 +0100

    Remove unused import

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/BaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/BaseTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.cmc.claimstore;
 
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
