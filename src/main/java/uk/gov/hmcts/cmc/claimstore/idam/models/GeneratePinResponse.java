defc3c9a703b6e6a427780793ba7f8bf3a9c6546
Tim Jacomb <t.jacomb@kainos.com>
2017-10-30 11:15:47 +0000

    Ignore unknown idam properties (#78)
    Fridam returns some properties we don't care about, ignore them
    {"pin":"VWY0IT3I","expiry":"1519727973134","userId":"39574e6c-8fb1-40a0-a82a-dbfe2b5f1860","roleId":"1fafb9c7-4bd2-40c6-af2c-aebafb29ba88"}

--- src/main/java/uk/gov/hmcts/cmc/claimstore/idam/models/GeneratePinResponse.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/idam/models/GeneratePinResponse.java
@@ -1,7 +1,9 @@
 package uk.gov.hmcts.cmc.claimstore.idam.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GeneratePinResponse {
 
     private final String pin;
