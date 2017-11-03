16f7cce41e4c13201ab1565a2353e30eab40d8ad
Tim Jacomb <tim.jacomb@hmcts.net>
2017-11-03 10:24:44 +0000

    Update config

--- src/main/java/uk/gov/hmcts/cmc/claimstore/ClaimStoreApplication.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/ClaimStoreApplication.java
@@ -6,13 +6,13 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class, scanBasePackages = {"uk.gov.hmcts.cmc.claimstore", "uk.gov.hmcts.document"})
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @SuppressWarnings({"HideUtilityClassConstructor", "squid:S1118"}) // Spring needs a constructor, its not a utility class
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = {"uk.gov.hmcts.cmc.claimstore", "uk.gov.hmcts.document"})
 public class ClaimStoreApplication {
 
-    public static final String[] BASE_PACKAGE_NAME = {ClaimStoreApplication.class.getPackage().getName(),"uk.gov.hmcts.document"};
+    public static final String BASE_PACKAGE_NAME = ClaimStoreApplication.class.getPackage().getName();
 
     public static void main(String[] args) {
         SpringApplication.run(ClaimStoreApplication.class, args);
