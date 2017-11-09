bee19048877d43e8fdc9ecac2018e34e2151890c
yarpo <jar.patryk@gmail.com>
2017-11-09 11:24:37 +0000

    ROC-2476: code refactored according to code review comments (#86)

--- src/main/java/uk/gov/hmcts/cmc/claimstore/events/OfferRejectedEvent.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/events/OfferRejectedEvent.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.cmc.claimstore.events;
 import uk.gov.hmcts.cmc.claimstore.models.Claim;
 import uk.gov.hmcts.cmc.claimstore.models.offers.MadeBy;
 
-public class OfferRejectedEvent extends OfferDecidedEvent {
+public class OfferRejectedEvent extends OfferRespondedEvent {
 
     public OfferRejectedEvent(final Claim claim, final MadeBy party) {
         this.claim = claim;
