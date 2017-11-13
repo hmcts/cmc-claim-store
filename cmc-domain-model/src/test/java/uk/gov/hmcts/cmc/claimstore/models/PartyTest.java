c046686b6038a3274bba351d22581af3cf32e98a
sabahirfan <sabah.irfan@gmail.com>
2017-11-13 15:34:46 +0000

    ROC-2759:  Refactor out domain models and sample data (#88)

---
+++ cmc-domain-model/src/test/java/uk/gov/hmcts/cmc/claimstore/models/PartyTest.java
@@ -0,0 +1,147 @@
+package uk.gov.hmcts.cmc.claimstore.models;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+import uk.gov.hmcts.cmc.claimstore.models.party.Party;
+import uk.gov.hmcts.cmc.claimstore.models.sampledata.SampleAddress;
+import uk.gov.hmcts.cmc.claimstore.models.sampledata.SampleParty;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.cmc.claimstore.utils.BeanValidator.validate;
+
+public class PartyTest {
+
+    @Test
+    public void shouldReturnValidationErrorsWhenNameIsNull() {
+        Party party = SampleParty.builder()
+            .withName(null)
+            .party();
+
+        Set<String> validationErrors = validate(party);
+
+        assertThat(validationErrors)
+            .hasSize(1)
+            .contains("name : may not be empty");
+    }
+
+    @Test
+    public void shouldReturnValidationErrorsWhenNameIsEmpty() {
+        Party party = SampleParty.builder()
+            .withName("")
+            .party();
+
+        Set<String> validationErrors = validate(party);
+
+        assertThat(validationErrors)
+            .hasSize(1)
+            .contains("name : may not be empty");
+    }
+
+    @Test
+    public void shouldReturnValidationErrorsWhenNameIsTooLong() {
+        Party party = SampleParty.builder()
+            .withName(StringUtils.repeat("nana", 200))
+            .party();
+
+        Set<String> validationErrors = validate(party);
+
+        assertThat(validationErrors)
+            .hasSize(1)
+            .contains("name : may not be longer than 255 characters");
+    }
+
+    @Test
+    public void shouldReturnValidationErrorsWhenGivenNullAddress() {
+        Party party = SampleParty.builder()
+            .withAddress(null)
+            .party();
+
+        Set<String> validationErrors = validate(party);
+
+        assertThat(validationErrors)
+            .hasSize(1)
+            .contains("address : may not be null");
+    }
+
+    @Test
+    public void shouldReturnValidationErrorsWhenGivenInvalidAddress() {
+        Party party = SampleParty.builder()
+            .withAddress(SampleAddress.builder()
+                .withPostcode("")
+                .build())
+            .party();
+
+        Set<String> validationErrors = validate(party);
+
+        assertThat(validationErrors)
+            .hasSize(1)
+            .contains("address.postcode : Postcode should not be empty");
+    }
+
+    @Test
+    public void shouldBeValidWhenGivenNotGivenCorrespondenceAddress() {
+        Party party = SampleParty.builder()
+            .withCorrespondenceAddress(null)
+            .party();
+
+        Set<String> validationErrors = validate(party);
+
+        assertThat(validationErrors).isEmpty();
+    }
+
+    @Test
+    public void shouldReturnValidationErrorsWhenGivenInvalidCorrespondenceAddress() {
+        Party party = SampleParty.builder()
+            .withCorrespondenceAddress(SampleAddress.builder()
+                .withPostcode("")
+                .build())
+            .party();
+
+        Set<String> validationErrors = validate(party);
+
+        assertThat(validationErrors)
+            .hasSize(1)
+            .contains("correspondenceAddress.postcode : Postcode should not be empty");
+    }
+
+    @Test
+    public void shouldReturnNoValidationErrorsWhenGivenNullMobilePhone() {
+        Party party = SampleParty.builder()
+            .withMobilePhone(null)
+            .party();
+
+        Set<String> validationErrors = validate(party);
+
+        assertThat(validationErrors)
+            .hasSize(0);
+    }
+
+    @Test
+    public void shouldReturnValidationErrorsWhenGivenEmptyMobilePhone() {
+        Party party = SampleParty.builder()
+            .withMobilePhone("")
+            .party();
+
+        Set<String> validationErrors = validate(party);
+
+        assertThat(validationErrors)
+            .hasSize(1)
+            .contains("mobilePhone : Mobile number is not valid UK number");
+    }
+
+    @Test
+    public void shouldReturnValidationErrorsWhenGivenInvalidMobilePhone() {
+        Party party = SampleParty.builder()
+            .withMobilePhone("432423")
+            .party();
+
+        Set<String> validationErrors = validate(party);
+
+        assertThat(validationErrors)
+            .hasSize(1)
+            .contains("mobilePhone : Mobile number is not valid UK number");
+    }
+
+}
--- src/test/java/uk/gov/hmcts/cmc/claimstore/events/utils/sampledata/SampleClaimIssuedEvent.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/events/utils/sampledata/SampleClaimIssuedEvent.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.cmc.claimstore.events.utils.sampledata;
 
-import uk.gov.hmcts.cmc.claimstore.controllers.utils.sampledata.SampleClaim;
 import uk.gov.hmcts.cmc.claimstore.models.Claim;
+import uk.gov.hmcts.cmc.claimstore.models.sampledata.SampleClaim;
 
 public final class SampleClaimIssuedEvent {
 
