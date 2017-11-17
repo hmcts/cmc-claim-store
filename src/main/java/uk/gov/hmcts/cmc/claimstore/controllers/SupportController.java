833c0a5b1fd9f1b235ffa593172f8398e2416437
Tim Jacomb <tim.jacomb@hmcts.net>
2017-11-17 13:16:47 +0000

    Almost working

--- src/main/java/uk/gov/hmcts/cmc/claimstore/controllers/SupportController.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/controllers/SupportController.java
@@ -14,6 +14,8 @@ import uk.gov.hmcts.cmc.claimstore.events.ccj.CCJStaffNotificationHandler;
 import uk.gov.hmcts.cmc.claimstore.events.ccj.CountyCourtJudgmentRequestedEvent;
 import uk.gov.hmcts.cmc.claimstore.events.claim.ClaimIssuedEvent;
 import uk.gov.hmcts.cmc.claimstore.events.claim.ClaimIssuedStaffNotificationHandler;
+import uk.gov.hmcts.cmc.claimstore.events.offer.OfferAcceptedEvent;
+import uk.gov.hmcts.cmc.claimstore.events.offer.OfferAcceptedStaffNotificationHandler;
 import uk.gov.hmcts.cmc.claimstore.events.response.DefendantResponseEvent;
 import uk.gov.hmcts.cmc.claimstore.events.response.DefendantResponseStaffNotificationHandler;
 import uk.gov.hmcts.cmc.claimstore.events.response.MoreTimeRequestedEvent;
@@ -37,6 +39,7 @@ public class SupportController {
     private final MoreTimeRequestedStaffNotificationHandler moreTimeRequestedStaffNotificationHandler;
     private final DefendantResponseStaffNotificationHandler defendantResponseStaffNotificationHandler;
     private final CCJStaffNotificationHandler ccjStaffNotificationHandler;
+    private final OfferAcceptedStaffNotificationHandler offerAcceptedStaffNotificationHandler;
 
     @Autowired
     public SupportController(
@@ -45,14 +48,16 @@ public class SupportController {
         final ClaimIssuedStaffNotificationHandler claimIssuedStaffNotificationHandler,
         final MoreTimeRequestedStaffNotificationHandler moreTimeRequestedStaffNotificationHandler,
         final DefendantResponseStaffNotificationHandler defendantResponseStaffNotificationHandler,
-        final CCJStaffNotificationHandler ccjStaffNotificationHandler
-    ) {
+        final CCJStaffNotificationHandler ccjStaffNotificationHandler,
+        final OfferAcceptedStaffNotificationHandler offerAcceptedStaffNotificationHandler
+        ) {
         this.claimRepository = claimRepository;
         this.userService = userService;
         this.claimIssuedStaffNotificationHandler = claimIssuedStaffNotificationHandler;
         this.moreTimeRequestedStaffNotificationHandler = moreTimeRequestedStaffNotificationHandler;
         this.defendantResponseStaffNotificationHandler = defendantResponseStaffNotificationHandler;
         this.ccjStaffNotificationHandler = ccjStaffNotificationHandler;
+        this.offerAcceptedStaffNotificationHandler = offerAcceptedStaffNotificationHandler;
     }
 
     @PutMapping("/claim/{referenceNumber}/event/{event}/resend-staff-notifications")
@@ -79,11 +84,13 @@ public class SupportController {
             case "ccj-request-submitted":
                 resendStaffNotificationCCJRequestSubmitted(claim);
                 break;
+            case "offer-accepted":
+                resendStaffNotificationOnOfferAccepted(claim);
+                break;
             default:
                 throw new NotFoundException("Event " + event + " is not supported");
         }
     }
-
 
     private void validateAuthorisationPresentWhenRequired(final String authorisation)
         throws ServletRequestBindingException {
@@ -144,6 +151,14 @@ public class SupportController {
         }
         final DefendantResponseEvent event = new DefendantResponseEvent(claim);
         defendantResponseStaffNotificationHandler.onDefendantResponseSubmitted(event);
+    }
+
+    private void resendStaffNotificationOnOfferAccepted(final Claim claim) {
+        if (claim.getSettlementReachedAt() == null) {
+            throw new ConflictException("Claim " + claim.getId() + " does not have a settlement");
+        }
+        final OfferAcceptedEvent event = new OfferAcceptedEvent(claim, null);
+        offerAcceptedStaffNotificationHandler.onOfferAccepted(event);
     }
 
 }
