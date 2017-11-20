e70f63a1f131007d2787b0bffa6b6f0ad9c69a0b
Tim Jacomb <t.jacomb@kainos.com>
2017-11-20 14:43:09 +0000

    ROC-2477 Add settlement agreement to document controller (#98)

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/DocumentsService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/DocumentsService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.cmc.claimstore.documents.CountyCourtJudgmentPdfService;
 import uk.gov.hmcts.cmc.claimstore.documents.DefendantResponseCopyService;
+import uk.gov.hmcts.cmc.claimstore.documents.SettlementAgreementCopyService;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 
 @Service
@@ -12,19 +13,22 @@ public class DocumentsService {
     private final ClaimService claimService;
     private final DefendantResponseCopyService defendantResponseCopyService;
     private final CountyCourtJudgmentPdfService countyCourtJudgmentPdfService;
+    private final SettlementAgreementCopyService settlementAgreementCopyService;
 
     @Autowired
     public DocumentsService(
         final SealedClaimDocumentService sealedClaimDocumentService,
         final ClaimService claimService,
         final DefendantResponseCopyService defendantResponseCopyService,
-        final CountyCourtJudgmentPdfService countyCourtJudgmentPdfService
+        final CountyCourtJudgmentPdfService countyCourtJudgmentPdfService,
+        final SettlementAgreementCopyService settlementAgreementCopyService
     ) {
 
         this.sealedClaimDocumentService = sealedClaimDocumentService;
         this.claimService = claimService;
         this.defendantResponseCopyService = defendantResponseCopyService;
         this.countyCourtJudgmentPdfService = countyCourtJudgmentPdfService;
+        this.settlementAgreementCopyService = settlementAgreementCopyService;
     }
 
     public byte[] generateDefendantResponseCopy(final String claimExternalId) {
@@ -39,5 +43,10 @@ public class DocumentsService {
     public byte[] generateCountyCourtJudgement(final String claimExternalId) {
         final Claim claim = claimService.getClaimByExternalId(claimExternalId);
         return countyCourtJudgmentPdfService.createPdf(claim);
+    }
+
+    public byte[] generateSettlementAgreement(final String claimExternalId) {
+        final Claim claim = claimService.getClaimByExternalId(claimExternalId);
+        return settlementAgreementCopyService.createPdf(claim);
     }
 }
