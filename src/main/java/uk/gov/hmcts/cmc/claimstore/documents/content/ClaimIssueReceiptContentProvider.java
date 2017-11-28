8373a76e0405dff1bff444c114dc3ee50408b5e7
jacmurphy <jack.murphy@hmcts.net>
2017-11-28 18:52:33 +0000

    Refactoring and removing unsued imports and variables

--- src/main/java/uk/gov/hmcts/cmc/claimstore/documents/content/ClaimIssueReceiptContentProvider.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/documents/content/ClaimIssueReceiptContentProvider.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.claimstore.services.staff.content.ClaimContentProvider;
 import uk.gov.hmcts.cmc.claimstore.services.staff.content.ClaimantContentProvider;
-import uk.gov.hmcts.cmc.claimstore.services.staff.content.InterestContentProvider;
 import uk.gov.hmcts.cmc.claimstore.services.staff.content.PersonContentProvider;
 import uk.gov.hmcts.cmc.domain.models.Claim;
-import uk.gov.hmcts.cmc.domain.models.Interest;
 import uk.gov.hmcts.cmc.domain.models.otherparty.TheirDetails;
 import uk.gov.hmcts.cmc.domain.utils.PartyUtils;
 
@@ -15,7 +13,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static java.util.Objects.requireNonNull;
-import static uk.gov.hmcts.cmc.claimstore.utils.Formatting.formatDate;
 
 @Component
 public class ClaimIssueReceiptContentProvider {
@@ -23,19 +20,16 @@ public class ClaimIssueReceiptContentProvider {
     private final ClaimantContentProvider claimantContentProvider;
     private final PersonContentProvider personContentProvider;
     private final ClaimContentProvider claimContentProvider;
-    private final InterestContentProvider interestContentProvider;
 
     @Autowired
     public ClaimIssueReceiptContentProvider(
         final ClaimantContentProvider claimantContentProvider,
         final PersonContentProvider personContentProvider,
         final ClaimContentProvider claimContentProvider,
-        final InterestContentProvider interestContentProvider)
     {
         this.claimantContentProvider = claimantContentProvider;
         this.personContentProvider = personContentProvider;
         this.claimContentProvider = claimContentProvider;
-        this.interestContentProvider = interestContentProvider;
     }
     public Map<String, Object> createContent(final Claim claim) {
         requireNonNull(claim);
@@ -57,10 +51,8 @@ public class ClaimIssueReceiptContentProvider {
             PartyUtils.getContactPerson(defendant).orElse(null),
             PartyUtils.getBusinessName(defendant).orElse(null))
         );
-
         map.put("claim", claimContentProvider.createContent(claim));
 
-        map.put("responseDeadline", formatDate(claim.getResponseDeadline())); //delete
         return map;
     }
 
