45c568a3a1945d55444fb5f7b1221c7d2a64e272
jacmurphy <jack.murphy@hmcts.net>
2017-11-29 16:36:22 +0000

    Refactoring Defendant response receipt

--- src/main/java/uk/gov/hmcts/cmc/claimstore/documents/content/DefendantResponseReceiptContentProvider.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/documents/content/DefendantResponseReceiptContentProvider.java
@@ -1,12 +1,11 @@
 package uk.gov.hmcts.cmc.claimstore.documents.content;
 
 import org.springframework.stereotype.Component;
-//import uk.gov.hmcts.cmc.claimstore.events.response.MoreTimeRequestedEvent;
+import uk.gov.hmcts.cmc.claimstore.config.properties.notifications.NotificationsProperties;
 import uk.gov.hmcts.cmc.claimstore.services.staff.content.ClaimContentProvider;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.ResponseData;
 import uk.gov.hmcts.cmc.domain.models.legalrep.StatementOfTruth;
-import uk.gov.hmcts.cmc.domain.utils.PartyUtils;
 
 
 import java.util.HashMap;
@@ -14,7 +13,6 @@ import java.util.Map;
 import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
-//import static uk.gov.hmcts.cmc.claimstore.services.notifications.NotificationReferenceBuilder.MoreTimeRequested.referenceForDefendant;
 import static uk.gov.hmcts.cmc.claimstore.utils.Formatting.formatDate;
 
 @Component
@@ -22,57 +20,42 @@ public class DefendantResponseReceiptContentProvider {
 
     private final PartyDetailsContentProvider partyDetailsContentProvider;
     private final ClaimContentProvider claimContentProvider;
+    private final NotificationsProperties notificationsProperties;
 
     public DefendantResponseReceiptContentProvider(
         final PartyDetailsContentProvider partyDetailsContentProvider,
-        final ClaimContentProvider claimContentProvider
-    ) {
+        final ClaimContentProvider claimContentProvider,
+        final NotificationsProperties notificationsProperties
+        ) {
         this.partyDetailsContentProvider = partyDetailsContentProvider;
         this.claimContentProvider = claimContentProvider;
+        this.notificationsProperties = notificationsProperties;
     }
-//    private void sendNotificationToDefendant(final MoreTimeRequestedEvent event) {
-//        notificationService.sendMail(
-//            event.getDefendantEmail(),
-//            notificationsProperties.getTemplates().getEmail().getDefendantMoreTimeRequested(),
-//            prepareNotificationParameters(event),
-//            referenceForDefendant(event.getClaim().getReferenceNumber())
-//        );
-//    }
+
 
     public Map<String, Object> createContent(final Claim claim) {
         requireNonNull(claim);
         ResponseData defendantResponse = claim.getResponse().orElseThrow(IllegalStateException::new);
 
         Map<String, Object> content = new HashMap<>();
-        content.put("claimReferenceNumber", claim.getReferenceNumber());
-        content.put("claimSubmittedOn", formatDate(claim.getCreatedAt()));
         content.put("claim", claimContentProvider.createContent(claim));
-        content.put("response", claim.getRespondedAt()); //?
-
-        content.put("freeMediation", defendantResponse.getFreeMediation()); //?
-
         content.put("defenceSubmittedOn", formatDate(claim.getRespondedAt()));
         content.put("defendant", partyDetailsContentProvider.createContent(
             claim.getClaimData().getDefendant(),
             defendantResponse.getDefendant(),
             claim.getDefendantEmail()
         ));
-
         content.put("claimant", partyDetailsContentProvider.createContent(
             claim.getClaimData().getDefendant(),
             claim.getClaimData().getClaimant(),
             claim.getSubmitterEmail()
         ));
-
-        content.put("responseDefence", defendantResponse.getDefence());
-        content.put("responseDashboardUrl", "https://localhost:3010");
-
         Optional<StatementOfTruth> optionalStatementOfTruth = defendantResponse.getStatementOfTruth();
         content.put("signerName", optionalStatementOfTruth.map((StatementOfTruth::getSignerName)).orElse(null));
         content.put("signerRole", optionalStatementOfTruth.map((StatementOfTruth::getSignerRole)).orElse(null));content.put("signerRole", optionalStatementOfTruth.map((StatementOfTruth::getSignerRole)).orElse(null));
-
+        content.put("responseDefence", defendantResponse.getDefence());
         content.put("freeMediation", defendantResponse.getFreeMediation().orElse(ResponseData.FreeMediationOption.NO).name().toLowerCase());
-       /* content.put("frontendBaseUrl", notificationsProperties.getFrontendBaseUrl());*/
+        content.put("responseDashboardUrl", notificationsProperties.getFrontendBaseUrl());
 
         return content;
     }
