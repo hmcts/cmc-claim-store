c5f19fb5ec85d7098b5125b4e74bc0898c977fe6
Surjeet Singh <surjeet.singh@hmcts.net>
2017-12-01 16:44:40 +0000

    ROC: 2760 - Adding test WIP

---
+++ ccd-client/src/test/java/uk/gov/hmcts/cmc/ccd/client/service/StartCaseTest.java
@@ -0,0 +1,69 @@
+package uk.gov.hmcts.cmc.ccd.client.service;
+
+import com.sun.tools.javadoc.Start;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.cmc.ccd.client.header.HttpHeadersFactory;
+import uk.gov.hmcts.cmc.ccd.client.model.EventRequestData;
+import uk.gov.hmcts.cmc.ccd.client.model.StartEventResponse;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.eq;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StartCaseTest {
+
+    @Mock
+    private RestTemplate restTemplate;
+    @Mock
+    private HttpHeadersFactory headersFactory;
+
+    private StartCase startCase;
+
+    private EventRequestData eventRequestData = new EventRequestData();
+
+    @Mock
+    private HttpHeaders httpHeaders;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void exchange() throws Exception {
+        when(headersFactory.getHttpHeader()).thenReturn(httpHeaders);
+        ResponseEntity<StartEventResponse> responseEntity = Mockito.mock(ResponseEntity.class);
+        HttpEntity<String> httpEntity = Mockito.mock(HttpEntity.class);
+
+        when(restTemplate.exchange(Mockito.anyString(),
+            eq(HttpMethod.GET),
+            eq(httpEntity),
+            eq(StartEventResponse.class) )).thenReturn(responseEntity);
+
+        StartEventResponse startEventResponse = startCase.exchange(eventRequestData);
+
+        Assert.assertNotNull(startEventResponse);
+        Mockito.verify(headersFactory).getHttpHeader();
+        Mockito.verify(restTemplate).exchange(Mockito.anyString(),
+            eq(HttpMethod.GET),
+            eq(httpEntity),
+            eq(StartEventResponse.class));
+
+    }
+
+    private EventRequestData getEventRequestData() {
+        EventRequestData eventRequestData = new EventRequestData();
+
+        return null;
+    }
+
+}
---
+++ ccd-client/src/test/java/uk/gov/hmcts/cmc/ccd/client/service/SubmitCaseTest.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.cmc.ccd.client.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SubmitCaseTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void submit() throws Exception {
+    }
+
+}
