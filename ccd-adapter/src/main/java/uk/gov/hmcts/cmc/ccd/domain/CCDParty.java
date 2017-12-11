bb792985fbc29e725f0308a347735066913b1989
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-12-11 12:27:39 +0000

    ccd working code

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CCDParty.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CCDParty.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.cmc.ccd.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
 public class CCDParty {
     private CCDPartyType type;
     private String email;
