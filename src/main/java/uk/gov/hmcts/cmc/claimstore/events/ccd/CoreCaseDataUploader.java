3ca151e499f55c2243a63f4225fe8f4eda5a2760
Sabah Irfan <Sabah.Irfan@hmcts.net>
2017-12-15 10:23:14 +0000

    Remove async listener

--- src/main/java/uk/gov/hmcts/cmc/claimstore/events/ccd/CoreCaseDataUploader.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/events/ccd/CoreCaseDataUploader.java
@@ -1,17 +1,19 @@
 package uk.gov.hmcts.cmc.claimstore.events.ccd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.claimstore.events.solicitor.RepresentedClaimIssuedEvent;
+import uk.gov.hmcts.cmc.claimstore.exceptions.CoreCaseDataStoreException;
 import uk.gov.hmcts.cmc.claimstore.services.ccd.CoreCaseDataService;
 
 @Component
 @ConditionalOnProperty(prefix = "feature_toggles", name = "core_case_data", havingValue = "true")
 public class CoreCaseDataUploader {
-
+    private final Logger logger = LoggerFactory.getLogger(CoreCaseDataUploader.class);
     private final CoreCaseDataService coreCaseDataService;
 
     @Autowired
@@ -19,9 +21,12 @@ public class CoreCaseDataUploader {
         this.coreCaseDataService = coreCaseDataService;
     }
 
-    @Async
     @EventListener
     public void saveClaim(RepresentedClaimIssuedEvent event) {
-        coreCaseDataService.save(event.getAuthorisation(), event.getClaim());
+        try {
+            coreCaseDataService.save(event.getAuthorisation(), event.getClaim());
+        } catch (CoreCaseDataStoreException ex) {
+            logger.error(ex.getMessage(), ex);
+        }
     }
 }
