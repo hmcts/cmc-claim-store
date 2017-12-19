2955081e41a048ac559dbe2223276d80dea1b8b3
Patryk Jar <jar.patryk@gmail.com>
2017-12-19 15:04:43 +0000

    ROC-2572: update unit test

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/InterestAmount.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/InterestAmount.java
@@ -1,16 +1,13 @@
 package uk.gov.hmcts.cmc.domain.models;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 public class InterestAmount {
 
     private BigDecimal amount;
 
     public InterestAmount(BigDecimal amount) {
-        if (amount != null) {
-            this.amount = amount.setScale(2, RoundingMode.HALF_UP);
-        }
+        this.amount = amount;
     }
 
     public BigDecimal getAmount() {
