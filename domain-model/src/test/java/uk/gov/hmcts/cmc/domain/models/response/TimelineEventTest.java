c7de047058a5bf15dc67367ec9cf08266822b043
Ranjit Rattu <rattur2002@yahoo.co.uk>
2017-12-20 16:14:21 +0000

    Removed redundant import

--- domain-model/src/test/java/uk/gov/hmcts/cmc/domain/models/response/TimelineEventTest.java
+++ domain-model/src/test/java/uk/gov/hmcts/cmc/domain/models/response/TimelineEventTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.cmc.domain.models.response;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
-import uk.gov.hmcts.cmc.domain.models.sampledata.SampleTimeLine;
 import uk.gov.hmcts.cmc.domain.models.sampledata.SampleTimeLineEvent;
 import java.util.Set;
 
