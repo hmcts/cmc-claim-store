c24a54f08e53df2bffbeffc1e3ee802f313b8244
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-12-21 15:50:18 +0000

    Configure PDF service client with our object mapper

--- src/main/java/uk/gov/hmcts/cmc/claimstore/config/PDFServiceConfiguration.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/config/PDFServiceConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.cmc.claimstore.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,10 +14,14 @@ public class PDFServiceConfiguration {
     @Bean
     public PDFServiceClient pdfServiceClient(
         RestTemplate restTemplate,
+        ObjectMapper objectMapper,
         AuthTokenGenerator authTokenGenerator,
         PDFServiceProperties properties
     ) {
-        return new PDFServiceClient(restTemplate, authTokenGenerator::generate, properties.getUrl());
+        return PDFServiceClient.builder()
+            .restOperations(restTemplate)
+            .objectMapper(objectMapper)
+            .build(authTokenGenerator::generate, properties.getUrl());
     }
 
 }
