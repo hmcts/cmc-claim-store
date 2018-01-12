11f7485e215790347891e2567fe24c64de86d0dc
Sabah Irfan <sabah.irfan@gmail.com>
2018-01-12 16:50:30 +0000

    Make reusable function to remove duplidate and fix anonymous function

--- src/main/resources/db/migration/V2018_01_12_1645__Merge_title_and_name_for_individuals.sql
+++ src/main/resources/db/migration/V2018_01_12_1645__Merge_title_and_name_for_individuals.sql
@@ -4,33 +4,34 @@
   name: { title, name } => name: {'Mr. full name'}
  */
 
--- Merge title and name for claimants
-DO $$
+-- Function to merge title and name
+
+CREATE OR REPLACE FUNCTION merge_title_and_name(p_partyType varchar ) RETURNS void AS $$
 DECLARE
       length int;
       i int;
 BEGIN
     FOR i IN SELECT id FROM claim
     LOOP
-    SELECT INTO length jsonb_array_length(jsonb_extract_path(claim::JSONB, 'claimants')) from Claim where id =id;
+    SELECT INTO length jsonb_array_length(jsonb_extract_path(claim::JSONB, p_partyType)) from Claim where id =id;
     FOR counter IN 0..length-1 LOOP
          WITH subquery AS (
           SELECT
             id,
             COALESCE(
               NULLIF(
-            ' ' || TRIM(json_extract_path_text(claim :: JSON, 'claimants', counter::text, 'title')) || ' ',
+            ' ' || TRIM(json_extract_path_text(claim :: JSON, p_partyType, counter::text, 'title')) || ' ',
             '  '),
           ' ')
-        || TRIM(json_extract_path_text(claim :: JSON, 'claimants', counter::text, 'name')) AS name,
-        json_extract_path_text(claim :: JSON, 'claimants', counter::text, 'type') as type
+        || TRIM(json_extract_path_text(claim :: JSON, p_partyType, counter::text, 'name')) AS name,
+        json_extract_path_text(claim :: JSON, p_partyType, counter::text, 'type') as type
           FROM claim
           where id = i
           )
         UPDATE
           claim
         SET
-          claim = jsonb_set(claim, ('{claimants, '  || counter || ', name}')::text[], to_jsonb(name) ,true)
+          claim = jsonb_set(claim, ('{'||p_partyType||', '  || counter || ', name}')::text[], to_jsonb(name) ,true)
         FROM
           subquery
         WHERE
@@ -39,113 +40,55 @@ BEGIN
           and claim.id=i;
     END LOOP;
     END LOOP;
-END; $$
+    END;
+$$ LANGUAGE plpgsql;
+
+
+
+-- Function to remove title from claimants
+
+CREATE OR REPLACE FUNCTION remove_title_from_individuals(p_partyType varchar ) RETURNS void AS $$
+DECLARE
+	length int;
+	i int;
+BEGIN
+	FOR i IN SELECT id FROM claim
+	LOOP
+		SELECT INTO length jsonb_array_length(jsonb_extract_path(claim::JSONB, p_partyType)) from Claim where id =id;
+		FOR counter IN 0..length-1
+		LOOP
+
+		WITH subquery AS (
+			SELECT
+			id,
+			jsonb_extract_path(claim::JSONB, p_partyType, counter::text) - 'title' as claimant,
+			json_extract_path_text(claim :: JSON, p_partyType, counter::text, 'type') as type
+			FROM claim
+			where id = i
+		)
+		UPDATE
+		claim
+			SET
+			claim = jsonb_set(claim, ('{'||p_partyType||', '  ||counter || '}')::text[], to_jsonb(claimant) ,true)
+		FROM
+			subquery
+		WHERE
+			subquery.id = claim.id
+			and subquery.type='individual'
+			and claim.id=i;
+		END LOOP;
+	END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Merge title and name for claimants
+SELECT merge_title_and_name('claimants');
 
 -- Remove title from claimants
-
-DO $$
-DECLARE
-	length int;
-	i int;
-BEGIN
-	FOR i IN SELECT id FROM claim
-	LOOP
-		SELECT INTO length jsonb_array_length(jsonb_extract_path(claim::JSONB, 'claimants')) from Claim where id =id;
-		FOR counter IN 0..length-1
-		LOOP
-
-		WITH subquery AS (
-			SELECT
-			id,
-			jsonb_extract_path(claim::JSONB, 'claimants', counter::text) - 'title' as claimant,
-			json_extract_path_text(claim :: JSON, 'claimants', counter::text, 'type') as type
-			FROM claim
-			where id = i
-		)
-		UPDATE
-		claim
-			SET
-			claim = jsonb_set(claim, ('{claimants, '  || counter || '}')::text[], to_jsonb(claimant) ,true)
-		FROM
-			subquery
-		WHERE
-			subquery.id = claim.id
-			and subquery.type='individual'
-			and claim.id=i;
-		END LOOP;
-	END LOOP;
-END; $$
-
+SELECT remove_title_from_individuals('claimants');
 
 -- Merge title and name for defendants
-
-DO $$
-DECLARE
-      length int;
-      i int;
-BEGIN
-    FOR i IN SELECT id FROM claim
-    LOOP
-    SELECT INTO length jsonb_array_length(jsonb_extract_path(claim::JSONB, 'defendants')) from Claim where id =id;
-    FOR counter IN 0..length-1 LOOP
-         WITH subquery AS (
-          SELECT
-            id,
-            COALESCE(
-              NULLIF(
-            ' ' || TRIM(json_extract_path_text(claim :: JSON, 'defendants', counter::text, 'title')) || ' ',
-            '  '),
-          ' ')
-        || TRIM(json_extract_path_text(claim :: JSON, 'defendants', counter::text, 'name')) AS name,
-        json_extract_path_text(claim :: JSON, 'defendants', counter::text, 'type') as type
-          FROM claim
-          where id = i
-          )
-        UPDATE
-          claim
-        SET
-          claim = jsonb_set(claim, ('{defendants, '  || counter || ', name}')::text[], to_jsonb(name) ,true)
-        FROM
-          subquery
-        WHERE
-          subquery.id = claim.id
-          and subquery.type='individual'
-          and claim.id=i;
-    END LOOP;
-    END LOOP;
-END; $$
+SELECT merge_title_and_name('defendants');
 
 -- Remove title from defendants
-
-DO $$
-DECLARE
-	length int;
-	i int;
-BEGIN
-	FOR i IN SELECT id FROM claim
-	LOOP
-		SELECT INTO length jsonb_array_length(jsonb_extract_path(claim::JSONB, 'defendants')) from Claim where id =id;
-		FOR counter IN 0..length-1
-		LOOP
-
-		WITH subquery AS (
-			SELECT
-			id,
-			jsonb_extract_path(claim::JSONB, 'defendants', counter::text) - 'title' as claimant,
-			json_extract_path_text(claim :: JSON, 'defendants', counter::text, 'type') as type
-			FROM claim
-			where id = i
-		)
-		UPDATE
-		claim
-			SET
-			claim = jsonb_set(claim, ('{defendants, '  || counter || '}')::text[], to_jsonb(claimant) ,true)
-		FROM
-			subquery
-		WHERE
-			subquery.id = claim.id
-			and subquery.type='individual'
-			and claim.id=i;
-		END LOOP;
-	END LOOP;
-END; $$
+SELECT remove_title_from_individuals('defendants');
