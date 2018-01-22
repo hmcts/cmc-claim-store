982e859815a37c0df1cf478e011515f9b2a8b66d
Sabah Irfan <sabah.irfan@gmail.com>
2018-01-22 12:46:43 +0000

    Add mappings for county court judgment component

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/mapper/CaseMapper.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/mapper/CaseMapper.java
@@ -2,7 +2,9 @@ package uk.gov.hmcts.cmc.ccd.mapper;
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.ccd.domain.CCDCase;
+import uk.gov.hmcts.cmc.ccd.mapper.ccj.CountyCourtJudgmentMapper;
 import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.CountyCourtJudgment;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,15 +16,27 @@ import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
 public class CaseMapper implements Mapper<CCDCase, Claim> {
 
     private final ClaimMapper claimMapper;
+    private final CountyCourtJudgmentMapper countyCourtJudgmentMapper;
 
-    public CaseMapper(ClaimMapper claimMapper) {
+    public CaseMapper(ClaimMapper claimMapper, CountyCourtJudgmentMapper countyCourtJudgmentMapper) {
         this.claimMapper = claimMapper;
+        this.countyCourtJudgmentMapper = countyCourtJudgmentMapper;
     }
 
     @Override
     public CCDCase to(Claim claim) {
 
-        return CCDCase.builder()
+        CCDCase.CCDCaseBuilder builder = CCDCase.builder();
+
+        if (claim.getCountyCourtJudgment() != null) {
+            builder.countyCourtJudgment(countyCourtJudgmentMapper.to(claim.getCountyCourtJudgment()));
+        }
+
+        if (claim.getCountyCourtJudgmentRequestedAt() != null) {
+            builder.countyCourtJudgmentRequestedAt(claim.getCountyCourtJudgmentRequestedAt().format(ISO_DATE_TIME));
+        }
+
+        return builder
             .id(claim.getId())
             .externalId(claim.getExternalId())
             .referenceNumber(claim.getReferenceNumber())
@@ -36,6 +50,17 @@ public class CaseMapper implements Mapper<CCDCase, Claim> {
 
     @Override
     public Claim from(CCDCase ccdCase) {
+
+        CountyCourtJudgment countyCourtJudgment = null;
+        if (ccdCase.getCountyCourtJudgment() != null) {
+            countyCourtJudgment = countyCourtJudgmentMapper.from(ccdCase.getCountyCourtJudgment());
+        }
+
+        LocalDateTime countyCourtJudgmentRequestedAt = null;
+        if (ccdCase.getCountyCourtJudgmentRequestedAt() != null) {
+            countyCourtJudgmentRequestedAt = LocalDateTime.parse(ccdCase.getCountyCourtJudgmentRequestedAt());
+        }
+
         return new Claim(
             ccdCase.getId(),
             ccdCase.getSubmitterId(),
@@ -52,8 +77,8 @@ public class CaseMapper implements Mapper<CCDCase, Claim> {
             null,
             null,
             null,
-            null,
-            null,
+            countyCourtJudgment,
+            countyCourtJudgmentRequestedAt,
             null,
             null,
             null
