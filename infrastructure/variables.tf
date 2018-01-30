9272dfc02eb7610befeeec325714c3509f333f4f
Simon Irving <simon@sics-ltd.co.uk>
2018-01-30 16:23:36 +0000

    RDM-1511 Point to CCD in CNP

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -24,10 +24,6 @@ variable "s2s-url" {
   default = "http://betaDevBccidamS2SLB.reform.hmcts.net"
 }
 
-variable "ccd-url" {
-  default = "http://ccd-data-store-api-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
-}
-
 variable "frontend-url" {
   default = "https://www.nonprod.moneyclaim.platform.hmcts.net"
 }
