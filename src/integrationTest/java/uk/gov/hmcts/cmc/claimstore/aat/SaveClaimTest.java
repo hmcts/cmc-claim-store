ac98cce83213b07e405f51a41280276086059b8f
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-02-06 13:56:41 +0000

    Remove commented out code

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/aat/SaveClaimTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/aat/SaveClaimTest.java
@@ -1,12 +1,9 @@
 package uk.gov.hmcts.cmc.claimstore.aat;
 
 import org.junit.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.cmc.claimstore.BaseSaveTest;
-//import uk.gov.hmcts.cmc.claimstore.RestTestClient;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.ClaimData;
 import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaimData;
@@ -23,37 +20,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     }
 )
 public class SaveClaimTest extends BaseSaveTest {
-
-//    @Autowired
-//    private RestTestClient restTestClient;
-
-//    @Test
-//    public void shouldSuccessfullySubmitClaimDataAndReturnCreatedCase() {
-//        ClaimData claimData = SampleClaimData.submittedByClaimant();
-//
-//        Claim createdCase = restTestClient.post(claimData)
-//            .then()
-//            .statusCode(HttpStatus.OK.value())
-//            .and()
-//            .extract().body().as(Claim.class);
-//
-//        assertThat(createdCase.getClaimData()).isEqualTo(claimData);
-//    }
-//
-//    @Test
-//    public void shouldReturnConflictResponseWhenClaimDataWithDuplicatedExternalIdIsSubmitted() {
-//        UUID externalId = UUID.randomUUID();
-//
-//        ClaimData claimData = SampleClaimData.submittedByClaimantBuilder()
-//            .withExternalId(externalId)
-//            .build();
-//
-//        restTestClient.post(claimData)
-//            .andReturn();
-//        restTestClient.post(claimData)
-//            .then()
-//            .statusCode(HttpStatus.CONFLICT.value());
-//    }
 
     @Test
     public void shouldReturnNewlyCreatedClaim() throws Exception {
