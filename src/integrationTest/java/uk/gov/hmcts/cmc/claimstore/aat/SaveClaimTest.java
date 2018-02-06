01768af92669e7eaa72b67c28fc9fc467f7feed6
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-02-06 15:05:27 +0000

    Use only RestAssured

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/aat/SaveClaimTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/aat/SaveClaimTest.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.web.servlet.MvcResult;
+//import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.cmc.claimstore.BaseSaveTest;
 import uk.gov.hmcts.cmc.claimstore.RestTestClient;
 import uk.gov.hmcts.cmc.claimstore.services.UserService;
@@ -33,7 +33,7 @@ public class SaveClaimTest extends BaseSaveTest {
     private RestTestClient restTestClient;
 
     @Test
-    public void shouldReturnNewlyCreatedClaim() throws Exception {
+    public void shouldReturnNewlyCreatedClaim() {
         System.out.println(">>> SaveClaimTest : AppContext " + applicationContext);
         System.out.println(">>> SaveClaimTest : AppContext hash " + applicationContext.hashCode());
         System.out.println(">>> SaveClaimTest : field UserService hash " + userService.hashCode());
@@ -43,14 +43,14 @@ public class SaveClaimTest extends BaseSaveTest {
 
         ClaimData claimData = SampleClaimData.submittedByClaimant();
 
-        System.out.println(">>> SaveClaimTest : calling with MockMvc");
-        MvcResult result = makeRequest(claimData)
-            .andExpect(status().isOk())
-            .andReturn();
-
-        assertThat(deserializeObjectFrom(result, Claim.class))
-            .extracting(Claim::getClaimData)
-            .contains(claimData);
+        //        System.out.println(">>> SaveClaimTest : calling with MockMvc");
+        //        MvcResult result = makeRequest(claimData)
+        //            .andExpect(status().isOk())
+        //            .andReturn();
+        //
+        //        assertThat(deserializeObjectFrom(result, Claim.class))
+        //            .extracting(Claim::getClaimData)
+        //            .contains(claimData);
 
         System.out.println(">>> SaveClaimTest : calling with RestAssured");
         Claim createdCase = restTestClient.post(claimData)
