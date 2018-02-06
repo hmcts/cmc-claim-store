8b5c3f4892bd5fa45574b62e680067ba0697de19
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-02-06 13:49:46 +0000

    Try disabling the RANDOM_PORT environment

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/aat/SaveClaimTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/aat/SaveClaimTest.java
@@ -1,11 +1,12 @@
 package uk.gov.hmcts.cmc.claimstore.aat;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.cmc.claimstore.BaseSaveTest;
-import uk.gov.hmcts.cmc.claimstore.RestTestClient;
+//import uk.gov.hmcts.cmc.claimstore.RestTestClient;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.ClaimData;
 import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaimData;
@@ -13,6 +14,7 @@ import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaimData;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TestPropertySource(
     properties = {
@@ -22,35 +24,59 @@ import static org.assertj.core.api.Assertions.assertThat;
 )
 public class SaveClaimTest extends BaseSaveTest {
 
-    @Autowired
-    private RestTestClient restTestClient;
+//    @Autowired
+//    private RestTestClient restTestClient;
+
+//    @Test
+//    public void shouldSuccessfullySubmitClaimDataAndReturnCreatedCase() {
+//        ClaimData claimData = SampleClaimData.submittedByClaimant();
+//
+//        Claim createdCase = restTestClient.post(claimData)
+//            .then()
+//            .statusCode(HttpStatus.OK.value())
+//            .and()
+//            .extract().body().as(Claim.class);
+//
+//        assertThat(createdCase.getClaimData()).isEqualTo(claimData);
+//    }
+//
+//    @Test
+//    public void shouldReturnConflictResponseWhenClaimDataWithDuplicatedExternalIdIsSubmitted() {
+//        UUID externalId = UUID.randomUUID();
+//
+//        ClaimData claimData = SampleClaimData.submittedByClaimantBuilder()
+//            .withExternalId(externalId)
+//            .build();
+//
+//        restTestClient.post(claimData)
+//            .andReturn();
+//        restTestClient.post(claimData)
+//            .then()
+//            .statusCode(HttpStatus.CONFLICT.value());
+//    }
 
     @Test
-    public void shouldSuccessfullySubmitClaimDataAndReturnCreatedCase() {
+    public void shouldReturnNewlyCreatedClaim() throws Exception {
         ClaimData claimData = SampleClaimData.submittedByClaimant();
 
-        Claim createdCase = restTestClient.post(claimData)
-            .then()
-            .statusCode(HttpStatus.OK.value())
-            .and()
-            .extract().body().as(Claim.class);
+        MvcResult result = makeRequest(claimData)
+            .andExpect(status().isOk())
+            .andReturn();
 
-        assertThat(createdCase.getClaimData()).isEqualTo(claimData);
+        assertThat(deserializeObjectFrom(result, Claim.class))
+            .extracting(Claim::getClaimData)
+            .contains(claimData);
     }
 
     @Test
-    public void shouldReturnConflictResponseWhenClaimDataWithDuplicatedExternalIdIsSubmitted() {
+    public void shouldFailWhenDuplicateExternalId() throws Exception {
         UUID externalId = UUID.randomUUID();
 
-        ClaimData claimData = SampleClaimData.submittedByClaimantBuilder()
-            .withExternalId(externalId)
-            .build();
+        ClaimData claimData = SampleClaimData.builder().withExternalId(externalId).build();
+        claimStore.saveClaim(claimData);
 
-        restTestClient.post(claimData)
-            .andReturn();
-        restTestClient.post(claimData)
-            .then()
-            .statusCode(HttpStatus.CONFLICT.value());
+        makeRequest(claimData)
+            .andExpect(status().isConflict());
     }
 
 }
