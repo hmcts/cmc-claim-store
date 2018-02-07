ce980f522a44d0d4519dc73dd04ab0fbcd32a053
Damian Dunajski <damdun@gmail.com>
2018-02-07 10:35:04 +0000

    Remove annotation

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/Claim.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/Claim.java
@@ -15,7 +15,6 @@ import java.util.Optional;
 
 import static uk.gov.hmcts.cmc.domain.utils.ToStringStyle.ourStyle;
 
-@JsonInclude(JsonInclude.Include.NON_ABSENT)
 /**
  * Create these fields in JSON when serialize Java object, ignore them when deserialize
  */
