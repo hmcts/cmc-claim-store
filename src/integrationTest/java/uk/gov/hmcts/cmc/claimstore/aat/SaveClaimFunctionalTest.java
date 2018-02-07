15906037a25a599d8979c96fc8ec6c89fdc9381a
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-02-07 09:57:34 +0000

    Provide mocks

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/aat/SaveClaimFunctionalTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/aat/SaveClaimFunctionalTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.cmc.claimstore.aat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +11,27 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestPropertySource;
-//import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.cmc.claimstore.BaseIntegrationTest;
-import uk.gov.hmcts.cmc.claimstore.BaseSaveTest;
 import uk.gov.hmcts.cmc.claimstore.RestTestClient;
+import uk.gov.hmcts.cmc.claimstore.idam.models.GeneratePinResponse;
+import uk.gov.hmcts.cmc.claimstore.services.JwtHelper;
 import uk.gov.hmcts.cmc.claimstore.services.UserService;
+import uk.gov.hmcts.cmc.claimstore.services.notifications.fixtures.SampleUserDetails;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.ClaimData;
 import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaimData;
 import uk.gov.hmcts.cmc.email.EmailService;
-
-import java.util.UUID;
+import uk.gov.hmcts.reform.pdf.service.client.PDFServiceClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.context.TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.cmc.claimstore.BaseSaveTest.AUTHORISATION_TOKEN;
+import static uk.gov.hmcts.cmc.claimstore.BaseSaveTest.PDF_BYTES;
 
 @TestPropertySource(
     value = "/environment.properties",
@@ -48,6 +54,29 @@ public class SaveClaimFunctionalTest {
 
     @MockBean
     protected EmailService emailService;
+
+    @MockBean
+    protected UserService userService;
+
+    @MockBean
+    protected PDFServiceClient pdfServiceClient;
+
+    @MockBean
+    protected JwtHelper jwtHelper;
+
+    @Before
+    public void setup() {
+        given(userService.getUserDetails(AUTHORISATION_TOKEN))
+            .willReturn(SampleUserDetails.builder().build());
+
+        given(userService.generatePin("John Smith", AUTHORISATION_TOKEN))
+            .willReturn(new GeneratePinResponse("my-pin", "2"));
+
+        given(pdfServiceClient.generateFromHtml(any(byte[].class), anyMap()))
+            .willReturn(PDF_BYTES);
+
+        given(jwtHelper.isSolicitor(anyString())).willReturn(false);
+    }
 
     @Test
     public void shouldReturnNewlyCreatedClaim() {
