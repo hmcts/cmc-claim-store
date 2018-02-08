447b23405e11bc5c4783d9aff6e23144c444f470
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-02-08 13:48:52 +0000

    Mock out mail sender

--- src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/BaseTest.java
+++ src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/BaseTest.java
@@ -3,10 +3,12 @@ package uk.gov.hmcts.cmc.claimstore.tests;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.cmc.claimstore.processors.JsonMapper;
+import uk.gov.hmcts.cmc.email.EmailService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,5 +24,8 @@ public abstract class BaseTest {
 
     @Autowired
     protected JsonMapper jsonMapper;
+
+    @MockBean
+    private EmailService emailService;
 
 }
