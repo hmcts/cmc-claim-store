32f29646033624f2d0b6cff56c678ddc2b54135b
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-02-15 17:13:14 +0000

    Roc-3030 Save offer request in ccd (#199)

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CaseEvent.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CaseEvent.java
@@ -5,7 +5,13 @@ public enum CaseEvent {
     SUBMIT_CLAIM("submitClaimEvent"),
     DEFAULT_CCJ_REQUESTED("DefaultCCJRequested"),
     DEFENCE_SUBMITTED("DefenceSubmitted"),
-    MORE_TIME_REQUESTED("MoreTimeRequested");
+    MORE_TIME_REQUESTED("MoreTimeRequested"),
+    OFFER_REJECTED_BY_CLAIMANT("OfferRejectedByClaimant"),
+    OFFER_REJECTED_BY_DEFENDANT("OfferRejectedByDefendant"),
+    OFFER_ACCEPTED_BY_CLAIMANT("OfferAcceptedByClaimant"),
+    OFFER_ACCEPTED_BY_DEFENDANT("OfferAcceptedByDefendant"),
+    OFFER_MADE_BY_CLAIMANT("OfferMadeByClaimant"),
+    OFFER_MADE_BY_DEFENDANT("OfferMadeByDefendant");
 
     private String value;
 
