7af150c765f5353448812515ec985eb5bae0aa04
Tim Jacomb <tim.jacomb@hmcts.net>
2018-02-20 09:49:23 +0000

    Use Azure AD to allow us vault access

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -87,11 +87,12 @@ module "claim-store-database" {
 }
 
 module "claim-store-vault" {
-  source              = "git@github.com:contino/moj-module-key-vault?ref=master"
+  source              = "git@github.com:contino/moj-module-key-vault?ref=feature/add-group-access-policy"
   name                = "cmc-claim-store-${var.env}"
   product             = "${var.product}"
   env                 = "${var.env}"
   tenant_id           = "${var.tenant_id}"
   object_id           = "${var.jenkins_AAD_objectId}"
   resource_group_name = "${module.claim-store-api.resource_group_name}"
+  product_group_object_id = "68839600-92da-4862-bb24-1259814d1384"
 }
