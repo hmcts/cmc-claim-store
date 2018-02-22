2e0839490b4182f213f3b0e86e18e126d43d0a24
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-02-22 21:17:38 +0000

    ROC-3031 Save Offer tests

---
+++ src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/functional/SettlementOfferTest.java
@@ -0,0 +1,162 @@
+package uk.gov.hmcts.cmc.claimstore.tests.functional;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import uk.gov.hmcts.cmc.claimstore.idam.models.User;
+import uk.gov.hmcts.cmc.claimstore.tests.BaseTest;
+import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.FullDefenceResponse;
+import uk.gov.hmcts.cmc.domain.models.Response;
+import uk.gov.hmcts.cmc.domain.models.offers.MadeBy;
+import uk.gov.hmcts.cmc.domain.models.offers.Offer;
+import uk.gov.hmcts.cmc.domain.models.sampledata.SampleResponse;
+import uk.gov.hmcts.cmc.domain.models.sampledata.offers.SampleOffer;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
+
+public class SettlementOfferTest extends BaseTest {
+
+    @Autowired
+    private FunctionalTestsUsers functionalTestsUsers;
+
+    @Test
+    public void shouldBeAbleToSuccessfullySubmitOffer() {
+        User defendant = idamTestService.createCitizen();
+
+        Claim updatedCase = createClaimWithResponse(defendant);
+
+
+        Offer offer = SampleOffer.validDefaults();
+
+        Claim caseWithOffer = commonOperations
+            .submitOffer(offer, updatedCase.getExternalId(), defendant.getAuthorisation(), MadeBy.DEFENDANT)
+            .then()
+            .statusCode(HttpStatus.CREATED.value())
+            .and()
+            .extract().body().as(Claim.class);
+
+        assertThat(caseWithOffer.getSettlement().isPresent()).isTrue();
+        assertThat(caseWithOffer.getSettlement().get().getPartyStatements().size()).isEqualTo(1);
+        assertThat(caseWithOffer.getSettlementReachedAt()).isNull();
+    }
+
+    @Test
+    public void shouldBeAbleToSuccessfullyAcceptOffer() {
+        User defendant = idamTestService.createCitizen();
+
+        Claim updatedCase = createClaimWithResponse(defendant);
+
+        Offer offer = SampleOffer.validDefaults();
+
+        Claim caseWithOffer = commonOperations
+            .submitOffer(offer, updatedCase.getExternalId(), defendant.getAuthorisation(), MadeBy.DEFENDANT)
+            .then()
+            .statusCode(HttpStatus.CREATED.value())
+            .and()
+            .extract().body().as(Claim.class);
+
+        Claim caseWithAcceptance = commonOperations
+            .acceptOffer(caseWithOffer.getExternalId(), defendant.getAuthorisation(), MadeBy.CLAIMANT)
+            .then()
+            .statusCode(HttpStatus.CREATED.value())
+            .and()
+            .extract().body().as(Claim.class);
+
+        assertThat(caseWithAcceptance.getSettlement().isPresent()).isTrue();
+        assertThat(caseWithAcceptance.getSettlement().get().getPartyStatements().size()).isEqualTo(2);
+        assertThat(caseWithAcceptance.getSettlementReachedAt()).isNull();
+    }
+
+    @Test
+    public void shouldBeAbleToSuccessfullyRejectOffer() {
+        User defendant = idamTestService.createCitizen();
+
+        Claim updatedCase = createClaimWithResponse(defendant);
+
+        Offer offer = SampleOffer.validDefaults();
+
+        Claim caseWithOffer = commonOperations
+            .submitOffer(offer, updatedCase.getExternalId(), defendant.getAuthorisation(), MadeBy.DEFENDANT)
+            .then()
+            .statusCode(HttpStatus.CREATED.value())
+            .and()
+            .extract().body().as(Claim.class);
+
+        Claim caseWithAcceptance = commonOperations
+            .rejectOffer(caseWithOffer.getExternalId(), defendant.getAuthorisation(), MadeBy.CLAIMANT)
+            .then()
+            .statusCode(HttpStatus.CREATED.value())
+            .and()
+            .extract().body().as(Claim.class);
+
+        assertThat(caseWithAcceptance.getSettlement().isPresent()).isTrue();
+        assertThat(caseWithAcceptance.getSettlement().get().getPartyStatements().size()).isEqualTo(2);
+        assertThat(caseWithAcceptance.getSettlementReachedAt()).isNull();
+    }
+
+    @Test
+    public void shouldBeAbleToSuccessfullyCountersignOffer() {
+        User defendant = idamTestService.createCitizen();
+
+        Claim updatedCase = createClaimWithResponse(defendant);
+
+        Offer offer = SampleOffer.validDefaults();
+
+        Claim caseWithOffer = commonOperations
+            .submitOffer(offer, updatedCase.getExternalId(), defendant.getAuthorisation(), MadeBy.DEFENDANT)
+            .then()
+            .statusCode(HttpStatus.CREATED.value())
+            .and()
+            .extract().body().as(Claim.class);
+
+        Claim caseWithAcceptance = commonOperations
+            .acceptOffer(caseWithOffer.getExternalId(), defendant.getAuthorisation(), MadeBy.CLAIMANT)
+            .then()
+            .statusCode(HttpStatus.CREATED.value())
+            .and()
+            .extract().body().as(Claim.class);
+
+        Claim caseWithCounterSign = commonOperations
+            .countersignOffer(caseWithAcceptance.getExternalId(), defendant.getAuthorisation(), MadeBy.DEFENDANT)
+            .then()
+            .statusCode(HttpStatus.CREATED.value())
+            .and()
+            .extract().body().as(Claim.class);
+
+        assertThat(caseWithCounterSign.getSettlement().isPresent()).isTrue();
+        assertThat(caseWithCounterSign.getSettlement().get().getPartyStatements().size()).isEqualTo(3);
+
+        assertThat(caseWithCounterSign.getSettlementReachedAt())
+            .isCloseTo(LocalDateTime.now(), within(10, ChronoUnit.SECONDS));
+    }
+
+    private Claim createClaimWithResponse(User defendant) {
+        Claim createdCase = commonOperations.submitClaim(
+            functionalTestsUsers.getClaimant().getAuthorisation(),
+            functionalTestsUsers.getClaimant().getUserDetails().getId()
+        );
+
+
+        commonOperations.linkDefendant(
+            createdCase.getExternalId(),
+            defendant.getAuthorisation(),
+            defendant.getUserDetails().getId()
+        );
+
+        Response response = SampleResponse.FullDefence.builder()
+            .withDefenceType(FullDefenceResponse.DefenceType.DISPUTE)
+            .build();
+
+        return commonOperations.submitResponse(response, createdCase.getExternalId(), defendant)
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .and()
+            .extract().body().as(Claim.class);
+    }
+
+}
