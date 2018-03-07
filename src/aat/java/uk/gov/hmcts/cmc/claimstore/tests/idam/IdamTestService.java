8465685c26f979c4c8ca9614792458925ff29fd4
Tim Jacomb <tim.jacomb@hmcts.net>
2018-03-07 10:14:57 +0000

    Idam linking working

--- src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/idam/IdamTestService.java
+++ src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/idam/IdamTestService.java
@@ -4,26 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.cmc.claimstore.idam.IdamApi;
-import uk.gov.hmcts.cmc.claimstore.idam.models.ActivationData;
 import uk.gov.hmcts.cmc.claimstore.idam.models.AuthenticateUserResponse;
 import uk.gov.hmcts.cmc.claimstore.idam.models.User;
-import uk.gov.hmcts.cmc.claimstore.idam.models.UserDetails;
 import uk.gov.hmcts.cmc.claimstore.services.UserService;
 import uk.gov.hmcts.cmc.claimstore.tests.AATConfiguration;
 import uk.gov.hmcts.cmc.claimstore.tests.helpers.TestData;
 
 import java.util.Base64;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @Service
 public class IdamTestService {
-    private static final String PIN = "Pin ";
-    private static final String BEARER = "Bearer ";
+    private static final String PIN_PREFIX = "Pin ";
+    private static final String BEARER_PREFIX = "Bearer ";
 
     private final IdamApi idamApi;
     private final IdamTestApi idamTestApi;
-    private final IdamTestEmailApi idamTestEmailApi;
     private final UserService userService;
     private final TestData testData;
     private final AATConfiguration aatConfiguration;
@@ -32,14 +27,12 @@ public class IdamTestService {
     public IdamTestService(
         IdamApi idamApi,
         IdamTestApi idamTestApi,
-        IdamTestEmailApi idamTestEmailApi,
         UserService userService,
         TestData testData,
         AATConfiguration aatConfiguration
     ) {
         this.idamApi = idamApi;
         this.idamTestApi = idamTestApi;
-        this.idamTestEmailApi = idamTestEmailApi;
         this.userService = userService;
         this.testData = testData;
         this.aatConfiguration = aatConfiguration;
@@ -52,23 +45,18 @@ public class IdamTestService {
     }
 
     public User createDefendant(String letterHolderId) {
-        ResponseEntity<String> pin = idamTestApi.getPinByLetterHolderId(letterHolderId);
-        String authorisation = PIN + new String(Base64.getEncoder().encode(pin.getBody().getBytes()));
-        AuthenticateUserResponse authenticateUserResponse = idamApi.authenticateUser(authorisation);
-
         String email = testData.nextUserEmail();
-        idamApi.register(new UserDetails(null, email, "john", "smith", null),
-            BEARER + authenticateUserResponse.getAccessToken());
+        String password = aatConfiguration.getSmokeTestCitizen().getPassword();
+        idamTestApi.createUser(createCitizenRequest(email, password));
 
-        ResponseEntity<?> activationEmail = idamTestEmailApi.getActivationEmail();
-        String body = (String) activationEmail.getBody();
+        ResponseEntity<String> pin = idamTestApi.getPinByLetterHolderId(letterHolderId);
+        String authorisation = PIN_PREFIX + new String(Base64.getEncoder().encode(pin.getBody().getBytes()));
+        AuthenticateUserResponse pinUser = idamApi.authenticateUser(authorisation);
 
-        ActivationData data = new ActivationData(aatConfiguration.getSmokeTestCitizen().getPassword());
-        ResponseEntity<?> activate = idamApi.activate(data, BEARER + body.split("=")[1]);
+        idamApi.upliftUser(userService.getBasicAuthHeader(email, password), pinUser.getAccessToken());
 
-        Map result = (LinkedHashMap) activate.getBody();
-        String defendantAuth = BEARER + result.get("access_token");
-        return new User(defendantAuth, idamApi.retrieveUserDetails(defendantAuth));
+        // Re-authenticate to get new roles on the user
+        return userService.authenticateUser(email, password);
     }
 
     private CreateUserRequest createCitizenRequest(String username, String password) {
