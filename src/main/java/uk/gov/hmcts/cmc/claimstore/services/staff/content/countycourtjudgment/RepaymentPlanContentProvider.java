1244a07cd930e5b2d2fbef2ac2a0afef75cf1c5f
Kiran Varma <varma.ap@gmail.com>
2018-03-16 16:01:48 +0000

    work in progress

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/staff/content/countycourtjudgment/RepaymentPlanContentProvider.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/staff/content/countycourtjudgment/RepaymentPlanContentProvider.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.cmc.claimstore.services.staff.content.countycourtjudgment;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.claimstore.services.staff.models.RepaymentPlanContent;
 import uk.gov.hmcts.cmc.domain.models.CountyCourtJudgment;
 
 import static uk.gov.hmcts.cmc.claimstore.utils.Formatting.formatDate;
 import static uk.gov.hmcts.cmc.claimstore.utils.Formatting.formatMoney;
 
+@Component
 public final class RepaymentPlanContentProvider {
 
+    @Autowired
     public RepaymentPlanContentProvider() {
     }
 
