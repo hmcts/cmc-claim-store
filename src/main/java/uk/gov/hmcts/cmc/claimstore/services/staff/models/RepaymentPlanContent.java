37ced7f9476a8a0e04e28cf48a5dbe3e9fc15e0a
Kiran Varma <varma.ap@gmail.com>
2018-03-21 10:06:46 +0000

    ROC-3258: Remove first payment amount from CCJ repayment plan (#264)

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/staff/models/RepaymentPlanContent.java
@@ -0,0 +1,51 @@
+package uk.gov.hmcts.cmc.claimstore.services.staff.models;
+
+import org.springframework.util.StringUtils;
+
+public class RepaymentPlanContent {
+    private String repaymentOption;
+    private String instalmentAmount;
+    private String firstPaymentDate;
+    private String paymentSchedule;
+    private String paySetByDate;
+
+    public RepaymentPlanContent(String repaymentOption) {
+        this.repaymentOption = repaymentOption;
+    }
+
+    public RepaymentPlanContent(String repaymentOption, String paySetByDate) {
+        this.repaymentOption = repaymentOption;
+        this.paySetByDate = paySetByDate;
+    }
+
+    public RepaymentPlanContent(
+        String repaymentOption,
+        String instalmentAmount,
+        String firstPaymentDate,
+        String paymentSchedule) {
+        this.repaymentOption = repaymentOption;
+        this.instalmentAmount = instalmentAmount;
+        this.firstPaymentDate = firstPaymentDate;
+        this.paymentSchedule = StringUtils.capitalize(paymentSchedule);
+    }
+
+    public String getRepaymentOption() {
+        return repaymentOption;
+    }
+
+    public String getInstalmentAmount() {
+        return instalmentAmount;
+    }
+
+    public String getFirstPaymentDate() {
+        return firstPaymentDate;
+    }
+
+    public String getPaymentSchedule() {
+        return paymentSchedule;
+    }
+
+    public String getPaySetByDate() {
+        return paySetByDate;
+    }
+}
