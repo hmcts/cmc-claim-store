ec6fc01b622077cf397b93d9c1ca39ee46a7e11e
Tim Jacomb <tim.jacomb@hmcts.net>
2018-03-28 10:53:54 +0100

    Revert "Revert "Revert "Workaround pipeline bug"""
    This reverts commit c6531951438dfcb08396b48405efccdee2b9b7fe.
    Hopefully works now

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -53,7 +53,6 @@ module "claim-store-api" {
   ilbIp = "${var.ilbIp}"
   is_frontend = false
   subscription = "${var.subscription}"
-  additional_host_name = ""
 
   app_settings = {
     //    logging vars
