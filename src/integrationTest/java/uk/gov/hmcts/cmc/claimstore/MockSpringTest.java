0423cdf385de8742fb9aff4bc0ba1e975ef09edb
Tim Jacomb <tim.jacomb@hmcts.net>
2018-04-08 11:24:20 +0100

    Fix tests

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/MockSpringTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/MockSpringTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.cmc.claimstore.appinsights.AppInsights;
 import uk.gov.hmcts.cmc.claimstore.processors.JsonMapper;
 import uk.gov.hmcts.cmc.claimstore.repositories.CaseRepository;
 import uk.gov.hmcts.cmc.claimstore.repositories.ClaimRepository;
@@ -82,4 +83,6 @@ public abstract class MockSpringTest {
     @MockBean
     protected CoreCaseDataService coreCaseDataService;
 
+    @MockBean
+    protected AppInsights appInsights;
 }
