7657330daea70e3e1270b03f6b85ebdce3f110f1
Sabah Irfan <sabah.irfan@gmail.com>
2018-04-16 13:55:24 +0100

    Fix issues

--- src/main/java/uk/gov/hmcts/cmc/claimstore/idam/models/AuthenticateUserResponse.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/idam/models/AuthenticateUserResponse.java
@@ -1,12 +1,14 @@
 package uk.gov.hmcts.cmc.claimstore.idam.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AuthenticateUserResponse {
     @JsonProperty("default-url")
     private String defaultUser;
     private String code;
-    @JsonProperty("access-token")
+    @JsonProperty("access_token")
     private String accessToken;
 
     public AuthenticateUserResponse(String defaultUser, String code, String accessToken) {
