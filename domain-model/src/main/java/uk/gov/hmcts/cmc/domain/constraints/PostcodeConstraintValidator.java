ad62d508d47061ee3c612cfb7d5f78368c945785
Sabah Irfan <sabah.irfan@gmail.com>
2018-04-23 10:56:36 +0100

    Compile pattern object

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/constraints/PostcodeConstraintValidator.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/constraints/PostcodeConstraintValidator.java
@@ -7,8 +7,10 @@ import javax.validation.ConstraintValidatorContext;
 
 public class PostcodeConstraintValidator implements ConstraintValidator<Postcode, String> {
 
-    private static final String PATTERN = "^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y]"
+    private static final String REGEX = "^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y]"
         + "[0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))[0-9][A-Za-z]{2})$";
+
+    private static final Pattern pattern = Pattern.compile(REGEX);
 
     @Override
     public void initialize(Postcode constraintAnnotation) {
@@ -21,6 +23,6 @@ public class PostcodeConstraintValidator implements ConstraintValidator<Postcode
             return true;
         }
         String normalised = value.replaceAll(" ", "");
-        return Pattern.matches(PATTERN, normalised);
+        return pattern.matcher(normalised).matches();
     }
 }
