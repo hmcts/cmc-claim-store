92bca7c7da34c21165a0a65a46cdaa598b424d0c
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-05-01 08:23:05 +0100

    resolve codecy error

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/utils/DatesProvider.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/utils/DatesProvider.java
@@ -6,15 +6,15 @@ import java.time.format.DateTimeFormatter;
 
 public final class DatesProvider {
 
-    private DatesProvider() {
-        // NO-OP
-    }
-
     public static final LocalDateTime NOW_IN_LOCAL_ZONE = LocalDateTimeFactory.nowInLocalZone();
     public static final LocalDate ISSUE_DATE = NOW_IN_LOCAL_ZONE.toLocalDate().plusDays(1);
     public static final LocalDate RESPONSE_DEADLINE = ISSUE_DATE.plusDays(14);
     public static final LocalDate INTEREST_DATE = NOW_IN_LOCAL_ZONE.toLocalDate().minusDays(101);
     public static final DateTimeFormatter MEDIUM_DATE_FORMAT = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
+
+    private DatesProvider() {
+        // NO-OP
+    }
 
     public static LocalDateTime toDateTime(String dateString) {
         return LocalDateTime.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
