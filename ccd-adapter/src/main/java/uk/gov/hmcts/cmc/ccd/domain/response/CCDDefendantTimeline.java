560036fa420a15b76cbe8d614447f6305a09bb54
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-05-11 15:03:27 +0100

    add json creator for ccd objects

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/response/CCDDefendantTimeline.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/response/CCDDefendantTimeline.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.cmc.ccd.domain.response;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Builder;
 import lombok.Value;
 import uk.gov.hmcts.cmc.ccd.domain.CCDCollectionElement;
@@ -12,4 +13,13 @@ import java.util.List;
 public class CCDDefendantTimeline {
     private List<CCDCollectionElement<CCDTimelineEvent>> events;
     private String comment;
+
+    @JsonCreator
+    public CCDDefendantTimeline(
+        List<CCDCollectionElement<CCDTimelineEvent>> events,
+        String comment
+    ){
+        this.events = events;
+        this.comment = comment;
+    }
 }
