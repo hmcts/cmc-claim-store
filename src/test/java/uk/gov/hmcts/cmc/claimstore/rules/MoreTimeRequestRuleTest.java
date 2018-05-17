c424a9958ad30686af0a76f033d24c382e105b43
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-05-17 11:01:12 +0100

    Extract new method to check if deadline has passed

--- src/test/java/uk/gov/hmcts/cmc/claimstore/rules/MoreTimeRequestRuleTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/rules/MoreTimeRequestRuleTest.java
@@ -1,16 +1,26 @@
 package uk.gov.hmcts.cmc.claimstore.rules;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.cmc.claimstore.exceptions.MoreTimeAlreadyRequestedException;
 import uk.gov.hmcts.cmc.claimstore.exceptions.MoreTimeRequestedAfterDeadlineException;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaim;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MoreTimeRequestRuleTest {
 
-    private final MoreTimeRequestRule moreTimeRequestRule = new MoreTimeRequestRule();
+    @Spy
+    private MoreTimeRequestRule moreTimeRequestRule = new MoreTimeRequestRule();
 
     @Test
     public void noExceptionThrownWhenMoreTimeRequestedFirstTime() {
@@ -38,4 +48,15 @@ public class MoreTimeRequestRuleTest {
             .build();
         moreTimeRequestRule.assertMoreTimeCanBeRequested(claim);
     }
+
+    @Test
+    public void shouldCallAssertIsNotPastDeadline() {
+        Claim claim = SampleClaim.builder()
+            .withResponseDeadline(LocalDate.now().plusDays(2))
+            .withMoreTimeRequested(false)
+            .build();
+        moreTimeRequestRule.assertMoreTimeCanBeRequested(claim);
+        verify(moreTimeRequestRule).assertIsNotPastDeadline(any(LocalDateTime.class), eq(claim.getResponseDeadline()));
+    }
+
 }
