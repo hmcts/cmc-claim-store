592639e0147b49efaa7312154f122bc706138377
Sabah Irfan <sabah.irfan@gmail.com>
2018-05-21 11:53:19 +0100

    Add financial details to full admission

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/response/FullAdmissionResponse.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/response/FullAdmissionResponse.java
@@ -1,13 +1,19 @@
 package uk.gov.hmcts.cmc.domain.models.response;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import uk.gov.hmcts.cmc.domain.constraints.DateNotInThePast;
 import uk.gov.hmcts.cmc.domain.models.ccj.PaymentOption;
 import uk.gov.hmcts.cmc.domain.models.legalrep.StatementOfTruth;
 import uk.gov.hmcts.cmc.domain.models.party.Party;
+import uk.gov.hmcts.cmc.domain.models.statementofmeans.FinancialDetails;
 
 import java.time.LocalDate;
+import java.util.Objects;
+import java.util.Optional;
 import javax.validation.constraints.NotNull;
+
+import static uk.gov.hmcts.cmc.domain.utils.ToStringStyle.ourStyle;
 
 public class FullAdmissionResponse extends Response {
 
@@ -21,6 +27,11 @@ public class FullAdmissionResponse extends Response {
     @DateNotInThePast
     private final LocalDate paymentDate;
 
+    private final RepaymentPlan repaymentPlan;
+
+    @NotNull
+    private final FinancialDetails financialDetails;
+
     public FullAdmissionResponse(
         YesNoOption freeMediation,
         YesNoOption moreTimeNeeded,
@@ -28,12 +39,65 @@ public class FullAdmissionResponse extends Response {
         StatementOfTruth statementOfTruth,
         DefenceType defenceType,
         PaymentOption paymentOption,
-        LocalDate paymentDate
+        LocalDate paymentDate,
+        RepaymentPlan repaymentPlan,
+        FinancialDetails financialDetails
     ) {
         super(freeMediation, moreTimeNeeded, defendant, statementOfTruth);
         this.defenceType = defenceType;
         this.paymentOption = paymentOption;
         this.paymentDate = paymentDate;
+        this.repaymentPlan = repaymentPlan;
+        this.financialDetails = financialDetails;
     }
 
+    public DefenceType getDefenceType() {
+        return defenceType;
+    }
+
+    public PaymentOption getPaymentOption() {
+        return paymentOption;
+    }
+
+    public Optional<LocalDate> getPaymentDate() {
+        return Optional.ofNullable(paymentDate);
+    }
+
+    public Optional<RepaymentPlan> getRepaymentPlan() {
+        return Optional.ofNullable(repaymentPlan);
+    }
+
+    public FinancialDetails getFinancialDetails() {
+        return financialDetails;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        if (!super.equals(other)) {
+            return false;
+        }
+        FullAdmissionResponse that = (FullAdmissionResponse) other;
+        return defenceType == that.defenceType
+            && paymentOption == that.paymentOption
+            && Objects.equals(paymentDate, that.paymentDate)
+            && Objects.equals(repaymentPlan, that.repaymentPlan)
+            && Objects.equals(financialDetails, that.financialDetails);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(super.hashCode(), defenceType, paymentOption, paymentDate, repaymentPlan, financialDetails);
+    }
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, ourStyle());
+    }
 }
--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/statementofmeans/Employment.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/statementofmeans/Employment.java
@@ -3,19 +3,31 @@ package uk.gov.hmcts.cmc.domain.models.statementofmeans;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import uk.gov.hmcts.cmc.domain.models.response.YesNoOption;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 import static uk.gov.hmcts.cmc.domain.utils.ToStringStyle.ourStyle;
 
 public class Employment {
 
     private final YesNoOption isCurrentlyEmployed;
-    private final boolean employed;
-    private final boolean selfEmployed;
+    private final boolean isEmployed;
+    private final boolean isSelfEmployed;
+    private final List<Employer> employers;
+    private final SelfEmployed selfEmployed;
 
-    public Employment(YesNoOption isCurrentlyEmployed, boolean employed, boolean selfEmployed) {
+    public Employment(
+        YesNoOption isCurrentlyEmployed,
+        boolean employed,
+        boolean isSelfEmployed,
+        List<Employer> employers,
+        SelfEmployed selfEmployed
+    ) {
         this.isCurrentlyEmployed = isCurrentlyEmployed;
-        this.employed = employed;
+        this.isEmployed = employed;
+        this.isSelfEmployed = isSelfEmployed;
+        this.employers = employers;
         this.selfEmployed = selfEmployed;
     }
 
@@ -24,11 +36,19 @@ public class Employment {
     }
 
     public boolean isEmployed() {
-        return employed;
+        return isEmployed;
     }
 
     public boolean isSelfEmployed() {
-        return selfEmployed;
+        return isSelfEmployed;
+    }
+
+    public List<Employer> getEmployers() {
+        return employers;
+    }
+
+    public Optional<SelfEmployed> getSelfEmployed() {
+        return Optional.ofNullable(selfEmployed);
     }
 
     @Override
@@ -40,15 +60,17 @@ public class Employment {
             return false;
         }
         Employment that = (Employment) other;
-        return employed == that.employed
-            && selfEmployed == that.selfEmployed
-            && isCurrentlyEmployed == that.isCurrentlyEmployed;
+        return isEmployed == that.isEmployed
+            && isSelfEmployed == that.isSelfEmployed
+            && isCurrentlyEmployed == that.isCurrentlyEmployed
+            && Objects.equals(employers, that.employers)
+            && Objects.equals(selfEmployed, that.selfEmployed);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(isCurrentlyEmployed, employed, selfEmployed);
+        return Objects.hash(isCurrentlyEmployed, isEmployed, isSelfEmployed, employers, selfEmployed);
     }
 
     @Override
