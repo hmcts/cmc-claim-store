95130e813b31599aba2383c97e7745359a32e373
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-05-22 12:33:37 +0100

    Use the new service to check if deadline has passed in CCJ Rule

--- src/test/java/uk/gov/hmcts/cmc/claimstore/rules/CountyCourtJudgmentRuleTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/rules/CountyCourtJudgmentRuleTest.java
@@ -1,18 +1,35 @@
 package uk.gov.hmcts.cmc.claimstore.rules;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.cmc.claimstore.exceptions.ForbiddenActionException;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaim;
 import uk.gov.hmcts.cmc.domain.models.sampledata.SampleCountyCourtJudgment;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static java.time.LocalDateTime.now;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CountyCourtJudgmentRuleTest {
 
-    private final CountyCourtJudgmentRule countyCourtJudgmentRule = new CountyCourtJudgmentRule();
+    @Spy
+    private ClaimDeadlineService claimDeadlineService = new ClaimDeadlineService();
+
+    private CountyCourtJudgmentRule countyCourtJudgmentRule;
+
+    @Before
+    public void beforeEachTest() {
+        countyCourtJudgmentRule = new CountyCourtJudgmentRule(claimDeadlineService);
+    }
 
     @Test
     public void shouldNotThrowExceptionWhenCCJCanBeRequested() {
@@ -30,6 +47,14 @@ public class CountyCourtJudgmentRuleTest {
     public void shouldThrowExceptionWhenUserCannotRequestCountyCourtJudgmentYet() {
         Claim claim = SampleClaim.getWithResponseDeadline(LocalDate.now().plusDays(12));
         countyCourtJudgmentRule.assertCountyCourtJudgementCanBeRequested(claim);
+    }
+
+    @Test
+    public void shouldCallClaimDeadlineServiceToCheckIfJudgementCanBeRequested() {
+        LocalDate deadlineDay = LocalDate.now().minusMonths(2);
+        Claim claim = SampleClaim.builder().withResponseDeadline(deadlineDay).build();
+        countyCourtJudgmentRule.assertCountyCourtJudgementCanBeRequested(claim);
+        verify(claimDeadlineService).isPastDeadline(any(LocalDateTime.class), eq(deadlineDay));
     }
 
     @Test(expected = ForbiddenActionException.class)
