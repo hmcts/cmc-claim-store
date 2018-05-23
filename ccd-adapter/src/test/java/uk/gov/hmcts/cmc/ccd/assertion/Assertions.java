2f30f0e8f94a916d7f65bb44f3707ce00ed2218a
Sabah Irfan <sabah.irfan@gmail.com>
2018-05-23 12:46:40 +0100

    Add unit tests

--- ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/assertion/Assertions.java
+++ ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/assertion/Assertions.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.cmc.ccd.assertion;
 
+import uk.gov.hmcts.cmc.ccd.assertion.response.FullDefenceResponseAssert;
+import uk.gov.hmcts.cmc.ccd.assertion.response.ResponseAssert;
 import uk.gov.hmcts.cmc.ccd.domain.CCDAddress;
 import uk.gov.hmcts.cmc.ccd.domain.CCDContactDetails;
 import uk.gov.hmcts.cmc.ccd.domain.CCDInterest;
@@ -40,6 +42,7 @@ import uk.gov.hmcts.cmc.domain.models.party.Party;
 import uk.gov.hmcts.cmc.domain.models.party.SoleTrader;
 import uk.gov.hmcts.cmc.domain.models.response.DefendantTimeline;
 import uk.gov.hmcts.cmc.domain.models.response.FullDefenceResponse;
+import uk.gov.hmcts.cmc.domain.models.response.Response;
 
 public class Assertions {
 
@@ -205,5 +208,9 @@ public class Assertions {
 
     public static DefendantEvidenceAssert assertThat(DefendantEvidence evidence) {
         return new DefendantEvidenceAssert(evidence);
+    }
+
+    public static ResponseAssert assertThat(Response response) {
+        return new ResponseAssert(response);
     }
 }
