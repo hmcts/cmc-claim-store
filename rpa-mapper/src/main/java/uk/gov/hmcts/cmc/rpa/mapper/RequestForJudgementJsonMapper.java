a105dd7db660f0cf9ac9fe0ca487c7a857007cd7
jacmurphy <jack.murphy@hmcts.net>
2018-05-29 12:34:05 +0100

    WIP Request for judgement

--- rpa-mapper/src/main/java/uk/gov/hmcts/cmc/rpa/mapper/RequestForJudgementJsonMapper.java
+++ rpa-mapper/src/main/java/uk/gov/hmcts/cmc/rpa/mapper/RequestForJudgementJsonMapper.java
@@ -1,7 +1,12 @@
 package uk.gov.hmcts.cmc.rpa.mapper;
 
+import jdk.nashorn.internal.runtime.options.Option;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.CountyCourtJudgment;
+import uk.gov.hmcts.cmc.domain.models.ccj.PaymentOption;
+import uk.gov.hmcts.cmc.domain.models.ccj.PaymentSchedule;
+import uk.gov.hmcts.cmc.domain.models.ccj.RepaymentPlan;
 import uk.gov.hmcts.cmc.rpa.DateFormatter;
 import uk.gov.hmcts.cmc.rpa.mapper.json.NullAwareJsonObjectBuilder;
 
@@ -9,10 +14,20 @@ import javax.json.JsonObject;
 
 import java.math.BigDecimal;
 import java.text.NumberFormat;
+import java.util.Date;
 import java.util.Locale;
+import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
 
+/*
+*           case number
+*           date of issue
+amount claimed including interest
+*           court fee
+amount already paid
+payment deadline
+ */
 @Component
 public class RequestForJudgementJsonMapper {
 
@@ -21,9 +36,46 @@ public class RequestForJudgementJsonMapper {
             .add("caseNumber", claim.getReferenceNumber())
             .add("issueDate", DateFormatter.format(claim.getIssuedOn()))
             .add("courtFee", claim.getClaimData().getFeesPaidInPound())
-            .add("alreadyPaid", formatMoney(claim.getCountyCourtJudgment().getPaidAmount().orElse(BigDecimal.ZERO)))
-            //.add("paymentDeadline", claim.getCountyCourtJudgment().getRepaymentPlan().ifPresent(plan -> claim.getCountyCourtJudgment().getClass().repaymentPlan(repaymentPlanMapper.to(plan)))
+            .add("alreadyPaid", formatMoney(claim.getCountyCourtJudgment().getPaidAmount().orElse(new BigDecimal(0))))
+            .add("paymentDeadline", getPaymentDeadline(claim.getCountyCourtJudgment()))
             .build();
+    }
+
+    private String getPaymentDeadline(CountyCourtJudgment countyCourtJudgment) {
+        String value;
+        switch (countyCourtJudgment.getPaymentOption()) {
+            case IMMEDIATELY:
+                value = "Forthwith";
+            break;
+            case FULL_BY_SPECIFIED_DATE:
+                value = "In Full by (" + DateFormatter.format(countyCourtJudgment.getPayBySetDate().get()) + ")";
+            break;
+            case INSTALMENTS:
+                value = getInstallmentValues(countyCourtJudgment.getRepaymentPlan().get());
+                break;
+            default:
+                throw new IllegalArgumentException("No payment option selected");
+
+        }
+        return value;
+    }
+
+    private String getInstallmentValues(RepaymentPlan repaymentPlan) {
+        PaymentSchedule paymentSchedule = repaymentPlan.getPaymentSchedule();
+        switch (paymentSchedule) {
+            case EACH_WEEK:
+                return "Weekly " + DateFormatter.format(repaymentPlan.getFirstPaymentDate());
+            break;
+            case EVERY_TWO_WEEKS:
+                return "Fortnightly " + DateFormatter.format(repaymentPlan.getFirstPaymentDate());
+            break;
+            case EVERY_MONTH:
+                return "Monthly " + DateFormatter.format(repaymentPlan.getFirstPaymentDate());
+            break;
+            default:
+                new IllegalArgumentException("No Payment Schedule selected");
+                break;
+        }
     }
 
     private static String formatMoney(BigDecimal amount) {
@@ -32,3 +84,4 @@ public class RequestForJudgementJsonMapper {
     }
 
 }
+
