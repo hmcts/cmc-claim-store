c3f0e33207ce008348e678c5a1009e6d7d9eb575
Surjeet Singh <surjeet.singh@hmcts.net>
2018-05-29 15:04:03 +0100

    ROC-3571: code commit for CCJ rpa work

--- rpa-mapper/src/main/java/uk/gov/hmcts/cmc/rpa/mapper/RequestForJudgementJsonMapper.java
+++ rpa-mapper/src/main/java/uk/gov/hmcts/cmc/rpa/mapper/RequestForJudgementJsonMapper.java
@@ -1,22 +1,17 @@
 package uk.gov.hmcts.cmc.rpa.mapper;
 
-import jdk.nashorn.internal.runtime.options.Option;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.CountyCourtJudgment;
-import uk.gov.hmcts.cmc.domain.models.ccj.PaymentOption;
 import uk.gov.hmcts.cmc.domain.models.ccj.PaymentSchedule;
 import uk.gov.hmcts.cmc.domain.models.ccj.RepaymentPlan;
 import uk.gov.hmcts.cmc.rpa.DateFormatter;
 import uk.gov.hmcts.cmc.rpa.mapper.json.NullAwareJsonObjectBuilder;
 
 import javax.json.JsonObject;
-
 import java.math.BigDecimal;
 import java.text.NumberFormat;
-import java.util.Date;
 import java.util.Locale;
-import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
 
@@ -36,7 +31,7 @@ public class RequestForJudgementJsonMapper {
             .add("caseNumber", claim.getReferenceNumber())
             .add("issueDate", DateFormatter.format(claim.getIssuedOn()))
             .add("courtFee", claim.getClaimData().getFeesPaidInPound())
-            .add("alreadyPaid", formatMoney(claim.getCountyCourtJudgment().getPaidAmount().orElse(new BigDecimal(0))))
+            .add("alreadyPaid", formatMoney(claim.getCountyCourtJudgment().getPaidAmount().orElse(null)))
             .add("paymentDeadline", getPaymentDeadline(claim.getCountyCourtJudgment()))
             .build();
     }
@@ -45,36 +40,29 @@ public class RequestForJudgementJsonMapper {
         String value;
         switch (countyCourtJudgment.getPaymentOption()) {
             case IMMEDIATELY:
-                value = "Forthwith";
-            break;
+                return "Forthwith";
             case FULL_BY_SPECIFIED_DATE:
-                value = "In Full by (" + DateFormatter.format(countyCourtJudgment.getPayBySetDate().get()) + ")";
-            break;
+                return "In Full by (" + DateFormatter.format(countyCourtJudgment.getPayBySetDate().get()) + ")";
             case INSTALMENTS:
-                value = getInstallmentValues(countyCourtJudgment.getRepaymentPlan().get());
-                break;
+                return getInstallmentValues(countyCourtJudgment.getRepaymentPlan().get());
             default:
                 throw new IllegalArgumentException("No payment option selected");
 
         }
-        return value;
     }
 
     private String getInstallmentValues(RepaymentPlan repaymentPlan) {
         PaymentSchedule paymentSchedule = repaymentPlan.getPaymentSchedule();
+        String firstPaymentDate = DateFormatter.format(repaymentPlan.getFirstPaymentDate());
         switch (paymentSchedule) {
             case EACH_WEEK:
-                return "Weekly " + DateFormatter.format(repaymentPlan.getFirstPaymentDate());
-            break;
+                return "Weekly " + firstPaymentDate;
             case EVERY_TWO_WEEKS:
-                return "Fortnightly " + DateFormatter.format(repaymentPlan.getFirstPaymentDate());
-            break;
+                return "Fortnightly " + firstPaymentDate;
             case EVERY_MONTH:
-                return "Monthly " + DateFormatter.format(repaymentPlan.getFirstPaymentDate());
-            break;
+                return "Monthly " + firstPaymentDate;
             default:
-                new IllegalArgumentException("No Payment Schedule selected");
-                break;
+                throw new IllegalArgumentException("No Payment Schedule selected");
         }
     }
 
