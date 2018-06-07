0bf9341ceea60798ba0a5257c034098e3d31f266
Sabah Irfan <sabah.irfan@gmail.com>
2018-06-07 16:32:30 +0100

    Remove unnecessary resource file

--- domain-model/src/test/java/uk/gov/hmcts/cmc/domain/models/ResponseTest.java
+++ domain-model/src/test/java/uk/gov/hmcts/cmc/domain/models/ResponseTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.cmc.domain.models;
 import org.junit.Test;
 import uk.gov.hmcts.cmc.domain.models.response.Response;
 import uk.gov.hmcts.cmc.domain.models.sampledata.SampleResponse;
-import uk.gov.hmcts.cmc.domain.utils.ResourceReader;
 
 import java.util.Set;
 import javax.validation.ConstraintViolation;
@@ -11,6 +10,7 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.cmc.domain.BeanValidator.validate;
 
@@ -67,10 +67,8 @@ public class ResponseTest {
     @Test
     public void shouldHaveValidationMessagesWhenDefenceExceedsSizeLimit() {
         //given
-        String defence = new ResourceReader().read("/defence_exceeding_size_limit.text");
-
         Response response = SampleResponse.FullDefence.builder()
-            .withDefence(defence)
+            .withDefence(randomAlphanumeric(99001))
             .build();
 
         //when
