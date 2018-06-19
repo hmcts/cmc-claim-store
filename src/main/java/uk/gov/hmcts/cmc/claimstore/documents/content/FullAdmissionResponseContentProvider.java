879b26d8b56f2fe2c08c0e3a602ae20b55997ee6
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-06-19 14:54:08 +0100

    Updates to content provider and template

--- src/main/java/uk/gov/hmcts/cmc/claimstore/documents/content/FullAdmissionResponseContentProvider.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/documents/content/FullAdmissionResponseContentProvider.java
@@ -2,11 +2,17 @@ package uk.gov.hmcts.cmc.claimstore.documents.content;
 
 import com.google.common.collect.ImmutableMap;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.cmc.domain.models.PaymentOption;
+import uk.gov.hmcts.cmc.domain.models.RepaymentPlan;
 import uk.gov.hmcts.cmc.domain.models.response.FullAdmissionResponse;
 
 import java.util.Map;
+import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.cmc.domain.models.PaymentOption.IMMEDIATELY;
+import static uk.gov.hmcts.cmc.domain.models.PaymentOption.FULL_BY_SPECIFIED_DATE;
+import static uk.gov.hmcts.cmc.domain.models.PaymentOption.INSTALMENTS;
 
 @Component
 public class FullAdmissionResponseContentProvider {
@@ -22,10 +28,29 @@ public class FullAdmissionResponseContentProvider {
     public Map<String, Object> createContent(FullAdmissionResponse fullAdmissionResponse) {
         requireNonNull(fullAdmissionResponse);
 
+        PaymentOption type = fullAdmissionResponse.getPaymentOption();
+        Optional<RepaymentPlan> repaymentPlan = fullAdmissionResponse.getRepaymentPlan();
         ImmutableMap.Builder<String, Object> contentBuilder = ImmutableMap.builder();
-        fullAdmissionResponse.getStatementOfMeans().ifPresent(
-            statementOfMeans -> contentBuilder.putAll(statementOfMeansContentProvider.createContent(statementOfMeans))
-        );
+
+        if (type.equals(IMMEDIATELY)) {
+            contentBuilder.put("paymentOption", type.getDescription());
+        }
+
+        if (type.equals(FULL_BY_SPECIFIED_DATE)) {
+            contentBuilder.put("paymentOption", type.getDescription());
+            fullAdmissionResponse.getStatementOfMeans().ifPresent(
+                statementOfMeans -> contentBuilder.putAll(statementOfMeansContentProvider.createContent(statementOfMeans))
+            );
+        }
+
+        if (type.equals(INSTALMENTS)) {
+            contentBuilder.put("paymentOption", type.getDescription());
+            contentBuilder.put("repaymentPlan", repaymentPlan);
+            fullAdmissionResponse.getStatementOfMeans().ifPresent(
+                statementOfMeans -> contentBuilder.putAll(statementOfMeansContentProvider.createContent(statementOfMeans))
+            );
+        }
+
         return contentBuilder.build();
     }
 }
