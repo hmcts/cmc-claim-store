da9ca3a8a302202a8eced688c517c0291224b4a5
Tim Jacomb <tim.jacomb@hmcts.net>
2018-06-19 14:30:47 +0100

    ROC-3800 Initial scheduler project

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/MockSpringTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/MockSpringTest.java
@@ -1,15 +1,23 @@
 package uk.gov.hmcts.cmc.claimstore;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import org.jetbrains.annotations.NotNull;
+import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.testcontainers.containers.PostgreSQLContainer;
 import uk.gov.hmcts.cmc.ccd.mapper.CaseMapper;
 import uk.gov.hmcts.cmc.claimstore.appinsights.AppInsights;
 import uk.gov.hmcts.cmc.claimstore.processors.JsonMapper;
@@ -18,9 +26,9 @@ import uk.gov.hmcts.cmc.claimstore.repositories.ClaimRepository;
 import uk.gov.hmcts.cmc.claimstore.repositories.TestingSupportRepository;
 import uk.gov.hmcts.cmc.claimstore.services.UserService;
 import uk.gov.hmcts.cmc.claimstore.services.bankholidays.PublicHolidaysCollection;
-import uk.gov.hmcts.cmc.claimstore.services.ccd.CoreCaseDataService;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.email.EmailService;
+import uk.gov.hmcts.cmc.scheduler.services.JobService;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CaseAccessApi;
@@ -32,13 +40,45 @@ import uk.gov.hmcts.reform.pdf.service.client.PDFServiceClient;
 import uk.gov.service.notify.NotificationClient;
 
 import java.io.UnsupportedEncodingException;
+import java.time.Duration;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext
 @AutoConfigureMockMvc
 @TestPropertySource("/environment.properties")
+@ContextConfiguration(initializers = {MockSpringTest.Initializer.class})
 public abstract class MockSpringTest {
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(@NotNull ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                "spring.datasource.claimstore.url=" + claimStorePostgreSQLContainer.getJdbcUrl(),
+                "spring.datasource.claimstore.username=" + claimStorePostgreSQLContainer.getUsername(),
+                "spring.datasource.claimstore.password=" + claimStorePostgreSQLContainer.getPassword(),
+                "spring.datasource.scheduler.url=" + schedulerPostgreSQLContainer.getJdbcUrl(),
+                "spring.datasource.scheduler.username=" + schedulerPostgreSQLContainer.getUsername(),
+                "spring.datasource.scheduler.password=" + schedulerPostgreSQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
+    @ClassRule
+    public static PostgreSQLContainer claimStorePostgreSQLContainer =
+        (PostgreSQLContainer) new PostgreSQLContainer("postgres:9.6-alpine")
+            .withDatabaseName("claimstore")
+            .withUsername("claimstore")
+            .withPassword("claimstore")
+            .withStartupTimeout(Duration.ofSeconds(600));
+
+    @ClassRule
+    public static PostgreSQLContainer schedulerPostgreSQLContainer =
+        (PostgreSQLContainer) new PostgreSQLContainer("postgres:10.3-alpine")
+            .withDatabaseName("scheduler")
+            .withUsername("scheduler")
+            .withPassword("scheduler")
+            .withStartupTimeout(Duration.ofSeconds(600));
 
     @Autowired
     protected JsonMapper jsonMapper;
@@ -89,7 +129,7 @@ public abstract class MockSpringTest {
     protected ServiceAuthorisationApi serviceAuthorisationApi;
 
     @MockBean
-    protected CoreCaseDataService coreCaseDataService;
+    protected JobService jobService;
 
     @MockBean
     protected AppInsights appInsights;
