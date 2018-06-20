7b12ca2dcf7c521e8eb13ca259005d0b40fe3e52
Tim Jacomb <t.jacomb@kainos.com>
2018-06-20 10:27:11 +0100

    ROC-3800 Initial task scheduler project (#390)

---
+++ job-scheduler/src/main/java/uk/gov/hmcts/cmc/scheduler/config/QuartzConfiguration.java
@@ -0,0 +1,109 @@
+package uk.gov.hmcts.cmc.scheduler.config;
+
+import org.flywaydb.core.Flyway;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import javax.sql.DataSource;
+
+@Configuration
+@ConfigurationProperties
+public class QuartzConfiguration {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    private final Map<String, String> quartzProperties = new HashMap<>();
+
+    @SuppressWarnings("unused") // this getter is needed by the framework
+    public Map<String, String> getQuartzProperties() {
+        return quartzProperties;
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.scheduler")
+    public DataSourceProperties schedulerDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean("schedulerDataSource")
+    @ConfigurationProperties("spring.datasource.scheduler")
+    public DataSource schedulerDataSource() {
+        return schedulerDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean("schedulerTransactionAwareDataSourceProxy")
+    public TransactionAwareDataSourceProxy transactionAwareDataSourceProxy(
+        @Qualifier("schedulerDataSource") DataSource dataSource
+    ) {
+        TransactionAwareDataSourceProxy dataSourceProxy = new TransactionAwareDataSourceProxy(dataSource);
+
+        migrateFlyway(dataSourceProxy);
+        return dataSourceProxy;
+    }
+
+    private void migrateFlyway(DataSource dataSource) {
+        final Flyway flyway = new Flyway();
+        flyway.setDataSource(dataSource);
+        flyway.setLocations("scheduler/db/migration");
+        flyway.migrate();
+    }
+
+    @Bean("schedulerTransactionManager")
+    public PlatformTransactionManager transactionManager(
+        TransactionAwareDataSourceProxy schedulerTransactionAwareDataSourceProxy
+    ) {
+        return new DataSourceTransactionManager(schedulerTransactionAwareDataSourceProxy);
+    }
+
+    @Bean
+    public SpringBeanJobFactory springBeanJobFactory() {
+        AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+        return jobFactory;
+    }
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean(
+        @Qualifier("schedulerTransactionAwareDataSourceProxy")
+            TransactionAwareDataSourceProxy transactionAwareDataSourceProxy,
+        @Qualifier("schedulerTransactionManager") PlatformTransactionManager transactionManager
+    ) {
+        Properties properties = new Properties();
+        properties.putAll(quartzProperties);
+
+        SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
+        schedulerFactory.setDataSource(transactionAwareDataSourceProxy);
+        schedulerFactory.setTransactionManager(transactionManager);
+        schedulerFactory.setQuartzProperties(properties);
+        schedulerFactory.setJobFactory(springBeanJobFactory());
+        schedulerFactory.setSchedulerName("CMC Job Scheduler");
+
+        return schedulerFactory;
+    }
+
+    @Bean
+    public Scheduler scheduler(SchedulerFactoryBean schedulerFactoryBean) throws SchedulerException {
+
+        Scheduler scheduler = schedulerFactoryBean.getScheduler();
+
+        scheduler.start();
+
+        return scheduler;
+    }
+}
