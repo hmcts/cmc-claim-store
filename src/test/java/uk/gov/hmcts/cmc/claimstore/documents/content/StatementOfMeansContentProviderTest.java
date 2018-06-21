1ca34ae9c8d043f596d3035a715db15d4016f3a9
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-06-21 13:09:27 +0100

    Initial test case for statement of means content provider

--- src/test/java/uk/gov/hmcts/cmc/claimstore/documents/content/StatementOfMeansContentProviderTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/documents/content/StatementOfMeansContentProviderTest.java
@@ -1,10 +1,31 @@
 package uk.gov.hmcts.cmc.claimstore.documents.content;
 
+import org.junit.Test;
+
+import uk.gov.hmcts.cmc.domain.models.sampledata.statementofmeans.SampleStatementOfMeans;
 import uk.gov.hmcts.cmc.domain.models.statementofmeans.StatementOfMeans;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StatementOfMeansContentProviderTest {
 
     private StatementOfMeansContentProvider provider = new StatementOfMeansContentProvider();
-    private StatementOfMeans statementOfMeans = StatementOfMeans.builder().build();
 
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerWhenGivenNullClaim() {
+        provider.createContent(null);
+    }
+
+    @Test
+    public void shouldProvideStatementOfMeans() {
+        StatementOfMeans statementOfMeans = SampleStatementOfMeans.builder().build();
+
+        Map<String, Object> content = provider.createContent(statementOfMeans);
+
+        assertThat(content)
+            .containsKeys("selfEmployment", "bankAccounts", "children",
+                "residence", "maintainedChildren", "incomes", "otherDependants",
+                "courtOrders", "expenses", "debts", "dependant", "employment");
+    }
 }
