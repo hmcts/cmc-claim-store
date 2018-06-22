2e55cde96befe9293795417f80158588c713038f
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-06-22 14:11:01 +0100

    update test case to include job type

--- src/test/java/uk/gov/hmcts/cmc/claimstore/documents/content/StatementOfMeansContentProviderTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/documents/content/StatementOfMeansContentProviderTest.java
@@ -3,9 +3,17 @@ package uk.gov.hmcts.cmc.claimstore.documents.content;
 import org.junit.Test;
 
 import uk.gov.hmcts.cmc.domain.models.sampledata.statementofmeans.SampleStatementOfMeans;
+import uk.gov.hmcts.cmc.domain.models.statementofmeans.Employer;
+import uk.gov.hmcts.cmc.domain.models.statementofmeans.Employment;
+import uk.gov.hmcts.cmc.domain.models.statementofmeans.OnTaxPayments;
+import uk.gov.hmcts.cmc.domain.models.statementofmeans.Residence;
+import uk.gov.hmcts.cmc.domain.models.statementofmeans.SelfEmployment;
 import uk.gov.hmcts.cmc.domain.models.statementofmeans.StatementOfMeans;
+import uk.gov.hmcts.cmc.domain.models.statementofmeans.Unemployment;
 import java.util.Map;
 
+import static java.math.BigDecimal.TEN;
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StatementOfMeansContentProviderTest {
@@ -28,4 +36,52 @@ public class StatementOfMeansContentProviderTest {
                 "residence", "maintainedChildren", "incomes", "otherDependants",
                 "courtOrders", "expenses", "debts", "dependant", "employment");
     }
+
+    @Test
+    public void shouldProvideStatementOfMeansWithJobTypeEmployedAndSelfmployed() {
+        StatementOfMeans statementOfMeans = StatementOfMeans.builder()
+            .residence(Residence.builder().type(Residence.ResidenceType.JOINT_OWN_HOME).build())
+            .employment(Employment.builder()
+                .employers(asList(Employer.builder().name("CMC").jobTitle("My sweet job").build()))
+                .unemployment(Unemployment.builder().retired(true).build())
+                .selfEmployment(SelfEmployment.builder()
+                    .jobTitle("Director")
+                    .annualTurnover(TEN)
+                    .onTaxPayments(OnTaxPayments.builder().amountYouOwe(TEN).reason("Genuine reason").build())
+                    .build())
+                .build()
+            )
+            .build();
+
+        Map<String, Object> content = provider.createContent(statementOfMeans);
+
+        assertThat(content)
+            .containsKeys("jobType");
+        assertThat(content)
+            .containsValues("Employed and self-employed");
+    }
+
+    @Test
+    public void shouldProvideStatementOfMeansWithJobTypeSelfEmployed() {
+        StatementOfMeans statementOfMeans = StatementOfMeans.builder()
+            .residence(Residence.builder().type(Residence.ResidenceType.JOINT_OWN_HOME).build())
+            .employment(Employment.builder()
+                .unemployment(Unemployment.builder().retired(true).build())
+                .selfEmployment(SelfEmployment.builder()
+                    .jobTitle("Director")
+                    .annualTurnover(TEN)
+                    .onTaxPayments(OnTaxPayments.builder().amountYouOwe(TEN).reason("Genuine reason").build())
+                    .build())
+                .build()
+            )
+            .build();
+
+        Map<String, Object> content = provider.createContent(statementOfMeans);
+
+        assertThat(content)
+            .containsKeys("jobType");
+        assertThat(content)
+            .containsValues("Self-employed");
+    }
+
 }
