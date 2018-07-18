1b2cf3bde0c116c69793ec0644ed84ba633dc524
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-07-18 11:34:40 +0100

    correct the query for ccj

--- src/main/resources/db/migration/claimstore/V2018_07_16_1318__Update_response_payment_option_for_full_by_set_date.sql
+++ src/main/resources/db/migration/claimstore/V2018_07_16_1318__Update_response_payment_option_for_full_by_set_date.sql
@@ -8,4 +8,4 @@ UPDATE
   claim
 SET
   county_court_judgment = JSONB_SET(county_court_judgment, '{paymentOption}',  '"BY_SPECIFIED_DATE"' :: JSONB)
-WHERE response IS NOT NULL AND jsonb_extract_path_text(county_court_judgment, 'paymentOption') = 'FULL_BY_SPECIFIED_DATE';
+WHERE county_court_judgment IS NOT NULL AND jsonb_extract_path_text(county_court_judgment, 'paymentOption') = 'FULL_BY_SPECIFIED_DATE';
