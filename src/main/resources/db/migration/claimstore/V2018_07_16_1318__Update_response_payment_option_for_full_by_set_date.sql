f2ac02b1979cd42afd888779861386db8b505da2
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-07-18 12:12:10 +0100

    keep response query in one sql file

--- src/main/resources/db/migration/claimstore/V2018_07_16_1318__Update_response_payment_option_for_full_by_set_date.sql
+++ src/main/resources/db/migration/claimstore/V2018_07_16_1318__Update_response_payment_option_for_full_by_set_date.sql
@@ -3,9 +3,3 @@ UPDATE
 SET
   response = JSONB_SET(response, '{paymentOption}',  '"BY_SPECIFIED_DATE"' :: JSONB)
 WHERE response IS NOT NULL AND jsonb_extract_path_text(response, 'paymentOption') = 'FULL_BY_SPECIFIED_DATE';
-
-UPDATE
-  claim
-SET
-  county_court_judgment = JSONB_SET(county_court_judgment, '{paymentOption}',  '"BY_SPECIFIED_DATE"' :: JSONB)
-WHERE county_court_judgment IS NOT NULL AND jsonb_extract_path_text(county_court_judgment, 'paymentOption') = 'FULL_BY_SPECIFIED_DATE';
