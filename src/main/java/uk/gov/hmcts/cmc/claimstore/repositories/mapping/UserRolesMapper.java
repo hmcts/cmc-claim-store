795de62b38159de71d32733467e3665e52463b76
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-08-02 17:08:52 +0100

    resolve cobacy bot error

--- src/main/java/uk/gov/hmcts/cmc/claimstore/repositories/mapping/UserRolesMapper.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/repositories/mapping/UserRolesMapper.java
@@ -2,15 +2,12 @@ package uk.gov.hmcts.cmc.claimstore.repositories.mapping;
 
 import org.skife.jdbi.v2.StatementContext;
 import org.skife.jdbi.v2.tweak.ResultSetMapper;
-import uk.gov.hmcts.cmc.claimstore.processors.JsonMapper;
 import uk.gov.hmcts.cmc.domain.models.UserRole;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserRolesMapper implements ResultSetMapper<UserRole> {
-    private final JsonMapper jsonMapper = JsonMapperFactory.create();
-
     @Override
     public UserRole map(int index, ResultSet result, StatementContext ctx) throws SQLException {
 
