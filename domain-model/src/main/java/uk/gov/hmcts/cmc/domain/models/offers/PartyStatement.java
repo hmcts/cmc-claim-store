95dbd9f979cb2e2fb0807dbdd6b7deab1b00ddee
Sabah Irfan <sabah.irfan@gmail.com>
2018-08-06 11:04:27 +0100

    Code cleanup

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/offers/PartyStatement.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/offers/PartyStatement.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.cmc.domain.models.offers;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
@@ -9,7 +8,6 @@ import java.util.Optional;
 
 import static uk.gov.hmcts.cmc.domain.utils.ToStringStyle.ourStyle;
 
-@Builder
 @EqualsAndHashCode
 public class PartyStatement {
 
