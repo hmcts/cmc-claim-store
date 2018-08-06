f4238374b5be78fe8dbf94a5829bf78750c79fd2
andrewwa-kainos <andrewwa@kainos.com>
2018-08-06 13:15:10 +0100

    Add response deadline controller

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/controllers/ResponseDeadlineController.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.cmc.claimstore.controllers;
+
+import io.swagger.annotations.Api;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.cmc.claimstore.services.ResponseDeadlineCalculator;
+
+import java.time.LocalDate;
+
+@Api
+@RestController
+@RequestMapping(
+    path = "/deadline",
+    produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+public class ResponseDeadlineController {
+    private final ResponseDeadlineCalculator calculator;
+
+    public ResponseDeadlineController(ResponseDeadlineCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    @GetMapping("/{issueDate}")
+    public LocalDate postponedDeadline(@PathVariable LocalDate issueDate) {
+        return calculator.calculatePostponedResponseDeadline(issueDate);
+    }
+}
