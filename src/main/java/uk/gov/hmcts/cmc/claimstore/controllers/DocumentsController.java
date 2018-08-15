e090aa41e1fef15f4426a499b2d8c87c627e1f20
Surjeet Singh <surjeet.singh@hmcts.net>
2018-08-15 16:57:39 +0100

    ROC-3619: fixing build failures

--- src/main/java/uk/gov/hmcts/cmc/claimstore/controllers/DocumentsController.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/controllers/DocumentsController.java
@@ -8,7 +8,12 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.cmc.claimstore.services.document.DocumentsService;
 
 import javax.validation.constraints.NotBlank;
