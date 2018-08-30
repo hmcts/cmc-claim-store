d2f1f9dcd07cfb154c39cc00103f73d3a3683da8
andrewwa-kainos <andrewwa@kainos.com>
2018-08-30 10:30:22 +0100

    remove commented code

--- domain-sample-data/src/main/java/uk/gov/hmcts/cmc/domain/models/sampledata/SampleResponse.java
+++ domain-sample-data/src/main/java/uk/gov/hmcts/cmc/domain/models/sampledata/SampleResponse.java
@@ -76,7 +76,6 @@ public abstract class SampleResponse<T extends SampleResponse<T>> {
                 .moreTimeNeeded(YesNoOption.NO)
                 .amount(BigDecimal.valueOf(120))
                 .paymentIntention(SamplePaymentIntention.immediately())
-//                .paymentDeclaration(null)
                 .defence(USER_DEFENCE)
                 .timeline(SampleDefendantTimeline.validDefaults())
                 .evidence(SampleDefendantEvidence.validDefaults())
