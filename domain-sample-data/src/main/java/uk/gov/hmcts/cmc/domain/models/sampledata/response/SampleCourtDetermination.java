98ef5ae90958e8cc7eb156a6fd400764506c79b8
Sabah Irfan <sabah.irfan@gmail.com>
2018-09-25 13:40:26 +0100

    Update functional tests

--- domain-sample-data/src/main/java/uk/gov/hmcts/cmc/domain/models/sampledata/response/SampleCourtDetermination.java
+++ domain-sample-data/src/main/java/uk/gov/hmcts/cmc/domain/models/sampledata/response/SampleCourtDetermination.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.cmc.domain.models.sampledata.response;
 import uk.gov.hmcts.cmc.domain.models.claimantresponse.CourtDetermination;
 import uk.gov.hmcts.cmc.domain.models.claimantresponse.CourtDetermination.CourtDeterminationBuilder;
 
+import static java.math.BigDecimal.TEN;
+
 public class SampleCourtDetermination {
 
     private SampleCourtDetermination() {
@@ -16,18 +18,21 @@ public class SampleCourtDetermination {
     public static CourtDetermination immediately() {
         return builder()
             .courtDecision(SamplePaymentIntention.immediately())
+            .disposableIncome(TEN)
             .build();
     }
 
     public static CourtDetermination bySetDate() {
         return builder()
             .courtDecision(SamplePaymentIntention.bySetDate())
+            .disposableIncome(TEN)
             .build();
     }
 
     public static CourtDetermination instalments() {
         return builder()
             .courtDecision(SamplePaymentIntention.instalments())
+            .disposableIncome(TEN)
             .build();
     }
 }
