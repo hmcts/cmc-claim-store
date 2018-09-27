c23f53cde208554e8fc181e91e2896af902ecce2
Sabah Irfan <sabah.irfan@gmail.com>
2018-09-27 16:48:13 +0100

    Add unit test

--- domain-model/src/test/java/uk/gov/hmcts/cmc/domain/models/claimantresponse/ResponseRejectionTest.java
+++ domain-model/src/test/java/uk/gov/hmcts/cmc/domain/models/claimantresponse/ResponseRejectionTest.java
@@ -5,6 +5,7 @@ import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaimantResponse;
 
 import java.util.Set;
 
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.cmc.domain.BeanValidator.validate;
 
@@ -27,5 +28,14 @@ public class ResponseRejectionTest {
         Set<String> response = validate(claimantResponse);
 
         assertThat(response).hasSize(0);
+    }
+
+    @Test
+    public void shouldBeInvalidWhenReasonTooLong() {
+        ClaimantResponse claimantResponse = ResponseRejection.builder().reason(randomAlphabetic(99001)).build();
+
+        Set<String> response = validate(claimantResponse);
+
+        assertThat(response).hasSize(1);
     }
 }
