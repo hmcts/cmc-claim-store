b6ef80e72173543cf7b5906ef7461f0601b2cc0d
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-09-28 11:09:42 +0100

    resolve imports

--- src/main/java/uk/gov/hmcts/cmc/claimstore/documents/ClaimantResponseReceiptService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/documents/ClaimantResponseReceiptService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.cmc.claimstore.config.properties.pdf.DocumentTemplates;
 import uk.gov.hmcts.cmc.claimstore.documents.content.ClaimantResponseContentProvider;
-import uk.gov.hmcts.cmc.claimstore.documents.content.DefendantResponseContentProvider;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.reform.pdf.service.client.PDFServiceClient;
 
