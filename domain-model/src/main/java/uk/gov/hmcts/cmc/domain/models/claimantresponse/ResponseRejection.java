e1fc62fef3e78345c780d61694bae2aeab8970ad
Sabah u Din Irfan <sabah.irfan@gmail.com>
2018-09-28 10:02:56 +0100

    ROC-4413: Add rejection reason for states paid claimant response (#506)

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/claimantresponse/ResponseRejection.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/claimantresponse/ResponseRejection.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
 import java.math.BigDecimal;
 import java.util.Optional;
+import javax.validation.constraints.Size;
 
 import static uk.gov.hmcts.cmc.domain.utils.ToStringStyle.ourStyle;
 
@@ -17,15 +18,23 @@ public class ResponseRejection extends ClaimantResponse {
 
     private final Boolean freeMediation;
 
+    @Size(max = 99000)
+    private final String reason;
+
     @Builder
     @JsonCreator
-    public ResponseRejection(BigDecimal amountPaid, boolean freeMediation) {
+    public ResponseRejection(BigDecimal amountPaid, boolean freeMediation, String reason) {
         super(amountPaid);
         this.freeMediation = freeMediation;
+        this.reason = reason;
     }
 
     public Optional<Boolean> getFreeMediation() {
         return Optional.ofNullable(freeMediation);
+    }
+
+    public Optional<String> getReason() {
+        return Optional.ofNullable(reason);
     }
 
     @Override
