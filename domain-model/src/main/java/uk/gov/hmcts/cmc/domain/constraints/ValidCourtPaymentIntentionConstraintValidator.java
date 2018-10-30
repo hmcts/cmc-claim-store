6b8ab64dc3a43fab111f695ae4230445838609a8
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-10-30 16:00:22 +0000

    Fix check style issue

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/constraints/ValidCourtPaymentIntentionConstraintValidator.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/constraints/ValidCourtPaymentIntentionConstraintValidator.java
@@ -5,9 +5,11 @@ import uk.gov.hmcts.cmc.domain.models.claimantresponse.DecisionType;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class ValidCourtPaymentIntentionConstraintValidator implements ConstraintValidator<ValidCourtDetermination, CourtDetermination> {
+public class ValidCourtPaymentIntentionConstraintValidator
+    implements ConstraintValidator<ValidCourtDetermination, CourtDetermination> {
     @Override
-    public boolean isValid(CourtDetermination courtDetermination, ConstraintValidatorContext constraintValidatorContext) {
+    public boolean isValid(CourtDetermination courtDetermination,
+                           ConstraintValidatorContext constraintValidatorContext) {
         return DecisionType.DEFENDANT == courtDetermination.getDecisionType()
             && !courtDetermination.getCourtPaymentIntention().isPresent();
     }
