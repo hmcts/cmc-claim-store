a8a451fdd204861208c628f735b33ceca116e48b
Dharmendra kumar <k_dharmendra@hotmail.com>
2018-12-04 22:40:48 +0000

    partly working solution

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/jackson/mixin/ClaimMixIn.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/jackson/mixin/ClaimMixIn.java
@@ -1,7 +1,9 @@
 package uk.gov.hmcts.cmc.ccd.jackson.mixin;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import uk.gov.hmcts.cmc.domain.amount.TotalAmountCalculator;
 import uk.gov.hmcts.cmc.domain.models.ClaimData;
 import uk.gov.hmcts.cmc.domain.models.Interest;
 import uk.gov.hmcts.cmc.domain.models.Payment;
@@ -12,11 +14,27 @@ import uk.gov.hmcts.cmc.domain.models.otherparty.TheirDetails;
 import uk.gov.hmcts.cmc.domain.models.particulars.HousingDisrepair;
 import uk.gov.hmcts.cmc.domain.models.party.Party;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 public abstract class ClaimMixIn {
 
-    @JsonUnwrapped
+//    @JsonUnwrapped
     abstract ClaimData getClaimData();
+
+    @JsonIgnore
+    abstract Optional<BigDecimal> getAmountWithInterest();
+
+    @JsonIgnore
+    abstract Optional<BigDecimal> getAmountWithInterestUntilIssueDate();
+
+    @JsonIgnore
+    abstract Optional<BigDecimal> getTotalAmountTillToday();
+
+    @JsonIgnore
+    abstract Optional<BigDecimal> getTotalAmountTillDateOfIssue();
+
+    @JsonIgnore
+    abstract Optional<BigDecimal> getTotalInterest();
 }
--- src/test/java/uk/gov/hmcts/cmc/claimstore/processors/MixMapperTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/processors/MixMapperTest.java
@@ -166,7 +166,6 @@ public class MixMapperTest {
         Claim claim = SampleClaim.builder().withResponse(null).build();
 
         String json = processor.toJson(claim);
-        System.out.println(json);
 
         Claim output = processor.fromJson(json, Claim.class);
         String outputJson = processor.toJson(output);
