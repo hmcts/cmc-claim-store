8119fa6b0aa9f78c775226b4321386ff62c0e6f4
Sabah Irfan <sabah.irfan@gmail.com>
2018-12-06 14:14:38 +0000

    Extend from Evidence

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/jackson/mixin/response/DefendantEvidenceMixIn.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/jackson/mixin/response/DefendantEvidenceMixIn.java
@@ -1,12 +1,13 @@
 package uk.gov.hmcts.cmc.ccd.jackson.mixin.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import uk.gov.hmcts.cmc.ccd.jackson.mixin.EvidenceMixIn;
 import uk.gov.hmcts.cmc.domain.models.evidence.EvidenceRow;
 
 import java.util.List;
 import java.util.Optional;
 
-public abstract class DefendantEvidenceMixIn {
+public abstract class DefendantEvidenceMixIn extends EvidenceMixIn {
 
     @JsonProperty("responseEvidenceRows")
     abstract List<EvidenceRow> getRows();
