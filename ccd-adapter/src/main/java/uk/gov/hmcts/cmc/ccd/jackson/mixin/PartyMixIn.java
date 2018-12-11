580101f69d886dab2a3ace83e6a570a3a9191cf1
Sabah Irfan <sabah.irfan@gmail.com>
2018-12-11 11:06:22 +0000

    Park code here

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/jackson/mixin/PartyMixIn.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/jackson/mixin/PartyMixIn.java
@@ -1,34 +1,18 @@
 package uk.gov.hmcts.cmc.ccd.jackson.mixin;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import uk.gov.hmcts.cmc.domain.models.Address;
 import uk.gov.hmcts.cmc.domain.models.legalrep.Representative;
-import uk.gov.hmcts.cmc.domain.models.party.Company;
-import uk.gov.hmcts.cmc.domain.models.party.Individual;
-import uk.gov.hmcts.cmc.domain.models.party.Organisation;
-import uk.gov.hmcts.cmc.domain.models.party.SoleTrader;
+import uk.gov.hmcts.cmc.domain.models.party.PartyType;
 
 import java.time.LocalDate;
 
 @SuppressWarnings("squid:S1610")
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-@JsonSubTypes(
-    {
-        @JsonSubTypes.Type(value = Individual.class, name = "individual"),
-        @JsonSubTypes.Type(value = SoleTrader.class, name = "soleTrader"),
-        @JsonSubTypes.Type(value = Company.class, name = "company"),
-        @JsonSubTypes.Type(value = Organisation.class, name = "organisation")
-    }
-)
 public abstract class PartyMixIn {
 
-    @JsonProperty("->type")
-    abstract String getType();
+    @JsonProperty("Type")
+    abstract PartyType getType();
 
     @JsonProperty("DateOfBirth")
     abstract LocalDate getDateOfBirth();
--- ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/jackson/mixin/response/ResponseMixInTest.java
+++ ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/jackson/mixin/response/ResponseMixInTest.java
@@ -1,11 +1,16 @@
 package uk.gov.hmcts.cmc.ccd.jackson.mixin.response;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.junit.Test;
 import uk.gov.hmcts.cmc.ccd.config.CCDAdapterConfig;
+import uk.gov.hmcts.cmc.ccd.jackson.custom.deserializer.ClaimDeserializer;
+import uk.gov.hmcts.cmc.ccd.jackson.custom.serializer.ClaimSerializer;
 import uk.gov.hmcts.cmc.ccd.processors.JsonMapper;
+import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.legalrep.StatementOfTruth;
 import uk.gov.hmcts.cmc.domain.models.response.FullDefenceResponse;
+import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaim;
 import uk.gov.hmcts.cmc.domain.models.sampledata.SampleResponse;
 
 import java.io.IOException;
@@ -14,6 +19,56 @@ public class ResponseMixInTest {
 
     private final ObjectMapper objectMapper = new CCDAdapterConfig().ccdObjectMapper();
     private JsonMapper processor = new JsonMapper(objectMapper);
+
+    private JsonMapper myMapper = new JsonMapper(new ObjectMapper().registerModule(
+        new SimpleModule()
+            .addSerializer(Claim.class, new ClaimSerializer())
+            .addDeserializer(Claim.class, new ClaimDeserializer())
+    ));
+
+
+    @Test
+    public void shouldSerialiseFullDefenceResponseToCCDClaim() throws IOException {
+
+        Claim claim = SampleClaim.getClaimWithFullDefenceNoMediation();
+
+        String json = myMapper.toJson(claim);
+
+        System.out.println(json);
+
+//        FullDefenceResponse output = processor.fromJson(json, FullDefenceResponse.class);
+//        System.out.println("===================");
+//        System.out.println(output);
+//        String individualFullDefenceResponse = new ResourceReader()
+//            .read("/serialization-samples/individual-full-defence-response.json");
+//        JsonNode expected = objectMapper.readTree(individualFullDefenceResponse);
+//        JsonNode result = objectMapper.readTree(json);
+//        assertEquals(result, expected);
+    }
+
+    @Test
+    public void shouldDeserialiseFullDefenceResponseToCCDClaim() throws IOException {
+
+        Claim claim = SampleClaim.getClaimWithFullDefenceNoMediation();
+
+        String json = myMapper.toJson(claim);
+
+        System.out.println(json);
+
+        Claim output = myMapper.fromJson(json, Claim.class);
+        System.out.println("==================");
+        System.out.println(output);
+
+//        FullDefenceResponse output = processor.fromJson(json, FullDefenceResponse.class);
+//        System.out.println("===================");
+//        System.out.println(output);
+//        String individualFullDefenceResponse = new ResourceReader()
+//            .read("/serialization-samples/individual-full-defence-response.json");
+//        JsonNode expected = objectMapper.readTree(individualFullDefenceResponse);
+//        JsonNode result = objectMapper.readTree(json);
+//        assertEquals(result, expected);
+    }
+
 
     @Test
     public void shouldSerialiseFullDefenceResponseToCCDJson() throws IOException {
@@ -25,28 +80,11 @@ public class ResponseMixInTest {
             )
             .build();
 
-
         String json = processor.toJson(fullDefenceResponse);
         System.out.println(json);
+        FullDefenceResponse response = processor.fromJson(json, FullDefenceResponse.class);
+        System.out.println("=========================");
+        System.out.println(response);
 
-        FullDefenceResponse output = processor.fromJson(json, FullDefenceResponse.class);
-        System.out.println("===================");
-        System.out.println(output);
-//        String individualFullDefenceResponse = new ResourceReader()
-//            .read("/serialization-samples/individual-full-defence-response.json");
-//        JsonNode expected = objectMapper.readTree(individualFullDefenceResponse);
-//        JsonNode result = objectMapper.readTree(json);
-//        assertEquals(result, expected);
     }
-
-//    @JsonSerialize(using = DefendantTimelineSerializer.class)
-//    @JsonUnwrapped
-//    DefendantTimeline timeline = SampleDefendantTimeline.validDefaults();
-//
-//    @Test
-//    public void defendantTimeLine() throws IOException {
-//        String json = processor.toJson(timeline);
-//        System.out.println(json);
-//
-//    }
 }
