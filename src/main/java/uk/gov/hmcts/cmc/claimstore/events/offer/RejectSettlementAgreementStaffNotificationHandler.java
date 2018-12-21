9e75fe1cd51462f2b3c8e3c55305c47441d6947b
Tharack Ahmed <41899680+tharacka@users.noreply.github.com>
2018-12-21 12:59:28 +0000

    ROC-4653 Send settlement rejection email to staff (#597)

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/events/offer/RejectSettlementAgreementStaffNotificationHandler.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.cmc.claimstore.events.offer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.cmc.claimstore.events.settlement.RejectSettlementAgreementEvent;
+import uk.gov.hmcts.cmc.claimstore.services.staff.RejectSettlementAgreementStaffNotificationService;
+
+@Component
+public class RejectSettlementAgreementStaffNotificationHandler {
+
+    private final RejectSettlementAgreementStaffNotificationService notificationService;
+
+    @Autowired
+    public RejectSettlementAgreementStaffNotificationHandler(
+        RejectSettlementAgreementStaffNotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @EventListener
+    public void onSettlementAgreementRejected(RejectSettlementAgreementEvent event) {
+        notificationService.notifySettlementRejected(
+            event.getClaim()
+        );
+    }
+}
