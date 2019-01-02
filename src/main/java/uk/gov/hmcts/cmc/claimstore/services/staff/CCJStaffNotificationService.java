bad068a4769626022a707a081b84049c5011ed54
Surjeet Singh <surjeet.singh@hmcts.net>
2019-01-02 17:57:23 +0000

    ROC-4973: Fixing failing tests

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/staff/CCJStaffNotificationService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/staff/CCJStaffNotificationService.java
@@ -9,6 +9,7 @@ import uk.gov.hmcts.cmc.claimstore.services.staff.content.countycourtjudgment.Re
 import uk.gov.hmcts.cmc.claimstore.services.staff.models.EmailContent;
 import uk.gov.hmcts.cmc.claimstore.utils.ResponseHelper;
 import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.response.Response;
 import uk.gov.hmcts.cmc.domain.utils.PartyUtils;
 import uk.gov.hmcts.cmc.email.EmailAttachment;
 import uk.gov.hmcts.cmc.email.EmailData;
@@ -72,8 +73,8 @@ public class CCJStaffNotificationService {
         map.put("claimantType", PartyUtils.getType(claim.getClaimData().getClaimant()));
         map.put("defendantName", claim.getClaimData().getDefendant().getName());
         map.put("paymentType", claim.getCountyCourtJudgment().getPaymentOption().getDescription());
-        map.put("admissionResponse", ResponseHelper
-            .admissionResponse(claim.getResponse().orElseThrow(IllegalArgumentException::new)));
+        Response response = claim.getResponse().orElse(null);
+        map.put("admissionResponse", ResponseHelper.admissionResponse(response));
         Optional.ofNullable(submitterName).ifPresent(name -> map.put("partyName", name));
         claim.getReDetermination()
             .ifPresent(reDetermination -> map.put("reasonForReDetermination", reDetermination.getExplanation()));
