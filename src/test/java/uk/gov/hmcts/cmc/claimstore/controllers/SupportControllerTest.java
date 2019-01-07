d1113b0588e26302ecb01e7ecb2cdc59f4452cfa
tharacka <tharacka@kainos.com>
2019-01-07 16:11:27 +0000

    unit test ammendments - checkstyleTest

--- src/test/java/uk/gov/hmcts/cmc/claimstore/controllers/SupportControllerTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/controllers/SupportControllerTest.java
@@ -27,7 +27,11 @@ import java.util.Optional;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.anyString;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class SupportControllerTest {
