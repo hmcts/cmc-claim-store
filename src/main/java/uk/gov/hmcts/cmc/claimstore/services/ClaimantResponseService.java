e29bad99a858cbff6421f59795a83f939ef391f7
Ellie Harrison <ellieh@kainos.com>
2019-01-11 00:58:23 +0000

    Fix issue where 2 emails sent to defendant on claimant response

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/ClaimantResponseService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ClaimantResponseService.java
@@ -76,14 +76,27 @@ public class ClaimantResponseService {
             updateDirectionsQuestionnaireDeadline(updatedClaim, authorization);
         }
         Response response = claim.getResponse().orElseThrow(IllegalArgumentException::new);
-        if (!isReferredToJudge(claimantResponse)
-            || (isReferredToJudge(claimantResponse) && PartyUtils.isCompanyOrOrganisation(response.getDefendant()))) {
+        if (!isSettlementAgreement(claim, claimantResponse)
+            && (!isReferredToJudge(claimantResponse)
+            || (isReferredToJudge(claimantResponse) && PartyUtils.isCompanyOrOrganisation(response.getDefendant())))) {
             eventProducer.createClaimantResponseEvent(updatedClaim);
         }
         ccdEventProducer.createCCDClaimantResponseEvent(claim, claimantResponse, authorization);
         appInsights.trackEvent(getAppInsightsEvent(claimantResponse), "referenceNumber", claim.getReferenceNumber());
     }
 
+    private boolean isSettlementAgreement(Claim claim, ClaimantResponse claimantResponse) {
+        Response response = claim.getResponse().orElseThrow(IllegalStateException::new);
+
+        if (shouldFormaliseResponseAcceptance(response, claimantResponse)) {
+            ResponseAcceptation responseAcceptation = (ResponseAcceptation) claimantResponse;
+            if (responseAcceptation.getFormaliseOption().isPresent()) {
+                return responseAcceptation.getFormaliseOption().get() == FormaliseOption.SETTLEMENT;
+            }
+        }
+        return false;
+    }
+    
     private boolean isReferredToJudge(ClaimantResponse response) {
         if (response.getType().equals(ACCEPTATION)) {
             ResponseAcceptation responseAcceptation = (ResponseAcceptation) response;
