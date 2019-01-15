604cde8348a649d8ae6dfb896d62187747dc0e30
karthikkannankainos <43203806+karthikkannankainos@users.noreply.github.com>
2019-01-15 13:43:47 +0000

    ROC-4726 Mapping for Settlement Agreement (#667)
    * Mapping for Settlement Agreement

---
+++ ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/util/SampleCCDPartyStatement.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.cmc.ccd.util;
+
+import uk.gov.hmcts.cmc.ccd.domain.defendant.CCDPartyStatement;
+import uk.gov.hmcts.cmc.ccd.domain.offers.CCDMadeBy;
+import uk.gov.hmcts.cmc.ccd.domain.offers.CCDStatementType;
+
+import java.time.LocalDate;
+
+public class SampleCCDPartyStatement {
+
+    private SampleCCDPartyStatement() {
+        // Empty constructor
+    }
+
+    public static CCDPartyStatement withOffer() {
+        return builder().madeBy(CCDMadeBy.CLAIMANT)
+            .type(CCDStatementType.ACCEPTATION)
+            .offerCompletionDate(LocalDate.now())
+            .offerContent("Offer Content")
+            .build();
+    }
+
+    public static CCDPartyStatement withPaymentIntention() {
+        return builder().madeBy(CCDMadeBy.CLAIMANT)
+            .type(CCDStatementType.ACCEPTATION)
+            .offerCompletionDate(LocalDate.now())
+            .offerContent("Offer Content")
+            .paymentIntention(SampleCCDPaymentIntention.withInstalment())
+            .build();
+    }
+
+    private static CCDPartyStatement.CCDPartyStatementBuilder builder() {
+        return CCDPartyStatement.builder();
+    }
+}
