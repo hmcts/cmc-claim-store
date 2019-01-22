a4f50006bc84af752df82ca54d7e4b88981bc18d
Lauren Taylor <l.taylor@kainos.com>
2019-01-22 15:32:29 +0000

    Added check for defendants email

--- src/main/java/uk/gov/hmcts/cmc/claimstore/events/paidinfull/PaidInFullCitizenNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/events/paidinfull/PaidInFullCitizenNotificationHandler.java
@@ -1,13 +1,18 @@
 package uk.gov.hmcts.cmc.claimstore.events.paidinfull;
 
 import com.google.common.collect.ImmutableMap;
+import org.apache.commons.lang3.StringUtils;
+import org.flywaydb.core.internal.logging.console.ConsoleLog;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.claimstore.config.properties.notifications.NotificationsProperties;
 import uk.gov.hmcts.cmc.claimstore.services.notifications.NotificationService;
 import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.otherparty.TheirDetails;
 
+import javax.swing.text.html.Option;
 import java.util.Map;
+import java.util.Optional;
 
 import static uk.gov.hmcts.cmc.claimstore.services.notifications.NotificationReferenceBuilder.PaidInFull.referenceForDefendant;
 
@@ -33,12 +38,23 @@ public class PaidInFullCitizenNotificationHandler {
     public void notifyDefendantForPaidInFull(PaidInFullEvent event) {
         Claim claim = event.getClaim();
         Map<String, String> parameters = aggregateParams(claim);
-        notificationService.sendMail(
-            claim.getDefendantEmail(),
-            notificationsProperties.getTemplates().getEmail().getClaimantSaysDefendantHasPaidInFull(),
-            parameters,
-            referenceForDefendant(claim.getReferenceNumber())
+
+        getDefendantEmail(claim).ifPresent(defendantEmail ->
+            notificationService.sendMail(
+                defendantEmail,
+                notificationsProperties.getTemplates().getEmail().getClaimantSaysDefendantHasPaidInFull(),
+                parameters,
+                referenceForDefendant(claim.getReferenceNumber())
+            )
         );
+    }
+
+    private Optional<String> getDefendantEmail(Claim claim) {
+        if (StringUtils.isNotBlank(claim.getDefendantEmail())) {
+            return Optional.ofNullable(claim.getDefendantEmail());
+        } else {
+            return claim.getClaimData().getDefendant().getEmail();
+        }
     }
 
     private Map<String, String> aggregateParams(Claim claim) {
