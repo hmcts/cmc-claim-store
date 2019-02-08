11051620be5cdb38496d93675782f4152288cfa7
Srineelesh <srineelesh.sudhakar@hmcts.net>
2019-02-08 15:42:55 +0000

    modifed the job name enum

--- dm-batch-upload/src/main/java/uk/gov/hmcts/cmc/dm/batch/enums/JobName.java
+++ dm-batch-upload/src/main/java/uk/gov/hmcts/cmc/dm/batch/enums/JobName.java
@@ -2,7 +2,7 @@ package uk.gov.hmcts.cmc.dm.batch.enums;
 
 public enum JobName {
     UPLOAD_SEALED_CLAIM,
-    UPLOAD_DEFENDANT_LETTER,
+    UPLOAD_DEFENDANT_PIN_LETTER,
     UPLOAD_DEFENDANT_RESPONSE_RECIEEPT,
     UPLOAD_CCJ,
     UPLOAD_SETTLEMENT;
