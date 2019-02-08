050060db4028264d85a854c8dc3d5b7d0304694a
Sabah u Din Irfan <sabah.irfan@gmail.com>
2019-02-08 15:13:35 +0000

    ROC-4674: Claimant response states paid settle pre judgement (#719)

--- ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/assertion/claimantresponse/ResponseAcceptationAssert.java
+++ ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/assertion/claimantresponse/ResponseAcceptationAssert.java
@@ -16,12 +16,16 @@ public class ResponseAcceptationAssert extends AbstractAssert<ResponseAcceptatio
     public ResponseAcceptationAssert isEqualTo(CCDResponseAcceptation ccdResponseAcceptation) {
         isNotNull();
 
-        if (!Objects.equals(
-            actual.getFormaliseOption().orElseThrow(AssertionError::new).name(),
-            ccdResponseAcceptation.getFormaliseOption().name()
-        )) {
-            failWithMessage("Expected ResponseAcceptation.formaliseOption to be <%s> but was <%s>",
-                ccdResponseAcceptation.getFormaliseOption(), actual.getFormaliseOption());
+        actual.getFormaliseOption().ifPresent(formaliseOption -> {
+            if (!Objects.equals(formaliseOption.name(), ccdResponseAcceptation.getFormaliseOption().name())) {
+                failWithMessage("Expected ResponseAcceptation.formaliseOption to be <%s> but was <%s>",
+                    ccdResponseAcceptation.getFormaliseOption(), actual.getFormaliseOption());
+            }
+        });
+
+        if (ccdResponseAcceptation.getFormaliseOption() != null && !actual.getFormaliseOption().isPresent()) {
+            failWithMessage("Expected ResponseAcceptation.formaliseOption to be not present but was <%s>",
+                actual.getFormaliseOption());
         }
 
         actual.getAmountPaid().ifPresent(amountPaid -> {
@@ -38,6 +42,31 @@ public class ResponseAcceptationAssert extends AbstractAssert<ResponseAcceptatio
         actual.getClaimantPaymentIntention().ifPresent(paymentIntention -> {
             assertThat(paymentIntention).isEqualTo(ccdResponseAcceptation.getClaimantPaymentIntention());
         });
+
+        actual.getSettleForAmount().ifPresent(settleForAmount -> {
+            if (!Objects.equals(settleForAmount.name(), ccdResponseAcceptation.getSettleForAmount().name())) {
+                failWithMessage("Expected ResponseAcceptation.settleForAmount to be <%s> but was <%s>",
+                    ccdResponseAcceptation.getSettleForAmount(), settleForAmount);
+            }
+        });
+
+        if (ccdResponseAcceptation.getSettleForAmount() != null && !actual.getSettleForAmount().isPresent()) {
+            failWithMessage("Expected ResponseAcceptation.settleForAmount to be not present but was <%s>",
+                actual.getSettleForAmount());
+        }
+
+        actual.getPaymentReceived().ifPresent(paymentReceived -> {
+            if (!Objects.equals(paymentReceived.name(), ccdResponseAcceptation.getPaymentReceived().name())) {
+                failWithMessage("Expected ResponseAcceptation.paymentReceived to be <%s> but was <%s>",
+                    ccdResponseAcceptation.getPaymentReceived(), paymentReceived);
+            }
+        });
+
+        if (ccdResponseAcceptation.getPaymentReceived() != null && !actual.getPaymentReceived().isPresent()) {
+            failWithMessage("Expected ResponseAcceptation.paymentReceived to be not present but was <%s>",
+                actual.getPaymentReceived());
+        }
+
         return this;
     }
 }
