bd537f313c8e5b1feff98ae0b6fcf24ede94842f
Mark Ryan <mark.ryan@hmcts.net>
2019-02-13 10:49:33 +0000

    tidy code

--- src/main/java/uk/gov/hmcts/cmc/claimstore/ClaimStoreApplication.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/ClaimStoreApplication.java
@@ -1,13 +1,10 @@
 package uk.gov.hmcts.cmc.claimstore;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.jdbc.DataSourcePoolMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import uk.gov.hmcts.cmc.claimstore.jobs.NotificationEmailJob;
 
 @SpringBootApplication(
     exclude = {
@@ -25,10 +22,8 @@ import uk.gov.hmcts.cmc.claimstore.jobs.NotificationEmailJob;
 public class ClaimStoreApplication {
 
     public static final String BASE_PACKAGE_NAME = ClaimStoreApplication.class.getPackage().getName();
-    private static final Logger logger = LoggerFactory.getLogger(ClaimStoreApplication.class);
 
     public static void main(String[] args) {
-        logger.info("STARTING CLAIM-STORE...");
         SpringApplication.run(ClaimStoreApplication.class, args);
     }
 }
