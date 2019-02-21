1b8b5d7b032af62af5879c689918943e8758f250
andrewwa-kainos <andrewwa@kainos.com>
2019-02-21 16:37:53 +0000

    Checkstyle fix

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/notifications/ResponseNeededNotificationServiceTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/notifications/ResponseNeededNotificationServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
