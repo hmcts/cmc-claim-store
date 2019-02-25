a0628bbbbe4cd13169255dc37425b51931abf989
Ellie Harrison <ellieh@kainos.com>
2019-02-25 14:25:04 +0000

    ROC-5255 Authorisation check when accessing claims (#761)
    * Add authorisation protection around claim access
    
    * Add test for ClaimAuthorisationRule
    
    * Update unauthorised claim access message
    
    * Add service tests for claim authorisation
    
    * Fix issue where fetching claim by letterholder id would fail authorisation
    
    * Add test for letterholder id check
    
    * Allow access to claim when userrole is caseworker
    
    * Fix StaffEmailServiceWithNotificationDisabledTest
    
    * Fix GetClaimsByDefendantIdTest
    
    * fix GetClaimsByClaimantIdTest
    
    * Fix DownloadRepresentedClaimCopyTest
    
    * Fix RequestMoreTimeForResponseWithCoreCaseDataTest
    
    * Fix GetMetadataTest
    
    * Fix checkstyle
    
    * Fix checkstyle
    
    * Fix codacy issue
    
    * Revert CCD change
    
    * Fix CCD Integration tests
    
    * Fix codacy issue

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/rules/ClaimAuthorisationRule.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.cmc.claimstore.rules;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.cmc.claimstore.exceptions.ForbiddenActionException;
+import uk.gov.hmcts.cmc.claimstore.idam.models.UserDetails;
+import uk.gov.hmcts.cmc.claimstore.services.UserService;
+import uk.gov.hmcts.cmc.domain.models.Claim;
+
+@Component
+public class ClaimAuthorisationRule {
+
+    private static final String USER_NOT_LINKED_MESSAGE =
+        "User id %s does not match linked ids on claim";
+
+    private static final String USER_ID_MISMATCH =
+        "Given user id %s does not match authorised id";
+
+    private final UserService userService;
+
+    @Autowired
+    public ClaimAuthorisationRule(UserService userService) {
+        this.userService = userService;
+
+    }
+
+    public void assertClaimCanBeAccessed(Claim claim, String authorisation) {
+        if (claim != null) {
+            UserDetails userDetails = userService.getUserDetails(authorisation);
+
+            if (!userDetails.isCaseworker()
+                && !userDetails.getId().equals(claim.getDefendantId())
+                && !userDetails.getId().equals(claim.getSubmitterId())
+                && !userDetails.getId().equals(claim.getLetterHolderId())) {
+                throw new ForbiddenActionException(String.format(USER_NOT_LINKED_MESSAGE,
+                    userDetails.getId()));
+            }
+        }
+    }
+
+    public void assertSubmitterIdMatchesAuthorisation(String userId, String authorisation) {
+        UserDetails userDetails = userService.getUserDetails(authorisation);
+
+        if (!userDetails.getId().equals(userId) && !userDetails.isCaseworker()) {
+            throw new ForbiddenActionException(String.format(USER_ID_MISMATCH, userId));
+        }
+    }
+}
