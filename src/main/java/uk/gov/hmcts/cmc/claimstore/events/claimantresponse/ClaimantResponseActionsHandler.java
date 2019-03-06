23f0c5c8f8eae3d4dc17b55c0ed2e1afd8a84be0
Tharack Ahmed <41899680+tharacka@users.noreply.github.com>
2019-03-06 10:10:42 +0000

    ROC 5194 notify email for defendant on claimant response rejection of states paid or part admission (#743)

--- src/main/java/uk/gov/hmcts/cmc/claimstore/events/claimantresponse/ClaimantResponseActionsHandler.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/events/claimantresponse/ClaimantResponseActionsHandler.java
@@ -5,6 +5,13 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.claimstore.events.ccj.InterlocutoryJudgmentEvent;
 import uk.gov.hmcts.cmc.claimstore.services.notifications.NotificationToDefendantService;
 import uk.gov.hmcts.cmc.claimstore.services.staff.ClaimantRejectOrgPaymentPlanStaffNotificationService;
+import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.claimantresponse.ClaimantResponse;
+import uk.gov.hmcts.cmc.domain.models.claimantresponse.ClaimantResponseType;
+import uk.gov.hmcts.cmc.domain.models.response.Response;
+import uk.gov.hmcts.cmc.domain.models.response.ResponseType;
+
+import static uk.gov.hmcts.cmc.domain.utils.ResponseUtils.isResponseStatesPaid;
 
 @Component
 public class ClaimantResponseActionsHandler {
@@ -24,7 +31,11 @@ public class ClaimantResponseActionsHandler {
 
     @EventListener
     public void sendNotificationToDefendant(ClaimantResponseEvent event) {
-        this.notificationService.notifyDefendant(event.getClaim());
+        if (isRejectedStatesPaidOrPartAdmission(event.getClaim())) {
+            this.notificationService.notifyDefendantOfRejection(event.getClaim());
+        } else {
+            this.notificationService.notifyDefendant(event.getClaim());
+        }
     }
 
     @EventListener
@@ -36,5 +47,12 @@ public class ClaimantResponseActionsHandler {
     public void sendClaimantRejectOrganisationPaymentPlanNotificationToStaff(RejectOrganisationPaymentPlanEvent event) {
         this.claimantRejectOrgPaymentPlanStaffNotificationService
             .notifyStaffClaimantRejectOrganisationPaymentPlan(event.getClaim());
+    }
+
+    private boolean isRejectedStatesPaidOrPartAdmission(Claim claim) {
+        ClaimantResponse claimantResponse = claim.getClaimantResponse().orElseThrow(IllegalStateException::new);
+        Response response = claim.getResponse().orElseThrow(IllegalArgumentException::new);
+        return claimantResponse.getType() == ClaimantResponseType.REJECTION
+            && (isResponseStatesPaid(response) || response.getResponseType() == ResponseType.PART_ADMISSION);
     }
 }
