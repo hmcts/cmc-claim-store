191872618649243ab99c4e49209ebcbe4a001732
Srineelesh <srineelesh.sudhakar@hmcts.net>
2019-03-12 10:37:43 +0000

    fixed checkstyle error

--- src/main/java/uk/gov/hmcts/cmc/claimstore/repositories/DBCaseRepository.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/repositories/DBCaseRepository.java
@@ -229,6 +229,7 @@ public class DBCaseRepository implements CaseRepository {
                                     ClaimDocumentCollection claimDocumentCollection) {
         claimRepository.saveClaimDocuments(claimId, jsonMapper.toJson(claimDocumentCollection));
         return claimRepository.getById(claimId).orElseThrow(() ->
-            new NotFoundException(String.format("Claim not found by primary key %s. This should never happen", claimId )));
+            new NotFoundException(
+                String.format("Claim not found by primary key %s. This should never happen", claimId)));
     }
 }
