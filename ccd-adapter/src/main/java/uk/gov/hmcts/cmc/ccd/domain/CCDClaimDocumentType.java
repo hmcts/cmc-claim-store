2586f744f9eccda8e5195934d517a49a7df9f2b4
johnhosolirius <john.ho@solirius.com>
2019-03-13 15:17:03 +0000

    Unused description field

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CCDClaimDocumentType.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CCDClaimDocumentType.java
@@ -2,20 +2,10 @@ package uk.gov.hmcts.cmc.ccd.domain;
 
 public enum CCDClaimDocumentType {
 
-    CLAIM_ISSUE_RECEIPT("ClaimReceiptUpload"),
-    SEALED_CLAIM("SealedClaimUpload"),
-    DEFENDANT_RESPONSE_RECEIPT("DefendantResponseUpload"),
-    CCJ_REQUEST("CcjRequestUpload"),
-    SETTLEMENT_AGREEMENT("SettlementAgreementUpload"),
-    DEFENDANT_PIN_LETTER("DefendantPinLetterUpload");
-
-    private String typeName;
-
-    CCDClaimDocumentType(String typeName) {
-        this.typeName = typeName;
-    }
-
-    public String getTypeName() {
-        return typeName;
-    }
+    CLAIM_ISSUE_RECEIPT,
+    SEALED_CLAIM,
+    DEFENDANT_RESPONSE_RECEIPT,
+    CCJ_REQUEST,
+    SETTLEMENT_AGREEMENT,
+    DEFENDANT_PIN_LETTER;
 }
