e600d7d34d53f7b10609ed02b5e8e5e4107ec616
johnhosolirius <john.ho@solirius.com>
2019-03-25 10:36:32 +0000

    Review comment

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/mapper/CaseEventMapper.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/mapper/CaseEventMapper.java
@@ -1,10 +1,8 @@
 package uk.gov.hmcts.cmc.ccd.mapper;
 
+import com.google.common.collect.ImmutableMap;
 import uk.gov.hmcts.cmc.ccd.domain.CaseEvent;
 import uk.gov.hmcts.cmc.domain.models.ClaimDocumentType;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.CCJ_REQUEST_UPLOAD;
 import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.CLAIM_ISSUE_RECEIPT_UPLOAD;
@@ -22,14 +20,15 @@ import static uk.gov.hmcts.cmc.domain.models.ClaimDocumentType.SETTLEMENT_AGREEM
 
 public class CaseEventMapper {
 
-    private static final Map<ClaimDocumentType, CaseEvent> claimDocumentTypeMap = new HashMap<ClaimDocumentType, CaseEvent>(){{
-        put(SEALED_CLAIM, SEALED_CLAIM_UPLOAD);
-        put(CLAIM_ISSUE_RECEIPT, CLAIM_ISSUE_RECEIPT_UPLOAD);
-        put(DEFENDANT_RESPONSE_RECEIPT, DEFENDANT_RESPONSE_UPLOAD);
-        put(CCJ_REQUEST, CCJ_REQUEST_UPLOAD);
-        put(SETTLEMENT_AGREEMENT, SETTLEMENT_AGREEMENT_UPLOAD);
-        put(DEFENDANT_PIN_LETTER, DEFENDANT_PIN_LETTER_UPLOAD);
-    }};
+    private static final ImmutableMap<ClaimDocumentType, CaseEvent> claimDocumentTypeMap =
+        ImmutableMap.<ClaimDocumentType, CaseEvent>builder()
+        .put(SEALED_CLAIM, SEALED_CLAIM_UPLOAD)
+        .put(CLAIM_ISSUE_RECEIPT, CLAIM_ISSUE_RECEIPT_UPLOAD)
+        .put(DEFENDANT_RESPONSE_RECEIPT, DEFENDANT_RESPONSE_UPLOAD)
+        .put(CCJ_REQUEST, CCJ_REQUEST_UPLOAD)
+        .put(SETTLEMENT_AGREEMENT, SETTLEMENT_AGREEMENT_UPLOAD)
+        .put(DEFENDANT_PIN_LETTER, DEFENDANT_PIN_LETTER_UPLOAD)
+        .build();
 
     public static CaseEvent map(ClaimDocumentType type) {
 
