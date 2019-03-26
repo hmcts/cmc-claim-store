8090e240668cde563bb5a48b5a0194a4248e4484
Kiran Varma <varma.ap@gmail.com>
2019-03-26 22:37:49 +0000

    small corrections after rebase

--- src/main/java/uk/gov/hmcts/cmc/claimstore/controllers/support/SupportController.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/controllers/support/SupportController.java
@@ -5,7 +5,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -33,6 +35,7 @@ import uk.gov.hmcts.cmc.claimstore.services.document.DocumentsService;
 import uk.gov.hmcts.cmc.domain.exceptions.BadRequestException;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.ClaimDocumentType;
+import uk.gov.hmcts.cmc.domain.models.MediationRequest;
 
 import java.util.ArrayList;
 import java.util.List;
