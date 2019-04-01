9b97054869282fae2656cc509b4fffb0bb19a431
Dharmendra kumar <k_dharmendra@hotmail.com>
2019-04-01 11:12:50 +0100

    add more functional tests

--- src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/functional/solicitor/SolicitorSubmitClaimTest.java
+++ src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/functional/solicitor/SolicitorSubmitClaimTest.java
@@ -2,9 +2,15 @@ package uk.gov.hmcts.cmc.claimstore.tests.functional.solicitor;
 
 import org.junit.Before;
 import uk.gov.hmcts.cmc.claimstore.tests.functional.BaseSubmitClaimTest;
+import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.ClaimDocument;
+import uk.gov.hmcts.cmc.domain.models.ClaimDocumentType;
 import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaimData;
 
+import java.util.List;
 import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolicitorSubmitClaimTest extends BaseSubmitClaimTest {
 
@@ -16,5 +22,18 @@ public class SolicitorSubmitClaimTest extends BaseSubmitClaimTest {
     @Override
     protected Supplier<SampleClaimData> getSampleClaimDataBuilder() {
         return testData::submittedBySolicitorBuilder;
+    }
+
+    @Override
+    protected void assertDocumentsCreated(Claim claim) {
+        claim.getClaimDocumentCollection().ifPresent(claimDocumentCollection ->
+            {
+                List<ClaimDocument> claimDocuments = claimDocumentCollection.getClaimDocuments();
+
+                assertThat(claimDocuments).hasSize(1);
+
+                assertThat(claimDocuments.get(0).getDocumentType()).isEqualTo(ClaimDocumentType.SEALED_CLAIM);
+            }
+        );
     }
 }
