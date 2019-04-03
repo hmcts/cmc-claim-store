3047899e86522fcbf07c802d381169645f29cbc9
Srineelesh <srineelesh.sudhakar@hmcts.net>
2019-04-03 10:11:30 +0100

    removed the unused imports

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/document/DocumentManagementBackedDocumentsService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/document/DocumentManagementBackedDocumentsService.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.cmc.claimstore.services.document;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
