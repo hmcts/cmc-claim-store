3ddcbf4f4d876d30af973f080f5f11406ef57315
Dharmendra kumar <k_dharmendra@hotmail.com>
2019-04-23 16:46:13 +0100

    add logger for it

--- src/main/java/uk/gov/hmcts/cmc/claimstore/documents/DummyBulkPrintService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/documents/DummyBulkPrintService.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.cmc.claimstore.documents;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.cmc.domain.models.Claim;
@@ -8,9 +10,10 @@ import uk.gov.hmcts.reform.sendletter.api.Document;
 @Service("bulkPrintService")
 @ConditionalOnProperty(prefix = "send-letter", name = "url", havingValue = "false")
 public class DummyBulkPrintService implements PrintService {
+    private static final Logger logger = LoggerFactory.getLogger(DummyBulkPrintService.class);
 
     @Override
     public void print(Claim claim, Document defendantLetterDocument, Document sealedClaimDocument) {
-        // No operation need to be performed
+        logger.info("No bulk print operation need to be performed as 'Bulk print url' is switched off.");
     }
 }
