cf41df69c23d7a854095c3a593630547adba2325
Sabah u Din Irfan <sabah.irfan@gmail.com>
2019-05-07 18:32:17 +0500

    ROC-5321: Add lead applicant indicator for first citizen claimant (#830)

--- ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/mapper/ClaimantMapperTest.java
+++ ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/mapper/ClaimantMapperTest.java
@@ -18,6 +18,8 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.cmc.ccd.assertion.Assertions.assertThat;
+import static uk.gov.hmcts.cmc.ccd.domain.CCDYesNoOption.NO;
+import static uk.gov.hmcts.cmc.ccd.domain.CCDYesNoOption.YES;
 import static uk.gov.hmcts.cmc.ccd.util.SampleData.getCCDApplicantCompany;
 import static uk.gov.hmcts.cmc.ccd.util.SampleData.getCCDApplicantIndividual;
 import static uk.gov.hmcts.cmc.ccd.util.SampleData.getCCDApplicantOrganisation;
@@ -38,10 +40,11 @@ public class ClaimantMapperTest {
         Party party = SampleParty.builder().individual();
 
         //when
-        CCDCollectionElement<CCDApplicant> ccdParty = claimantMapper.to(party, claim);
+        CCDCollectionElement<CCDApplicant> ccdParty = claimantMapper.to(party, claim, true);
         CCDApplicant applicant = ccdParty.getValue();
 
         //then
+        assertThat(applicant.getLeadApplicantIndicator()).isEqualTo(YES);
         assertThat(party).isEqualTo(applicant);
         assertThat(applicant.getPartyDetail().getEmailAddress()).isEqualTo(claim.getSubmitterEmail());
     }
@@ -52,10 +55,11 @@ public class ClaimantMapperTest {
         Party party = SampleParty.builder().company();
 
         //when
-        CCDCollectionElement<CCDApplicant> ccdParty = claimantMapper.to(party, claim);
+        CCDCollectionElement<CCDApplicant> ccdParty = claimantMapper.to(party, claim, false);
         CCDApplicant applicant = ccdParty.getValue();
 
         //then
+        assertThat(applicant.getLeadApplicantIndicator()).isEqualTo(NO);
         assertThat(party).isEqualTo(applicant);
         assertThat(applicant.getPartyDetail().getEmailAddress()).isEqualTo(claim.getSubmitterEmail());
     }
@@ -66,10 +70,11 @@ public class ClaimantMapperTest {
         Party party = SampleParty.builder().organisation();
 
         //when
-        CCDCollectionElement<CCDApplicant> ccdParty = claimantMapper.to(party, claim);
+        CCDCollectionElement<CCDApplicant> ccdParty = claimantMapper.to(party, claim, true);
         CCDApplicant applicant = ccdParty.getValue();
 
         //then
+        assertThat(applicant.getLeadApplicantIndicator()).isEqualTo(YES);
         assertThat(party).isEqualTo(applicant);
         assertThat(applicant.getPartyDetail().getEmailAddress()).isEqualTo(claim.getSubmitterEmail());
     }
@@ -80,10 +85,11 @@ public class ClaimantMapperTest {
         Party party = SampleParty.builder().soleTrader();
 
         //when
-        CCDCollectionElement<CCDApplicant> ccdParty = claimantMapper.to(party, claim);
+        CCDCollectionElement<CCDApplicant> ccdParty = claimantMapper.to(party, claim, true);
         CCDApplicant applicant = ccdParty.getValue();
 
         //then
+        assertThat(applicant.getLeadApplicantIndicator()).isEqualTo(YES);
         assertThat(party).isEqualTo(applicant);
         assertThat(applicant.getPartyDetail().getEmailAddress()).isEqualTo(claim.getSubmitterEmail());
     }
