51f7b529193c29d1b60106fe9a4885c664954951
Dharmendra kumar <k_dharmendra@hotmail.com>
2019-05-09 12:04:14 +0100

    resolve import issue

--- src/main/java/uk/gov/hmcts/cmc/claimstore/config/CCDStoreConfiguration.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/config/CCDStoreConfiguration.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import uk.gov.hmcts.cmc.ccd.mapper.CaseMapper;
 import uk.gov.hmcts.cmc.claimstore.appinsights.AppInsights;
