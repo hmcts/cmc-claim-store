599bd40798c5eef5fa11e990a5d8b38859d56342
Silvia Mandala <silvia.mandala@hmcts.net>
2019-05-17 14:38:00 +0200

    ROC-5471 Rename direction comment to other direction

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/legaladvisor/CCDOrderGenerationData.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/legaladvisor/CCDOrderGenerationData.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.cmc.ccd.domain.legaladvisor;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
