1fadd0ac7e57da8d831691808b4188b327d48971
Andrew Walker <andrewwa@kainos.com>
2019-05-22 10:53:27 +0100

    Allow zero value for an income

--- domain-model/src/test/java/uk/gov/hmcts/cmc/domain/models/statementofmeans/IncomeTest.java
+++ domain-model/src/test/java/uk/gov/hmcts/cmc/domain/models/statementofmeans/IncomeTest.java
@@ -20,13 +20,26 @@ public class IncomeTest {
     }
 
     @Test
-    public void shouldBeSuccessfulValidationForIncome() {
+    public void shouldBeSuccessfulValidationForPositiveIncome() {
         //given
         Income income = newSampleOfIncomeBuilder().build();
         //when
         Set<String> response = validate(income);
         //then
-        assertThat(response).hasSize(0);
+        assertThat(response).isEmpty();
+    }
+
+    @Test
+    public void shouldBeSuccessfulValidationForZeroIncome() {
+        //given
+        Income income = Income.builder()
+            .type(JOB)
+            .frequency(MONTH)
+            .amount(BigDecimal.valueOf(0)).build();
+        //when
+        Set<String> response = validate(income);
+        //then
+        assertThat(response).isEmpty();
     }
 
     @Test
@@ -41,7 +54,7 @@ public class IncomeTest {
         //when
         Set<String> response = validate(income);
         //then
-        assertThat(response).hasSize(0);
+        assertThat(response).isEmpty();
     }
 
     @Test
@@ -55,7 +68,9 @@ public class IncomeTest {
         //when
         Set<String> response = validate(income);
         //then
-        assertThat(response).hasSize(1).contains("otherSource : may not be null when type is 'Other'");
+        assertThat(response)
+            .hasSize(1)
+            .contains("otherSource : may not be null when type is 'Other'");
     }
 
     @Test
@@ -70,7 +85,8 @@ public class IncomeTest {
         //when
         Set<String> response = validate(income);
         //then
-        assertThat(response).hasSize(1)
+        assertThat(response)
+            .hasSize(1)
             .contains("otherSource : may not be provided when type is 'Income from your job'");
     }
 
@@ -152,13 +168,13 @@ public class IncomeTest {
         Income income = Income.builder()
             .type(JOB)
             .frequency(MONTH)
-            .amount(BigDecimal.valueOf(0))
+            .amount(BigDecimal.valueOf(-0.01))
             .build();
         //when
         Set<String> errors = validate(income);
         //then
         assertThat(errors)
             .hasSize(1)
-            .contains("amount : must be greater than or equal to 0.01");
+            .contains("amount : must be greater than or equal to 0.00");
     }
 }
