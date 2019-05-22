0ff5a0f967ddee5f89c44e286838201d74f6e8d9
karthikkannankainos <43203806+karthikkannankainos@users.noreply.github.com>
2019-05-22 15:54:04 +0100

    ROC-5168: Remove Pre payment calls and its reference. (#795)
    ROC-5168 Update ccd case number in Payhub

--- src/main/java/uk/gov/hmcts/cmc/claimstore/repositories/CCDCaseRepository.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/repositories/CCDCaseRepository.java
@@ -19,7 +19,6 @@ import uk.gov.hmcts.cmc.domain.models.ReDetermination;
 import uk.gov.hmcts.cmc.domain.models.claimantresponse.ClaimantResponse;
 import uk.gov.hmcts.cmc.domain.models.offers.MadeBy;
 import uk.gov.hmcts.cmc.domain.models.offers.Settlement;
-import uk.gov.hmcts.cmc.domain.models.response.CaseReference;
 import uk.gov.hmcts.cmc.domain.models.response.Response;
 
 import java.time.LocalDate;
@@ -162,11 +161,6 @@ public class CCDCaseRepository implements CaseRepository {
     ) {
         coreCaseDataService.reachSettlementAgreement(claim.getId(), settlement, nowInUTC(), authorisation,
             caseEvent);
-    }
-
-    @Override
-    public CaseReference savePrePaymentClaim(String externalId, String authorisation) {
-        return new CaseReference(externalId);
     }
 
     @Override
