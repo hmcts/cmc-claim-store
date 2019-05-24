5e6c3a7eb660d93660b291c83bf27031d8749059
srineelesh <srineelesh@users.noreply.github.com>
2019-05-24 16:40:28 +0100

    ROC-5732: Add the document binary Url and document name in ccd document (#970)

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CCDDocument.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CCDDocument.java
@@ -1,21 +1,27 @@
 package uk.gov.hmcts.cmc.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
-@JsonIgnoreProperties(value = {"document_filename", "document_binary_url"})
 public class CCDDocument {
 
     @JsonProperty("document_url")
     private String documentUrl;
 
+    @JsonProperty("document_binary_url")
+    private String documentBinaryUrl;
+
+    @JsonProperty("document_filename")
+    private String documentFileName;
+
     @JsonCreator
-    public CCDDocument(String documentUrl) {
+    public CCDDocument(String documentUrl, String documentBinaryUrl, String documentFileName) {
         this.documentUrl = documentUrl;
+        this.documentBinaryUrl = documentBinaryUrl;
+        this.documentFileName = documentFileName;
     }
 }
