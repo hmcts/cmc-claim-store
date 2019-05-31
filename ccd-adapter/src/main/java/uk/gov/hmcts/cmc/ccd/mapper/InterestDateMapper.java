83f1be446d8b21df720d91b97c9dc3167636879c
dharmendra kumar <dharmendra.kumar@hmcts.net>
2019-05-31 13:52:01 +0100

    Resolve ccd interest date mapper issue (#987)

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/mapper/InterestDateMapper.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/mapper/InterestDateMapper.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.cmc.ccd.mapper;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.ccd.domain.CCDCase;
 import uk.gov.hmcts.cmc.ccd.domain.CCDInterestDateType;
@@ -7,33 +8,47 @@ import uk.gov.hmcts.cmc.ccd.domain.CCDInterestEndDateType;
 import uk.gov.hmcts.cmc.domain.models.InterestDate;
 import uk.gov.hmcts.cmc.domain.models.InterestDate.InterestEndDateType;
 
+import java.util.Optional;
+
 @Component
 public class InterestDateMapper implements BuilderMapper<CCDCase, InterestDate, CCDCase.CCDCaseBuilder> {
 
     @Override
     public void to(InterestDate interestDate, CCDCase.CCDCaseBuilder builder) {
-        if (interestDate == null || interestDate.getType() == null) {
+        if (interestDate == null) {
             return;
         }
+        Optional.ofNullable(interestDate.getType())
+            .ifPresent(dateType -> builder.interestDateType(CCDInterestDateType.valueOf(dateType.name())));
 
-        builder.interestDateType(CCDInterestDateType.valueOf(interestDate.getType().name()))
+        Optional.ofNullable(interestDate.getEndDateType())
+            .ifPresent(endDateType -> builder.interestEndDateType(CCDInterestEndDateType.valueOf(endDateType.name())));
+
+        builder
             .interestClaimStartDate(interestDate.getDate())
-            .interestStartDateReason(interestDate.getReason())
-            .interestEndDateType(CCDInterestEndDateType.valueOf(interestDate.getEndDateType().name()));
+            .interestStartDateReason(interestDate.getReason());
     }
 
     @Override
     public InterestDate from(CCDCase ccdCase) {
-        if (ccdCase.getInterestDateType() == null) {
+        if (ccdCase.getInterestDateType() == null
+            && ccdCase.getInterestEndDateType() == null
+            && ccdCase.getInterestClaimStartDate() == null
+            && StringUtils.isBlank(ccdCase.getInterestStartDateReason())
+        ) {
             return null;
         }
 
-        InterestEndDateType endDateType = ccdCase.getInterestDateType() != null
+        InterestEndDateType endDateType = ccdCase.getInterestEndDateType() != null
             ? InterestEndDateType.valueOf(ccdCase.getInterestEndDateType().name())
             : null;
 
+        InterestDate.InterestDateType interestDateType = ccdCase.getInterestDateType() != null
+            ? InterestDate.InterestDateType.valueOf(ccdCase.getInterestDateType().name())
+            : null;
+
         return new InterestDate(
-            InterestDate.InterestDateType.valueOf(ccdCase.getInterestDateType().name()),
+            interestDateType,
             ccdCase.getInterestClaimStartDate(),
             ccdCase.getInterestStartDateReason(),
             endDateType
