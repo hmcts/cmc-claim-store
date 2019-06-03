5b5d85da5843f5584e804841ea07e9175f0b376d
Dharmendra kumar <k_dharmendra@hotmail.com>
2019-06-03 13:18:56 +0100

    revert count

--- ccd-claim-migration/src/main/java/uk/gov/hmcts/cmc/ccd/migration/repositories/ClaimRepository.java
+++ ccd-claim-migration/src/main/java/uk/gov/hmcts/cmc/ccd/migration/repositories/ClaimRepository.java
@@ -16,7 +16,7 @@ import java.util.List;
 @UseStringTemplate3StatementLocator
 public interface ClaimRepository {
 
-    @SqlQuery("SELECT * FROM claim WHERE is_migrated = false order by id desc limit 100")
+    @SqlQuery("SELECT * FROM claim WHERE is_migrated = false order by id asc")
     List<Claim> getAllNotMigratedClaims();
 
     @SqlQuery("SELECT * FROM claim WHERE reference_number in (<references>) order by id asc")
