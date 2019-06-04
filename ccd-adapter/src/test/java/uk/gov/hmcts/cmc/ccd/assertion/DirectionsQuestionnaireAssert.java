7ea4bfbbf4f3d54e47e1b15e82992f29b3e566a1
Kiran Varma <varma.ap@gmail.com>
2019-06-04 18:57:10 +0100

    added unit tests

--- ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/assertion/DirectionsQuestionnaireAssert.java
+++ ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/assertion/DirectionsQuestionnaireAssert.java
@@ -1,18 +1,85 @@
 package uk.gov.hmcts.cmc.ccd.assertion;
 
 import org.assertj.core.api.AbstractAssert;
+import uk.gov.hmcts.cmc.ccd.domain.directionsquestionnaire.CCDDirectionsQuestionnaire;
 import uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.DirectionsQuestionnaire;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.util.Objects;
 
 public class DirectionsQuestionnaireAssert extends AbstractAssert<DirectionsQuestionnaireAssert, DirectionsQuestionnaire> {
-    public DirectionsQuestionnaireAssert(DirectionsQuestionnaire directionsQuestionnaire) {
-        super(directionsQuestionnaire, DirectionsQuestionnaire.class);
+    public DirectionsQuestionnaireAssert(DirectionsQuestionnaire actual) {
+        super(actual, DirectionsQuestionnaire.class);
     }
 
-    public DirectionsQuestionnaireAssert isEqualTo(DirectionsQuestionnaire directionsQuestionnaire) {
+    public DirectionsQuestionnaireAssert isEqualTo(CCDDirectionsQuestionnaire ccdDirectionsQuestionnaire) {
 
-        assertThat(actual.getExceptionalCircumstancesReason()).isEqualTo(directionsQuestionnaire.getExceptionalCircumstancesReason());
+        isNotNull();
+
+
+        if (!Objects.equals(actual.getSelfWitness(), ccdDirectionsQuestionnaire.getSelfWitness())) {
+            failWithMessage("Expected self witness to be <%s> but was <%s>",
+                ccdDirectionsQuestionnaire.getSelfWitness(), actual.getSelfWitness());
+        }
+
+        if (!Objects.equals(actual.getHowManyOtherWitness(), ccdDirectionsQuestionnaire.getHowManyOtherWitness())) {
+            failWithMessage("Expected how many other witnesses to be <%s> but was <%s>",
+                ccdDirectionsQuestionnaire.getHowManyOtherWitness(), actual.getHowManyOtherWitness());
+        }
+
+        if (!Objects.equals(actual.getHearingLocation(), ccdDirectionsQuestionnaire.getHearingLocation())) {
+            failWithMessage("Expected hearing location to be <%s> but was <%s>",
+                ccdDirectionsQuestionnaire.getHearingLocation(), actual.getHearingLocation());
+        }
+
+        if (!Objects.equals(actual.getHearingLocationSlug(), ccdDirectionsQuestionnaire.getHearingLocationSlug())) {
+            failWithMessage("Expected hearing location slug to be <%s> but was <%s>",
+                ccdDirectionsQuestionnaire.getHearingLocationSlug(), actual.getHearingLocationSlug());
+        }
+
+        if (!Objects.equals(actual.getExceptionalCircumstancesReason(), ccdDirectionsQuestionnaire.getExceptionalCircumstancesReason())) {
+            failWithMessage("Expected exceptional circumstances reason to be <%s> but was <%s>",
+                ccdDirectionsQuestionnaire.getExceptionalCircumstancesReason(), actual.getExceptionalCircumstancesReason());
+        }
+
+        if (!Objects.equals(actual.getAvailableDate(), ccdDirectionsQuestionnaire.getAvailableDate())) {
+            failWithMessage("Expected available date to be <%s> but was <%s>",
+                ccdDirectionsQuestionnaire.getAvailableDate(), actual.getAvailableDate());
+        }
+
+        if (!Objects.equals(actual.getLanguageInterpreted(), ccdDirectionsQuestionnaire.getLanguageInterpreted())) {
+            failWithMessage("Expected language interpreted to be <%s> but was <%s>",
+                ccdDirectionsQuestionnaire.getLanguageInterpreted(), actual.getLanguageInterpreted());
+        }
+
+        if (!Objects.equals(actual.getSignLanguageInterpreted(), ccdDirectionsQuestionnaire.getSignLanguageInterpreted())) {
+            failWithMessage("Expected sign language interpreted to be <%s> but was <%s>",
+                ccdDirectionsQuestionnaire.getSignLanguageInterpreted(), actual.getSignLanguageInterpreted());
+        }
+
+        if (!Objects.equals(actual.getHearingLoop(), ccdDirectionsQuestionnaire.getHearingLoop())) {
+            failWithMessage("Expected hearing loop to be <%s> but was <%s>",
+                ccdDirectionsQuestionnaire.getHearingLoop(), actual.getHearingLoop());
+        }
+
+        if (!Objects.equals(actual.getDisabledAccess(), ccdDirectionsQuestionnaire.getDisabledAccess())) {
+            failWithMessage("Expected disabled access to be <%s> but was <%s>",
+                ccdDirectionsQuestionnaire.getDisabledAccess(), actual.getDisabledAccess());
+        }
+
+        if (!Objects.equals(actual.getOtherSupportRequired(), ccdDirectionsQuestionnaire.getOtherSupportRequired())) {
+            failWithMessage("Expected other support required to be <%s> but was <%s>",
+                ccdDirectionsQuestionnaire.getOtherSupportRequired(), actual.getOtherSupportRequired());
+        }
+
+        if (!Objects.equals(actual.getExpertEvidenceToExamine(), ccdDirectionsQuestionnaire.getExpertEvidenceToExamine())) {
+            failWithMessage("Expected expert evidence to examine to be <%s> but was <%s>",
+                ccdDirectionsQuestionnaire.getExpertEvidenceToExamine(), actual.getExpertEvidenceToExamine());
+        }
+
+        if (!Objects.equals(actual.getReasonForExpertAdvice(), ccdDirectionsQuestionnaire.getReasonForExpertAdvice())) {
+            failWithMessage("Expected reason for expert advice <%s> but was <%s>",
+                ccdDirectionsQuestionnaire.getReasonForExpertAdvice(), actual.getReasonForExpertAdvice());
+        }
 
         return this;
     }
---
+++ ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/util/SampleCCDDirectionsQuestionnaire.java
@@ -0,0 +1,58 @@
+package uk.gov.hmcts.cmc.ccd.util;
+
+import uk.gov.hmcts.cmc.ccd.domain.CCDCollectionElement;
+import uk.gov.hmcts.cmc.ccd.domain.directionsquestionnaire.CCDDirectionsQuestionnaire;
+import uk.gov.hmcts.cmc.ccd.domain.directionsquestionnaire.CCDExpertReportRow;
+import uk.gov.hmcts.cmc.ccd.domain.directionsquestionnaire.CCDUnavailableDate;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static uk.gov.hmcts.cmc.ccd.domain.CCDYesNoOption.NO;
+import static uk.gov.hmcts.cmc.ccd.domain.CCDYesNoOption.YES;
+
+public class SampleCCDDirectionsQuestionnaire {
+
+    public static SampleCCDDirectionsQuestionnaire builder() { return new SampleCCDDirectionsQuestionnaire(); }
+
+    public CCDDirectionsQuestionnaire build() {
+
+        List<CCDCollectionElement<CCDUnavailableDate>> unavailableDates = asList(
+            CCDCollectionElement.<CCDUnavailableDate>builder()
+                .value(CCDUnavailableDate
+                    .builder()
+                    .unavailableDate(LocalDate.of(2050,1,1))
+                    .build())
+                .build()
+        );
+
+        List<CCDCollectionElement<CCDExpertReportRow>> expertReportRow = asList(
+            CCDCollectionElement.<CCDExpertReportRow>builder()
+                .value(CCDExpertReportRow
+                    .builder()
+                    .expertName("expert1")
+                    .expertReportDate(LocalDate.of(2040,1,1))
+                    .build())
+                .build()
+        );
+
+        return CCDDirectionsQuestionnaire.builder()
+                    .selfWitness(YES)
+                    .howManyOtherWitness(1)
+                    .hearingLocation("London")
+                    .hearingLocationSlug("London-Court")
+                    .exceptionalCircumstancesReason("disabled")
+                    .unavailableDates(unavailableDates)
+                    .availableDate(LocalDate.of(2050,1,2))
+                    .languageInterpreted("some language")
+                    .signLanguageInterpreted("yes")
+                    .hearingLoop(NO)
+                    .disabledAccess(YES)
+                    .otherSupportRequired("maybe")
+                    .expertReportsRows(expertReportRow)
+                    .expertEvidenceToExamine("nothing")
+                    .reasonForExpertAdvice("for specified reason")
+                    .build();
+    }
+}
