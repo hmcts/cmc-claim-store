85a12ffa931db948a5971ad024195cdb8809da26
Kiran Varma <varma.ap@gmail.com>
2019-06-04 21:01:01 +0100

    remove unused import

--- ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/mapper/UnavailableDateMapperTest.java
+++ ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/mapper/UnavailableDateMapperTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.cmc.ccd.mapper;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
