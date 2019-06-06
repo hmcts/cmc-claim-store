14889711bcdbe77bffec23dff9044368970a5894
Srineelesh <srineelesh.sudhakar@hmcts.net>
2019-06-06 15:39:09 +0100

    fixed checkstyle error

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/ClaimServiceTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/ClaimServiceTest.java
@@ -47,7 +47,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
