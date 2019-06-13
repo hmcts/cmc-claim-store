13baaddda9f35f1fab44771954d9446c6a5849dd
Sabah Irfan <sabah.irfan@gmail.com>
2019-06-13 10:10:59 +0100

    Add skeleton for mapping from|

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/mapper/DirectionsQuestionnaireMapper.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/mapper/DirectionsQuestionnaireMapper.java
@@ -6,11 +6,20 @@ import uk.gov.hmcts.cmc.ccd.domain.CCDYesNoOption;
 import uk.gov.hmcts.cmc.ccd.domain.directionsquestionnaire.CCDCourtLocationOption;
 import uk.gov.hmcts.cmc.ccd.domain.directionsquestionnaire.CCDDirectionsQuestionnaire;
 import uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.DirectionsQuestionnaire;
+import uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.ExpertReport;
+import uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.ExpertRequest;
 import uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.HearingLocation;
+import uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.RequireSupport;
+import uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.UnavailableDate;
+import uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.Witness;
 import uk.gov.hmcts.cmc.domain.models.response.YesNoOption;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
+
+import static uk.gov.hmcts.cmc.ccd.util.StreamUtil.asStream;
 
 @Component
 public class DirectionsQuestionnaireMapper implements Mapper<CCDDirectionsQuestionnaire, DirectionsQuestionnaire> {
@@ -36,34 +45,14 @@ public class DirectionsQuestionnaireMapper implements Mapper<CCDDirectionsQuesti
             return null;
         }
 
-        final CCDDirectionsQuestionnaire.CCDDirectionsQuestionnaireBuilder builder = CCDDirectionsQuestionnaire.builder();
-        directionsQuestionnaire.getRequireSupport().ifPresent(requireSupport -> {
-            requireSupport.getLanguageInterpreter().ifPresent(builder::languageInterpreted);
-            requireSupport.getSignLanguageInterpreter().ifPresent(builder::signLanguageInterpreted);
-            requireSupport.getOtherSupport().ifPresent(builder::otherSupportRequired);
-
-            requireSupport.getHearingLoop()
-                .map(YesNoOption::name)
-                .map(CCDYesNoOption::valueOf)
-                .ifPresent(builder::hearingLoop);
-
-            requireSupport.getDisabledAccess()
-                .map(YesNoOption::name)
-                .map(CCDYesNoOption::valueOf)
-                .ifPresent(builder::disabledAccess);
-        });
+        CCDDirectionsQuestionnaire.CCDDirectionsQuestionnaireBuilder builder = CCDDirectionsQuestionnaire.builder();
+        directionsQuestionnaire.getRequireSupport().ifPresent(toRequireSupport(builder));
 
         toHearingLocation(directionsQuestionnaire.getHearingLocation(), builder);
 
-        directionsQuestionnaire.getWitness().ifPresent(witness -> {
-            builder.selfWitness(CCDYesNoOption.valueOf(witness.getSelfWitness().name()));
-            witness.getNoOfOtherWitness().ifPresent(builder::howManyOtherWitness);
-        });
+        directionsQuestionnaire.getWitness().ifPresent(toWitness(builder));
 
-        directionsQuestionnaire.getExpertRequest().ifPresent(expertRequest -> {
-            builder.expertEvidenceToExamine(expertRequest.getExpertEvidenceToExamine());
-            builder.reasonForExpertAdvice(expertRequest.getReasonForExpertAdvice());
-        });
+        directionsQuestionnaire.getExpertRequest().ifPresent(toExpertRequest(builder));
 
         builder.expertReports(directionsQuestionnaire.getExpertReports()
             .stream()
@@ -80,6 +69,42 @@ public class DirectionsQuestionnaireMapper implements Mapper<CCDDirectionsQuesti
         );
 
         return builder.build();
+    }
+
+    private Consumer<RequireSupport> toRequireSupport(
+        CCDDirectionsQuestionnaire.CCDDirectionsQuestionnaireBuilder builder
+    ) {
+        return requireSupport -> {
+            requireSupport.getLanguageInterpreter().ifPresent(builder::languageInterpreted);
+            requireSupport.getSignLanguageInterpreter().ifPresent(builder::signLanguageInterpreted);
+            requireSupport.getOtherSupport().ifPresent(builder::otherSupportRequired);
+
+            requireSupport.getHearingLoop()
+                .map(YesNoOption::name)
+                .map(CCDYesNoOption::valueOf)
+                .ifPresent(builder::hearingLoop);
+
+            requireSupport.getDisabledAccess()
+                .map(YesNoOption::name)
+                .map(CCDYesNoOption::valueOf)
+                .ifPresent(builder::disabledAccess);
+        };
+    }
+
+    private Consumer<ExpertRequest> toExpertRequest(
+        CCDDirectionsQuestionnaire.CCDDirectionsQuestionnaireBuilder builder
+    ) {
+        return expertRequest -> {
+            builder.expertEvidenceToExamine(expertRequest.getExpertEvidenceToExamine());
+            builder.reasonForExpertAdvice(expertRequest.getReasonForExpertAdvice());
+        };
+    }
+
+    private Consumer<Witness> toWitness(CCDDirectionsQuestionnaire.CCDDirectionsQuestionnaireBuilder builder) {
+        return witness -> {
+            builder.selfWitness(CCDYesNoOption.valueOf(witness.getSelfWitness().name()));
+            witness.getNoOfOtherWitness().ifPresent(builder::howManyOtherWitness);
+        };
     }
 
     private void toHearingLocation(
@@ -100,9 +125,54 @@ public class DirectionsQuestionnaireMapper implements Mapper<CCDDirectionsQuesti
             return null;
         }
 
-        return DirectionsQuestionnaire.builder()
-            .build();
+        DirectionsQuestionnaire.DirectionsQuestionnaireBuilder builder = DirectionsQuestionnaire.builder();
+        fromHearingLocation(builder, ccdDirectionsQuestionnaire);
+        fromWitness(builder, ccdDirectionsQuestionnaire);
+        fromExpertRequest(builder, ccdDirectionsQuestionnaire);
+        fromRequireSupport(builder, ccdDirectionsQuestionnaire);
+
+        List<ExpertReport> expertReports = asStream(ccdDirectionsQuestionnaire.getExpertReports())
+            .map(expertRowMapper::from)
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
+
+        builder.expertReports(expertReports);
+
+        List<UnavailableDate> unavailableDates = asStream(ccdDirectionsQuestionnaire.getUnavailableDates())
+            .map(unavailableDateMapper::from)
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
+
+        builder.unavailableDates(unavailableDates);
+
+        return builder.build();
     }
 
+    private void fromRequireSupport(
+        DirectionsQuestionnaire.DirectionsQuestionnaireBuilder builder,
+        CCDDirectionsQuestionnaire ccdDirectionsQuestionnaire
+    ) {
 
+    }
+
+    private void fromExpertRequest(
+        DirectionsQuestionnaire.DirectionsQuestionnaireBuilder builder,
+        CCDDirectionsQuestionnaire ccdDirectionsQuestionnaire
+    ) {
+
+    }
+
+    private void fromWitness(
+        DirectionsQuestionnaire.DirectionsQuestionnaireBuilder builder,
+        CCDDirectionsQuestionnaire ccdDirectionsQuestionnaire
+    ) {
+
+    }
+
+    private void fromHearingLocation(
+        DirectionsQuestionnaire.DirectionsQuestionnaireBuilder builder,
+        CCDDirectionsQuestionnaire ccdDirectionsQuestionnaire
+    ) {
+
+    }
 }
