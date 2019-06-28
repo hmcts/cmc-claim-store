6536fc790b5c6dd96f3fb26d4e76cbbe08d5e52b
Sabah Irfan <sabah.irfan@gmail.com>
2019-06-28 12:59:38 +0100

    Add get preferred court

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/DirectionsQuestionnaireService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/DirectionsQuestionnaireService.java
@@ -2,15 +2,24 @@ package uk.gov.hmcts.cmc.claimstore.services;
 
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.cmc.ccd.domain.CaseEvent;
+import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.claimantresponse.ClaimantResponse;
 import uk.gov.hmcts.cmc.domain.models.claimantresponse.ResponseRejection;
 import uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.DirectionsQuestionnaire;
+import uk.gov.hmcts.cmc.domain.models.response.FullDefenceResponse;
+import uk.gov.hmcts.cmc.domain.models.response.PartAdmissionResponse;
+import uk.gov.hmcts.cmc.domain.models.response.Response;
 
 import java.util.Optional;
 
 import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.ASSIGN_FOR_DIRECTIONS;
 import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.REFERRED_TO_MEDIATION;
 import static uk.gov.hmcts.cmc.claimstore.utils.ClaimantResponseHelper.isOptedForMediation;
+import static uk.gov.hmcts.cmc.claimstore.utils.TheirDetailsHelper.isDefendantBusiness;
+import static uk.gov.hmcts.cmc.domain.models.claimantresponse.ClaimantResponseType.REJECTION;
 import static uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.PilotCourt.isPilotCourt;
+import static uk.gov.hmcts.cmc.domain.models.response.ResponseType.FULL_DEFENCE;
+import static uk.gov.hmcts.cmc.domain.models.response.ResponseType.PART_ADMISSION;
 
 @Service
 public class DirectionsQuestionnaireService {
@@ -31,4 +40,29 @@ public class DirectionsQuestionnaireService {
         return Optional.empty();
     }
 
+    public String getPreferredCourt(Claim claim) {
+        if (isDefendantBusiness(claim.getClaimData().getDefendant())) {
+            ClaimantResponse claimantResponse = claim.getClaimantResponse().orElseThrow(IllegalStateException::new);
+            if (claimantResponse.getType() == REJECTION) {
+                return ((ResponseRejection) claimantResponse).getDirectionsQuestionnaire()
+                    .orElseThrow(IllegalStateException::new)
+                    .getHearingLocation().getCourtName();
+            } else {
+                throw new IllegalStateException("No preferred court as claimant response is not rejection.");
+            }
+        } else {
+            Response defendantResponse = claim.getResponse().orElseThrow(IllegalStateException::new);
+            if (defendantResponse.getResponseType() == FULL_DEFENCE) {
+                return ((FullDefenceResponse) defendantResponse).getDirectionsQuestionnaire()
+                    .orElseThrow(IllegalStateException::new)
+                    .getHearingLocation().getCourtName();
+            } else if (defendantResponse.getResponseType() == PART_ADMISSION) {
+                return ((PartAdmissionResponse) defendantResponse).getDirectionsQuestionnaire()
+                    .orElseThrow(IllegalStateException::new)
+                    .getHearingLocation().getCourtName();
+            } else {
+                throw new IllegalStateException("No preferred court as defendant response is full admission");
+            }
+        }
+    }
 }
