43bbd575937aa46cf562b871a64e33c05b201836
Sabah Irfan <sabah.irfan@gmail.com>
2019-07-10 12:10:26 +0100

    Add domain model for review order

---
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CCDReviewOrder.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.cmc.ccd.domain;
+
+import lombok.Builder;
+import lombok.Value;
+
+import java.time.LocalDateTime;
+
+@Value
+@Builder
+public class CCDReviewOrder {
+
+    public enum PartyType {CLAIMANT, DEFENDANT}
+
+    private String reason;
+    private PartyType requestedBy;
+    private LocalDateTime requestedAt;
+}
--- ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/assertion/Assertions.java
+++ ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/assertion/Assertions.java
@@ -20,6 +20,7 @@ import uk.gov.hmcts.cmc.ccd.domain.defendant.CCDPartyStatement;
 import uk.gov.hmcts.cmc.domain.models.Address;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.CountyCourtJudgment;
+import uk.gov.hmcts.cmc.domain.models.ReviewOrder;
 import uk.gov.hmcts.cmc.domain.models.TimelineEvent;
 import uk.gov.hmcts.cmc.domain.models.claimantresponse.CourtDetermination;
 import uk.gov.hmcts.cmc.domain.models.claimantresponse.ResponseAcceptation;
@@ -150,5 +151,9 @@ public class Assertions {
 
     public static MoneyAssert assertMoney(BigDecimal amount) {
         return new MoneyAssert(amount);
+    }
+
+    public static ReviewOrderAssert assertThat(ReviewOrder reviewOrder) {
+        return new ReviewOrderAssert(reviewOrder);
     }
 }
