696d038f8aa84f2306520cc5138cc24b003b2842
Sabah Irfan <sabah.irfan@gmail.com>
2019-07-11 11:40:12 +0100

    Save review order request in CCD

---
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/rules/ReviewOrderRuleTest.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.cmc.claimstore.rules;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.cmc.claimstore.exceptions.ConflictException;
+import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaim;
+import uk.gov.hmcts.cmc.domain.models.sampledata.SampleReviewOrder;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReviewOrderRuleTest {
+
+    private ReviewOrderRule reviewOrderRule = new ReviewOrderRule();
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowExceptionWhenClaimIsNull() {
+        reviewOrderRule.assertReviewOrder(null);
+    }
+
+    @Test(expected = ConflictException.class)
+    public void shouldFailIfAlreadySubmittedReviewOrder() {
+        Claim claim = SampleClaim.builder().withReviewOrder(SampleReviewOrder.getDefault()).build();
+        reviewOrderRule.assertReviewOrder(claim);
+    }
+
+    @Test(expected = Test.None.class)
+    public void shouldAllowForValidPaidInFull() {
+        Claim claim = SampleClaim.getDefault();
+        reviewOrderRule.assertReviewOrder(claim);
+    }
+}
