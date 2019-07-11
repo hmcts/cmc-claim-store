518249dda755648442660524d62ef3456bb85541
Sabah u Din Irfan <sabah.irfan@gmail.com>
2019-07-11 13:43:24 +0100

    ROC-5387 Save directions questionnaire (#1006)

---
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/mapper/ExpertReportMapper.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.cmc.ccd.mapper;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.cmc.ccd.domain.CCDCollectionElement;
+import uk.gov.hmcts.cmc.ccd.domain.directionsquestionnaire.CCDExpertReport;
+import uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.ExpertReport;
+
+@Component
+public class ExpertReportMapper implements Mapper<CCDCollectionElement<CCDExpertReport>, ExpertReport> {
+
+    @Override
+    public CCDCollectionElement<CCDExpertReport> to(ExpertReport expertReport) {
+        if (expertReport == null) {
+            return null;
+        }
+
+        return CCDCollectionElement.<CCDExpertReport>builder()
+            .value(CCDExpertReport.builder()
+                .expertName(expertReport.getExpertName())
+                .expertReportDate(expertReport.getExpertReportDate())
+                .build())
+            .id(expertReport.getId())
+            .build();
+    }
+
+    @Override
+    public ExpertReport from(CCDCollectionElement<CCDExpertReport> collectionElement) {
+        CCDExpertReport expertReport = collectionElement.getValue();
+
+        if (expertReport == null) {
+            return null;
+        }
+
+        return ExpertReport
+            .builder()
+            .id(collectionElement.getId())
+            .expertName(expertReport.getExpertName())
+            .expertReportDate(expertReport.getExpertReportDate())
+            .build();
+    }
+}
---
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/mapper/YesNoMapper.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.cmc.ccd.mapper;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.cmc.ccd.domain.CCDYesNoOption;
+import uk.gov.hmcts.cmc.domain.models.response.YesNoOption;
+
+@Component
+public class YesNoMapper implements Mapper<CCDYesNoOption, YesNoOption> {
+
+    @Override
+    public CCDYesNoOption to(YesNoOption yesNoOption) {
+        if (yesNoOption == null) {
+            return null;
+        }
+        return CCDYesNoOption.valueOf(yesNoOption.name());
+    }
+
+    @Override
+    public YesNoOption from(CCDYesNoOption ccdYesNoOption) {
+        if (ccdYesNoOption == null) {
+            return null;
+        }
+        return YesNoOption.valueOf(ccdYesNoOption.name());
+    }
+}
