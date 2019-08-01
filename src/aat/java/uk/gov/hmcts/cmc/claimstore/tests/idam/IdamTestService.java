bdb3aafcf28c931328b67f46df7cd913cd5158ec
markry-kainos <43344584+markry-kainos@users.noreply.github.com>
2019-08-01 10:31:20 +0100

    Update IdamTestService.java

--- src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/idam/IdamTestService.java
+++ src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/idam/IdamTestService.java
@@ -154,7 +154,7 @@ public class IdamTestService {
         //recommended delay from IDAM team to stop intermittent auth failures
         idamTestApi.createUser(createUserRequest);
         try {
-            Thread.sleep(5000);
+            Thread.sleep(15000);
         } catch (InterruptedException ex) {
             logger.error("Error trying to sleep after creating a user", ex);
         }
