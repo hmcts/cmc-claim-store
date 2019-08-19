6c3953184e4a50c9999e1a56563e23b778416522
Silvia Mandala <silvia.mandala@hmcts.net>
2019-08-19 15:41:52 +0100

    ROC-6117 add snake case strategy to payment and remove sout

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/Payment.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/Payment.java
@@ -1,20 +1,22 @@
 package uk.gov.hmcts.cmc.domain.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Data;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.hibernate.validator.constraints.NotBlank;
 
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import javax.validation.constraints.NotNull;
 
 import static uk.gov.hmcts.cmc.domain.utils.ToStringStyle.ourStyle;
 
 @Builder
 @Data
 @JsonIgnoreProperties(value = {"description", "state"})
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Payment {
     private final String id;
     /**
@@ -24,11 +26,9 @@ public class Payment {
     private final BigDecimal amount;
     @NotBlank
     private final String reference;
-    @JsonProperty("date_created")
     private final String dateCreated;
     private final String status;
     @NotBlank
-    @JsonProperty("next_url")
     private final String nextUrl;
 
     public Payment(
@@ -37,7 +37,8 @@ public class Payment {
         String reference,
         String dateCreated,
         String status,
-        String nextUrl) {
+        String nextUrl
+    ) {
         this.id = id;
         this.amount = amount;
         this.reference = reference;
