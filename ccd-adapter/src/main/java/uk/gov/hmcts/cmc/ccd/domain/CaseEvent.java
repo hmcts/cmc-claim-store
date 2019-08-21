0be66c5b74b607bfa6edd640fc7a915f3e2317fb
Kiran Varma <varma.ap@gmail.com>
2019-08-21 16:24:05 +0100

    rename

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CaseEvent.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CaseEvent.java
@@ -49,7 +49,7 @@ public enum CaseEvent {
     CLAIMANT_DIRECTIONS_QUESTIONNAIRE_UPLOAD("ClaimantDirectionsQuestionnaireUpload"),
     ORDER_REVIEW_REQUESTED("OrderReviewRequested"),
     WAITING_TRANSFER("WaitingTransfer"),
-    CREATE_CLAIM_LEGAL_REP("CreateClaimLegalRep");
+    CREATE_LEGAL_REP_CLAIM("CreateClaimLegalRep");
 
     private String value;
 
