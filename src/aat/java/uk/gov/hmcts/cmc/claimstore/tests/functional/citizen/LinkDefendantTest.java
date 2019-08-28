ce86ab5f3a393cac8af93fd7facce99ee13f1de9
Mark Ryan <mark.ryan@hmcts.net>
2019-08-28 17:50:13 +0100

    tidy imports

--- src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/functional/citizen/LinkDefendantTest.java
+++ src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/functional/citizen/LinkDefendantTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.cmc.claimstore.tests.functional.citizen;
 
 import io.restassured.RestAssured;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.HttpHeaders;
