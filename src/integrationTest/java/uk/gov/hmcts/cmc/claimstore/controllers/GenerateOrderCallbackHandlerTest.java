fd122476bbf1c5927dd122cc5e1a20a3f821b5b8
dharmendra kumar <dharmendra.kumar@hmcts.net>
2019-09-18 12:08:49 +0100

    Revert "ROC-5800:support endpoint to reset claim submission indicators (#995)"
    This reverts commit 54a732b11e682c74fd7c9b73de933cb952fa4f2b.

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/controllers/GenerateOrderCallbackHandlerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/controllers/GenerateOrderCallbackHandlerTest.java
@@ -134,7 +134,7 @@ public class GenerateOrderCallbackHandlerTest extends MockSpringTest {
     }
 
     private ResultActions makeRequest(String callbackType) throws Exception {
-        CaseDetails caseDetailsTemp = successfulCoreCaseDataStoreSubmitResponseWithDQ();
+        CaseDetails caseDetailsTemp =  successfulCoreCaseDataStoreSubmitResponseWithDQ();
         CaseDetails caseDetailsBefore = CaseDetails.builder()
             .id(caseDetailsTemp.getId())
             .data(caseDetailsTemp.getData())
@@ -188,9 +188,14 @@ public class GenerateOrderCallbackHandlerTest extends MockSpringTest {
     }
 
     private ResultActions makeRequestGenerateOrder(String callbackType) throws Exception {
-        CaseDetails caseDetails = successfulCoreCaseDataStoreSubmitResponseWithDQ();
-        caseDetails.getData().put("preferredDQCourt", "Preferred court");
+        CaseDetails caseDetailsTemp =  successfulCoreCaseDataStoreSubmitResponseWithDQ();
+        Map<String, Object> data = new HashMap<>(caseDetailsTemp.getData());
+        data.put("preferredDQCourt", "Preferred court");
 
+        CaseDetails caseDetails = CaseDetails.builder()
+            .id(caseDetailsTemp.getId())
+            .data(data)
+            .build();
         CallbackRequest callbackRequest = CallbackRequest.builder()
             .eventId(CaseEvent.GENERATE_ORDER.getValue())
             .caseDetails(caseDetails)
