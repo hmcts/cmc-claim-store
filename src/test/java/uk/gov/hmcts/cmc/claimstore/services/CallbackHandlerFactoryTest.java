3d57ce962aee2b60139547bc2b39c3def8560771
Dharmendra kumar <k_dharmendra@hotmail.com>
2019-09-30 19:59:31 +0100

    resolve tests

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/CallbackHandlerFactoryTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/CallbackHandlerFactoryTest.java
@@ -17,7 +17,6 @@ import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 
 import static org.mockito.ArgumentMatchers.anyMap;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.verify;
