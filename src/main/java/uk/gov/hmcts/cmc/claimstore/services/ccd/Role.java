f9665094b46e93d57e7c7b9d8e36cc6b074c0f6e
Silvia Mandala <silvia.mandala@hmcts.net>
2019-10-03 14:26:31 +0100

    ROC-6118 Add roles for citizen callbacks and enum for roles

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/Role.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.cmc.claimstore.services.ccd;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+    CITIZEN("citizen"),
+    CASEWORKER("caseworker-cmc"),
+    SOLICITOR("solicitor"),
+    LEGAL_ADVISOR("caseworker-cmc-legaladvisor"),
+    JUDGE("caseworker-cmc-judge");
+
+    private String role;
+
+    public static Role fromValue(String value) {
+        return Arrays.stream(Role.values())
+            .filter(val -> val.name().equalsIgnoreCase(value))
+            .findFirst()
+            .orElseThrow(IllegalArgumentException::new);
+    }
+}
