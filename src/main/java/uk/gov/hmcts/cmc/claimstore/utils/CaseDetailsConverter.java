be3c0e0535c7a60eb6891156a3f80cb5a58b4b0f
Dave Oxtoby <david.oxtoby@hmcts.net>
2019-10-10 12:39:32 +0100

    ROC-6642: Add intentionToProceedDeadline to claim

--- src/main/java/uk/gov/hmcts/cmc/claimstore/utils/CaseDetailsConverter.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/utils/CaseDetailsConverter.java
@@ -1,12 +1,16 @@
 package uk.gov.hmcts.cmc.claimstore.utils;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.ccd.domain.CCDCase;
 import uk.gov.hmcts.cmc.ccd.mapper.CaseMapper;
 import uk.gov.hmcts.cmc.claimstore.processors.JsonMapper;
+import uk.gov.hmcts.cmc.claimstore.services.WorkingDayIndicator;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,14 +19,35 @@ public class CaseDetailsConverter {
 
     private final CaseMapper caseMapper;
     private final JsonMapper jsonMapper;
+    private final int intentionToProceedDeadline;
+    private final WorkingDayIndicator workingDayIndicator;
 
-    public CaseDetailsConverter(CaseMapper caseMapper, JsonMapper jsonMapper) {
+    public CaseDetailsConverter(
+        CaseMapper caseMapper,
+        JsonMapper jsonMapper,
+        WorkingDayIndicator workingDayIndicator,
+        @Value("${intention.to.proceed.deadline:33}")int intentionToProceedDeadline
+        ) {
+
         this.caseMapper = caseMapper;
         this.jsonMapper = jsonMapper;
+        this.intentionToProceedDeadline = intentionToProceedDeadline;
+        this.workingDayIndicator = workingDayIndicator;
     }
 
     public Claim extractClaim(CaseDetails caseDetails) {
-        return caseMapper.from(extractCCDCase(caseDetails));
+        Claim claim = caseMapper.from(extractCCDCase(caseDetails));
+
+        if (claim.getRespondedAt() == null) {
+            return claim;
+        }
+
+        // Calculating the intention to proceed here rather than in the mapper as we have access
+        // to the WorkingDayIndicator here
+        LocalDate intentionToProceedDeadline = calculateIntentionToProceedDeadline(claim.getRespondedAt());
+        return claim.toBuilder()
+                    .intentionToProceedDeadline(intentionToProceedDeadline)
+                    .build();
     }
 
     public CCDCase extractCCDCase(CaseDetails caseDetails) {
@@ -40,5 +65,15 @@ public class CaseDetailsConverter {
     @SuppressWarnings("unchecked")
     public Map<String, Object> convertToMap(CCDCase ccdCase) {
         return (Map<String, Object>) jsonMapper.convertValue(ccdCase, Map.class);
+    }
+
+    private LocalDate calculateIntentionToProceedDeadline(LocalDateTime respondedDate) {
+        LocalDate deadline = respondedDate.toLocalDate().plusDays(this.intentionToProceedDeadline);
+
+        while (!workingDayIndicator.isWorkingDay(deadline)) {
+            deadline = deadline.plusDays(1);
+        }
+
+        return deadline;
     }
 }
