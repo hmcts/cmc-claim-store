7edd58227e3d3887322b75ae8c79b83e5df54cb3
Dave Oxtoby <daveoxtoby@project-zeta.co.uk>
2019-10-17 12:30:20 +0100

    ROC-6641: Trigger intention to proceed failure emails

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/IntentionToProceedServiceTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/IntentionToProceedServiceTest.java
@@ -1,21 +1,32 @@
 package uk.gov.hmcts.cmc.claimstore.services;
 
+import com.google.common.collect.ImmutableMap;
+import groovy.lang.IntRange;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.cmc.claimstore.appinsights.AppInsights;
+import uk.gov.hmcts.cmc.claimstore.config.properties.emails.StaffEmailProperties;
+import uk.gov.hmcts.cmc.claimstore.documents.content.IntentionToProceedContentProvider;
+import uk.gov.hmcts.cmc.claimstore.idam.models.User;
 import uk.gov.hmcts.cmc.claimstore.repositories.CaseRepository;
 import uk.gov.hmcts.cmc.claimstore.repositories.CaseSearchApi;
+import uk.gov.hmcts.cmc.claimstore.services.staff.models.EmailContent;
+import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.email.EmailService;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.cmc.claimstore.utils.VerificationModeUtils.once;
@@ -40,6 +51,15 @@ public class IntentionToProceedServiceTest {
     @Mock
     private AppInsights appInsights;
 
+    @Mock
+    private IntentionToProceedContentProvider emailContentProvider;
+
+    @Mock
+    private EmailService emailService;
+
+    @Mock
+    private StaffEmailProperties emailProperties;
+
     private final int intentionToProceedAdjustment = 33;
 
     private IntentionToProceedDeadlineCalculator intentionToProceedDeadlineCalculator;
@@ -57,6 +77,9 @@ public class IntentionToProceedServiceTest {
             userService,
             appInsights,
             caseRepository,
+            emailContentProvider,
+            emailService,
+            emailProperties,
             intentionToProceedDeadlineCalculator
         );
     }
@@ -120,5 +143,49 @@ public class IntentionToProceedServiceTest {
         verify(caseSearchApi, once()).getClaimsPastIntentionToProceed(any(), eq(responseDate));
 
     }
-    
+
+    @Test
+    public void claimStayingSuccessShouldNotSendNotificationEmail() {
+        List<Claim> claims =
+            new IntRange(1, 3).stream()
+                .map(id -> Claim.builder().id(id.longValue()).build())
+                .collect(Collectors.toList());
+
+        when(workingDayIndicator.getPreviousWorkingDay(any())).then(returnsFirstArg());
+        when(caseSearchApi.getClaimsPastIntentionToProceed(any(), any())).thenReturn(claims);
+        when(userService.authenticateAnonymousCaseWorker()).thenReturn(new User("", null));
+
+        intentionToProceedService.checkClaimsPastIntentionToProceedDeadline(LocalDateTime.now());
+
+        verify(emailService, never()).sendEmail(any(), any());
+    }
+
+    @Test
+    public void claimStayingFailuresShouldSendNotificationEmail() {
+        List<Claim> claims =
+            new IntRange(1, 3).stream()
+                .map(id -> Claim.builder().id(id.longValue()).build())
+                .collect(Collectors.toList());
+
+        when(workingDayIndicator.getPreviousWorkingDay(any())).then(returnsFirstArg());
+        when(caseRepository.saveCaseEvent(any(), any(), any())).thenThrow(RuntimeException.class);
+        when(caseSearchApi.getClaimsPastIntentionToProceed(any(), any())).thenReturn(claims);
+        when(userService.authenticateAnonymousCaseWorker()).thenReturn(new User("", null));
+        when(emailContentProvider.createContent(any())).thenReturn(new EmailContent("", ""));
+
+        final ImmutableMap<String, Object> input = ImmutableMap.of(
+            "noOfClaims", claims.size(),
+            "claimIds", claims.stream().map(c -> c.getId().toString())
+                .collect(Collectors.joining("\n"))
+        );
+
+        when(emailContentProvider.createParameters(claims)).thenReturn(input);
+
+        intentionToProceedService.checkClaimsPastIntentionToProceedDeadline(LocalDateTime.now());
+
+        verify(emailService, once()).sendEmail(any(), any());
+
+        verify(emailContentProvider, once()).createContent(input);
+    }
+
 }
