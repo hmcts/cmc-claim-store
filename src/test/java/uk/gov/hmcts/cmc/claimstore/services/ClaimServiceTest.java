c5a5f7445e74550845cee859e9381075d8e7a6fd
Silvia Mandalà <simad@users.noreply.github.com>
2019-10-22 08:41:04 +0100

    ROC-6121 Create citizen claim endpoint for ioc (#1214)
    * ROC-6121 Create citizen claim endpoint for ioc

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/ClaimServiceTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/ClaimServiceTest.java
@@ -60,6 +60,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.CREATE_CITIZEN_CLAIM;
 import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.RESET_CLAIM_SUBMISSION_OPERATION_INDICATORS;
 import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.RESUME_CLAIM_PAYMENT_CITIZEN;
 import static uk.gov.hmcts.cmc.claimstore.appinsights.AppInsightsEvent.NUMBER_OF_RECONSIDERATION;
@@ -111,8 +112,6 @@ public class ClaimServiceTest {
     @Mock
     private ResponseDeadlineCalculator responseDeadlineCalculator;
     @Mock
-    private DirectionsQuestionnaireDeadlineCalculator directionsQuestionnaireDeadlineCalculator;
-    @Mock
     private EventProducer eventProducer;
     @Mock
     private AppInsights appInsights;
@@ -135,7 +134,7 @@ public class ClaimServiceTest {
             new PaidInFullRule(),
             new ClaimAuthorisationRule(userService),
             new ReviewOrderRule(),
-            false,
+            true,
             RETURN_URL);
     }
 
@@ -196,51 +195,6 @@ public class ClaimServiceTest {
 
     @Test
     public void saveClaimShouldFinishSuccessfully() {
-
-        ClaimData claimData = SampleClaimData.validDefaults();
-
-        when(userService.getUser(eq(AUTHORISATION))).thenReturn(USER);
-        when(issueDateCalculator.calculateIssueDay(any(LocalDateTime.class))).thenReturn(ISSUE_DATE);
-        when(responseDeadlineCalculator.calculateResponseDeadline(eq(ISSUE_DATE))).thenReturn(RESPONSE_DEADLINE);
-        when(caseRepository.saveClaim(eq(USER), any())).thenReturn(claim);
-
-        Claim createdClaim = claimService
-            .saveClaim(USER_ID, claimData, AUTHORISATION, singletonList("admissions"));
-
-        assertThat(createdClaim.getClaimData()).isEqualTo(claim.getClaimData());
-
-        verify(caseRepository, once()).saveClaim(any(User.class), any(Claim.class));
-        verify(eventProducer, once()).createClaimIssuedEvent(eq(createdClaim), eq(null),
-            anyString(), eq(AUTHORISATION));
-    }
-
-    @Test
-    public void saveLegalRepClaimShouldFinishSuccessfully() {
-        ClaimData claimData = SampleClaimData.submittedByLegalRepresentative();
-
-        when(userService.getUser(eq(AUTHORISATION))).thenReturn(USER);
-        when(caseRepository.saveRepresentedClaim(eq(USER), any(Claim.class))).thenReturn(representedClaim);
-
-        Claim createdLegalRepClaim = claimService
-            .saveRepresentedClaim(
-                USER_ID, claimData, AUTHORISATION);
-
-        assertThat(createdLegalRepClaim.getClaimData()).isEqualTo(representedClaim.getClaimData());
-
-        verify(caseRepository, once()).saveRepresentedClaim(
-            eq(USER), claimArgumentCaptor.capture());
-        verify(eventProducer, once()).createRepresentedClaimCreatedEvent(eq(createdLegalRepClaim),
-            anyString(), eq(AUTHORISATION));
-
-        Claim argumentCaptorValue = claimArgumentCaptor.getValue();
-        assertThat(argumentCaptorValue.getClaimData()).isEqualTo(claimData);
-        assertThat(argumentCaptorValue.getExternalId()).isEqualTo(claimData.getExternalId().toString());
-        assertThat(argumentCaptorValue.getSubmitterId()).isEqualTo(USER_ID);
-        assertThat(argumentCaptorValue.getSubmitterEmail()).isEqualTo(USER.getUserDetails().getEmail());
-    }
-
-    @Test
-    public void saveClaimShouldFinishWithoutPinGenerationSuccessfully() {
         //given
 
         when(userService.getUser(eq(AUTHORISATION))).thenReturn(USER);
@@ -276,6 +230,31 @@ public class ClaimServiceTest {
         verify(userService, never()).generatePin(eq(outputClaimData.getDefendant().getName()), eq(AUTHORISATION));
         verify(caseRepository, once()).saveClaim(any(User.class), any(Claim.class));
         verify(eventProducer, once()).createClaimCreatedEvent(eq(createdClaim), anyString(), eq(AUTHORISATION));
+    }
+
+    @Test
+    public void saveLegalRepClaimShouldFinishSuccessfully() {
+        ClaimData claimData = SampleClaimData.submittedByLegalRepresentative();
+
+        when(userService.getUser(eq(AUTHORISATION))).thenReturn(USER);
+        when(caseRepository.saveRepresentedClaim(eq(USER), any(Claim.class))).thenReturn(representedClaim);
+
+        Claim createdLegalRepClaim = claimService
+            .saveRepresentedClaim(
+                USER_ID, claimData, AUTHORISATION);
+
+        assertThat(createdLegalRepClaim.getClaimData()).isEqualTo(representedClaim.getClaimData());
+
+        verify(caseRepository, once()).saveRepresentedClaim(
+            eq(USER), claimArgumentCaptor.capture());
+        verify(eventProducer, once()).createClaimCreatedEvent(eq(createdLegalRepClaim),
+            anyString(), eq(AUTHORISATION));
+
+        Claim argumentCaptorValue = claimArgumentCaptor.getValue();
+        assertThat(argumentCaptorValue.getClaimData()).isEqualTo(claimData);
+        assertThat(argumentCaptorValue.getExternalId()).isEqualTo(claimData.getExternalId().toString());
+        assertThat(argumentCaptorValue.getSubmitterId()).isEqualTo(USER_ID);
+        assertThat(argumentCaptorValue.getSubmitterEmail()).isEqualTo(USER.getUserDetails().getEmail());
     }
 
     @Test
@@ -528,7 +507,6 @@ public class ClaimServiceTest {
             .nextUrl("http://nexturl.test")
             .build();
         assertThat(response).isEqualTo(expectedResponse);
-
     }
 
     @Test
@@ -575,6 +553,23 @@ public class ClaimServiceTest {
         CreatePaymentResponse response = claimService.resumePayment(AUTHORISATION, claimData);
 
         assertThat(response.getNextUrl()).isEqualTo("http://payment.nexturl.test");
+    }
+
+    @Test
+    public void saveCitizenClaimShouldFinishSuccessfully() {
+        when(userService.getUser(eq(AUTHORISATION))).thenReturn(USER);
+        when(caseRepository.getClaimByExternalId(VALID_APP.getExternalId().toString(), USER))
+            .thenReturn(Optional.of(claim));
+        when(caseRepository
+            .saveCaseEvent(eq(AUTHORISATION), any(Claim.class), eq(CREATE_CITIZEN_CLAIM)))
+            .thenReturn(claim);
+
+        Claim createdClaim = claimService.saveCitizenClaim(
+            AUTHORISATION,
+            VALID_APP,
+            singletonList("admissions"));
+
+        assertThat(createdClaim).isEqualTo(claim);
     }
 
     @Test
