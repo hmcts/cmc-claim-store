fdac134e4de90161201f81c3afa45629db4b9774
Dave Oxtoby <dave.oxtoby@gmail.com>
2019-11-07 22:02:03 +0000

    ROC-6616: Stay claims after intention to proceed deadline passed (#1236)
    * ROC-6616: Stay claims after intention to proceed deadline passed
    
    * ROC-6616: Update elastic search query
    
    * ROC-6616: Persist Intention to proceed in CCD
    
    * ROC-6616: Add line spacing
    
    * move intetnion to proceed deadline days in dateCalucators
    
    * ROC-6616: Add config placeholder
    
    * ROC-6616: Add infrastructure config
    
    * ROC-6616: Rename event to be more generic
    
    * ROC-6102: Move from JMX to support controller
    
    * ROC-6102: Fix tests
    
    * ROC-6649: Properly parse date
    
    * ROC-6649: Add support controller tests
    
    * ROC-6616: Remove comments
    
    * ROC-6616: Review changes
    
    * ROC-6616: Environment configuration
    
    * ROC-6616: Codestyle fix
    
    * ROC-6616: Add more tests
    
    * ROC-6616: Checkstyle fixes
    
    * ROC-6616: Checkstyle fixes
    
    * Roc 6641 trigger intention to proceed failure emails (#1243)
    
    * ROC-6641: Trigger intention to proceed failure emails
    
    * ROC-6641: Remove comments
    
    * ROC-6641: Add testing logging
    
    * ROC-6641: Add more testing logging
    
    * ROC-6641: Remove testing logging
    
    * ROC-6641: Rebase fixes

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/CoreCaseDataServiceTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/CoreCaseDataServiceTest.java
@@ -11,7 +11,7 @@ import uk.gov.hmcts.cmc.ccd.domain.CaseEvent;
 import uk.gov.hmcts.cmc.ccd.mapper.CaseMapper;
 import uk.gov.hmcts.cmc.claimstore.idam.models.User;
 import uk.gov.hmcts.cmc.claimstore.idam.models.UserDetails;
-import uk.gov.hmcts.cmc.claimstore.services.IntentionToProceedService;
+import uk.gov.hmcts.cmc.claimstore.services.IntentionToProceedDeadlineCalculator;
 import uk.gov.hmcts.cmc.claimstore.services.JobSchedulerService;
 import uk.gov.hmcts.cmc.claimstore.services.ReferenceNumberService;
 import uk.gov.hmcts.cmc.claimstore.services.UserService;
@@ -99,7 +99,7 @@ public class CoreCaseDataServiceTest {
     @Mock
     private CaseDetailsConverter caseDetailsConverter;
     @Mock
-    private IntentionToProceedService intentionToProceedService;
+    private IntentionToProceedDeadlineCalculator intentionToProceedDeadlineCalculator;
 
     private CoreCaseDataService service;
 
@@ -149,7 +149,7 @@ public class CoreCaseDataServiceTest {
             jobSchedulerService,
             ccdCreateCaseService,
             caseDetailsConverter,
-            intentionToProceedService
+            intentionToProceedDeadlineCalculator
         );
     }
 
