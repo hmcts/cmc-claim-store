a4e90b9e46dda2f13cc2959e391b42d03e3dad96
Andy Walker <andrewwa@kainos.com>
2019-11-15 14:57:00 +0000

    ROC-6549 Refactor custom assertions (#1282)

--- ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/assertion/TimelineEventAssert.java
+++ ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/assertion/TimelineEventAssert.java
@@ -1,29 +1,26 @@
 package uk.gov.hmcts.cmc.ccd.assertion;
 
-import org.assertj.core.api.AbstractAssert;
 import uk.gov.hmcts.cmc.ccd.domain.CCDTimelineEvent;
 import uk.gov.hmcts.cmc.domain.models.TimelineEvent;
 
-import java.util.Objects;
+import java.util.Optional;
 
-public class TimelineEventAssert extends AbstractAssert<TimelineEventAssert, TimelineEvent> {
+public class TimelineEventAssert extends CustomAssert<TimelineEventAssert, TimelineEvent> {
 
     public TimelineEventAssert(TimelineEvent actual) {
-        super(actual, TimelineEventAssert.class);
+        super("TimelineEvent", actual, TimelineEventAssert.class);
     }
 
-    public TimelineEventAssert isEqualTo(CCDTimelineEvent ccdTimelineEvent) {
+    public TimelineEventAssert isEqualTo(CCDTimelineEvent expected) {
         isNotNull();
 
-        if (!Objects.equals(actual.getDate(), ccdTimelineEvent.getDate())) {
-            failWithMessage("Expected TimelineEvent.date to be <%s> but was <%s>",
-                ccdTimelineEvent.getDate(), actual.getDate());
-        }
+        compare("date",
+            expected.getDate(),
+            Optional.ofNullable(actual.getDate()));
 
-        if (!Objects.equals(actual.getDescription(), ccdTimelineEvent.getDescription())) {
-            failWithMessage("Expected TimelineEvent.description to be <%s> but was <%s>",
-                ccdTimelineEvent.getDescription(), actual.getDescription());
-        }
+        compare("description",
+            expected.getDescription(),
+            Optional.ofNullable(actual.getDescription()));
 
         return this;
     }
