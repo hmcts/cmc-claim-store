bfab3d225c5bee4858e771c4834f53256a894645
Sabah Irfan <sabah.irfan@gmail.com>
2019-12-05 10:59:22 +0000

    Move the bd dependant tests in deprecated package

---
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/deprecated/MockedDatabaseConfiguration.java
@@ -0,0 +1,78 @@
+package uk.gov.hmcts.cmc.claimstore.deprecated;
+
+import org.flywaydb.core.Flyway;
+import org.mockito.Answers;
+import org.quartz.Scheduler;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.TransactionStatus;
+import uk.gov.hmcts.cmc.claimstore.repositories.ClaimRepository;
+import uk.gov.hmcts.cmc.claimstore.repositories.TestingSupportRepository;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("mocked-database-tests")
+@SuppressWarnings("unused")
+class MockedDatabaseConfiguration {
+
+    private static final PlatformTransactionManager NO_OP_TRANSACTION_MANAGER = new PlatformTransactionManager() {
+        @Override
+        public TransactionStatus getTransaction(TransactionDefinition definition) throws TransactionException {
+            return null;
+        }
+
+        @Override
+        public void commit(TransactionStatus status) throws TransactionException {
+            // NO-OP
+        }
+
+        @Override
+        public void rollback(TransactionStatus status) throws TransactionException {
+            // NO-OP
+        }
+    };
+
+    @MockBean
+    private Flyway flyway;
+
+    @MockBean
+    private ClaimRepository claimRepository;
+
+    @MockBean
+    private TestingSupportRepository testingSupportRepository;
+
+    @MockBean(name = "claimStoreDataSource", answer = Answers.RETURNS_MOCKS)
+    private DataSource dataSource;
+
+    @MockBean(name = "cmcDataSource", answer = Answers.RETURNS_MOCKS)
+    private DataSource cmcDataSource;
+
+    @MockBean
+    private SpringBeanJobFactory springBeanJobFactory;
+
+    @MockBean
+    private SchedulerFactoryBean schedulerFactoryBean;
+
+    @MockBean
+    private Scheduler scheduler;
+
+    @MockBean(name = "cmcTransactionAwareDataSourceProxy")
+    private TransactionAwareDataSourceProxy transactionAwareDataSourceProxy;
+
+    @MockBean(name = "cmcTransactionManager")
+    private PlatformTransactionManager cmcTransactionManager;
+
+    @Bean
+    protected PlatformTransactionManager transactionManager() {
+        return NO_OP_TRANSACTION_MANAGER;
+    }
+}
---
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/deprecated/controllers/GetClaimByClaimReferenceTest.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.cmc.claimstore.deprecated.controllers;
+
+import org.junit.Test;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MvcResult;
+import uk.gov.hmcts.cmc.claimstore.deprecated.BaseGetTest;
+import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaimData;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@TestPropertySource(
+    properties = {
+        "core_case_data.api.url=false"
+    }
+)
+public class GetClaimByClaimReferenceTest extends BaseGetTest {
+    @Test
+    public void shouldReturn200HttpStatusWhenClaimFound() throws Exception {
+        Claim claim = claimStore.saveClaim(SampleClaimData.builder().build());
+
+        MvcResult result = makeRequest("/claims/" + claim.getReferenceNumber())
+            .andExpect(status().isOk())
+            .andReturn();
+
+        assertThat(deserializeObjectFrom(result, Claim.class))
+            .extracting(Claim::getReferenceNumber).isEqualTo(claim.getReferenceNumber());
+    }
+
+    @Test
+    public void shouldReturn404HttpStatusWhenNoClaimFound() throws Exception {
+        String nonExistingReferenceNumber = "999LR999";
+
+        makeRequest("/claims/" + nonExistingReferenceNumber)
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shouldReturn400HttpStatusWhenNoAuthorizationHeaderSet() throws Exception {
+        webClient.perform(get("/claims/" + "000LR001"))
+            .andExpect(status().isBadRequest());
+    }
+}
