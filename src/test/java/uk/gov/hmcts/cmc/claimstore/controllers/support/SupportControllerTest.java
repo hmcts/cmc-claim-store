4da8afcab1fa2854e2984b050ea0ac62594252cc
Kiran Varma <varma.ap@gmail.com>
2020-01-15 09:35:31 +0000

    used version variable

--- src/test/java/uk/gov/hmcts/cmc/claimstore/controllers/support/SupportControllerTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/controllers/support/SupportControllerTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.cmc.claimstore.controllers.support;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
