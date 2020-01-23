98a5fa2c8481bcda08e675652a2e5d7b53b782d1
Andrew Walker <andrewwa@kainos.com>
2020-01-23 17:33:02 +0000

    Add support to resubmit refer to judge claimant responses

--- src/test/java/uk/gov/hmcts/cmc/claimstore/controllers/support/SupportControllerTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/controllers/support/SupportControllerTest.java
@@ -481,7 +481,7 @@ class SupportControllerTest {
             }
 
             @Test
-            void shouldNotResendClaimantResponseNotificationsIfReferToJudge() {
+            void shouldResendClaimantResponseNotificationsIfReferToJudge() {
                 sampleClaim = SampleClaim.builder()
                     .withResponse(PartAdmission.builder().buildWithPaymentOptionImmediately())
                     .withClaimantResponse(
@@ -495,7 +495,7 @@ class SupportControllerTest {
 
                 controller.resendStaffNotifications(sampleClaim.getReferenceNumber(), "claimant-response");
 
-                verify(claimantResponseStaffNotificationHandler, never()).onClaimantResponse(any());
+                verify(claimantResponseStaffNotificationHandler).onClaimantResponse(any());
             }
 
             @Test
