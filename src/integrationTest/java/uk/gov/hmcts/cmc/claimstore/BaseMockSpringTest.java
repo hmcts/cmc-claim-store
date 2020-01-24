ae8d1667ad5cfa580793b9ae102ca8618836a5c0
Sabah Irfan <sabah.irfan@gmail.com>
2020-01-24 15:52:00 +0000

    Cleanup mocks

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/BaseMockSpringTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/BaseMockSpringTest.java
@@ -2,23 +2,16 @@ package uk.gov.hmcts.cmc.claimstore;
 
 import com.google.common.collect.ImmutableMap;
 import com.microsoft.applicationinsights.TelemetryClient;
-import org.flywaydb.core.Flyway;
 import org.junit.runner.RunWith;
-import org.mockito.Answers;
-import org.quartz.Scheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
-import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.transaction.PlatformTransactionManager;
 import uk.gov.hmcts.cmc.ccd.mapper.CaseMapper;
 import uk.gov.hmcts.cmc.claimstore.appinsights.AppInsights;
 import uk.gov.hmcts.cmc.claimstore.courtfinder.CourtFinderApi;
@@ -26,7 +19,6 @@ import uk.gov.hmcts.cmc.claimstore.events.EventProducer;
 import uk.gov.hmcts.cmc.claimstore.helper.JsonMappingHelper;
 import uk.gov.hmcts.cmc.claimstore.idam.models.UserDetails;
 import uk.gov.hmcts.cmc.claimstore.repositories.ReferenceNumberRepository;
-import uk.gov.hmcts.cmc.claimstore.repositories.TestingSupportRepository;
 import uk.gov.hmcts.cmc.claimstore.services.UserService;
 import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.ioc.PaymentsService;
 import uk.gov.hmcts.cmc.claimstore.services.document.DocumentManagementService;
@@ -42,8 +34,6 @@ import uk.gov.hmcts.reform.pdf.service.client.PDFServiceClient;
 import uk.gov.hmcts.reform.sendletter.api.SendLetterApi;
 import uk.gov.service.notify.NotificationClient;
 
-import javax.sql.DataSource;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
@@ -53,14 +43,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 public abstract class BaseMockSpringTest {
 
     protected static final String SUBMITTER_ID = "123";
-    protected static final String DEFENDANT_ID = "555";
-    protected static final String DEFENDANT_EMAIL = "j.smith@example.com";
     protected static final String BEARER_TOKEN = "Bearer let me in";
     protected static final String SERVICE_TOKEN = "S2S token";
 
     protected static final String AUTHORISATION_TOKEN = "Bearer token";
-
-    protected static final byte[] PDF_BYTES = new byte[]{1, 2, 3, 4};
 
     protected static final String USER_ID = "1";
     protected static final String JURISDICTION_ID = "CMC";
@@ -114,23 +100,6 @@ public abstract class BaseMockSpringTest {
     protected NotificationClient notificationClient;
     @MockBean
     protected JobService jobService;
-
-    @MockBean
-    private Flyway flyway;
-    @MockBean
-    private TestingSupportRepository testingSupportRepository;
-    @MockBean(name = "dataSource", answer = Answers.RETURNS_MOCKS)
-    private DataSource dataSource;
-    @MockBean
-    private SpringBeanJobFactory springBeanJobFactory;
-    @MockBean
-    private SchedulerFactoryBean schedulerFactoryBean;
-    @MockBean
-    private Scheduler scheduler;
-    @MockBean(name = "transactionAwareDataSourceProxy")
-    private TransactionAwareDataSourceProxy transactionAwareDataSourceProxy;
-    @MockBean(name = "transactionManager")
-    private PlatformTransactionManager transactionManager;
 
     protected ImmutableMap<String, String> searchCriteria(String externalId) {
         return ImmutableMap.of(
