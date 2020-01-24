bcbc1e6bed8376c41ac84aa6bd301f515f0cbf95
Sabah Irfan <sabah.irfan@gmail.com>
2020-01-24 11:24:04 +0000

    Remove unnecessary mocks

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/BaseMockSpringTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/BaseMockSpringTest.java
@@ -2,23 +2,16 @@ package uk.gov.hmcts.cmc.claimstore;
 
 import com.google.common.collect.ImmutableMap;
 import com.microsoft.applicationinsights.TelemetryClient;
-import org.flywaydb.core.Flyway;
 import org.junit.runner.RunWith;
-import org.mockito.Answers;
-import org.quartz.Scheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
-import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.transaction.PlatformTransactionManager;
 import uk.gov.hmcts.cmc.ccd.mapper.CaseMapper;
 import uk.gov.hmcts.cmc.claimstore.appinsights.AppInsights;
 import uk.gov.hmcts.cmc.claimstore.courtfinder.CourtFinderApi;
@@ -26,7 +19,6 @@ import uk.gov.hmcts.cmc.claimstore.events.EventProducer;
 import uk.gov.hmcts.cmc.claimstore.helper.JsonMappingHelper;
 import uk.gov.hmcts.cmc.claimstore.idam.models.UserDetails;
 import uk.gov.hmcts.cmc.claimstore.repositories.ReferenceNumberRepository;
-import uk.gov.hmcts.cmc.claimstore.repositories.TestingSupportRepository;
 import uk.gov.hmcts.cmc.claimstore.services.UserService;
 import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.ioc.PaymentsService;
 import uk.gov.hmcts.cmc.claimstore.services.document.DocumentManagementService;
@@ -41,8 +33,6 @@ import uk.gov.hmcts.reform.docassembly.DocAssemblyApi;
 import uk.gov.hmcts.reform.pdf.service.client.PDFServiceClient;
 import uk.gov.hmcts.reform.sendletter.api.SendLetterApi;
 import uk.gov.service.notify.NotificationClient;
-
-import javax.sql.DataSource;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
@@ -114,23 +104,6 @@ public abstract class BaseMockSpringTest {
     protected NotificationClient notificationClient;
     @MockBean
     protected JobService jobService;
-
-    @MockBean
-    private Flyway flyway;
-    @MockBean
-    private TestingSupportRepository testingSupportRepository;
-    @MockBean(name = "dataSource", answer = Answers.RETURNS_MOCKS)
-    private DataSource dataSource;
-    @MockBean
-    private SpringBeanJobFactory springBeanJobFactory;
-    @MockBean
-    private SchedulerFactoryBean schedulerFactoryBean;
-    @MockBean
-    private Scheduler scheduler;
-    @MockBean(name = "transactionAwareDataSourceProxy")
-    private TransactionAwareDataSourceProxy transactionAwareDataSourceProxy;
-    @MockBean(name = "transactionManager")
-    private PlatformTransactionManager transactionManager;
 
     protected ImmutableMap<String, String> searchCriteria(String externalId) {
         return ImmutableMap.of(
