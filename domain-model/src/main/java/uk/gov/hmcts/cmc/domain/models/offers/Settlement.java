112e56f927ab296ecb8690997368e54baf4e97bc
Andrew Walker <andrewwa@kainos.com>
2020-01-24 17:47:32 +0000

    Java > Data flow > Boolean method is always inverted

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/offers/Settlement.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/offers/Settlement.java
@@ -120,7 +120,7 @@ public class Settlement {
     private void assertOfferCanBeRejected(MadeBy party) {
         assertOfferCanBeMadeBy(party);
 
-        if (!lastStatementIsOffer() && !lastStatementIsAcceptationNotBy(party)) {
+        if (!lastStatementIsOffer() && lastStatementIsNotAcceptationBy(party)) {
             throw new IllegalSettlementStatementException(
                 format("Last statement was: %s, offer or acceptation expected.",
                     getLastStatement().getType().name().toLowerCase())
@@ -137,7 +137,7 @@ public class Settlement {
     }
 
     private void assertOfferHasBeenAcceptedByOtherParty(MadeBy party) {
-        if (!lastStatementIsAcceptationNotBy(party)) {
+        if (lastStatementIsNotAcceptationBy(party)) {
             throw new IllegalSettlementStatementException(
                 format("Last statement was: %s , offer acceptation expected.",
                     getLastStatement().getType().name().toLowerCase())
@@ -153,10 +153,10 @@ public class Settlement {
         return getLastStatement().getType().equals(StatementType.OFFER);
     }
 
-    private boolean lastStatementIsAcceptationNotBy(MadeBy madeBy) {
+    private boolean lastStatementIsNotAcceptationBy(MadeBy madeBy) {
         PartyStatement lastStatement = getLastStatement();
-        return lastStatement.getType().equals(StatementType.ACCEPTATION)
-            && !lastStatement.getMadeBy().equals(madeBy);
+        return !lastStatement.getType().equals(StatementType.ACCEPTATION)
+            || lastStatement.getMadeBy().equals(madeBy);
     }
 
     @Override
