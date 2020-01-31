51de8ddcfcc16f8308c13d7f7001d0e0015ae5be
Kiran Varma <varma.ap@gmail.com>
2020-01-31 14:51:26 +0000

    add app insights event to draw order

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/legaladvisor/DrawOrderCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/legaladvisor/DrawOrderCallbackHandler.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.cmc.ccd.domain.CCDCase;
 import uk.gov.hmcts.cmc.ccd.domain.CCDDocument;
 import uk.gov.hmcts.cmc.ccd.domain.CaseEvent;
-import uk.gov.hmcts.cmc.ccd.domain.legaladvisor.CCDOrderGenerationData;
-import uk.gov.hmcts.cmc.claimstore.exceptions.CallbackException;
+import uk.gov.hmcts.cmc.claimstore.appinsights.AppInsights;
+import uk.gov.hmcts.cmc.claimstore.appinsights.AppInsightsEvent;
 import uk.gov.hmcts.cmc.claimstore.services.ccd.DocAssemblyService;
 import uk.gov.hmcts.cmc.claimstore.services.ccd.Role;
 import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.Callback;
@@ -26,7 +26,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.DRAW_ORDER;
 import static uk.gov.hmcts.cmc.claimstore.services.ccd.Role.JUDGE;
 import static uk.gov.hmcts.cmc.claimstore.services.ccd.Role.LEGAL_ADVISOR;
 
@@ -34,22 +33,25 @@ import static uk.gov.hmcts.cmc.claimstore.services.ccd.Role.LEGAL_ADVISOR;
 @ConditionalOnProperty(prefix = "doc_assembly", name = "url")
 public class DrawOrderCallbackHandler extends CallbackHandler {
     private static final List<Role> ROLES = ImmutableList.of(LEGAL_ADVISOR, JUDGE);
-    private static final List<CaseEvent> EVENTS = Collections.singletonList(DRAW_ORDER);
+    private static final List<CaseEvent> EVENTS = Collections.singletonList(CaseEvent.DRAW_ORDER);
     private static final String DRAFT_ORDER_DOC = "draftOrderDoc";
 
     private final CaseDetailsConverter caseDetailsConverter;
     private final DocAssemblyService docAssemblyService;
     private final OrderPostProcessor orderPostProcessor;
+    private final AppInsights appInsights;
 
     @Autowired
     public DrawOrderCallbackHandler(
         OrderPostProcessor orderPostProcessor,
         CaseDetailsConverter caseDetailsConverter,
-        DocAssemblyService docAssemblyService
+        DocAssemblyService docAssemblyService,
+        AppInsights appInsights
     ) {
         this.orderPostProcessor = orderPostProcessor;
         this.caseDetailsConverter = caseDetailsConverter;
         this.docAssemblyService = docAssemblyService;
+        this.appInsights = appInsights;
     }
 
     @Override
@@ -76,6 +78,10 @@ public class DrawOrderCallbackHandler extends CallbackHandler {
         CCDCase ccdCase = caseDetailsConverter.extractCCDCase(caseDetails);
         String authorisation = callbackParams.getParams().get(CallbackParams.Params.BEARER_TOKEN).toString();
         DocAssemblyResponse docAssemblyResponse = docAssemblyService.createOrder(ccdCase, authorisation);
+        appInsights.trackEvent(
+            AppInsightsEvent.DRAW_ORDER,
+            AppInsights.REFERENCE_NUMBER,
+            ccdCase.getPreviousServiceCaseReference());
 
         return AboutToStartOrSubmitCallbackResponse
             .builder()
