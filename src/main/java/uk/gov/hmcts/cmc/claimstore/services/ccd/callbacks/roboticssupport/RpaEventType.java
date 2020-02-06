5a91bbcd77965ac19ce1844dfe6049bcb9b4ecef
Akriti Sinha <akritis@kainos.com@Caecilius.local>
2020-02-06 11:35:07 +0000

    ROC-7041 | removing unused enum

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/roboticssupport/RpaEventType.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.roboticssupport;
-
-public enum RpaEventType {
-    CLAIM,
-    MORE_TIME,
-    CCJ,
-    DEFENDANT_RESPONSE,
-    PAID_IN_FULL
-}
