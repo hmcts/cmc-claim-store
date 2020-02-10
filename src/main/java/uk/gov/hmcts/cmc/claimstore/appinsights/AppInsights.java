249e52370661dff2dd2c2a78719258847195db53
dharmendra kumar <dharmendra.kumar@hmcts.net>
2020-02-10 13:14:57 +0000

    ROC-7498 Mediation successful template missing claim reference in fai… (#1429)

--- src/main/java/uk/gov/hmcts/cmc/claimstore/appinsights/AppInsights.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/appinsights/AppInsights.java
@@ -3,12 +3,15 @@ package uk.gov.hmcts.cmc.claimstore.appinsights;
 import com.microsoft.applicationinsights.TelemetryClient;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 import static java.util.Collections.singletonMap;
 
 @Component
 public class AppInsights {
 
     public static final String REFERENCE_NUMBER = "referenceNumber";
+    public static final String EMAIL_REFERENCE = "emailReference";
     public static final String CCD_LINK_DEFENDANT_ID = "ccdLink.defendantId";
     public static final String CLAIM_EXTERNAL_ID = "claim.externalId";
     public static final String DOCUMENT_NAME = "document.name";
@@ -20,7 +23,11 @@ public class AppInsights {
     }
 
     public void trackEvent(AppInsightsEvent appInsightsEvent, String referenceType, String value) {
-        telemetry.trackEvent(appInsightsEvent.toString(), singletonMap(referenceType, value), null);
+        trackEvent(appInsightsEvent, singletonMap(referenceType, value));
+    }
+
+    public void trackEvent(AppInsightsEvent appInsightsEvent, Map<String, String> properties) {
+        telemetry.trackEvent(appInsightsEvent.toString(), properties, null);
     }
 
     public void trackException(Exception exception) {
