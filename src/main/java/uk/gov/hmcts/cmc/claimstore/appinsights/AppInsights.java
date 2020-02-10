ea070616aa11344983eb32ce793016788fafcdf8
Andy Walker <andrewwa@kainos.com>
2020-02-10 14:57:56 +0000

    ROC-7466 Enable local logging of app insights events (#1417)

--- src/main/java/uk/gov/hmcts/cmc/claimstore/appinsights/AppInsights.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/appinsights/AppInsights.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.cmc.claimstore.appinsights;
 
 import com.microsoft.applicationinsights.TelemetryClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -9,6 +12,7 @@ import static java.util.Collections.singletonMap;
 
 @Component
 public class AppInsights {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppInsights.class);
 
     public static final String REFERENCE_NUMBER = "referenceNumber";
     public static final String EMAIL_REFERENCE = "emailReference";
@@ -17,9 +21,14 @@ public class AppInsights {
     public static final String DOCUMENT_NAME = "document.name";
 
     private final TelemetryClient telemetry;
+    private final boolean stdout;
 
-    public AppInsights(TelemetryClient telemetry) {
+    public AppInsights(
+        TelemetryClient telemetry,
+        @Value("${APPINSIGHTS_INSTRUMENTATIONKEY:}") String instrumentationKey
+    ) {
         this.telemetry = telemetry;
+        stdout = "STDOUT".equals(instrumentationKey);
     }
 
     public void trackEvent(AppInsightsEvent appInsightsEvent, String referenceType, String value) {
@@ -28,10 +37,16 @@ public class AppInsights {
 
     public void trackEvent(AppInsightsEvent appInsightsEvent, Map<String, String> properties) {
         telemetry.trackEvent(appInsightsEvent.toString(), properties, null);
+        if (stdout) {
+            LOGGER.info("AppInsights event: {}: {}", appInsightsEvent, properties);
+        }
     }
 
     public void trackException(Exception exception) {
         telemetry.trackException(exception);
+        if (stdout) {
+            LOGGER.warn("AppInsights exception: {}", exception.getClass().getSimpleName(), exception);
+        }
     }
 
 }
