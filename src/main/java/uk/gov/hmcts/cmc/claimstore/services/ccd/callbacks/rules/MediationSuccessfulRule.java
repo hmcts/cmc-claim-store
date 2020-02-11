fb51162ecbfda4ba7290b35786db97bd506be535
Jana <janal@outlook.com>
2020-02-11 14:59:19 +0000

    Add correct error messages

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/rules/MediationSuccessfulRule.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/rules/MediationSuccessfulRule.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.ccd.domain.CCDCase;
 import uk.gov.hmcts.cmc.ccd.domain.CCDClaimDocument;
 import uk.gov.hmcts.cmc.ccd.domain.CCDClaimDocumentType;
+import uk.gov.hmcts.cmc.ccd.domain.CCDCollectionElement;
 import uk.gov.hmcts.cmc.ccd.domain.CCDDocument;
 
 import java.util.ArrayList;
@@ -18,29 +19,39 @@ public class MediationSuccessfulRule {
     public static final String STAFF_UPLOAD_TYPE_MEDIATION_AGREEMENT =
             "Document needs to be a Mediation Agreement";
     public static final String STAFF_UPLOAD_PDF_MEDIATION_AGREEMENT =
-            "Document needs to be of type PDF";
-
-    public CCDClaimDocument ccdClaimDocument;
-    public CCDDocument ccdDocument;
+            "Mediation agreement needs to be of type PDF";
 
     public List<String> validateMediationAgreementUploadedByCaseworker(CCDCase ccdCase) {
         Objects.requireNonNull(ccdCase, "CCD case object can not be null");
+        List<CCDCollectionElement<CCDClaimDocument>> staffDocuments = ccdCase.getStaffUploadedDocuments();
+
 
         List<String> validationErrors = new ArrayList<>();
 
-        if ((ccdCase.getStaffUploadedDocuments().isEmpty())
+        if ((staffDocuments == null)
         ) {
             validationErrors.add(STAFF_UPLOAD_MEDIATION_AGREEMENT);
-        }
-        if ((ccdClaimDocument.getDocumentType() != CCDClaimDocumentType.MEDIATION_AGREEMENT)
-        ) {
-            validationErrors.add(STAFF_UPLOAD_TYPE_MEDIATION_AGREEMENT);
-        }
-        if ((FilenameUtils.getExtension(ccdDocument.getDocumentFileName()).contains("pdf"))
-        ) {
-            validationErrors.add(STAFF_UPLOAD_PDF_MEDIATION_AGREEMENT);
         } else {
-            System.out.println(FilenameUtils.getExtension(ccdDocument.getDocumentFileName()));
+            for (CCDCollectionElement<CCDClaimDocument> document : staffDocuments)
+            {
+                if ((document.getValue().getDocumentType() == CCDClaimDocumentType.MEDIATION_AGREEMENT) && (FilenameUtils.getExtension(document.getValue().getDocumentLink().getDocumentFileName()).contains("pdf"))
+                ) {
+                    validationErrors.clear();
+                    return validationErrors;
+                }
+                if ((document.getValue().getDocumentType() == CCDClaimDocumentType.MEDIATION_AGREEMENT) && !(FilenameUtils.getExtension(document.getValue().getDocumentLink().getDocumentFileName()).contains("pdf"))
+                ) {
+                    if (!validationErrors.contains(STAFF_UPLOAD_PDF_MEDIATION_AGREEMENT)){
+                        validationErrors.add(STAFF_UPLOAD_PDF_MEDIATION_AGREEMENT);
+                    }
+                }
+                if ((document.getValue().getDocumentType() != CCDClaimDocumentType.MEDIATION_AGREEMENT)
+                ) {
+                    if (!validationErrors.contains(STAFF_UPLOAD_MEDIATION_AGREEMENT)){
+                        validationErrors.add(STAFF_UPLOAD_MEDIATION_AGREEMENT);
+                    }
+                }
+            }
         }
         return validationErrors;
     }
