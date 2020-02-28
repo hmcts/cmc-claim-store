ba21620d6dccd5a62f5384eb7dbe30cc0f3ce060
Akriti Sinha <akritis@kainos.com>
2020-02-28 11:50:28 +0000

    ROC-7389 | code review comments

--- src/main/java/uk/gov/hmcts/cmc/claimstore/events/offer/RejectSettlementAgreementStaffNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/events/offer/RejectSettlementAgreementStaffNotificationHandler.java
@@ -1,32 +1,27 @@
 package uk.gov.hmcts.cmc.claimstore.events.offer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.claimstore.events.settlement.RejectSettlementAgreementEvent;
 import uk.gov.hmcts.cmc.claimstore.services.staff.RejectSettlementAgreementStaffNotificationService;
 
 @Component
+@ConditionalOnProperty("feature_toggles.staff_emails_enabled")
 public class RejectSettlementAgreementStaffNotificationHandler {
 
     private final RejectSettlementAgreementStaffNotificationService notificationService;
-    private final boolean staffEmailEnabled;
 
     @Autowired
     public RejectSettlementAgreementStaffNotificationHandler(
-        RejectSettlementAgreementStaffNotificationService notificationService,
-        @Value("${feature_toggles.staff_emails_enabled}") boolean staffEmailEnabled) {
+        RejectSettlementAgreementStaffNotificationService notificationService) {
         this.notificationService = notificationService;
-        this.staffEmailEnabled = staffEmailEnabled;
     }
 
     @EventListener
     public void onSettlementAgreementRejected(RejectSettlementAgreementEvent event) {
-        if (staffEmailEnabled) {
             notificationService.notifySettlementRejected(
-                event.getClaim()
-            );
+                event.getClaim());
         }
-    }
 }
