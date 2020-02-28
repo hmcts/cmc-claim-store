6330ef3fd1859ddef4a543a8c6912e706a34bf47
Akriti Sinha <akritis@kainos.com>
2020-02-28 09:26:43 +0000

    ROC-7388 | added a toggle for staff email when settlement reached

--- src/main/java/uk/gov/hmcts/cmc/claimstore/events/offer/AgreementCountersignedStaffNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/events/offer/AgreementCountersignedStaffNotificationHandler.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.cmc.claimstore.events.offer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.claimstore.services.staff.SettlementReachedStaffNotificationService;
@@ -9,18 +10,23 @@ import uk.gov.hmcts.cmc.claimstore.services.staff.SettlementReachedStaffNotifica
 public class AgreementCountersignedStaffNotificationHandler {
 
     private final SettlementReachedStaffNotificationService notificationService;
+    private final boolean staffEmailEnabled;
 
     @Autowired
     public AgreementCountersignedStaffNotificationHandler(
-        SettlementReachedStaffNotificationService notificationService) {
+        SettlementReachedStaffNotificationService notificationService,
+        @Value("${feature_toggles.staff_emails_enabled}") boolean staffEmailEnabled) {
         this.notificationService = notificationService;
+        this.staffEmailEnabled = staffEmailEnabled;
     }
 
     @EventListener
     public void onAgreementCountersigned(AgreementCountersignedEvent event) {
-        notificationService.notifySettlementReached(
-            event.getClaim()
-        );
+        if (staffEmailEnabled) {
+            notificationService.notifySettlementReached(
+                event.getClaim()
+            );
+        }
     }
 
 }
