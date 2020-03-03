050a76adee065b44ee58d0e37c64f965b38a7ca0
Dave Oxtoby <david.oxtoby@hmcts.net>
2020-03-03 15:45:16 +0000

    ROC-5833: remove import

--- src/main/java/uk/gov/hmcts/cmc/claimstore/appinsights/AppInsights.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/appinsights/AppInsights.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.Map;
 
 import static java.util.Collections.singletonMap;
