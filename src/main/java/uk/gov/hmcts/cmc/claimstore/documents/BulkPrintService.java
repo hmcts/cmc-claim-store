b08b3f651cc6bbc6bf7b35ec6875b5a67625c2a9
Dharmendra kumar <k_dharmendra@hotmail.com>
2020-03-05 10:12:22 +0000

    remove empty space

--- src/main/java/uk/gov/hmcts/cmc/claimstore/documents/BulkPrintService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/documents/BulkPrintService.java
@@ -27,7 +27,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
-    
+
 import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.cmc.claimstore.appinsights.AppInsights.REFERENCE_NUMBER;
 import static uk.gov.hmcts.cmc.claimstore.appinsights.AppInsightsEvent.BULK_PRINT_FAILED;
