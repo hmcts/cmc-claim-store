96b8eed8988ed0227fd0ce3c15737da497914b83
akritis22 <60217152+akritis22@users.noreply.github.com>
2020-03-09 14:15:17 +0000

    ROC-7377 | added staff email notification toggle (#1525)

--- src/main/java/uk/gov/hmcts/cmc/claimstore/events/ccj/CCJStaffNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/events/ccj/CCJStaffNotificationHandler.java
@@ -1,12 +1,14 @@
 package uk.gov.hmcts.cmc.claimstore.events.ccj;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.claimstore.services.staff.CCJStaffNotificationService;
 import uk.gov.hmcts.cmc.claimstore.services.staff.InterlocutoryJudgmentStaffNotificationService;
 
 @Component
+@ConditionalOnProperty("feature_toggles.staff_emails_enabled")
 public class CCJStaffNotificationHandler {
 
     private final CCJStaffNotificationService ccjStaffNotificationService;
