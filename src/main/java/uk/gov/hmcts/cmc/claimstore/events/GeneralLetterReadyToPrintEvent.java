6c618b377b555f4d529017d9ec0490682eaf9968
Akriti Sinha <akritis@kainos.com>
2020-03-20 12:34:56 +0000

    ROC-7557 | code review comments

--- src/main/java/uk/gov/hmcts/cmc/claimstore/events/GeneralLetterReadyToPrintEvent.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/events/GeneralLetterReadyToPrintEvent.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.cmc.claimstore.events;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import uk.gov.hmcts.cmc.domain.models.Claim;
@@ -7,12 +8,8 @@ import uk.gov.hmcts.reform.sendletter.api.Document;
 
 @Builder
 @Getter
+@AllArgsConstructor
 public class GeneralLetterReadyToPrintEvent {
     private final Claim claim;
     private final Document generalLetterDocument;
-
-    public GeneralLetterReadyToPrintEvent(Claim claim, Document generalLetterDocument) {
-        this.claim = claim;
-        this.generalLetterDocument = generalLetterDocument;
-    }
 }
