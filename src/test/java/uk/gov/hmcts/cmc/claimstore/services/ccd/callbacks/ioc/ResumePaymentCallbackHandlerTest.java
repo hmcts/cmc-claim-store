f4669792062a9e4fcb4e2d0a0a1dcdfbeea15a60
Andy Walker <andrewwa@kainos.com>
2020-03-23 12:02:19 +0000

    ROC-6905 ROC-7726 Add auto cancel payments feature toggle (#1587)

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/ioc/ResumePaymentCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/ioc/ResumePaymentCallbackHandlerTest.java
@@ -36,7 +36,9 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.data.MapEntry.entry;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.RESUME_CLAIM_PAYMENT_CITIZEN;
@@ -64,6 +66,8 @@ public class ResumePaymentCallbackHandlerTest {
 
     private CallbackRequest callbackRequest;
 
+    private CaseDetailsConverter caseDetailsConverter;
+
     private ResumePaymentCallbackHandler handler;
 
     @Mock
@@ -78,14 +82,15 @@ public class ResumePaymentCallbackHandlerTest {
         when(caseMapper.from(any(CCDCase.class))).thenReturn(claim);
         when(caseMapper.to(any(Claim.class))).thenReturn(ccdCase);
         JsonMapper jsonMapper = new JsonMapper(new JacksonConfiguration().objectMapper());
-        CaseDetailsConverter caseDetailsConverter
+        caseDetailsConverter
             = new CaseDetailsConverter(caseMapper, jsonMapper, workingDayIndicator, 12);
         handler = new ResumePaymentCallbackHandler(
             paymentsService,
             caseDetailsConverter,
             caseMapper,
             issueDateCalculator,
-            responseDeadlineCalculator);
+            responseDeadlineCalculator,
+            true);
 
         callbackRequest = CallbackRequest
             .builder()
@@ -223,6 +228,69 @@ public class ResumePaymentCallbackHandlerTest {
         assertThat(payment).isEqualTo(newPayment);
 
         verify(paymentsService).cancelPayment(BEARER_TOKEN, originalPayment.getReference());
+    }
+
+    @Test
+    public void shouldNotCancelPaymentIfPaymentIsPendingButFeatureIsDisabled() {
+        handler = new ResumePaymentCallbackHandler(
+            paymentsService,
+            caseDetailsConverter,
+            caseMapper,
+            issueDateCalculator,
+            responseDeadlineCalculator,
+            false);
+
+        Payment originalPayment = Payment.builder()
+            .reference("reference")
+            .status(PENDING)
+            .dateCreated("2017-02-03T10:15:30+01:00")
+            .nextUrl(NEXT_URL)
+            .build();
+
+        when(paymentsService.retrievePayment(
+            eq(BEARER_TOKEN),
+            any(ClaimData.class)))
+            .thenReturn(Optional.of(originalPayment));
+
+        Payment newPayment = Payment.builder()
+            .reference("reference2")
+            .status(SUCCESS)
+            .amount(BigDecimal.TEN)
+            .dateCreated("2017-02-03T10:15:30+01:00")
+            .nextUrl(NEXT_URL)
+            .build();
+
+        when(paymentsService.createPayment(
+            eq(BEARER_TOKEN),
+            any(Claim.class)))
+            .thenReturn(newPayment);
+
+        LocalDate date = LocalDate.now();
+        when(issueDateCalculator.calculateIssueDay(any(LocalDateTime.class))).thenReturn(date);
+        when(responseDeadlineCalculator.calculateResponseDeadline(any(LocalDate.class))).thenReturn(date);
+
+        CallbackParams callbackParams = CallbackParams.builder()
+            .type(CallbackType.ABOUT_TO_SUBMIT)
+            .request(callbackRequest)
+            .params(ImmutableMap.of(CallbackParams.Params.BEARER_TOKEN, BEARER_TOKEN))
+            .build();
+
+        AboutToStartOrSubmitCallbackResponse response =
+            (AboutToStartOrSubmitCallbackResponse) handler.handle(callbackParams);
+
+        assertThat(response.getData()).contains(entry("feeAmountInPennies", "1000"));
+
+        verify(caseMapper).to(claimArgumentCaptor.capture());
+
+        Claim toBeSaved = claimArgumentCaptor.getValue();
+        assertThat(toBeSaved.getIssuedOn()).isEqualTo(date);
+        assertThat(toBeSaved.getServiceDate()).isEqualTo(date.plusDays(5));
+        assertThat(toBeSaved.getResponseDeadline()).isEqualTo(date);
+
+        Payment payment = toBeSaved.getClaimData().getPayment().orElse(null);
+        assertThat(payment).isEqualTo(newPayment);
+
+        verify(paymentsService, never()).cancelPayment(anyString(), anyString());
     }
 
     @Test
