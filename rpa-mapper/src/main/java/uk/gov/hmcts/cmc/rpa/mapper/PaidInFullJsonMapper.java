aae86956d50f39b15824687c0bc38dff8106046c
tharacka <tharacka@kainos.com>
2020-03-29 23:19:49 +0100

    reverse refactor

--- rpa-mapper/src/main/java/uk/gov/hmcts/cmc/rpa/mapper/PaidInFullJsonMapper.java
+++ rpa-mapper/src/main/java/uk/gov/hmcts/cmc/rpa/mapper/PaidInFullJsonMapper.java
@@ -6,7 +6,6 @@ import uk.gov.hmcts.cmc.rpa.DateFormatter;
 import uk.gov.hmcts.cmc.rpa.mapper.helper.RPAMapperHelper;
 import uk.gov.hmcts.cmc.rpa.mapper.json.NullAwareJsonObjectBuilder;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import javax.json.JsonObject;
 
@@ -14,12 +13,11 @@ import javax.json.JsonObject;
 public class PaidInFullJsonMapper {
 
     public JsonObject map(Claim claim) {
-        final LocalDate claimantPaidOnDate = RPAMapperHelper.claimantPaidOnDate(claim);
 
         return new NullAwareJsonObjectBuilder()
             .add("caseNumber", claim.getReferenceNumber())
             .add("paidInFullSubmittedOn", DateFormatter.format(LocalDateTime.now()))
-            .add("claimantPaidOn", DateFormatter.format(claimantPaidOnDate))
+            .add("claimantPaidOn", DateFormatter.format(RPAMapperHelper.claimantPaidOnDate(claim)))
             .build();
     }
 
