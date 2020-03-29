d885bb077a008fa321291b3823fd607b90e7869e
tharacka <tharacka@kainos.com>
2020-03-29 23:05:59 +0100

    rpa mapper refactor

--- rpa-mapper/src/main/java/uk/gov/hmcts/cmc/rpa/mapper/PaidInFullJsonMapper.java
+++ rpa-mapper/src/main/java/uk/gov/hmcts/cmc/rpa/mapper/PaidInFullJsonMapper.java
@@ -3,22 +3,23 @@ package uk.gov.hmcts.cmc.rpa.mapper;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.rpa.DateFormatter;
+import uk.gov.hmcts.cmc.rpa.mapper.helper.RPAMapperHelper;
 import uk.gov.hmcts.cmc.rpa.mapper.json.NullAwareJsonObjectBuilder;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import javax.json.JsonObject;
-
-import static uk.gov.hmcts.cmc.rpa.mapper.helper.RPAMapperHelper.claimantPaidOnDate;
 
 @Component
 public class PaidInFullJsonMapper {
 
     public JsonObject map(Claim claim) {
+        final LocalDate claimantPaidOnDate = RPAMapperHelper.claimantPaidOnDate(claim);
 
         return new NullAwareJsonObjectBuilder()
             .add("caseNumber", claim.getReferenceNumber())
             .add("paidInFullSubmittedOn", DateFormatter.format(LocalDateTime.now()))
-            .add("claimantPaidOn", DateFormatter.format(claimantPaidOnDate(claim)))
+            .add("claimantPaidOn", DateFormatter.format(claimantPaidOnDate))
             .build();
     }
 
