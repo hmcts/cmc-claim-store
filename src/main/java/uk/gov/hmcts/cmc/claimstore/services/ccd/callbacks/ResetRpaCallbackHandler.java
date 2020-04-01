8ac9baf6ade9cb590641ec7b6856cc7f121a4f33
akritis22 <60217152+akritis22@users.noreply.github.com>
2020-04-01 18:38:27 +0100

    ROC-7041 RPA callback (#1427)
    * resend pin initial commit
    
    * resend pin initial commit
    
    * resend pin after initial review
    
    * added unit test
    
    * fix codestyle issues
    
    * extract test data to constant
    
    * change test name
    
    * simplifed get pinResponse
    
    * add exception for defendant not having email address
    
    * added test when defendant has no email address
    
    * fixed checkstyle issue
    
    * change log message
    
    * simplified defendant already linked check
    
    * simplified defendant already linked check
    
    * rename resend to resetAndSend
    
    * ROC-7041-RPA-callback | initial changes for RESET RPA
    
    * ROC-7041 | rpa callback
    
    * ROC-7041 | resend RPA
    
    * 7041 | new service and call back added
    
    * ROC-7041 | fixing checkstyle errors
    
    * ROC-7041 | reverting robotics support controller
    
    * ROC-7041 | removing unused enum
    
    * ROC-7041 | code tidy up
    
    * ROC-7041 | renamed event type as per ccd
    
    * ROC-7041 | addressing code review comments
    
    * ROC-7041 | addressing code review comments
    
    * ROC-7041 | updating RPA event types to enum
    
    * ROC-7041 | added enum for RPA event type
    
    * addressing code review comment and changing code format
    
    * Update src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/roboticssupport/RoboticsNotificationServiceImpl.java
    
    Co-Authored-By: Dave Oxtoby <dave.oxtoby@gmail.com>
    
    * Update src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/roboticssupport/RoboticsNotificationServiceImpl.java
    
    Co-Authored-By: Dave Oxtoby <dave.oxtoby@gmail.com>
    
    * ROC-7041 | code review comments refactor
    
    * ROC-7041 | added letter linkholder call
    
    * ROC-7041 | addressed code review comments
    
    * ROC-7041 | updated tests as per PR comments
    
    * Add ctsc_enabled variable
    
    * ROC-7041 | addressed code review comments
    
    * Bump chart version
    
    * ROC-7041 | adding feature flag
    
    * ROC-7041: Move to about to submit
    
    * ROC-7041 | updates to handler
    
    * ROC-7041 | fix for failing claim issue rpa notification
    
    * bump chart
    
    Co-authored-by: Kiran Varma <varma.ap@gmail.com>
    Co-authored-by: Akriti Sinha <akritis@kainos.com@Caecilius.local>
    Co-authored-by: Akriti Sinha <akritis@kainos.com@Caecilius.kainos.com>
    Co-authored-by: Dave Oxtoby <dave.oxtoby@gmail.com>
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>
    Co-authored-by: Ellie Harrison <ellieh@kainos.com>
    Co-authored-by: Dave Oxtoby <david.oxtoby@hmcts.net>

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/ResetRpaCallbackHandler.java
@@ -0,0 +1,95 @@
+package uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks;
+
+import com.google.common.collect.ImmutableMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.cmc.ccd.domain.CaseEvent;
+import uk.gov.hmcts.cmc.ccd.mapper.CaseMapper;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.Role;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.roboticssupport.RoboticsNotificationService;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.roboticssupport.RpaEventType;
+import uk.gov.hmcts.cmc.claimstore.utils.CaseDetailsConverter;
+import uk.gov.hmcts.cmc.domain.exceptions.BadRequestException;
+import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.hmcts.cmc.claimstore.services.ccd.Role.CASEWORKER;
+
+@Service
+@ConditionalOnProperty("feature_toggles.ctsc_enabled")
+public class ResetRpaCallbackHandler extends CallbackHandler {
+    private static final String RPA_EVENT_TYPE = "RPAEventType";
+    private static final String RPA_STATE_INVALID = "invalid";
+
+    private static final List<Role> ROLES = Collections.singletonList(CASEWORKER);
+
+    private CaseDetailsConverter caseDetailsConverter;
+    private CaseMapper caseMapper;
+    private RoboticsNotificationService roboticsNotificationService;
+
+    @Autowired
+    public ResetRpaCallbackHandler(
+        CaseDetailsConverter caseDetailsConverter,
+        CaseMapper caseMapper, RoboticsNotificationService roboticsNotificationService) {
+        this.caseDetailsConverter = caseDetailsConverter;
+        this.caseMapper = caseMapper;
+        this.roboticsNotificationService = roboticsNotificationService;
+    }
+
+    @Override
+    protected Map<CallbackType, Callback> callbacks() {
+        return ImmutableMap.of(
+            CallbackType.ABOUT_TO_SUBMIT, this::requestResetRpa
+        );
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return Collections.singletonList(CaseEvent.RESEND_RPA);
+    }
+
+    @Override
+    public List<Role> getSupportedRoles() {
+        return ROLES;
+    }
+
+    private AboutToStartOrSubmitCallbackResponse requestResetRpa(CallbackParams callbackParams) {
+        CallbackRequest callbackRequest = callbackParams.getRequest();
+        Claim claim = convertCallbackToClaim(callbackRequest);
+        handleRoboticsNotification(callbackRequest, claim.getReferenceNumber());
+        Map<String, Object> map = new HashMap<>(caseDetailsConverter.convertToMap(caseMapper.to(claim)));
+        AboutToStartOrSubmitCallbackResponse response = AboutToStartOrSubmitCallbackResponse.builder()
+            .data(map)
+            .build();
+        return response;
+    }
+
+    private Claim convertCallbackToClaim(CallbackRequest callbackRequest) {
+        return caseDetailsConverter.extractClaim(callbackRequest.getCaseDetails());
+    }
+
+    private String handleRoboticsNotification(CallbackRequest callbackRequest, String referenceNumber) {
+        switch (RpaEventType.fromValue((String)(callbackRequest.getCaseDetails()
+            .getData().get(RPA_EVENT_TYPE)))) {
+            case CLAIM:
+                return roboticsNotificationService.rpaClaimNotification(referenceNumber);
+            case MORE_TIME:
+                return roboticsNotificationService.rpaMoreTimeNotifications(referenceNumber);
+            case CCJ:
+                return roboticsNotificationService.rpaCCJNotifications(referenceNumber);
+            case DEFENDANT_RESPONSE:
+                return roboticsNotificationService.rpaResponseNotifications(referenceNumber);
+            case PAID_IN_FULL:
+                return roboticsNotificationService.rpaPIFNotifications(referenceNumber);
+            default:
+                throw new BadRequestException(RPA_STATE_INVALID);
+        }
+    }
+}
