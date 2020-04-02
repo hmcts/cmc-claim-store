262ca072fb8cd4343415ff857284301e3b9835a9
Akriti Sinha <akritis@kainos.com>
2020-04-02 14:50:51 +0100

    ROC-7700 | tidy up

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CaseEvent.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CaseEvent.java
@@ -71,7 +71,6 @@ public enum CaseEvent {
     UPDATE_CLAIM("UpdateClaim"),
     RESET_PIN("ResetPin"),
     ISSUE_GENERAL_LETTER("IssueGeneralLetter"),
-    REDETERMINATION_RECEIPT_UPLOAD("RedeterminationReceiptUpload"),
     CLAIMANT_RESPONSE_RECEIPT_UPLOAD("ClaimantResponseReceiptUpload");
 
     private final String value;
