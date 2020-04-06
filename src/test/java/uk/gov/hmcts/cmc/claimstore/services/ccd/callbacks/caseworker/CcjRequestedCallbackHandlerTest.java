3616cb017a72fafcb149aef9897b573de46c84ee
akritis22 <60217152+akritis22@users.noreply.github.com>
2020-04-06 16:46:16 +0100

    ROC-7607 CCJ Request to update CCJ state and upload document to CCD (#1606)
    * ROC-7377 | added new case event
    
    * ROC-7377 | added new claim state
    
    * staff_emails_enabled feature toggle added to application.yml
    
    * add staff_emails_enabled to integration tests
    
    * ROC-7377 | updated handler tests and added new service to upload claimaint response document to ccd
    
    * ROC-7377 | updated staff notification handler and added new callback handler for proceeds to caseman
    
    * ROC-7377 | updated handlers
    
    * ROC-7377 | updated unit tests
    
    * ROC-7377 | using feature toggle
    
    * uploading claim docs on event listener and reverting staff notification disabling changes as needs to be done on a different branch
    
    * ROC-7377 | reverting staff email toggle changes
    
    * ROC-7377 | added new case event
    
    * ROC-7377 | added new claim state
    
    * staff_emails_enabled feature toggle added to application.yml
    
    * ROC-7377 | updated handler tests and added new service to upload claimaint response document to ccd
    
    * ROC-7377 | updated staff notification handler and added new callback handler for proceeds to caseman
    
    * ROC-7377 | updated handlers
    
    * ROC-7377 | updated unit tests
    
    * add staff_emails_enabled to integration tests
    
    * ROC-7377 | using feature toggle
    
    * uploading claim docs on event listener and reverting staff notification disabling changes as needs to be done on a different branch
    
    * ROC-7377 | reverting staff email toggle changes
    
    * ROC-7607 | updated saveclaimantresponsedocumentservice
    
    * ROC-7607 | update to handler and tests as per story
    
    * ROC-7607 | deleted handler as not needed
    
    * ROC-7607 | code review comments
    
    * ROC-7607 | added a new pdf service
    
    * ROC-7607 | addressed code review comments
    
    * ROC-7607 | code review comments
    
    * Add ctsc_enabled variable
    
    * ROC-7607 | added a feature toggle for release
    
    * ROC-7607 | addressing code review comments
    
    * ROC-7607 | addressing code review comments
    
    * ROC-7607 | tidy up
    
    * ROC-7607 | code review comments
    
    * ROC-7607 | updating typo
    
    * ROC-7607 | making code production ready
    
    * ROC-7607 | tidy up
    
    * ROC-7607 | refcatoring
    
    * ROC-7607: Add callback
    
    * ROC-7607: Use correct toggle
    
    * ROC-7607 | tidying up the ClaimantCCJRequestService
    
    * ROC-7607 | moving response doc upload logic to ccj service
    
    * ROC-7607 | updates to callback handler
    
    * ROC-7607 | updating ccj document upload logic
    
    * ROC-7607 | updated handler
    
    * ROC-7607 | updates to handler and tests
    
    * ROC-7607 | updating handler
    
    * ROC-7607 | test added
    
    * ROC-7607 | test updates
    
    * ROC-7607 | tide up
    
    * ROC-7607 | updating tests
    
    * ROC-7607 | checkstyle
    
    * Update src/main/java/uk/gov/hmcts/cmc/claimstore/services/CountyCourtJudgmentService.java
    
    Co-Authored-By: Dave Oxtoby <dave.oxtoby@gmail.com>
    
    * ROC-7607 | checkstyles
    
    Co-authored-by: tharacka <tharacka@kainos.com>
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>
    Co-authored-by: Ellie Harrison <ellieh@kainos.com>
    Co-authored-by: Dave Oxtoby <david.oxtoby@hmcts.net>
    Co-authored-by: Dave Oxtoby <dave.oxtoby@gmail.com>

---
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/CcjRequestedCallbackHandlerTest.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.caseworker;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.cmc.ccd.domain.CaseEvent;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackParams;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackType;
+import uk.gov.hmcts.cmc.domain.models.ClaimState;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+import java.util.Collections;
+
+@ExtendWith(MockitoExtension.class)
+class CcjRequestedCallbackHandlerTest {
+
+    private CcjRequestedCallbackHandler handler;
+    private CallbackParams callbackParams;
+    private CallbackRequest callbackRequest;
+    private static final String AUTHORISATION = "Bearer: aaaa";
+
+    @BeforeEach
+    public void setUp() {
+        callbackRequest = CallbackRequest
+            .builder()
+            .caseDetails(CaseDetails.builder().data(Collections.EMPTY_MAP).build())
+            .eventId(CaseEvent.CCJ_REQUESTED.getValue())
+            .build();
+
+        callbackParams = CallbackParams.builder()
+            .type(CallbackType.ABOUT_TO_SUBMIT)
+            .request(callbackRequest)
+            .params(ImmutableMap.of(CallbackParams.Params.BEARER_TOKEN, AUTHORISATION))
+            .build();
+    }
+
+    @Test
+    void shouldReturnOpenStateIfCtscNotEnabled() {
+        handler = new CcjRequestedCallbackHandler(false);
+
+        AboutToStartOrSubmitCallbackResponse response =
+            (AboutToStartOrSubmitCallbackResponse) handler.handle(callbackParams);
+
+        Assertions.assertEquals(ClaimState.OPEN.getValue(), response.getData().get("state"));
+    }
+
+    @Test
+    void shouldReturnJudgmentRequestedStateIfCtscEnabled() {
+        handler = new CcjRequestedCallbackHandler(true);
+
+        AboutToStartOrSubmitCallbackResponse response =
+            (AboutToStartOrSubmitCallbackResponse) handler.handle(callbackParams);
+
+        Assertions.assertEquals(ClaimState.JUDGMENT_REQUESTED.getValue(), response.getData().get("state"));
+    }
+}
