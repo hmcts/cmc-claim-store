aed3601a115abc91a33745decfc1e1a59afc10aa
akritis22 <60217152+akritis22@users.noreply.github.com>
2020-04-06 16:46:16 +0100

    ROC-7607 CCJ Request to update CCJ state and upload document to CCD (#1606)
    * ROC-7377 | added new case event
    
    * ROC-7377 | added new claim state
    
    * staff_emails_enabled feature toggle added to application.yml
    
    * add staff_emails_enabled to integration tests
    
    * ROC-7377 | updated handler tests and added new service to upload claimaint response document to ccd
    
    * ROC-7377 | updated staff notification handler and added new callback handler for proceeds to caseman
    
    * ROC-7377 | updated handlers
    
    * ROC-7377 | updated unit tests
    
    * ROC-7377 | using feature toggle
    
    * uploading claim docs on event listener and reverting staff notification disabling changes as needs to be done on a different branch
    
    * ROC-7377 | reverting staff email toggle changes
    
    * ROC-7377 | added new case event
    
    * ROC-7377 | added new claim state
    
    * staff_emails_enabled feature toggle added to application.yml
    
    * ROC-7377 | updated handler tests and added new service to upload claimaint response document to ccd
    
    * ROC-7377 | updated staff notification handler and added new callback handler for proceeds to caseman
    
    * ROC-7377 | updated handlers
    
    * ROC-7377 | updated unit tests
    
    * add staff_emails_enabled to integration tests
    
    * ROC-7377 | using feature toggle
    
    * uploading claim docs on event listener and reverting staff notification disabling changes as needs to be done on a different branch
    
    * ROC-7377 | reverting staff email toggle changes
    
    * ROC-7607 | updated saveclaimantresponsedocumentservice
    
    * ROC-7607 | update to handler and tests as per story
    
    * ROC-7607 | deleted handler as not needed
    
    * ROC-7607 | code review comments
    
    * ROC-7607 | added a new pdf service
    
    * ROC-7607 | addressed code review comments
    
    * ROC-7607 | code review comments
    
    * Add ctsc_enabled variable
    
    * ROC-7607 | added a feature toggle for release
    
    * ROC-7607 | addressing code review comments
    
    * ROC-7607 | addressing code review comments
    
    * ROC-7607 | tidy up
    
    * ROC-7607 | code review comments
    
    * ROC-7607 | updating typo
    
    * ROC-7607 | making code production ready
    
    * ROC-7607 | tidy up
    
    * ROC-7607 | refcatoring
    
    * ROC-7607: Add callback
    
    * ROC-7607: Use correct toggle
    
    * ROC-7607 | tidying up the ClaimantCCJRequestService
    
    * ROC-7607 | moving response doc upload logic to ccj service
    
    * ROC-7607 | updates to callback handler
    
    * ROC-7607 | updating ccj document upload logic
    
    * ROC-7607 | updated handler
    
    * ROC-7607 | updates to handler and tests
    
    * ROC-7607 | updating handler
    
    * ROC-7607 | test added
    
    * ROC-7607 | test updates
    
    * ROC-7607 | tide up
    
    * ROC-7607 | updating tests
    
    * ROC-7607 | checkstyle
    
    * Update src/main/java/uk/gov/hmcts/cmc/claimstore/services/CountyCourtJudgmentService.java
    
    Co-Authored-By: Dave Oxtoby <dave.oxtoby@gmail.com>
    
    * ROC-7607 | checkstyles
    
    Co-authored-by: tharacka <tharacka@kainos.com>
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>
    Co-authored-by: Ellie Harrison <ellieh@kainos.com>
    Co-authored-by: Dave Oxtoby <david.oxtoby@hmcts.net>
    Co-authored-by: Dave Oxtoby <dave.oxtoby@gmail.com>

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/documents/CCJByAdmissionOrDeterminationPdfService.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.cmc.claimstore.documents;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.cmc.claimstore.config.properties.pdf.DocumentTemplates;
+import uk.gov.hmcts.cmc.claimstore.documents.output.PDF;
+import uk.gov.hmcts.cmc.claimstore.services.staff.content.countycourtjudgment.ContentProvider;
+import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.ClaimDocumentType;
+import uk.gov.hmcts.reform.pdf.service.client.PDFServiceClient;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.cmc.claimstore.utils.DocumentNameUtils.buildRequestForJudgmentByAdmissionOrDeterminationFileBaseName;
+
+@Service
+public class CCJByAdmissionOrDeterminationPdfService implements PdfService {
+
+    private final DocumentTemplates documentTemplates;
+    private final PDFServiceClient pdfServiceClient;
+    private final ContentProvider contentProvider;
+
+    @Autowired
+    public CCJByAdmissionOrDeterminationPdfService(
+        DocumentTemplates documentTemplates,
+        PDFServiceClient pdfServiceClient,
+        ContentProvider contentProvider
+    ) {
+        this.documentTemplates = documentTemplates;
+        this.pdfServiceClient = pdfServiceClient;
+        this.contentProvider = contentProvider;
+    }
+
+    @Override
+    public PDF createPdf(Claim claim) {
+        requireNonNull(claim);
+        return new PDF(
+            buildRequestForJudgmentByAdmissionOrDeterminationFileBaseName(claim.getReferenceNumber(),
+                claim.getCountyCourtJudgment().getCcjType().name().toLowerCase()),
+            pdfServiceClient.generateFromHtml(
+                documentTemplates.getCountyCourtJudgmentByRequest(),
+                contentProvider.createContent(claim)),
+            ClaimDocumentType.CCJ_REQUEST
+        );
+    }
+}
