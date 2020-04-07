089df355dbfdbab25a0dbcf123aa852eb6938c5c
justin-r-oliver <justin.oliver@hmcts.net>
2020-04-07 09:03:14 +0100

    Added document generation integration test

---
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/helper/FileUtils.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.cmc.claimstore.helper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileUtils {
+
+    private FileUtils() {
+    }
+
+    public static String readFile(String filename) throws IOException {
+        Path path = Paths.get(filename);
+
+        return new String(Files.readAllBytes(path));
+    }
+
+    public static void writeFile(String filename, String contents) throws IOException {
+        Path pathActual = Paths.get(filename);
+
+        Files.write(pathActual, contents.getBytes());
+    }
+}
