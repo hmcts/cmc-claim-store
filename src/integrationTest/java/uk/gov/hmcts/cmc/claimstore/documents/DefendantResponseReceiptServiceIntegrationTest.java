999760ec5622afa572b7fe3e6c6b1c06de1d08d0
justin-r-oliver <justin.oliver@hmcts.net>
2020-04-07 15:13:14 +0100

    Refactored context loading

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/documents/DefendantResponseReceiptServiceIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/documents/DefendantResponseReceiptServiceIntegrationTest.java
@@ -2,40 +2,52 @@ package uk.gov.hmcts.cmc.claimstore.documents;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.cmc.claimstore.BaseMockSpringTest;
 import uk.gov.hmcts.cmc.claimstore.config.properties.pdf.DocumentTemplates;
 import uk.gov.hmcts.cmc.claimstore.documents.content.DefendantResponseContentProvider;
 import uk.gov.hmcts.cmc.claimstore.helper.DocumentComparisonHelper;
+import uk.gov.hmcts.cmc.claimstore.helper.HTMLTemplateProcessor;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.response.DefenceType;
 import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaim;
 import uk.gov.hmcts.cmc.domain.models.sampledata.SampleResponse;
+import uk.gov.hmcts.reform.pdf.service.client.PDFServiceClient;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.cmc.claimstore.helper.FileUtils.readFile;
 import static uk.gov.hmcts.cmc.claimstore.helper.FileUtils.writeFile;
 import static wiremock.org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
 
+@RunWith(SpringRunner.class)
 public class DefendantResponseReceiptServiceIntegrationTest extends BaseMockSpringTest {
 
+    @Autowired
     private DefendantResponseReceiptService defendantResponseReceiptService;
 
     @Autowired
-    private DefendantResponseContentProvider contentProvider;
+    protected DefendantResponseContentProvider contentProvider;
 
     @Autowired
-    private DocumentTemplates documentTemplates;
+    protected DocumentTemplates documentTemplates;
 
     @Autowired
-    private DocumentComparisonHelper documentComparisonHelper;
+    protected DocumentComparisonHelper documentComparisonHelper;
+
+    @Autowired
+    protected HTMLTemplateProcessor htmlTemplateProcessor;
 
     @Before
     public void beforeEachTest() {
 
-        defendantResponseReceiptService = new DefendantResponseReceiptService(
-            contentProvider, documentTemplates, documentComparisonHelper.provideLocalPdfService());
+        provideLocalPdfService();
     }
 
     @Test
@@ -62,5 +74,20 @@ public class DefendantResponseReceiptServiceIntegrationTest extends BaseMockSpri
         String expectedHtml = readFile("src/integrationTest/resources/documents/expectedDefendantResponseReceipt.html");
 
         assertXMLEqual(actualHtml, expectedHtml);
+    }
+
+    private PDFServiceClient provideLocalPdfService() {
+
+        when(pdfServiceClient.generateFromHtml(any(), any()))
+            .thenAnswer((Answer<byte[]>) invocation -> {
+                Object[] args = invocation.getArguments();
+                byte[] template = (byte[]) args[0];
+                Map<String, Object> placeholders = (Map<String, Object>) args[1];
+
+                String output = htmlTemplateProcessor.process(new String(template), placeholders);
+                return output.getBytes();
+            });
+
+        return pdfServiceClient;
     }
 }
