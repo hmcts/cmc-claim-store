1b9455474a95089960a325d36b4f352925819665
Jana Latzberg <j.latzberg@outlook.com>
2020-04-24 12:40:35 +0100

    Update code with join and newline
    Co-Authored-By: Dave Oxtoby <dave.oxtoby@gmail.com>

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/MoreTimeRequestedCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/MoreTimeRequestedCallbackHandler.java
@@ -72,13 +72,13 @@ public class MoreTimeRequestedCallbackHandler extends CallbackHandler {
     private static final String PREVIEW_SENTENCE = "The response deadline will be %s .";
     private static final String ERROR_MESSAGE =
         "There was a technical problem. Nothing has been sent. You need to try again.";
-    private static final String STANDARD_DEADLINE_TEXT = "You’ve been given an extra 14 days to respond to the"
-            + " claim made against you by %s.\n"
-            + "\n"
-            + "You now need to respond to the claim before 4pm on %s.\n"
-            + "\n"
-            + "If you don’t respond, you could get a County Court Judgment (CCJ). This may make it harder to get "
-            + "credit, such as a mobile phone contract, credit card or mortgage.";
+    private static final String STANDARD_DEADLINE_TEXT = String.join(newLine,
+    "You’ve been given an extra 14 days to respond to the claim made against you by %s.",
+            "",
+            "You now need to respond to the claim before 4pm on %s.",
+            "",
+            "If you don’t respond, you could get a County Court Judgment (CCJ). This may make it harder to get "
+            + "credit, such as a mobile phone contract, credit card or mortgage.");
     private static final String RESPONSE_DEADLINE_PREVIEW = "responseDeadlinePreview";
 
     @Autowired
