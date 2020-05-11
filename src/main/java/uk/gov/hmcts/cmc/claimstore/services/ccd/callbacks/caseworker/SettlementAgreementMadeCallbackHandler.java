6cdb1ebbd8b2c8bd58cd5188a9e1bad5ea439d41
akritis22 <60217152+akritis22@users.noreply.github.com>
2020-05-11 15:46:00 +0100

    Bug - ROC-7950 settlement agreement made state change to be controlled by feature flag (#1687)
    * ROC-7950 | added a new handler for settlement agreement made controlled by ctsc feature flag
    
    * ROC-7950 | resolving checkstyle issues
    
    * ROC-7950 | added missed role
    
    * ROC-7950 | resolving checkstyle errors
    
    * ROC-7950 | refactoring callback handler
    
    * ROC-7950 | refactor
    
    Co-authored-by: justin-r-oliver <justin-r-oliver@users.noreply.github.com>
    Co-authored-by: Christophe Kalista <christophe.kalista@gmail.com>

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/SettlementAgreementMadeCallbackHandler.java
@@ -0,0 +1,67 @@
+package uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.caseworker;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.cmc.ccd.domain.CaseEvent;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.Role;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.Callback;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackHandler;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackParams;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackType;
+import uk.gov.hmcts.cmc.domain.models.ClaimState;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.AGREEMENT_COUNTER_SIGNED_BY_DEFENDANT;
+import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.AGREEMENT_SIGNED_BY_BOTH;
+import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.OFFER_COUNTER_SIGNED_BY_DEFENDANT;
+import static uk.gov.hmcts.cmc.claimstore.services.ccd.Role.CASEWORKER;
+import static uk.gov.hmcts.cmc.claimstore.services.ccd.Role.CITIZEN;
+
+@Service
+public class SettlementAgreementMadeCallbackHandler extends CallbackHandler {
+    private static final List<CaseEvent> EVENTS =
+        Arrays.asList(AGREEMENT_COUNTER_SIGNED_BY_DEFENDANT,
+            OFFER_COUNTER_SIGNED_BY_DEFENDANT,
+            AGREEMENT_SIGNED_BY_BOTH);
+    private static final List<Role> ROLES = ImmutableList.of(CASEWORKER, CITIZEN);
+
+    private final boolean ctscEnabled;
+    private static final String STATE = "state";
+
+    @Override
+    protected Map<CallbackType, Callback> callbacks() {
+        return ImmutableMap.of(CallbackType.ABOUT_TO_SUBMIT, this::determineState);
+    }
+
+    public SettlementAgreementMadeCallbackHandler(
+        @Value("${feature_toggles.ctsc_enabled}") boolean ctscEnabled) {
+        this.ctscEnabled = ctscEnabled;
+    }
+
+    private CallbackResponse determineState(CallbackParams callbackParams) {
+        ClaimState state = ctscEnabled ? ClaimState.SETTLEMENT_AGREEMENT_MADE : ClaimState.OPEN;
+        Map<String, Object> data = new HashMap<>(callbackParams.getRequest().getCaseDetails().getData());
+        data.put(STATE, state.getValue());
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(data)
+            .build();
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    @Override
+    public List<Role> getSupportedRoles() {
+        return ROLES;
+    }
+}
