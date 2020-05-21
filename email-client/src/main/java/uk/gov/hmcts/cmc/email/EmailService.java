e3b68802cdce03bb1be56fa5def9a6c82c8543d3
Andrew Walker <andrewwa@kainos.com>
2020-05-21 16:34:00 +0100

    Add toggled SendGrid integration

--- email-client/src/main/java/uk/gov/hmcts/cmc/email/EmailService.java
+++ email-client/src/main/java/uk/gov/hmcts/cmc/email/EmailService.java
@@ -1,10 +1,18 @@
 package uk.gov.hmcts.cmc.email;
 
 import com.microsoft.applicationinsights.TelemetryClient;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -12,7 +20,9 @@ import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.cmc.launchdarkly.LaunchDarklyClient;
 
+import java.io.IOException;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
@@ -28,20 +38,34 @@ public class EmailService {
     private final TelemetryClient telemetryClient;
     private final JavaMailSender sender;
     private final boolean asyncEventOperationEnabled;
+    private final SendGrid sendGrid;
+    private final LaunchDarklyClient launchDarklyClient;
 
     @Autowired
     public EmailService(
         TelemetryClient telemetryClient,
         JavaMailSender sender,
-        @Value("${feature_toggles.async_event_operations_enabled:false}") boolean asyncEventOperationEnabled
+        @Value("${feature_toggles.async_event_operations_enabled:false}") boolean asyncEventOperationEnabled,
+        @Value("${sendgrid.api-key}") String sendGridApiKey,
+        LaunchDarklyClient launchDarklyClient
     ) {
         this.telemetryClient = telemetryClient;
         this.sender = sender;
         this.asyncEventOperationEnabled = asyncEventOperationEnabled;
+        this.sendGrid = new SendGrid(sendGridApiKey);
+        this.launchDarklyClient = launchDarklyClient;
     }
 
     @Retryable(value = EmailSendFailedException.class, backoff = @Backoff(delay = 100, maxDelay = 500))
     public void sendEmail(String from, EmailData emailData) {
+        if (launchDarklyClient.isFeatureEnabled("sendgrid-roc-7497", LaunchDarklyClient.CLAIM_STORE_USER)) {
+            sendEmailSendGrid(from, emailData);
+        } else {
+            sendEmailMTA(from, emailData);
+        }
+    }
+
+    private void sendEmailMTA(String from, EmailData emailData) {
         try {
             MimeMessage message = sender.createMimeMessage();
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true);
@@ -63,6 +87,29 @@ public class EmailService {
         }
     }
 
+    private void sendEmailSendGrid(String from, EmailData emailData) {
+        try {
+            Email sender = new Email(from);
+            String subject = emailData.getSubject();
+            Email recipient = new Email("test@example.com");
+            Content content = new Content(MediaType.TEXT_PLAIN_VALUE, emailData.getMessage());
+            Mail mail = new Mail(sender, subject, recipient, content);
+
+            Request request = new Request();
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+
+            Response response = sendGrid.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException e) {
+            throw new EmailSendFailedException(e);
+        }
+    }
+
+    @SuppressWarnings("unused")
     @Recover
     public void logSendMessageWithAttachmentFailure(
         EmailSendFailedException exception,
