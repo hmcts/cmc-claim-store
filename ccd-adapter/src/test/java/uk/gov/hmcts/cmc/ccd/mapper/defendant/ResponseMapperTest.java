dbae5ee95a590788f8c9144a94d3b3709fad1c1b
Dave Oxtoby <david.oxtoby@hmcts.net>
2020-05-21 09:40:04 +0100

    ROC-7939: Fix imports

--- ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/mapper/defendant/ResponseMapperTest.java
+++ ccd-adapter/src/test/java/uk/gov/hmcts/cmc/ccd/mapper/defendant/ResponseMapperTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.cmc.ccd.mapper.defendant;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
