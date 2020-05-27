ec78fb2d77c12580f80f5447fb923817646257ca
Andrew Walker <andrewwa@kainos.com>
2020-05-27 11:26:54 +0100

    Update and add tests

--- email-client/src/main/java/uk/gov/hmcts/cmc/email/EmailService.java
+++ email-client/src/main/java/uk/gov/hmcts/cmc/email/EmailService.java
@@ -1,13 +1,6 @@
 package uk.gov.hmcts.cmc.email;
 
 import com.microsoft.applicationinsights.TelemetryClient;
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
