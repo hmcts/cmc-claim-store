0b74d9f6fc0555f53a6c972f7abedcff0f60562f
Dave Oxtoby <dave.oxtoby@gmail.com>
2020-06-04 17:38:52 +0100

    ROC-7939 paper response data model (#1709)
    * ROC-7939: Paper response data model initial commit
    
    * ROC-7939: Use British spelling
    
    * ROC-7939: Handle null values in Statement of Means
    
    * ROC-7939: ROC-7939: attachScannedDocs callback handler, Set Response method for online defence responses and fix tests
    
    * ROC-7939: Update enum values
    
    * ROC-7939: handle null response method
    
    * ROC-7939: Set response method for existing claims
    
    * ROC-7939: Fis test
    
    * ROC-7939: Fix imports
    
    * ROC-7939: Remove empty line
    
    * ROC-7939: Remove import
    
    * ROC-7939: Fix test
    
    * ROC-7939: Move Response method migration to CaseDetailsConverter
    
    * ROC-7939: Remove redundant CCDCase
    
    * ROC-7939: Remove redundant CCDCase from tests
    
    * ROC-7939: Remove import
    
    * ROC-7939: Rename method
    
    * ROC-7993: Move to builder pattern
    
    * ROC-7939: Fix test
    
    * ROC-7939: Review Fixes
    
    * ROC-7939: Sonar cloud Fixes
    
    * ROC-7939: Use Collections.emptyMap()
    
    * ROC-7939: Use Collections.emptyMap()
    
    Co-authored-by: Dave Oxtoby <david.oxtoby@hmcts.net>
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>

---
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/response/ResponseMethod.java
@@ -0,0 +1,8 @@
+package uk.gov.hmcts.cmc.domain.models.response;
+
+//NOTE: Enum names need to be identical to enum names in CCDResponseMethod
+public enum ResponseMethod {
+    DIGITAL,
+    OFFLINE,
+    OCON_FORM  // Paper response that enables online claimant response
+}
