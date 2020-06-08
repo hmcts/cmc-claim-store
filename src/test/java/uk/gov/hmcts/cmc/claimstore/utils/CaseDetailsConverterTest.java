d4938ba9f0de7c94c785d0ff0283822ec592d3c3
Dave Oxtoby <david.oxtoby@hmcts.net>
2020-06-08 13:37:26 +0100

    ROC-8055: place ResponseMethod behind feature flag

--- src/test/java/uk/gov/hmcts/cmc/claimstore/utils/CaseDetailsConverterTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/utils/CaseDetailsConverterTest.java
@@ -1,13 +1,14 @@
 package uk.gov.hmcts.cmc.claimstore.utils;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.cmc.ccd.config.CCDMapperConfig;
 import uk.gov.hmcts.cmc.ccd.domain.CCDCase;
 import uk.gov.hmcts.cmc.ccd.mapper.CaseMapper;
@@ -16,15 +17,17 @@ import uk.gov.hmcts.cmc.claimstore.services.WorkingDayIndicator;
 import uk.gov.hmcts.cmc.claimstore.services.bankholidays.NonWorkingDaysCollection;
 import uk.gov.hmcts.cmc.claimstore.services.bankholidays.PublicHolidaysCollection;
 import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.response.ResponseMethod;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.cmc.claimstore.utils.ResourceLoader.successfulCoreCaseDataStoreSubmitResponse;
+import static uk.gov.hmcts.cmc.claimstore.utils.ResourceLoader.successfulCoreCaseDataStoreSubmitResponseWithDQ;
 
 @SpringBootTest
 @ContextConfiguration(classes = CCDMapperConfig.class)
-@RunWith(SpringJUnit4ClassRunner.class)
-public class CaseDetailsConverterTest {
+@RunWith(SpringRunner.class)
+class CaseDetailsConverterTest {
 
     private CaseDetailsConverter caseDetailsConverter;
 
@@ -37,34 +40,66 @@ public class CaseDetailsConverterTest {
     @Mock
     private NonWorkingDaysCollection nonWorkingDaysCollection;
 
-    @Before
-    public void setup() {
-        caseDetailsConverter = new CaseDetailsConverter(caseMapper,
-            JsonMapperFactory.create(),
-            new WorkingDayIndicator(publicHolidaysCollection, nonWorkingDaysCollection),
-            33);
+    @Nested
+    class CtscSDisabledTests {
+
+        @BeforeEach
+        void setup() {
+            caseDetailsConverter = new CaseDetailsConverter(caseMapper,
+                JsonMapperFactory.create(),
+                new WorkingDayIndicator(publicHolidaysCollection, nonWorkingDaysCollection),
+                33,
+                false);
+        }
+
+        @Test
+        void convertsCaseDetailsToCCDCase() {
+            CaseDetails caseDetails = successfulCoreCaseDataStoreSubmitResponse();
+            CCDCase ccdCase = caseDetailsConverter.extractCCDCase(caseDetails);
+            assertThat(ccdCase.getId()).isEqualTo(caseDetails.getId());
+            assertThat(ccdCase.getState()).isEqualTo(caseDetails.getState());
+        }
+
+        @Test
+        void convertsCaseDetailsToClaim() {
+            CaseDetails caseDetails = successfulCoreCaseDataStoreSubmitResponse();
+            Claim claim = caseDetailsConverter.extractClaim(caseDetails);
+            assertThat(claim.getId()).isEqualTo(caseDetails.getId());
+            assertThat(claim.getState().getValue()).isEqualTo(caseDetails.getState());
+        }
+
+        @Test
+        void convertsCaseDetailsToClaimWithIntentionToProceedDeadline() {
+            CaseDetails caseDetails = successfulCoreCaseDataStoreSubmitResponse();
+            Claim claim = caseDetailsConverter.extractClaim(caseDetails);
+            assertThat(claim.getIntentionToProceedDeadline()).isNotNull();
+        }
+
+        @Test
+        void convertsCaseDetailsToClaimWithResponseMethod() {
+            CaseDetails caseDetails = successfulCoreCaseDataStoreSubmitResponseWithDQ();
+            Claim claim = caseDetailsConverter.extractClaim(caseDetails);
+            assertThat(claim.getResponse().get().getResponseMethod()).isNotPresent();
+        }
     }
 
-    @Test
-    public void convertsCaseDetailsToCCDCase() {
-        CaseDetails caseDetails = successfulCoreCaseDataStoreSubmitResponse();
-        CCDCase ccdCase = caseDetailsConverter.extractCCDCase(caseDetails);
-        assertThat(ccdCase.getId()).isEqualTo(caseDetails.getId());
-        assertThat(ccdCase.getState()).isEqualTo(caseDetails.getState());
-    }
+    @Nested
+    class CtscEnabledTests {
 
-    @Test
-    public void convertsCaseDetailsToClaim() {
-        CaseDetails caseDetails = successfulCoreCaseDataStoreSubmitResponse();
-        Claim claim = caseDetailsConverter.extractClaim(caseDetails);
-        assertThat(claim.getId()).isEqualTo(caseDetails.getId());
-        assertThat(claim.getState().getValue()).isEqualTo(caseDetails.getState());
-    }
+        @BeforeEach
+        void setUp() {
+            caseDetailsConverter = new CaseDetailsConverter(caseMapper,
+                JsonMapperFactory.create(),
+                new WorkingDayIndicator(publicHolidaysCollection, nonWorkingDaysCollection),
+                33,
+                true);
+        }
 
-    @Test
-    public void convertsCaseDetailsToClaimWithIntentionToProceedDeadline() {
-        CaseDetails caseDetails = successfulCoreCaseDataStoreSubmitResponse();
-        Claim claim = caseDetailsConverter.extractClaim(caseDetails);
-        assertThat(claim.getIntentionToProceedDeadline()).isNotNull();
+        @Test
+        void convertsCaseDetailsToClaimWithResponseMethod() {
+            CaseDetails caseDetails = successfulCoreCaseDataStoreSubmitResponseWithDQ();
+            Claim claim = caseDetailsConverter.extractClaim(caseDetails);
+            assertThat(claim.getResponse().get().getResponseMethod().get()).isEqualTo(ResponseMethod.DIGITAL);
+        }
     }
 }
