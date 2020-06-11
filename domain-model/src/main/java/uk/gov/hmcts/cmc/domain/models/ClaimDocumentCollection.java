bca8323b1826b142aa9819a62b9f541d76fea5fc
justin-r-oliver <justin.r.oliver@gmail.com>
2020-06-11 10:42:58 +0100

    Addressed review comments from Dave

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/ClaimDocumentCollection.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/ClaimDocumentCollection.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.cmc.domain.models;
 import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -39,6 +40,6 @@ public class ClaimDocumentCollection {
         return scannedDocuments.stream()
             .filter(scannedDocument -> scannedDocument.getDocumentType().equals(scannedDocumentType))
             .filter(scannedDocument -> scannedDocument.getSubtype().equals(subtype.value))
-            .findFirst();
+            .max(Comparator.comparing(ScannedDocument::getDeliveryDate));
     }
 }
--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/ScannedDocumentSubtype.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/ScannedDocumentSubtype.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.cmc.domain.models;
 
 public enum ScannedDocumentSubtype {
 
+    /* Note: This is not an exhaustive list of the subtypes used in CCD */
     OCON9X("OCON9x");
 
     public final String value;
