fc5529129ff63dc082f88efa48b43c39c8cd3fbf
akritis22 <60217152+akritis22@users.noreply.github.com>
2020-06-11 11:56:52 +0100

    ROC-8002 | fixing missing preferred court (#1723)

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/mapper/CaseMapper.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/mapper/CaseMapper.java
@@ -132,6 +132,10 @@ public class CaseMapper {
             builder.channel(ChannelType.valueOf(ccdCase.getChannel().name()));
         }
 
+        if (ccdCase.getPreferredDQCourt() != null) {
+            builder.preferredDQCourt(ccdCase.getPreferredDQCourt());
+        }
+
         return builder.build();
     }
 }
