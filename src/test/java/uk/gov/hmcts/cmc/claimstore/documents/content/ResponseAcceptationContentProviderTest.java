fdf90668b45aaf3ec37b2a4afebf74833b618b05
Tharack Ahmed <41899680+tharacka@users.noreply.github.com>
2020-06-15 16:57:25 +0100

    ROC 5346 Claimant Response PDF changes for staff users (#831)

--- src/test/java/uk/gov/hmcts/cmc/claimstore/documents/content/ResponseAcceptationContentProviderTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/documents/content/ResponseAcceptationContentProviderTest.java
@@ -1,11 +1,20 @@
 package uk.gov.hmcts.cmc.claimstore.documents.content;
 
+import org.junit.Before;
 import org.junit.Test;
+import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.PaymentOption;
 import uk.gov.hmcts.cmc.domain.models.claimantresponse.CourtDetermination;
 import uk.gov.hmcts.cmc.domain.models.claimantresponse.DecisionType;
+import uk.gov.hmcts.cmc.domain.models.claimantresponse.FormaliseOption;
 import uk.gov.hmcts.cmc.domain.models.claimantresponse.ResponseAcceptation;
 import uk.gov.hmcts.cmc.domain.models.response.PaymentIntention;
+import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaim;
+import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaimData;
+import uk.gov.hmcts.cmc.domain.models.sampledata.SampleParty;
+import uk.gov.hmcts.cmc.domain.models.sampledata.SampleResponse;
+import uk.gov.hmcts.cmc.domain.models.sampledata.SampleTheirDetails;
+import uk.gov.hmcts.cmc.domain.models.sampledata.response.SamplePaymentIntention;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -17,26 +26,14 @@ import static uk.gov.hmcts.cmc.domain.models.sampledata.response.SamplePaymentIn
 
 public class ResponseAcceptationContentProviderTest {
 
-    private final PaymentIntentionContentProvider paymentIntentionContentProvider =
-        new PaymentIntentionContentProvider();
-
-    private final ResponseAcceptation responseAcceptation = ResponseAcceptation.builder()
-        .courtDetermination(CourtDetermination.builder()
-            .courtDecision(bySetDate())
-            .courtPaymentIntention(PaymentIntention.builder()
-                .paymentOption(BY_SPECIFIED_DATE)
-                .paymentDate(ResponseAcceptationContentProvider.SYSTEM_MAX_DATE)
-                .build())
-            .disposableIncome(BigDecimal.valueOf(-1))
-            .decisionType(DecisionType.COURT)
-            .build())
-        .build();
+    private final PaymentIntentionContentProvider paymentIntentionContentProvider
+        = new PaymentIntentionContentProvider();
 
     private final ResponseAcceptationContentProvider contentProvider =
         new ResponseAcceptationContentProvider(paymentIntentionContentProvider);
 
-    @Test
-    public void shouldGetContentSayingDisposableIncomeIsNegativeAndSelectDefendantRepaymentPlan() {
+    @Before
+    public void setup() {
         paymentIntentionContentProvider.createContent(
             PaymentOption.IMMEDIATELY,
             null,
@@ -45,10 +42,86 @@ public class ResponseAcceptationContentProviderTest {
             null
         );
 
-        Map<String, Object> content = contentProvider.createContent(responseAcceptation);
+    }
+
+    @Test
+    public void shouldGetContentSayingDisposableIncomeIsNegativeAndSelectDefendantRepaymentPlan() {
+        Claim claim = SampleClaim.builder()
+            .withResponse(SampleResponse
+                .PartAdmission
+                .builder()
+                .buildWithPaymentOptionInstalmentsAndParty(SampleParty.builder().company()))
+            .withClaimantResponse(ResponseAcceptation.builder()
+                .courtDetermination(CourtDetermination.builder()
+                    .courtDecision(bySetDate())
+                    .courtPaymentIntention(PaymentIntention.builder()
+                        .paymentOption(BY_SPECIFIED_DATE)
+                        .paymentDate(ResponseAcceptationContentProvider.SYSTEM_MAX_DATE)
+                        .build())
+                    .disposableIncome(BigDecimal.valueOf(-1))
+                    .decisionType(DecisionType.COURT)
+                    .build())
+                .build())
+            .build();
+
+        Map<String, Object> content = contentProvider.createContent(claim);
 
         assertThat(content).containsKeys("hasNegativeDisposableIncome");
         assertThat(content).containsValue("The defendant’s disposable income is -£1."
             + " As such, the court has selected the defendant’s repayment plan.");
+    }
+
+    @Test
+    public void shouldExcludeCourtDeterminationSectionWhenReferToJudgeForCompanyOrOrganisation() {
+
+        Claim claim = SampleClaim.builder()
+            .withResponse(
+                SampleResponse
+                    .PartAdmission
+                    .builder()
+                    .buildWithPaymentOptionInstalmentsAndParty(SampleParty.builder().company()))
+            .withClaimantResponse(ResponseAcceptation
+               .builder()
+               .courtDetermination(
+                   CourtDetermination.builder()
+                       .courtDecision(bySetDate())
+                       .courtPaymentIntention(PaymentIntention.builder()
+                           .paymentOption(BY_SPECIFIED_DATE)
+                           .paymentDate(ResponseAcceptationContentProvider.SYSTEM_MAX_DATE)
+                           .build())
+                       .disposableIncome(BigDecimal.valueOf(-1))
+                       .decisionType(DecisionType.COURT)
+                       .build())
+                .formaliseOption(FormaliseOption.REFER_TO_JUDGE)
+                .build())
+            .build();
+
+        Map<String, Object> content = contentProvider.createContent(claim);
+
+        assertThat(content).doesNotContainKey("courtDetermination");
+    }
+
+    @Test
+    public void shouldCourtDeterminationSectionWhenReferToJudgeForIndividuals() {
+
+        Claim claim = SampleClaim.builder()
+            .withClaimData(SampleClaimData.builder()
+                .withDefendant(SampleTheirDetails.builder().individualDetails())
+                .build())
+            .withResponse(SampleResponse.PartAdmission.builder().buildWithPaymentOptionInstalments())
+            .withClaimantResponse(ResponseAcceptation.builder()
+                .courtDetermination(CourtDetermination.builder()
+                    .courtDecision(bySetDate())
+                    .courtPaymentIntention(SamplePaymentIntention.instalments())
+                    .disposableIncome(BigDecimal.valueOf(10))
+                    .decisionType(DecisionType.COURT)
+                    .build())
+                .formaliseOption(FormaliseOption.REFER_TO_JUDGE)
+                .build())
+            .build();
+
+        Map<String, Object> content = contentProvider.createContent(claim);
+
+        assertThat(content).containsKey("courtDetermination");
     }
 }
