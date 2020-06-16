cd400d8b53968247feb15f67006a39cbfc255608
Dave Oxtoby <dave.oxtoby@gmail.com>
2020-06-16 16:26:30 +0100

    ROC-7993: Framework for rendering template (#1715)
    * ROC-7993: Framework around rendering template
    
    * ROC-7993: BUild template model
    
    * ROC-7939: Use correct deadline and add comment as per review discussion
    
    * ROC-7939: Reorder methods
    
    Co-authored-by: Dave Oxtoby <david.oxtoby@hmcts.net>
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/generalletter/GeneralLetterServiceTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/generalletter/GeneralLetterServiceTest.java
@@ -18,6 +18,8 @@ import uk.gov.hmcts.cmc.claimstore.idam.models.UserDetails;
 import uk.gov.hmcts.cmc.claimstore.services.UserService;
 import uk.gov.hmcts.cmc.claimstore.services.ccd.DocAssemblyService;
 import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.PrintableDocumentService;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.legaladvisor.DocAssemblyTemplateBody;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.legaladvisor.DocAssemblyTemplateBodyMapper;
 import uk.gov.hmcts.cmc.claimstore.services.document.DocumentManagementService;
 import uk.gov.hmcts.cmc.claimstore.services.notifications.fixtures.SampleUserDetails;
 import uk.gov.hmcts.cmc.domain.models.Claim;
@@ -94,6 +96,8 @@ class GeneralLetterServiceTest {
     private Clock clock;
     @Mock
     private UserService userService;
+    @Mock
+    private  DocAssemblyTemplateBodyMapper docAssemblyTemplateBodyMapper;
 
     private GeneralLetterService generalLetterService;
     private UserDetails userDetails;
@@ -105,7 +109,9 @@ class GeneralLetterServiceTest {
             publisher,
             new PrintableDocumentService(documentManagementService),
             clock,
-            userService, documentManagementService);
+            userService,
+            docAssemblyTemplateBodyMapper,
+            documentManagementService);
 
         String documentUrl = DOCUMENT_URI.toString();
         CCDDocument document = new CCDDocument(documentUrl, documentUrl, GENERAL_LETTER_PDF);
@@ -136,17 +142,28 @@ class GeneralLetterServiceTest {
     @Test
     void shouldCreateAndPreviewLetter() {
         when(docAssemblyService
-            .createGeneralLetter(any(CCDCase.class), anyString(), anyString())).thenReturn(docAssemblyResponse);
+            .renderTemplate(any(CCDCase.class), anyString(), anyString(), any(DocAssemblyTemplateBody.class)))
+            .thenReturn(docAssemblyResponse);
+
+        DocAssemblyTemplateBody docAssemblyTemplateBody = DocAssemblyTemplateBody.builder().build();
+        when(docAssemblyTemplateBodyMapper.generalLetterBody(any(CCDCase.class))).thenReturn(docAssemblyTemplateBody);
         when(docAssemblyResponse.getRenditionOutputLocation()).thenReturn(DOC_URL);
+
         generalLetterService.generateLetter(ccdCase, BEARER_TOKEN.name(), GENERAL_LETTER_TEMPLATE_ID);
-        verify(docAssemblyService, once()).createGeneralLetter(eq(ccdCase), eq(BEARER_TOKEN.name()),
-            eq(GENERAL_LETTER_TEMPLATE_ID));
+
+        verify(docAssemblyService, once()).renderTemplate(eq(ccdCase), eq(BEARER_TOKEN.name()),
+            eq(GENERAL_LETTER_TEMPLATE_ID), eq(docAssemblyTemplateBody));
     }
 
     @Test
     void shouldThrowExceptionWhenDocAssemblyFails() {
-        when(docAssemblyService.createGeneralLetter(any(CCDCase.class), anyString(), anyString()))
+        when(docAssemblyService
+            .renderTemplate(any(CCDCase.class), anyString(), anyString(), any(DocAssemblyTemplateBody.class)))
             .thenThrow(new DocumentGenerationFailedException(new RuntimeException("exception")));
+
+        DocAssemblyTemplateBody docAssemblyTemplateBody = DocAssemblyTemplateBody.builder().build();
+        when(docAssemblyTemplateBodyMapper.generalLetterBody(any(CCDCase.class))).thenReturn(docAssemblyTemplateBody);
+
         assertThrows(DocumentGenerationFailedException.class,
             () -> generalLetterService.generateLetter(ccdCase, BEARER_TOKEN.name(),
                 GENERAL_LETTER_TEMPLATE_ID));
@@ -176,6 +193,7 @@ class GeneralLetterServiceTest {
 
         CCDCase updatedCase = generalLetterService
             .publishLetter(ccdCase, claim, BEARER_TOKEN.name(), GENERAL_DOCUMENT_NAME);
+
         verify(documentManagementService, once()).downloadDocument(eq(BEARER_TOKEN.name()), any(ClaimDocument.class));
         assertThat(updatedCase).isEqualTo(expected);
     }
@@ -192,8 +210,13 @@ class GeneralLetterServiceTest {
 
     @Test
     void shouldThrowExceptionWhenPrintAndUpdateCaseDocumentFails() {
-        when(docAssemblyService.createGeneralLetter(any(CCDCase.class), anyString(), anyString()))
+        when(docAssemblyService
+            .renderTemplate(any(CCDCase.class), anyString(), anyString(), any(DocAssemblyTemplateBody.class)))
             .thenThrow(new RuntimeException("exception"));
+
+        DocAssemblyTemplateBody docAssemblyTemplateBody = DocAssemblyTemplateBody.builder().build();
+        when(docAssemblyTemplateBodyMapper.generalLetterBody(any(CCDCase.class))).thenReturn(docAssemblyTemplateBody);
+
         assertThrows(RuntimeException.class,
             () -> generalLetterService.generateLetter(ccdCase, BEARER_TOKEN.name(),
                 GENERAL_LETTER_TEMPLATE_ID));
