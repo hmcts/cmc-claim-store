b5e402ff741e62e4033b0bdd76ddde01b791abba
Andy Walker <andrewwa@kainos.com>
2020-06-19 11:15:45 +0100

    ROC-7497 SendGrid integration (#1717)

--- email-client/src/main/java/uk/gov/hmcts/cmc/email/EmailSendFailedException.java
+++ email-client/src/main/java/uk/gov/hmcts/cmc/email/EmailSendFailedException.java
@@ -2,6 +2,10 @@ package uk.gov.hmcts.cmc.email;
 
 public class EmailSendFailedException extends RuntimeException {
 
+    public EmailSendFailedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
     public EmailSendFailedException(Throwable cause) {
         super(cause);
     }
