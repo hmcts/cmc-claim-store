5471ca1a86053736c402a3e47fdb2b34df46c233
Akriti Sinha <akritis@kainos.com>
2020-06-23 09:07:43 +0100

    ROC-8038 | putting behind feature toggle

--- src/main/java/uk/gov/hmcts/cmc/claimstore/jobs/cron/BulkPrintTransferJob.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/jobs/cron/BulkPrintTransferJob.java
@@ -6,6 +6,7 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.caseworker.transfercase.BulkPrintTransferService;
 import uk.gov.hmcts.cmc.scheduler.model.CronJob;
@@ -13,6 +14,7 @@ import uk.gov.hmcts.cmc.scheduler.model.CronJob;
 @Component
 @Getter
 @DisallowConcurrentExecution
+@ConditionalOnProperty("feature_toggles.ctsc_enabled")
 public class BulkPrintTransferJob implements CronJob {
 
     @Value("${schedule.transfer-claims}")
