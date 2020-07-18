21fc9416ef3123dfef2aba0285598dcb69524ef3
Rachit Saxena <rachit.saxena@hmcts.net>
2020-07-18 13:29:22 +0100

    Feature/roc 7985 add bulk print details to case details (#1805)
    * ROC-7985: Add bulk print details to case
    
    * Resolve all tests
    
    * working code
    
    * more tests resolution
    
    * resolved all tests
    
    * action review comments
    
    * resolve sonar issues
    
    * resolve sonar issues
    
    * resolve failing smoke tests
    
    * updated Junit to accomodate change in method return type
    
    * Updated the tomcat version due to vulnerability issues - CVE-2020-11996
    
    * Fixed contact details change callback handler failing tests
    
    * Fixed broken Junit
    
    * Fixed merging issues
    
    * Updated version to 9.0.37 Fix security vulnerabilities in Apache Tomcat CVE-2020-13935
    
    Co-authored-by: Dharmendra kumar <k_dharmendra@hotmail.com>
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>
    Co-authored-by: yogendraU <66823759+yogendraU@users.noreply.github.com>
    Co-authored-by: Tanveer Singh <tanveer.singh@hmcts.net>
    Co-authored-by: sebing <sebingeo@yahoo.com>

--- src/main/java/uk/gov/hmcts/cmc/claimstore/documents/PrintService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/documents/PrintService.java
@@ -2,11 +2,14 @@ package uk.gov.hmcts.cmc.claimstore.documents;
 
 import uk.gov.hmcts.cmc.claimstore.documents.bulkprint.Printable;
 import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.bulkprint.BulkPrintDetails;
 
 import java.util.List;
 
 public interface PrintService {
-    void print(Claim claim, List<Printable> documents);
+    BulkPrintDetails printHtmlLetter(Claim claim, List<Printable> documents,
+                                     BulkPrintRequestType letterType, String authorisation);
 
-    void printPdf(Claim claim, List<Printable> documents, String letterType);
+    BulkPrintDetails printPdf(Claim claim, List<Printable> documents,
+                              BulkPrintRequestType letterType, String authorisation);
 }
