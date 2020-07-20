606b502bb4d35165b5271da7ff50254692135be0
541839 <sushant.choudhari@hmcts.net>
2020-07-20 15:33:31 +0100

    feat: changes for HWF-Rejection

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/HWFHalfRemissionCallbackHandler.java
@@ -0,0 +1,86 @@
+
+package uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks;
+
+    import com.google.common.collect.ImmutableList;
+    import com.google.common.collect.ImmutableMap;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Service;
+    import uk.gov.hmcts.cmc.ccd.domain.CaseEvent;
+    import uk.gov.hmcts.cmc.ccd.mapper.CaseMapper;
+    import uk.gov.hmcts.cmc.claimstore.services.DirectionsQuestionnaireDeadlineCalculator;
+    import uk.gov.hmcts.cmc.claimstore.services.ccd.Role;
+    import uk.gov.hmcts.cmc.claimstore.utils.CaseDetailsConverter;
+    import uk.gov.hmcts.cmc.domain.models.Claim;
+    import uk.gov.hmcts.cmc.domain.utils.FeaturesUtils;
+    import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+    import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+    import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+
+    import java.time.LocalDate;
+    import java.time.LocalDateTime;
+    import java.util.ArrayList;
+    import java.util.Collections;
+    import java.util.List;
+    import java.util.Map;
+    import java.util.Optional;
+
+    import static uk.gov.hmcts.cmc.claimstore.services.ccd.Role.CASEWORKER;
+
+@Service
+public class HWFHalfRemissionCallbackHandler extends CallbackHandler {
+
+    private static final List<Role> ROLES = Collections.singletonList(CASEWORKER);
+
+    private static final List<CaseEvent> EVENTS = ImmutableList.of(CaseEvent.PART_REMISSION_HWF_REJECTED);
+
+    private final CaseDetailsConverter caseDetailsConverter;
+
+    private final DirectionsQuestionnaireDeadlineCalculator deadlineCalculator;
+
+    private final CaseMapper caseMapper;
+
+    @Autowired
+    public HWFHalfRemissionCallbackHandler(CaseDetailsConverter caseDetailsConverter,
+                                         DirectionsQuestionnaireDeadlineCalculator deadlineCalculator,
+                                         CaseMapper caseMapper) {
+        this.caseDetailsConverter = caseDetailsConverter;
+        this.deadlineCalculator = deadlineCalculator;
+        this.caseMapper = caseMapper;
+    }
+
+    @Override
+    protected Map<CallbackType, Callback> callbacks() {
+        return ImmutableMap.of(
+            CallbackType.ABOUT_TO_SUBMIT, this::updateFeeRemitted
+        );
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    @Override
+    public List<Role> getSupportedRoles() {
+        return ROLES;
+    }
+
+    private CallbackResponse updateFeeRemitted(CallbackParams callbackParams) {
+        CallbackRequest callbackRequest = callbackParams.getRequest();
+
+        Claim claim = caseDetailsConverter.extractClaim(callbackRequest.getCaseDetails());
+        List<String> errors = new ArrayList<>();
+        var responseBuilder = AboutToStartOrSubmitCallbackResponse.builder();
+
+        if (!FeaturesUtils.isOnlineDQ(claim)) {
+            LocalDate deadline = deadlineCalculator.calculate(LocalDateTime.now());
+            claim = claim.toBuilder().directionsQuestionnaireDeadline(deadline).build();
+        }
+
+        Map<String, Object> dataMap = caseDetailsConverter.convertToMap(caseMapper.to(claim));
+
+        responseBuilder.data(dataMap);
+
+        return responseBuilder.build();
+    }
+}
