78e02bb36a60a62cc009bb7e13e9cabeb8f9b493
Thiru <Thirumurugan.Devarajan@cognizant.com>
2020-07-20 17:01:08 +0100

    Fix to remove duplicates introduced from Merge

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/Claim.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/Claim.java
@@ -190,10 +190,6 @@ public class Claim {
         return Optional.ofNullable(issuedOn);
     }
 
-    public Optional<LocalDate> getIssuedOn() {
-        return Optional.ofNullable(issuedOn);
-    }
-
     public Optional<Response> getResponse() {
         return Optional.ofNullable(response);
     }
