518ee05d542cea395557487a28f918cf802528c2
541839 <sushant.choudhari@hmcts.net>
2020-07-20 23:15:03 +0100

    feat: Added Junits

---
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/HWFMoreInfoRemissionCallbackHandlerTest.java
@@ -0,0 +1,87 @@
+package uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.cmc.ccd.domain.CaseEvent;
+import uk.gov.hmcts.cmc.ccd.mapper.CaseMapper;
+import uk.gov.hmcts.cmc.claimstore.idam.models.UserDetails;
+import uk.gov.hmcts.cmc.claimstore.services.ClaimService;
+import uk.gov.hmcts.cmc.claimstore.services.DirectionsQuestionnaireDeadlineCalculator;
+import uk.gov.hmcts.cmc.claimstore.services.notifications.DefendantResponseNotificationService;
+import uk.gov.hmcts.cmc.claimstore.utils.CaseDetailsConverter;
+import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaim;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("HWF Part Remission Callback Handler")
+class HWFMoreInfoRemissionCallbackHandlerTest {
+
+    private static final String AUTHORISATION = "Bearer: aaaa";
+    private HWFMoreInfoRemissionCallbackHandler handler;
+    private CallbackParams callbackParams;
+    private CallbackRequest callbackRequest;
+    @Mock
+    private DirectionsQuestionnaireDeadlineCalculator deadlineCalculator;
+
+    @Mock
+    private CaseMapper caseMapper;
+
+    @Mock
+    private CaseDetailsConverter caseDetailsConverter;
+
+    @Mock
+    private DefendantResponseNotificationService defendantResponseNotificationService;
+
+    @Mock
+    private ClaimService claimService;
+
+
+
+    @BeforeEach
+    public void setUp() {
+        handler = new HWFMoreInfoRemissionCallbackHandler(caseDetailsConverter, deadlineCalculator, caseMapper);
+        callbackRequest = CallbackRequest
+            .builder()
+            .caseDetails(CaseDetails.builder().data(Collections.emptyMap()).build())
+            .eventId(CaseEvent.MORE_INFO_REQUIRED_FOR_HWF.getValue())
+            .build();
+        callbackParams = CallbackParams.builder()
+            .type(CallbackType.ABOUT_TO_SUBMIT)
+            .request(callbackRequest)
+            .params(ImmutableMap.of(CallbackParams.Params.BEARER_TOKEN, AUTHORISATION))
+            .build();
+    }
+
+    @Test
+    void shouldUpdateInfo() {
+        Claim claim = SampleClaim.getClaimWithFullAdmission();
+        when(caseDetailsConverter.extractClaim(any(CaseDetails.class))).thenReturn(claim);
+        Map<String, Object> mappedCaseData = new HashMap<>();
+        mappedCaseData.put("helpWithFeesNumber", 1234);
+        mappedCaseData.put("moreInfoDetails", "Details");
+        when(caseDetailsConverter.convertToMap(caseMapper.to(claim))).thenReturn(mappedCaseData);
+        AboutToStartOrSubmitCallbackResponse response
+            = (AboutToStartOrSubmitCallbackResponse) handler.handle(callbackParams);
+        Map<String, Object> data = response.getData();
+
+        assertThat(data).containsEntry("helpWithFeesNumber", 1234);
+        assertThat(data).containsEntry("moreInfoDetails", "Details");
+
+    }
+
+}
