a2aa5869b4a0b2e11b8874f79628f6dea6f74044
yogendra.upasani <yogendra.upasani@hmcts.net>
2020-07-31 19:34:09 +0100

    Updated smoke tests and userservice

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/UserServiceTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/UserServiceTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.cmc.claimstore.services;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
