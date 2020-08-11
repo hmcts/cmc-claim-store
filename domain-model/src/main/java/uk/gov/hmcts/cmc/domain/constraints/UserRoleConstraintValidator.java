2722f8dfcf5e3690a6f6996b4a7f26411a121832
Thiru <Thirumurugan.Devarajan@cognizant.com>
2020-08-11 12:17:05 +0100

    OIDC Part 2 ITHC - Cross-Site Request Forgery

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/constraints/UserRoleConstraintValidator.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/constraints/UserRoleConstraintValidator.java
@@ -1,12 +1,12 @@
 package uk.gov.hmcts.cmc.domain.constraints;
 
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
 import java.util.Arrays;
 import java.util.List;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
 
 public class UserRoleConstraintValidator implements ConstraintValidator<UserRole, String> {
-    List<String> userRoles = Arrays.asList("cmc-new-features-consent-given","cmc-new-features-consent-not-given");
+    List<String> userRoles = Arrays.asList("cmc-new-features-consent-given", "cmc-new-features-consent-not-given");
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
