b67572d922cf1bfbfa20725fb418c55d879c6d98
tribhuwan-hmcts <tribhuvan.dhapola@hmcts.net>
2020-08-13 19:37:49 +0100

    ROC-7440 adding test related changes

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/transfercase/TransferCaseCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/transfercase/TransferCaseCallbackHandlerTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.caseworker.transfercase;
 
+import com.google.common.collect.ImmutableSet;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,14 +12,21 @@ import uk.gov.hmcts.cmc.ccd.domain.CCDDirectionOrder;
 import uk.gov.hmcts.cmc.ccd.domain.CCDTransferContent;
 import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackParams;
 import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackType;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.legaladvisor.HearingCourt;
+import uk.gov.hmcts.cmc.claimstore.services.pilotcourt.PilotCourtService;
 import uk.gov.hmcts.cmc.claimstore.utils.CaseDetailsConverter;
+import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackType.ABOUT_TO_START;
@@ -28,6 +36,7 @@ import static uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackType.AB
 class TransferCaseCallbackHandlerTest {
 
     private static final String COURT_NAME = "Bristol";
+    private static final String DYNAMIC_LIST_ITEMS = "list_items";
 
     @InjectMocks
     private TransferCaseCallbackHandler handler;
@@ -47,6 +56,9 @@ class TransferCaseCallbackHandlerTest {
     @Mock
     private CCDAddress hearingCourtAddress;
 
+    @Mock
+    private PilotCourtService pilotCourtService;
+
     @Test
     void shouldHandleAboutToStartCallbackTypeForCaseWithoutDirectionOrder() {
 
@@ -54,10 +66,14 @@ class TransferCaseCallbackHandlerTest {
         CCDCase ccdCase = getCase(false);
 
         when(caseDetailsConverter.extractCCDCase(callbackParams.getRequest().getCaseDetails())).thenReturn(ccdCase);
+        when(caseDetailsConverter.extractClaim(callbackRequest.getCaseDetails())).thenReturn(Claim.builder().build());
 
         CallbackResponse response = handler.handle(callbackParams);
-
-        expectedResponse = AboutToStartOrSubmitCallbackResponse.builder().build();
+        Map<String, Object> hearingCourtListDefinition = new HashMap<>();
+        hearingCourtListDefinition.put(DYNAMIC_LIST_ITEMS, new ArrayList<String>());
+        expectedResponse = AboutToStartOrSubmitCallbackResponse.builder()
+            .data(Map.of("hearingCourt", hearingCourtListDefinition))
+            .build();
 
         assertEquals(expectedResponse, response);
     }
@@ -69,6 +85,18 @@ class TransferCaseCallbackHandlerTest {
         CCDCase ccdCase = getCase(true);
 
         when(caseDetailsConverter.extractCCDCase(callbackParams.getRequest().getCaseDetails())).thenReturn(ccdCase);
+        when(caseDetailsConverter.extractClaim(callbackRequest.getCaseDetails())).thenReturn(Claim.builder().build());
+        given(pilotCourtService.getPilotHearingCourts(any(), any()))
+            .willReturn(ImmutableSet.of(HearingCourt.builder()
+                .name("Clerkenwell Court")
+                .address(CCDAddress.builder()
+                    .addressLine1("line1")
+                    .addressLine2("line2")
+                    .postCode("SW1P4BB")
+                    .postTown("Clerkenwell").build())
+                .build()
+            ));
+        given(pilotCourtService.getPilotCourtId(any())).willReturn("COURT_ID");
 
         CallbackResponse response = handler.handle(callbackParams);
 
@@ -93,6 +121,7 @@ class TransferCaseCallbackHandlerTest {
                 .hearingCourtName(COURT_NAME)
                 .hearingCourtAddress(hearingCourtAddress)
                 .build())
+            .hearingCourtName(COURT_NAME)
             .build()
             : mock(CCDCase.class);
     }
--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/transfercase/TransferCasePostProcessorTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/transfercase/TransferCasePostProcessorTest.java
@@ -9,10 +9,14 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.cmc.ccd.domain.CCDAddress;
 import uk.gov.hmcts.cmc.ccd.domain.CCDCase;
 import uk.gov.hmcts.cmc.ccd.domain.CCDTransferContent;
 import uk.gov.hmcts.cmc.ccd.sample.data.SampleData;
+import uk.gov.hmcts.cmc.claimstore.services.DirectionOrderService;
 import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackParams;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.legaladvisor.HearingCourt;
+import uk.gov.hmcts.cmc.claimstore.services.pilotcourt.PilotCourtService;
 import uk.gov.hmcts.cmc.claimstore.utils.CaseDetailsConverter;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
@@ -51,6 +55,12 @@ class TransferCasePostProcessorTest {
     private BulkPrintTransferService bulkPrintTransferService;
 
     @Mock
+    private PilotCourtService pilotCourtService;
+
+    @Mock
+    private DirectionOrderService directionOrderService;
+
+    @Mock
     private CallbackRequest callbackRequest;
 
     @Mock
@@ -81,13 +91,25 @@ class TransferCasePostProcessorTest {
 
         CaseDetails caseDetails = CaseDetails.builder().id(1L).build();
         when(caseDetailsConverter.extractCCDCase(caseDetails)).thenReturn(ccdCase);
-        when(caseDetailsConverter.extractClaim(caseDetails)).thenReturn(claim);
+        when(caseDetailsConverter.extractClaim(caseDetails)).thenReturn(Claim.builder().build());
 
         when(callbackRequest.getCaseDetails()).thenReturn(caseDetails);
 
         when(bulkPrintTransferService.transferCase(any(CCDCase.class), any(Claim.class), any(String.class),
             any(TriFunction.class), any(BiConsumer.class), any(UnaryOperator.class))).thenReturn(ccdCase);
 
+        when(directionOrderService.getHearingCourt(any()))
+            .thenReturn(HearingCourt.builder()
+                .name("Birmingham Court")
+                .address(CCDAddress.builder()
+                    .addressLine1("line1")
+                    .addressLine2("line2")
+                    .addressLine3("line3")
+                    .postCode("SW1P4BB")
+                    .postTown("Birmingham")
+                    .build()
+                )
+                .build());
     }
 
     @Test
