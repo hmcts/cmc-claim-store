ac5f6a674b7dfda826a4b96fa06fc676e9e92c8f
Tanveer Singh <tanveer.singh@hmcts.net>
2020-08-19 13:50:12 +0100

    ROC-7102 - Display user friendly Direction Order file name

--- src/main/java/uk/gov/hmcts/cmc/claimstore/utils/DocumentNameUtils.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/utils/DocumentNameUtils.java
@@ -162,6 +162,14 @@ public class DocumentNameUtils {
         return getFileName(caseRef, "defendant-case-handoff");
     }
 
+    public static String buildLADirectionOrderFileName(String caseRef) {
+        return getFileName(caseRef, "Legal-Adviser-Direction-Order");
+    }
+
+    public static String buildJudgeDirectionOrderFileName(String caseRef) {
+        return getFileName(caseRef, "Judge-Direction-Order");
+    }
+
     private static String getFileName(String caseRef, String fileNameSuffix) {
         requireNonBlank(caseRef);
         return format("%s-%s", caseRef, fileNameSuffix);
