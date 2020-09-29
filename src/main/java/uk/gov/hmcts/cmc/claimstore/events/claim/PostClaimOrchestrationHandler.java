32513eb167089102f869ac3d26b6a3dd37752124
Tanveer Singh <tanveer.singh@hmcts.net>
2020-09-29 11:19:10 +0100

    avoid logging entire claim details

--- src/main/java/uk/gov/hmcts/cmc/claimstore/events/claim/PostClaimOrchestrationHandler.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/events/claim/PostClaimOrchestrationHandler.java
@@ -99,8 +99,10 @@ public class PostClaimOrchestrationHandler {
     @LogExecutionTime
     @EventListener
     public void citizenIssueHandler(CitizenClaimCreatedEvent event) {
+
+        final Claim claim = event.getClaim();
+
         try {
-            Claim claim = event.getClaim();
             String authorisation = event.getAuthorisation();
 
             UnaryOperator<Claim> doPinOperation = c -> generatePinOperation.perform(c, event);
@@ -124,7 +126,7 @@ public class PostClaimOrchestrationHandler {
                 );
             }
         } catch (Exception e) {
-            logger.error("Claim Issue - Failed operation processing for event {}", event, e);
+            logger.error("Claim Issue - Failed operation processing for claim: {}", claim.getReferenceNumber(), e);
         }
     }
 
