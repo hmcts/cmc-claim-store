32b431e6b799e2dca5001b14847fe1a67c3c99f0
pradhap87 <prathap.mathiyalagan@hmcts.net>
2020-09-30 17:25:05 +0100

    Update SolicitorPdfTest.java

--- src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/functional/solicitor/SolicitorPdfTest.java
+++ src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/functional/solicitor/SolicitorPdfTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.cmc.claimstore.tests.functional.solicitor;
 
 import junit.framework.AssertionFailedError;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import uk.gov.hmcts.cmc.claimstore.tests.functional.BasePdfTest;
