65ebaca58780657b3318b233b5e7408cea9f4210
yogendraupasani <yogendra.upasani@hmcts.net>
2020-11-16 14:54:44 +0000

    Merge from s2s-token branch

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/ioc/CreateCitizenClaimCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/ioc/CreateCitizenClaimCallbackHandler.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.ioc;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,8 +22,6 @@ import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackType;
 import uk.gov.hmcts.cmc.claimstore.utils.CaseDetailsConverter;
 import uk.gov.hmcts.cmc.domain.models.ChannelType;
 import uk.gov.hmcts.cmc.domain.models.Claim;
-import uk.gov.hmcts.cmc.domain.models.Payment;
-import uk.gov.hmcts.cmc.domain.models.PaymentStatus;
 import uk.gov.hmcts.cmc.domain.utils.LocalDateTimeFactory;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
@@ -35,7 +32,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import static java.lang.String.format;
 import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.CREATE_CITIZEN_CLAIM;
 import static uk.gov.hmcts.cmc.claimstore.services.ccd.Role.CITIZEN;
 import static uk.gov.hmcts.cmc.domain.utils.LocalDateTimeFactory.nowInLocalZone;
@@ -103,26 +99,12 @@ public class CreateCitizenClaimCallbackHandler extends CallbackHandler {
             callbackParams.getType(),
             claim.getExternalId());
         String authorisation = callbackParams.getParams().get(CallbackParams.Params.BEARER_TOKEN).toString();
-        Payment payment = paymentsService.retrievePayment(authorisation, claim.getClaimData())
-            .orElseThrow(() -> new IllegalStateException(format(
-                "Claim with external id %s has no payment record",
-                claim.getExternalId()))
-            );
-
-        if (payment.getStatus() != PaymentStatus.SUCCESS) {
-            logger.info("Payment not successful for claim with external id {}", claim.getExternalId());
-
-            return AboutToStartOrSubmitCallbackResponse.builder()
-                .errors(ImmutableList.of("Payment not successful"))
-                .build();
-        }
-
+        logger.info("Payment not marked successful for claim with external id {}", claim.getExternalId());
         LocalDate issuedOn = issueDateCalculator.calculateIssueDay(nowInLocalZone());
 
         Claim updatedClaim = claim.toBuilder()
             .channel(ChannelType.CITIZEN)
             .claimData(claim.getClaimData().toBuilder()
-                .payment(payment)
                 .build())
             .referenceNumber(referenceNumberRepository.getReferenceNumberForCitizen())
             .createdAt(LocalDateTimeFactory.nowInUTC())
