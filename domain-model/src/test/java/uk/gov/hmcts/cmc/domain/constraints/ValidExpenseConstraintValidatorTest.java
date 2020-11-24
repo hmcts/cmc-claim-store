24967d2f7ef39d32e658b3193c290926c502f974
Shashi Kumar <66266334+shashisk97@users.noreply.github.com>
2020-11-24 09:36:59 +0000

    code smell fix (#2026)
    * code smell fix-1
    
    * code smell fix-2
    
    * code smell fix-3
    
    * code smell fix-3
    
    * code smell fix-4
    
    * code coverage-1
    
    * code coverage-2
    
    * code smell fix-4
    
    * code smell fix- 5
    
    * build issue fix
    
    * Junit-5 code smell fix
    
    * code smell fix - 6
    
    * code smell fix-7
    
    * code smell fix-8

--- domain-model/src/test/java/uk/gov/hmcts/cmc/domain/constraints/ValidExpenseConstraintValidatorTest.java
+++ domain-model/src/test/java/uk/gov/hmcts/cmc/domain/constraints/ValidExpenseConstraintValidatorTest.java
@@ -1,13 +1,20 @@
 package uk.gov.hmcts.cmc.domain.constraints;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.cmc.domain.models.statementofmeans.Expense;
+import uk.gov.hmcts.cmc.domain.models.statementofmeans.PaymentFrequency;
 
 import javax.validation.ConstraintValidatorContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ValidExpenseConstraintValidatorTest {
@@ -15,10 +22,54 @@ public class ValidExpenseConstraintValidatorTest {
     @Mock
     private ConstraintValidatorContext validatorContext;
 
-    private final ValidEmploymentConstraintValidator validator = new ValidEmploymentConstraintValidator();
+    private final ValidExpenseConstraintValidator validator = new ValidExpenseConstraintValidator();
+
+    @Before
+    public void setUp() {
+        ConstraintValidatorContext.ConstraintViolationBuilder builder = mock(
+            ConstraintValidatorContext.ConstraintViolationBuilder.class
+        );
+
+        when(builder.addPropertyNode(anyString()))
+            .thenReturn(
+                mock(ConstraintValidatorContext.ConstraintViolationBuilder.NodeBuilderCustomizableContext.class)
+            );
+
+        when(validatorContext.buildConstraintViolationWithTemplate(any())).thenReturn(builder);
+    }
+
+    @Test
+    public void shouldBeValidWhenPayemtnFrequencyIsPopulated() {
+        Expense model = Expense.builder()
+            .frequency(PaymentFrequency.FOUR_WEEKS)
+            .build();
+        assertThat(validator.isValid(model, validatorContext)).isTrue();
+    }
+
+    @Test
+    public void shouldBeValidWhenExpenseTypeOtherIsPopulated() {
+        Expense model = Expense.builder()
+            .type(Expense.ExpenseType.OTHER).otherName("abc").build();
+        assertThat(validator.isValid(model, validatorContext)).isTrue();
+    }
+
+    @Test
+    public void shouldBeInValidWhenExpenseTypeOtherIsNotPopulated() {
+        Expense model = Expense.builder()
+            .type(Expense.ExpenseType.OTHER).build();
+        assertThat(validator.isValid(model, validatorContext)).isFalse();
+    }
+
+    @Test
+    public void shouldBeInValidWhenOtherExpenseTypeWithOtherNameIsNotPopulated() {
+        Expense model = Expense.builder()
+            .type(Expense.ExpenseType.COUNCIL_TAX).otherName("abc").build();
+        assertThat(validator.isValid(model, validatorContext)).isFalse();
+    }
 
     @Test
     public void shouldBeValidWhenNull() {
         assertThat(validator.isValid(null, validatorContext)).isTrue();
     }
+
 }
