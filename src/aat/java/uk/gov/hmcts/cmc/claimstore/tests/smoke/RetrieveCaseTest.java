78690952eba91d0db5b2b27818c2f5eb0b572ead
Thiru <Thirumurugan.Devarajan@cognizant.com>
2020-12-04 14:11:16 +0000

    update smoke test for ES

--- src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/smoke/RetrieveCaseTest.java
+++ src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/smoke/RetrieveCaseTest.java
@@ -25,7 +25,8 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.cmc.claimstore.repositories.CCDCaseApi.CASE_TYPE_ID;
 
 @RunWith(MockitoJUnitRunner.class)
