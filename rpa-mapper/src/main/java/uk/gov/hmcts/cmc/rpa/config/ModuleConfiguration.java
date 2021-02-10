4d19326dead1ba833a720b8c2f359e3d2b7180ef
Rachit Saxena <rachit.saxena@hmcts.net>
2021-02-10 21:33:29 +0000

    undo changes

--- rpa-mapper/src/main/java/uk/gov/hmcts/cmc/rpa/config/ModuleConfiguration.java
+++ rpa-mapper/src/main/java/uk/gov/hmcts/cmc/rpa/config/ModuleConfiguration.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"uk.gov.hmcts.cmc"})
+@ComponentScan(basePackages = {"uk.gov.hmcts.cmc.rpa"})
 public class ModuleConfiguration {
 }
