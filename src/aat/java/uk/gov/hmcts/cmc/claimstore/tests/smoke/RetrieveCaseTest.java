ba7ff410a1ddbb07d323fe7308abbf7cc2419dcb
yogendraupasani <yogendra.upasani@hmcts.net>
2021-02-16 23:15:18 +0000

    Fix Unit tests

--- src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/smoke/RetrieveCaseTest.java
+++ src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/smoke/RetrieveCaseTest.java
@@ -23,6 +23,8 @@ import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.SearchResult;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyString;
@@ -30,12 +32,15 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.cmc.claimstore.repositories.CCDCaseApi.CASE_TYPE_ID;
+import static uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaim.USER_ID;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RetrieveCaseTest extends BaseTest {
 
     private static final String AUTHORISATION = "Bearer: aaa";
     private static final String SERVICE_AUTH = "Authorization";
+    public static final String SUBMITTER_SURNAME = "Smith";
+    public static final String SUBMITTER_FORENAME = "Steven";
     private static final JsonMapper jsonMapper = JsonMapperFactory.create();
     private static final UserDetails USER_DETAILS = SampleUserDetails.builder()
         .withRoles(Role.CITIZEN.getRole())
@@ -80,6 +85,51 @@ public class RetrieveCaseTest extends BaseTest {
         ccdElasticSearchRepository.getClaimsForDefendant(SampleClaim.USER_ID, user, 0);
         verify(coreCaseDataApi).searchCases(AUTHORISATION, SERVICE_AUTH, CASE_TYPE_ID,
             SampleQueryConstants.getClaimCountForDefendant);
+    }
+
+    private static class SampleUserDetails {
+        private String userId = USER_ID;
+        private String userEmail = "user@example.com";
+        private String forename = SUBMITTER_FORENAME;
+        private String surname = SUBMITTER_SURNAME;
+        private List<String> roles = Collections.singletonList("citizen");
+
+        public static SampleUserDetails builder() {
+            return new SampleUserDetails();
+        }
+
+        public SampleUserDetails withMail(String userEmail) {
+            this.userEmail = userEmail;
+            return this;
+        }
+
+        public SampleUserDetails withUserId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public SampleUserDetails withForename(String forename) {
+            this.forename = forename;
+            return this;
+        }
+
+        public SampleUserDetails withSurname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public SampleUserDetails withRoles(String... roles) {
+            this.roles = Arrays.asList(roles);
+            return this;
+        }
+
+        public UserDetails build() {
+            return new UserDetails(userId, userEmail, forename, surname, roles);
+        }
+
+        public static UserDetails getDefault() {
+            return SampleUserDetails.builder().build();
+        }
     }
 
     /* commenting this out as currently there is no ES service running to validate this. Once ES service is
