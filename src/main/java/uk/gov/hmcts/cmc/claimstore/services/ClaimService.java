b289190919ce6c5c8e6b127c5b6e60fc56863ac1
yogendraupasani <yogendra.upasani@hmcts.net>
2021-02-19 15:42:54 +0000

    ROC-8893 - remove anonmymous user call

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/ClaimService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ClaimService.java
@@ -37,7 +37,6 @@ import uk.gov.hmcts.cmc.domain.utils.LocalDateTimeFactory;
 import uk.gov.hmcts.cmc.launchdarkly.LaunchDarklyClient;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -107,21 +106,9 @@ public class ClaimService {
     }
 
     public List<Claim> getClaimBySubmitterId(String submitterId, String authorisation, Integer pageNumber) {
-        List<Claim> outputClaimList = new ArrayList<>();
         claimAuthorisationRule.assertUserIdMatchesAuthorisation(submitterId, authorisation);
         List<Claim> claimList = caseRepository.getBySubmitterId(submitterId, authorisation, pageNumber);
-        User user = userService.authenticateAnonymousCaseWorker();
-        claimList.stream().forEach(claim -> {
-            if (claim.getState().equals(ClaimState.AWAITING_RESPONSE_HWF)
-                || claim.getState().equals(ClaimState.HWF_APPLICATION_PENDING)) {
-                List<CaseEvent> caseEventList = caseEventService.findEventsForCase(
-                    String.valueOf(claim.getId()), user);
-                claim = claim.toBuilder().lastEventTriggeredForHwfCase(caseEventList.get(0).getValue()).build();
-            }
-            outputClaimList.add(claim);
-
-        });
-        return outputClaimList;
+        return claimList;
     }
 
     public Claim getClaimByLetterHolderId(String id, String authorisation) {
@@ -137,13 +124,6 @@ public class ClaimService {
     public Claim getFilteredClaimByExternalId(String externalId, String authorisation) {
         User user = userService.getUser(authorisation);
         Claim claim = getClaimByExternalId(externalId, user);
-        User anonymousCaseWorkerForFetchingEvents = userService.authenticateAnonymousCaseWorker();
-        if (claim.getState().equals(ClaimState.AWAITING_RESPONSE_HWF)
-            || claim.getState().equals(ClaimState.HWF_APPLICATION_PENDING)) {
-            List<CaseEvent> caseEventList = caseEventService.findEventsForCase(
-                String.valueOf(claim.getId()), anonymousCaseWorkerForFetchingEvents);
-            claim = claim.toBuilder().lastEventTriggeredForHwfCase(caseEventList.get(0).getValue()).build();
-        }
 
         return DocumentsFilter.filterDocuments(
             claim, user.getUserDetails(), ctscEnabled,
