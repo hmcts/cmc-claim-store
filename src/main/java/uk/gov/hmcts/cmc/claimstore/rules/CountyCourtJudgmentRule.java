d3d3da7e943106ec158f24661cac27922a1bfda6
Rachit Saxena <rachit.saxena@hmcts.net>
2021-03-24 10:56:07 +0000

    ROC-8758 changes

--- src/main/java/uk/gov/hmcts/cmc/claimstore/rules/CountyCourtJudgmentRule.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/rules/CountyCourtJudgmentRule.java
@@ -3,7 +3,9 @@ package uk.gov.hmcts.cmc.claimstore.rules;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.cmc.claimstore.exceptions.ForbiddenActionException;
+import uk.gov.hmcts.cmc.domain.models.BreathingSpace;
 import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.cmc.domain.models.ClaimData;
 import uk.gov.hmcts.cmc.domain.models.CountyCourtJudgmentType;
 import uk.gov.hmcts.cmc.domain.models.offers.StatementType;
 import uk.gov.hmcts.cmc.domain.models.response.PaymentIntention;
@@ -11,6 +13,8 @@ import uk.gov.hmcts.cmc.domain.models.response.PaymentIntention;
 import java.time.LocalDate;
 import javax.validation.constraints.NotNull;
 
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.cmc.claimstore.utils.CommonErrors.MISSING_OFFER;
 import static uk.gov.hmcts.cmc.claimstore.utils.CommonErrors.MISSING_PAYMENT_INTENTION;
@@ -21,9 +25,11 @@ import static uk.gov.hmcts.cmc.domain.utils.LocalDateTimeFactory.nowInLocalZone;
 @Service
 public class CountyCourtJudgmentRule {
 
-    private final ClaimDeadlineService claimDeadlineService;
     private static final String CLAIM_OBJECT_CANNOT_BE_NULL = "claim object can not be null";
     private static final String COUNTY_COURT_JUDGMENT_FOR_CLAIM = "County Court Judgment for the claim ";
+    private static final String COUNTY_COURT_JUDGMENT_FOR_BREATHING_SPACE_CLAIM
+        = "Breathing Space Entered County Court Judgment for the claim ";
+    private final ClaimDeadlineService claimDeadlineService;
 
     @Autowired
     public CountyCourtJudgmentRule(ClaimDeadlineService claimDeadlineService) {
@@ -121,6 +127,30 @@ public class CountyCourtJudgmentRule {
 
         }
 
+        return false;
+    }
+
+    public void assertCountyCourtJudgmentCannotBeRequested(@NotNull Claim claim) {
+
+        requireNonNull(claim, CLAIM_OBJECT_CANNOT_BE_NULL);
+
+        if (isBreathingSpaceEntered(claim)) {
+            throw new ForbiddenActionException(COUNTY_COURT_JUDGMENT_FOR_BREATHING_SPACE_CLAIM + claim.getExternalId()
+                + " cannot be submitted");
+        }
+
+    }
+
+    private boolean isBreathingSpaceEntered(Claim claim) {
+        BreathingSpace breathingSpace;
+        ClaimData claimData = claim.getClaimData();
+        if (claimData.getBreathingSpace().isPresent()) {
+            breathingSpace = claimData.getBreathingSpace().get();
+            if (nonNull(breathingSpace.getBsEnteredDate())
+                && isNull(breathingSpace.getBsLiftedDate())) {
+                return true;
+            }
+        }
         return false;
     }
 }
