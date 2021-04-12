31d4e83545bc56337e04255cd8e19bdfa3d26bb9
yogendraU <66823759+yogendraU@users.noreply.github.com>
2021-04-12 10:45:04 +0100

    Roc 8751 breathing space entered (#2273)
    * Bumping chart version
    
    * ROC-8812 and ROC-8813 Breathing Space Lifted Changes
    
    * ROC-8812 and ROC-8813 Breathing Space Lifted Changes
    
    * ROC-8812 and ROC-8813 Breathing Space Lifted Changes
    
    * ROC-8812 and ROC-8813 Breathing Space Lifted Changes
    
    * ROC-8812 and ROC-8813 Breathing Space Lifted Changes
    
    * ROC-8812 and ROC-8813 Breathing Space Lifted Changes
    
    * ROC-8812 and ROC-8813 Breathing Space Lifted Changes
    
    * ROC-8812 and ROC-8813 Breathing Space Lifted Changes
    
    * ROC-8812 and ROC-8813 Breathing Space Lifted Changes
    
    * ROC-8812 and ROC-8813 Breathing Space Lifted Changes
    
    Co-authored-by: Jenkins <nobody@nowhere>
    Co-authored-by: hmcts-jenkins-cmc <62421507+hmcts-jenkins-cmc[bot]@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/breathingspace/BreathingSpaceLiftedCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/breathingspace/BreathingSpaceLiftedCallbackHandler.java
@@ -129,7 +129,18 @@ public class BreathingSpaceLiftedCallbackHandler extends CallbackHandler {
 
     private void validateBreathingSpaceDetails(CCDCase ccdCase, CallbackType callbackType) {
         CCDBreathingSpace breathingSpace = ccdCase.getBreathingSpace();
-        if (breathingSpace != null) {
+        if (callbackType.equals(CallbackType.ABOUT_TO_START)
+            && (ccdCase.getState().equals(ClaimState.TRANSFERRED.getValue())
+            || ccdCase.getState().equals(ClaimState.BUSINESS_QUEUE.getValue())
+            || ccdCase.getState().equals(ClaimState.HWF_APPLICATION_PENDING.getValue())
+            || ccdCase.getState().equals(ClaimState.SETTLED.getValue())
+            || ccdCase.getState().equals(ClaimState.CLOSED.getValue())
+            || ccdCase.getState().equals(ClaimState.PROCEEDS_IN_CASE_MAN.getValue())
+            || ccdCase.getState().equals(ClaimState.AWAITING_RESPONSE_HWF.getValue())
+            || ccdCase.getState().equals(ClaimState.CLOSED_HWF.getValue()))) {
+            validationMessage = "This Event cannot be triggered "
+                + "since the claim is no longer part of the online civil money claims journey";
+        } else if (breathingSpace != null) {
             if (breathingSpace.getBsLiftedDateByInsolvencyTeam() != null) {
                 if (breathingSpace.getBsLiftedDateByInsolvencyTeam().isAfter(LocalDate.now())) {
                     validationMessage = "The end date must not be after today's date";
@@ -145,20 +156,7 @@ public class BreathingSpaceLiftedCallbackHandler extends CallbackHandler {
                 validationMessage = "Breathing Space is already lifted for this claim";
             }
         } else {
-            if (callbackType.equals(CallbackType.ABOUT_TO_START)
-                && (ccdCase.getState().equals(ClaimState.TRANSFERRED.getValue())
-                || ccdCase.getState().equals(ClaimState.BUSINESS_QUEUE.getValue())
-                || ccdCase.getState().equals(ClaimState.HWF_APPLICATION_PENDING.getValue())
-                || ccdCase.getState().equals(ClaimState.SETTLED.getValue())
-                || ccdCase.getState().equals(ClaimState.CLOSED.getValue())
-                || ccdCase.getState().equals(ClaimState.PROCEEDS_IN_CASE_MAN.getValue())
-                || ccdCase.getState().equals(ClaimState.AWAITING_RESPONSE_HWF.getValue())
-                || ccdCase.getState().equals(ClaimState.CLOSED_HWF.getValue()))) {
-                validationMessage = "This Event cannot be triggered "
-                    + "since the claim is no longer part of the online civil money claims journey";
-            } else {
-                validationMessage = "This claim is still not entered into Breathing space";
-            }
+            validationMessage = "This claim is still not entered into Breathing space";
         }
     }
 
