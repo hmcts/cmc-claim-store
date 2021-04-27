45bbd97313d38ecd94ea2e890550526a2ef1020a
Rachit Saxena <rachit.saxena@hmcts.net>
2021-04-27 11:08:27 +0100

    Feature/roc 9034 ocon disable n9 form (#2304)
    * ROC-9034
    
    * ocon changes
    
    * ROC-9034 changes
    
    * ROC-9034 changes
    
    * ROC-9034 changes
    
    * added LD toggle code for 9034
    
    * sonar code fixes
    
    * sonar code fixes
    
    * resolved merged conflicts

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/paperresponsetests/IssuePaperDefenceCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/paperresponsetests/IssuePaperDefenceCallbackHandlerTest.java
@@ -40,10 +40,12 @@ import uk.gov.hmcts.reform.docassembly.exception.DocumentGenerationFailedExcepti
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
@@ -58,6 +60,9 @@ class IssuePaperDefenceCallbackHandlerTest {
     private static final String CLAIMANT_ISSUED_CCJ =
         "OCON9x form cannot be sent out as CCJ already issued by claimant.";
     private static final String AUTHORISATION = "auth";
+    private static final boolean DISABLEN9FORM = false;
+    private static final boolean OCON_FLAG = false;
+    private static final List<CaseEvent> EVENTS = Arrays.asList(CaseEvent.ISSUE_PAPER_DEFENSE_FORMS);
 
     @Mock
     private CaseDetailsConverter caseDetailsConverter;
@@ -112,6 +117,7 @@ class IssuePaperDefenceCallbackHandlerTest {
     @Test
     void shouldHandleAboutToSubmitCallback() {
         ccdCase = CCDCase.builder()
+            .issuedOn(LocalDate.now())
             .previousServiceCaseReference("000MC001")
             .respondents(ImmutableList.of(
                 CCDCollectionElement.<CCDRespondent>builder()
@@ -145,7 +151,7 @@ class IssuePaperDefenceCallbackHandlerTest {
         issuePaperDefenceCallbackHandler.handle(callbackParams);
 
         verify(documentPublishService).publishDocuments(any(CCDCase.class), any(Claim.class), eq(AUTHORISATION),
-            eq(date));
+            eq(date), eq(DISABLEN9FORM), eq(OCON_FLAG));
         verify(issuePaperResponseNotificationService).notifyClaimant(any(Claim.class));
     }
 
@@ -189,7 +195,7 @@ class IssuePaperDefenceCallbackHandlerTest {
 
         issuePaperDefenceCallbackHandler.handle(callbackParams);
         verify(documentPublishService).publishDocuments(any(CCDCase.class), any(Claim.class), eq(AUTHORISATION),
-            eq(LocalDate.now()));
+            eq(LocalDate.now()), any(Boolean.class), any(Boolean.class));
         verify(issuePaperResponseNotificationService, times(0)).notifyClaimant(any(Claim.class));
     }
 
@@ -222,7 +228,7 @@ class IssuePaperDefenceCallbackHandlerTest {
 
         issuePaperDefenceCallbackHandler.handle(callbackParams);
         verify(documentPublishService).publishDocuments(any(CCDCase.class), any(Claim.class), eq(AUTHORISATION),
-            eq(LocalDate.now()));
+            eq(LocalDate.now()), any(Boolean.class), any(Boolean.class));
         verify(issuePaperResponseNotificationService).notifyClaimant(any(Claim.class));
     }
 
@@ -246,7 +252,8 @@ class IssuePaperDefenceCallbackHandlerTest {
             .thenReturn(date);
         when(caseDetailsConverter.extractCCDCase(any(CaseDetails.class))).thenReturn(ccdCase);
         when(caseDetailsConverter.extractClaim(any(CaseDetails.class))).thenReturn(claim);
-        when(documentPublishService.publishDocuments(ccdCase, claim, AUTHORISATION, LocalDate.now()))
+        when(documentPublishService.publishDocuments(ccdCase, claim, AUTHORISATION, LocalDate.now(),
+            DISABLEN9FORM, OCON_FLAG))
             .thenThrow(DocumentGenerationFailedException.class);
         AboutToStartOrSubmitCallbackResponse actualResponse =
             (AboutToStartOrSubmitCallbackResponse) issuePaperDefenceCallbackHandler.handle(callbackParams);
@@ -342,6 +349,12 @@ class IssuePaperDefenceCallbackHandlerTest {
     @Test
     void shouldHaveCorrectCaseworkerRole() {
         assertThat(issuePaperDefenceCallbackHandler.getSupportedRoles()).containsOnly(CASEWORKER);
+    }
+
+    @Test
+    void handledEvents() {
+        List<CaseEvent> caseEventList = issuePaperDefenceCallbackHandler.handledEvents();
+        assertEquals(EVENTS, caseEventList);
     }
 
     @Nested
