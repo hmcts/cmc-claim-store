2b2d76a942322b0da4dda2000f1bde19a3c1ac51
Thiru <Thirumurugan.Devarajan@cognizant.com>
2021-05-14 17:33:24 +0100

    fix minor code smells

--- src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/services/staff/PaidInFullStaffNotificationServiceWithEmailServiceRetryTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/services/staff/PaidInFullStaffNotificationServiceWithEmailServiceRetryTest.java
@@ -1,12 +1,8 @@
 package uk.gov.hmcts.cmc.claimstore.services.staff;
 
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.MailSendException;
@@ -15,15 +11,9 @@ import uk.gov.hmcts.cmc.claimstore.BaseMockSpringTest;
 import uk.gov.hmcts.cmc.claimstore.events.claim.PostClaimOrchestrationHandler;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaim;
-import uk.gov.hmcts.cmc.email.EmailService;
-import uk.gov.hmcts.cmc.email.sendgrid.SendGridClient;
-import uk.gov.hmcts.cmc.email.sendgrid.SendGridFactory;
 
-import java.io.IOException;
-import java.lang.ref.ReferenceQueue;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Collections;
 import java.util.Properties;
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
@@ -38,20 +28,13 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 //As no more java mail sender in place and sendgrid covers this.
-
+@Ignore
 public class PaidInFullStaffNotificationServiceWithEmailServiceRetryTest extends BaseMockSpringTest {
 
     private static final byte[] PDF_CONTENT = {1, 2, 3, 4};
-    private static final String API_KEY = "dummy key";
 
-    @Mock
-    private SendGrid sendGrid;
-
-    @Mock
-    private SendGridFactory factory;
-
-    @Mock
-    private SendGridClient sendGridClient;
+    @MockBean
+    private JavaMailSenderImpl javaMailSender;
 
     @MockBean
     protected PostClaimOrchestrationHandler postClaimOrchestrationHandler;
@@ -62,13 +45,12 @@ public class PaidInFullStaffNotificationServiceWithEmailServiceRetryTest extends
     @Before
     public void beforeEachTest() {
         when(pdfServiceClient.generateFromHtml(any(byte[].class), anyMap())).thenReturn(PDF_CONTENT);
-        when(factory.createSendGrid(API_KEY, true)).thenReturn(sendGrid);
     }
 
     @Test
-    public void shouldSendEmailWithExpectedContentPaidInFull() throws IOException {
+    public void shouldSendEmailWithExpectedContentPaidInFull() {
         MimeMessage msg = new MimeMessage(Session.getDefaultInstance(new Properties(), null));
-        when(sendGrid.api(any(Request.class))).thenReturn(new Response(202, "response body", Collections.emptyMap()));
+        when(javaMailSender.createMimeMessage()).thenReturn(msg);
 
         Claim claimWithPaidInFull = SampleClaim.builder()
             .withMoneyReceivedOn(LocalDate.parse("01/12/2025", DateTimeFormatter.ofPattern("dd/MM/yyyy")))
@@ -76,10 +58,10 @@ public class PaidInFullStaffNotificationServiceWithEmailServiceRetryTest extends
 
         service.notifyPaidInFull(claimWithPaidInFull);
 
-        verify(sendGrid).api(any(Request.class));
+        verify(javaMailSender).send(any(MimeMessage.class));
     }
 
-    /*@Test
+    @Test
     public void shouldRetryOnFailure() {
         given(javaMailSender.createMimeMessage())
             .willThrow(new MailSendException("first time"))
@@ -99,5 +81,5 @@ public class PaidInFullStaffNotificationServiceWithEmailServiceRetryTest extends
             eq(singletonMap("EmailSubject", "Paid in Full 000MC001: John Rambo v Dr. John Smith")),
             eq(null)
         );
-    }*/
+    }
 }
