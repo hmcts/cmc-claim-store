5423e0e39d5571176c13cce01700fff66b49d482
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-06-30 14:26:29 +0100

    update

--- security.sh
+++ security.sh
@@ -2,7 +2,6 @@
 export TEST_URL=http://cmc-claim-store-aat.service.core-compute-aat.internal
 echo ${TEST_URL}
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL
-cat zap.out
 echo "ZAP has successfully started"
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 export LC_ALL=C.UTF-8
