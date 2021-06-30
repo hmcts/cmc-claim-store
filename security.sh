ec7c6c42904b82c47a5312c6f7c1c4f60e47262d
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-06-30 14:07:18 +0100

    update zap

--- security.sh
+++ security.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+export TEST_URL=http://cmc-claim-store-aat.service.core-compute-aat.internal
 echo ${TEST_URL}
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-zap-api-scan.py -t ${TEST_URL}v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL
 cat zap.out
 echo "ZAP has successfully started"
+curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
+zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
 mkdir -p functional-output
 chmod a+wx functional-output
-curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 cp /zap/api-report.html functional-output/
-zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
