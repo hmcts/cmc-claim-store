8deb8edbb85dd30f37ff45a25a84b6abd6399db6
Aloknath Datta <alokdatta.uit@gmail.com>
2021-07-08 14:11:37 +0100

    High level data set up

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/befta/HighLevelDataSetupApp.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.cmc.claimstore.befta;
+
+import uk.gov.hmcts.befta.dse.ccd.CcdEnvironment;
+import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
+
+public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
+
+    public HighLevelDataSetupApp(CcdEnvironment dataSetupEnvironment) {
+        super(dataSetupEnvironment);
+    }
+
+    public static void main(String[] args) throws Throwable {
+        main(HighLevelDataSetupApp.class, args);
+    }
+
+    @Override
+    protected boolean shouldTolerateDataSetupFailure() {
+        return true;
+    }
+}
