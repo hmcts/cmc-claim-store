2a500010fa7ea47a2b2a68193cf0e0fc8d44d114
Ben Neill <benjaminrneill@gmail.com>
2021-07-29 09:04:58 +0100

    remove postgres9 resource

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -138,20 +138,20 @@ resource "azurerm_key_vault_secret" "sendgrid_api_key-2" {
   value        = data.azurerm_key_vault_secret.send_grid_api_key.value
 }
 
-module "database" {
-  source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product = "${var.product}"
-  location = "${var.location}"
-  env = "${var.env}"
-  postgresql_user = "cmc"
-  database_name = "${var.database-name}"
-  postgresql_version = "${var.postgresql_version}"
-  sku_name = "${var.database_sku_name}"
-  sku_tier = "GeneralPurpose"
-  storage_mb = "${var.database_storage_mb}"
-  common_tags = "${var.common_tags}"
-  subscription = "${var.subscription}"
-}
+#module "database" {
+#  source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+#  product = "${var.product}"
+#  location = "${var.location}"
+#  env = "${var.env}"
+#  postgresql_user = "cmc"
+#  database_name = "${var.database-name}"
+#  postgresql_version = "${var.postgresql_version}"
+#  sku_name = "${var.database_sku_name}"
+#  sku_tier = "GeneralPurpose"
+#  storage_mb = "${var.database_storage_mb}"
+#  common_tags = "${var.common_tags}"
+#  subscription = "${var.subscription}"
+#}
 
 // DB version 11
 module "database-v11" {
