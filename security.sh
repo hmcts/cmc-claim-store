be3cee5b2d6701b58a666ca82c9c92e922de813d
satya <1426759+satyachundur@users.noreply.github.com>
2021-08-17 12:44:43 +0100

    Update security.sh

--- security.sh
+++ security.sh
@@ -4,4 +4,4 @@ echo ${TEST_URL}
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL -J report.json -r api-report.html
 mkdir -p functional-output
 chmod a+wx functional-output
-cp /zap/api-report.html functional-output/
+cp api-report.html functional-output/
