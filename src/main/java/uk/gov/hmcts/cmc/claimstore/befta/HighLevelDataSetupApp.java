cf753a6ca15de05050877b7e6fe44c823b43b197
Aloknath Datta <alokdatta.uit@gmail.com>
2021-08-23 13:37:53 +0100

    removed dependency

--- src/main/java/uk/gov/hmcts/cmc/claimstore/befta/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/befta/HighLevelDataSetupApp.java
@@ -6,7 +6,6 @@ import uk.gov.hmcts.befta.dse.ccd.CcdEnvironment;
 import uk.gov.hmcts.befta.dse.ccd.CcdRoleConfig;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
 
-import java.util.List;
 import java.util.Locale;
 
 public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
@@ -37,27 +36,5 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
     @Override
     protected boolean shouldTolerateDataSetupFailure() {
         return true;
-    }
-
-    @Override
-    public void addCcdRoles() {
-        for (CcdRoleConfig roleConfig : CCD_ROLES_NEEDED_FOR_CMC) {
-            try {
-                logger.info("\n\nAdding CCD Role {}.", roleConfig);
-                addCcdRole(roleConfig);
-                logger.info("\n\nAdded CCD Role {}.", roleConfig);
-            } catch (Exception e) {
-                logger.error("\n\nCouldn't add CCD Role {} - Exception: {}.\n\n", roleConfig, e);
-                if (!shouldTolerateDataSetupFailure()) {
-                    throw e;
-                }
-            }
-        }
-    }
-
-    @Override
-    protected List<String> getAllDefinitionFilesToLoadAt(String definitionsPath) {
-        String environmentName = environment.name().toLowerCase(Locale.UK);
-        return List.of(String.format("build/ccd-release-config/cmc-ccd-%s.xlsx", environmentName));
     }
 }
