a58a43e9efb886d2f044f113615ab2766c6ba89d
Aloknath Datta <alokdatta.uit@gmail.com>
2021-08-23 13:30:15 +0100

    Befta setup only

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/befta/HighLevelDataSetupApp.java
@@ -0,0 +1,63 @@
+package uk.gov.hmcts.cmc.claimstore.befta;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.hmcts.befta.dse.ccd.CcdEnvironment;
+import uk.gov.hmcts.befta.dse.ccd.CcdRoleConfig;
+import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
+
+import java.util.List;
+import java.util.Locale;
+
+public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
+
+    private static final Logger logger = LoggerFactory.getLogger(HighLevelDataSetupApp.class);
+
+    private final CcdEnvironment environment;
+
+    private static final CcdRoleConfig[] CCD_ROLES_NEEDED_FOR_CMC = {
+        new CcdRoleConfig("caseworker", "PUBLIC"),
+        new CcdRoleConfig("caseworker-cmc", "PUBLIC"),
+    };
+
+    public HighLevelDataSetupApp(CcdEnvironment dataSetupEnvironment) {
+        super(dataSetupEnvironment);
+        environment = dataSetupEnvironment;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        if (CcdEnvironment.valueOf(args[0].toUpperCase(Locale.UK)).equals(CcdEnvironment.PROD)) {
+            logger.info("Environment is Production");
+            return;
+        }
+        logger.info("Environment is :" + CcdEnvironment.valueOf(args[0].toUpperCase(Locale.UK)));
+        main(HighLevelDataSetupApp.class, args);
+    }
+
+    @Override
+    protected boolean shouldTolerateDataSetupFailure() {
+        return true;
+    }
+
+    @Override
+    public void addCcdRoles() {
+        for (CcdRoleConfig roleConfig : CCD_ROLES_NEEDED_FOR_CMC) {
+            try {
+                logger.info("\n\nAdding CCD Role {}.", roleConfig);
+                addCcdRole(roleConfig);
+                logger.info("\n\nAdded CCD Role {}.", roleConfig);
+            } catch (Exception e) {
+                logger.error("\n\nCouldn't add CCD Role {} - Exception: {}.\n\n", roleConfig, e);
+                if (!shouldTolerateDataSetupFailure()) {
+                    throw e;
+                }
+            }
+        }
+    }
+
+    @Override
+    protected List<String> getAllDefinitionFilesToLoadAt(String definitionsPath) {
+        String environmentName = environment.name().toLowerCase(Locale.UK);
+        return List.of(String.format("build/ccd-release-config/cmc-ccd-%s.xlsx", environmentName));
+    }
+}
