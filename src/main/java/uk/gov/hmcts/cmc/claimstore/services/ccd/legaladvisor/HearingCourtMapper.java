fa47034aa108f572da4d9d1f9c157ceb56bdf24d
Shashi Kumar <66266334+shashisk97@users.noreply.github.com>
2021-09-22 19:28:04 +0100

    Feature/roc 9406 court finder lookup changes (#2497)
    * Court finder api changes
    
    * changing the url
    
    * changing the url
    
    * ccd api changes
    
    * ccd api changes
    
    * ccd api changes
    
    * To handle both API response
    
    * Changing to http bec of certificate issue
    
    * FACT URL updated. Preparing for release.
    
    Co-authored-by: shashis97 <shashikumar.sannakariyappa@cognizant.com>
    Co-authored-by: Aloknath Datta <alokdatta.uit@gmail.com>

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/legaladvisor/HearingCourtMapper.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/legaladvisor/HearingCourtMapper.java
@@ -5,14 +5,25 @@ import uk.gov.hmcts.cmc.ccd.domain.CCDAddress;
 import uk.gov.hmcts.cmc.claimstore.courtfinder.models.Address;
 import uk.gov.hmcts.cmc.claimstore.courtfinder.models.Court;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class HearingCourtMapper {
 
     public HearingCourt from(Court court) {
         return HearingCourt.builder()
             .name(court.getName())
-            .address(mapHearingAddress(court.getAddress()))
+            .address(mapHearingAddress(court.getAddress() != null
+                ? court.getAddress() : fetchAddress(court.getAddresses())))
             .build();
+    }
+
+    private Address fetchAddress(List<Address> addresses) {
+        Optional<Address> address = addresses.stream()
+            .filter(a -> a.getType().equals("Write to us"))
+            .findFirst();
+        return address.orElse(addresses.get(0));
     }
 
     private CCDAddress mapHearingAddress(Address address) {
