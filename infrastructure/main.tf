134d83cd08fc4039370c15405fc5ce6cfa3257ee
n-clarke-777 <94960571+n-clarke-777@users.noreply.github.com>
2022-02-07 16:03:26 +0000

    Civ 1166 resolve terraform infrastructure warnings (#2538)
    * CIV-1166 : terraform type constraints no longer require quotes around string; therefore removed
    
    * CIV-1166: updated non-constant expressions to be provided via interpolation syntax
    
    * CIV-1166: updated non-constant expressions to be provided via interpolation syntax

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,114 +7,114 @@ locals {
 }
 
 data "azurerm_key_vault" "cmc_key_vault" {
-  name = "${local.vaultName}"
-  resource_group_name = "${local.vaultName}"
+  name = local.vaultName
+  resource_group_name = local.vaultName
 }
 
 data "azurerm_key_vault_secret" "notify_api_key" {
   name = "notify-api-key"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "s2s_secret" {
   name = "claim-store-s2s-secret"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "db_password" {
   name = "claim-store-db-password"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "staff_email" {
   name = "staff-email"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "staff_email_legal_rep" {
   name = "staff-email-legal-rep"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "live_support_email" {
   name = "live-support-email"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "milo_recipient" {
   name = "milo-recipient"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "rpa_email_sealed_claim" {
   name = "rpa-email-sealed-claim"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "rpa-email-breathing-space" {
   name = "rpa-email-breathing-space"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "rpa_email_more_time_requested" {
   name = "rpa-email-more-time-requested"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "rpa_email_defence_response" {
   name = "rpa-email-response"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "rpa_email_ccj" {
   name = "rpa-email-ccj"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "rpa_email_paid_in_full" {
   name = "rpa-email-paid-in-full"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "anonymous_caseworker_username" {
   name = "anonymous-caseworker-username"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "anonymous_caseworker_password" {
   name = "anonymous-caseworker-password"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "system_update_username" {
   name = "system-update-username"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "system_update_password" {
   name = "system-update-password"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "oauth_client_secret" {
   name = "citizen-oauth-client-secret"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "launch_darkly_sdk_key" {
   name = "launchDarkly-sdk-key"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "sendgrid_api_key" {
   name = "sendgrid-api-key"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "cmc-db-password" {
   name      = "cmc-db-password"
-  value     = "${module.database.postgresql_password}"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  value     = module.database.postgresql_password
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault" "send_grid" {
@@ -140,17 +140,17 @@ resource "azurerm_key_vault_secret" "sendgrid_api_key-2" {
 
 module "database" {
   source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product = "${var.product}"
-  location = "${var.location}"
-  env = "${var.env}"
+  product = var.product
+  location = var.location
+  env = var.env
   postgresql_user = "cmc"
-  database_name = "${var.database-name}"
-  postgresql_version = "${var.postgresql_version}"
-  sku_name = "${var.database_sku_name}"
+  database_name = var.database-name
+  postgresql_version = var.postgresql_version
+  sku_name = var.database_sku_name
   sku_tier = "GeneralPurpose"
-  storage_mb = "${var.database_storage_mb}"
-  common_tags = "${var.common_tags}"
-  subscription = "${var.subscription}"
+  storage_mb = var.database_storage_mb
+  common_tags = var.common_tags
+  subscription = var.subscription
 }
 
 // DB version 11
@@ -158,21 +158,21 @@ module "database-v11" {
   source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
   product = "${var.product}-db-v11"
   name  = "cmc-db-v11"
-  location = "${var.location}"
-  env = "${var.env}"
+  location = var.location
+  env = var.env
   postgresql_user = "cmc"
-  database_name = "${var.database-name}"
+  database_name = var.database-name
   postgresql_version = "11"
-  sku_name = "${var.database_sku_name}"
+  sku_name = var.database_sku_name
   sku_tier = "GeneralPurpose"
-  storage_mb = "${var.database_storage_mb}"
-  common_tags = "${var.common_tags}"
-  subscription = "${var.subscription}"
+  storage_mb = var.database_storage_mb
+  common_tags = var.common_tags
+  subscription = var.subscription
 }
 
 resource "azurerm_key_vault_secret" "cmc-db-password-v11" {
   name      = "cmc-db-password-v11"
-  value     = "${module.database-v11.postgresql_password}"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  value     = module.database-v11.postgresql_password
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
