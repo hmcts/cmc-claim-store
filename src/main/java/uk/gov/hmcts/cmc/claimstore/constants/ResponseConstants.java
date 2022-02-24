f0569b78a65bb654759ede4c7bb88723342466e4
n-clarke-777 <nathan.clarke@HMCTS.net>
2022-02-24 16:58:42 +0000

    CIV-1378 : removed checkstyle warnings

--- src/main/java/uk/gov/hmcts/cmc/claimstore/constants/ResponseConstants.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/constants/ResponseConstants.java
@@ -2,9 +2,12 @@ package uk.gov.hmcts.cmc.claimstore.constants;
 
 import lombok.experimental.UtilityClass;
 
-/** Constants class to store response messages */
+/**
+ * Constants class to store response messages.
+ */
 @UtilityClass
 public class ResponseConstants {
 
-    public static final String UNPROCESSABLE_ENTITY_UNRECOGNISED_DEADLINE_TYPE = "Unprocessable Entity Unrecognised Deadline Type";
+    public static final String UNPROCESSABLE_ENTITY_UNRECOGNISED_DEADLINE_TYPE
+        = "Unprocessable Entity Unrecognised Deadline Type";
 }
