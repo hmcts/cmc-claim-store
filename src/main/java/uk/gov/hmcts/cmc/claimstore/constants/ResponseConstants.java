8963b749440a2fdf66a76cedafab560c65ac7bdf
n-clarke-777 <94960571+n-clarke-777@users.noreply.github.com>
2022-02-25 10:10:25 +0000

    CIV-1378 : resolved cross-site scripting vulnerability with deadline … (#2554)
    * CIV-1378 : resolved cross-site scripting vulnerability with deadline support
    
    * CIV-1378 : resolved cross-site scripting vulnerability with deadline support
    
    * CIV-1378 : removed checkstyle warnings

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/constants/ResponseConstants.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.cmc.claimstore.constants;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Constants class to store response messages.
+ */
+@UtilityClass
+public class ResponseConstants {
+
+    public static final String UNPROCESSABLE_ENTITY_UNRECOGNISED_DEADLINE_TYPE
+        = "Unprocessable Entity Unrecognised Deadline Type";
+}
