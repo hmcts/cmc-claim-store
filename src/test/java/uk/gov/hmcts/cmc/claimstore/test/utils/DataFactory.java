089f319a081e7f5fff16c5546d3ed5828f15d844
n-clarke-777 <nathan.clarke@HMCTS.net>
2022-04-01 09:25:26 +0100

    CIV-1799 : added the ability to generate test data from JSON

--- src/test/java/uk/gov/hmcts/cmc/claimstore/test/utils/DataFactory.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/test/utils/DataFactory.java
@@ -3,12 +3,8 @@ package uk.gov.hmcts.cmc.claimstore.test.utils;
 import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.experimental.UtilityClass;
 import uk.gov.hmcts.cmc.ccd.domain.*;
-import uk.gov.hmcts.cmc.ccd.domain.defendant.CCDRespondent;
-import uk.gov.hmcts.cmc.ccd.sample.data.SampleData;
 import uk.gov.hmcts.cmc.claimstore.services.ccd.legaladvisor.HearingCourt;
-import uk.gov.hmcts.cmc.domain.models.ClaimFeatures;
 
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -17,17 +13,7 @@ import java.util.Set;
 @UtilityClass
 public class DataFactory {
 
-    public static CourtFinderResponse createCourtFinderResponseFromJson(String jsonFileSrc) {
-        return JsonParserUtils.fromJson(jsonFileSrc, new TypeReference<>() {
-        });
-    }
-
     public static Set<String> createStringSetFromJson(String jsonFileSrc) {
-        return JsonParserUtils.fromJson(jsonFileSrc, new TypeReference<>() {
-        });
-    }
-
-    public static CourtDetails createCourtDetailsFromJson(String jsonFileSrc) {
         return JsonParserUtils.fromJson(jsonFileSrc, new TypeReference<>() {
         });
     }
@@ -40,29 +26,6 @@ public class DataFactory {
     public static CCDCase createCCDCitizenCaseFromJson(String jsonFileSrc) {
         return JsonParserUtils.fromJson(jsonFileSrc, new TypeReference<>() {
         });
-    }
-
-    public static CCDCase getCCDData(String postcode, CCDCase.CCDCaseBuilder builder, CCDPartyType company) {
-        return builder
-            .features(ClaimFeatures.DQ_FLAG.getValue())
-            .respondents(List.of(CCDCollectionElement.<CCDRespondent>builder()
-                .value(CCDRespondent.builder()
-                    .partyDetail(CCDParty.builder()
-                        .primaryAddress(CCDAddress.builder().postCode(postcode).build())
-                        .build())
-                    .claimantProvidedDetail(CCDParty.builder()
-                        .emailAddress("abc@def.com")
-                        .type(company)
-                        .build())
-                    .build())
-                .build()))
-            .applicants(
-                com.google.common.collect.ImmutableList.of(
-                    CCDCollectionElement.<CCDApplicant>builder()
-                        .value(SampleData.getCCDApplicantIndividual())
-                        .build()
-                ))
-            .build();
     }
 
 }
