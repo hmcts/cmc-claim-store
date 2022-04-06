f0094aa6fe88bfa5dbd7652cf1df4a740aa31502
n-clarke-777 <nathan.clarke@HMCTS.net>
2022-04-06 16:11:43 +0100

    CIV-1168 : update hearing duration type

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/orders/HearingDurationType.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/orders/HearingDurationType.java
@@ -4,9 +4,9 @@ public enum HearingDurationType {
     ONE_DAY("One day"),
     FOUR_HOURS("Four hours"),
     THREE_HOURS("Three hours"),
-    TWO_AND_HALF_HOURS("Two and a half hours"),
+    TWO_AND_A_HALF_HOURS("Two and a half hours"),
     TWO_HOURS("Two hours"),
-    ONE_AND_HALF_HOURS("One and a half hours"),
+    ONE_AND_A_HALF_HOURS("One and a half hours"),
     ONE_HOUR("One hour"),
     HALF_HOUR("30 minutes");
 
