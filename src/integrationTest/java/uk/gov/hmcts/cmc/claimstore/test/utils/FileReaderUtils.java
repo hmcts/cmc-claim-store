f6924e0f4b21f38b33b1ac913aca3133ddf7af08
n-clarke-777 <94960571+n-clarke-777@users.noreply.github.com>
2022-04-07 13:06:36 +0100

    Civ 1197 court finder update (#2574)
    * CIV-1197 : updated court finder logic
    
    * CIV-1197 : updated unit tests for new fact find court finder
    
    * CIV-1197 : update integration tests for new fact find court finder & minor refactor
    
    * Bumping chart version
    
    * CIV-1197 : updated gradle build
    
    * CIV-1197 : temp suppressed dependency vulnerabilities
    
    * CIV-1197 : update integration tests for new fact find court finder
    
    * CIV-1197 : update unit tests for new fact find court finder
    
    Co-authored-by: hmcts-jenkins-cft <93778408+hmcts-jenkins-cft[bot]@users.noreply.github.com>

---
+++ src/integrationTest/java/uk/gov/hmcts/cmc/claimstore/test/utils/FileReaderUtils.java
@@ -0,0 +1,50 @@
+package uk.gov.hmcts.cmc.claimstore.test.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * A utility class that aids in the reading of file contents.
+ */
+class FileReaderUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileReaderUtils.class);
+
+    private FileReaderUtils() {
+        throw new RuntimeException();
+    }
+
+    /**
+     * Reads the contents of a JSON file and stores it as a string.
+     *
+     * @param jsonFile for a provided json file
+     * @return {@linkplain String} json file contents as string
+     */
+    static String readJsonFromFile(String jsonFile) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            BufferedReader bufferedReader =
+                new BufferedReader(
+                    new InputStreamReader(
+                        new ClassPathResource(jsonFile).getInputStream()
+                    )
+                );
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            logger.error(String.valueOf(e));
+        }
+
+        return stringBuilder.toString();
+    }
+}
