4c55a0ad78daa0c1bffb351bc3cb6779226630e4
n-clarke-777 <94960571+n-clarke-777@users.noreply.github.com>
2022-04-12 15:47:00 +0100

    CIV-1168 : manual rollback (#2577)
    * CIV-1168 : manual rollback
    
    * CIV-1168 : manual rollback

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/legaladvisor/CCDHearingDurationType.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/legaladvisor/CCDHearingDurationType.java
@@ -4,9 +4,9 @@ public enum CCDHearingDurationType {
     ONE_DAY,
     FOUR_HOURS,
     THREE_HOURS,
-    TWO_AND_A_HALF_HOURS,
     TWO_HOURS,
-    ONE_AND_A_HALF_HOURS,
-    ONE_HOUR,
     HALF_HOUR,
+    ONE_AND_HALF_HOUR,
+    ONE_HOUR,
+    TWO_AND_HALF_HOURS
 }
