e72584364e0f0ff7edb1516bcad101e4c6cce202
zikrurR <workzikr@gmail.com>
2022-06-30 14:43:50 +0100

    commit 1b773ec (#2597)

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/staff/models/HearingContent.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/staff/models/HearingContent.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.cmc.claimstore.services.staff.models;
 
 import lombok.Builder;
 import lombok.Value;
+import uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.VulnerabilityQuestions;
 import uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.Witness;
 
 import java.util.List;
@@ -22,4 +23,5 @@ public class HearingContent {
     String evidenceRequired;
     Witness witness;
     List<String> unavailableDates;
+    VulnerabilityQuestions vulnerabilityQuestions;
 }
