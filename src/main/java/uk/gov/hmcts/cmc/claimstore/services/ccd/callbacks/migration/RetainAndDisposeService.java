6c4b52eda01d51c57afba5554af0e636e2c43a7b
Sabah Irfan <sabah.irfan@gmail.com>
2022-07-05 00:03:27 +0100

    clean up calculate ttl method

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/migration/RetainAndDisposeService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/migration/RetainAndDisposeService.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.migration;
 
+import com.google.common.collect.Iterables;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.cmc.ccd.domain.CCDClaimTTL;
@@ -11,6 +12,7 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.CaseEventDetail;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -38,33 +40,36 @@ public class RetainAndDisposeService {
         STAY_CLAIM, 2190, CCJ_REQUESTED, 2281,
         PAPER_HAND_OFF, 2190, TRANSFER, 1095, PROCEEDS_IN_CASEMAN, 2190, TRANSFER_TO_CCBC, 2190,
         DEFAULT_CCJ_REQUESTED, 2281, MEDIATION_SUCCESSFUL, 2190);
-    List<CaseEventDetail> timeToLiveEventsOnCase;
 
     @Autowired
     public RetainAndDisposeService(CaseEventService caseEventService, UserService userService) {
         this.caseEventService = caseEventService;
-        this.userService =  userService;
+        this.userService = userService;
     }
 
     public CCDClaimTTL calculateTTL(CaseDetails caseDetails, String authorisation) {
         User user = userService.getUser(authorisation);
         String ccdCaseId = String.valueOf(caseDetails.getId());
         List<CaseEventDetail> caseEventDetails = caseEventService.getEventDetailsForCase(ccdCaseId, user);
-        // TODO: Traverse through these events and calculate the appropriate TTL value.
+        // Traverse through these events and calculate the appropriate TTL value.
+        List<CaseEventDetail> timeToLiveEventsOnCase = Collections.emptyList();
         for (CaseEventDetail event : caseEventDetails) {
-            if (event.getEventName().contains((CharSequence)timeToLiveEvent)) {
+            if (event.getEventName().contains((CharSequence) timeToLiveEvent)) {
                 timeToLiveEventsOnCase.add(event);
             }
         }
-        //TTLEventsOnCase.sort(Comparator.comparing(CaseEventDetail::getCreatedDate));
-        var index = timeToLiveEventsOnCase.size() - 1;
-        LocalDate eventDate = timeToLiveEventsOnCase.get(index).getCreatedDate().toLocalDate();
-        CaseEvent eventName = CaseEvent.valueOf(timeToLiveEventsOnCase.get(index).getEventName());
-        var ttlIncrement = eventMapTimeToLive.get(eventName);
+        // TTLEventsOnCase.sort(Comparator.comparing(CaseEventDetail::getCreatedDate));
+        CCDClaimTTL.CCDClaimTTLBuilder builder = CCDClaimTTL.builder();
+        if (timeToLiveEventsOnCase.size() > 0) {
+            var lastEvent = Iterables.getLast(timeToLiveEventsOnCase);
+            LocalDate eventDate = lastEvent.getCreatedDate().toLocalDate();
+            CaseEvent eventName = CaseEvent.valueOf(lastEvent.getEventName());
+            var ttlIncrement = eventMapTimeToLive.getOrDefault(eventName, 0);
+            if (ttlIncrement > 0) {
+                builder.overrideTTL(eventDate.plusDays(ttlIncrement));
+            }
+        }
 
-        // TODO: calculate TTL value for the provided case based on the event history.
-        return CCDClaimTTL.builder()
-            .OverrideTTL(eventDate.plusDays(ttlIncrement)) // TODO: Set this value
-            .build();
+        return builder.build();
     }
 }
