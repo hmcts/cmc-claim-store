358795ce7eb911d52369b5d46ef8f52187d7af3a
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2022-08-02 10:37:25 +0100

    Update CCDDocument.java (#2609)
    * Update CCDDocument.java
    
    * Update CCDDocument.java
    
    * Update Chart.yaml
    
    * Bumping chart version/ fixing aliases
    
    Co-authored-by: hmcts-jenkins-a-to-c <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>

--- ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CCDDocument.java
+++ ccd-adapter/src/main/java/uk/gov/hmcts/cmc/ccd/domain/CCDDocument.java
@@ -1,12 +1,14 @@
 package uk.gov.hmcts.cmc.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder(toBuilder = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CCDDocument {
 
     @JsonProperty("document_url")
