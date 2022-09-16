50733e21c0747b65767847849b3d63be30ebb4a8
Sabah u Din Irfan <sabah.irfan@gmail.com>
2022-09-16 17:28:50 +0100

    Fix deprecation warnings (#2634)

--- src/main/java/uk/gov/hmcts/cmc/claimstore/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/config/JacksonConfiguration.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
@@ -18,13 +19,18 @@ public class JacksonConfiguration {
     @Bean
     @Primary
     public ObjectMapper objectMapper() {
-        return new ObjectMapper()
+        ObjectMapper objectMapper = JsonMapper
+            .builder()
             .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
-            .registerModule(new Jdk8Module())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .serializationInclusion(JsonInclude.Include.NON_EMPTY)
+            .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS).build();
+
+        objectMapper.registerModule(new Jdk8Module())
             .registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES))
             .registerModule(new JavaTimeModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-            .setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
-            .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+            .setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        return objectMapper;
     }
 
 }
