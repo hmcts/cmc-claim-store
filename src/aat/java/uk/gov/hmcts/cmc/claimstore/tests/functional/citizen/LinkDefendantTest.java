0971a5097b07bc38e8c644da58868adec9b3804f
zikrurR <workzikr@gmail.com>
2022-09-30 16:22:00 +0100

    fix space

--- src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/functional/citizen/LinkDefendantTest.java
+++ src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/functional/citizen/LinkDefendantTest.java
@@ -25,7 +25,7 @@ public class LinkDefendantTest extends BaseTest {
     @Before
     public void before() {
         claimant = bootstrap.getClaimant();
-        logger.info("Retrieving claimant details {}",claimant);
+        logger.info("Retrieving claimant details {}", claimant);
     }
 
     @Rule
