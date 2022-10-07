f5ed54c04832b22669a571a0553fb182648d5654
Sabah Irfan <sabah.irfan@gmail.com>
2022-10-07 16:29:21 +0100

    Fix sonar issue

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/CoreCaseDataServiceFailureTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/CoreCaseDataServiceFailureTest.java
@@ -318,6 +318,7 @@ public class CoreCaseDataServiceFailureTest {
             any(CaseDataContent.class)
         );
     }
+
     @Test(expected = CoreCaseDataStoreException.class)
     public void linkSealedClaimDocumentFailure() {
 
