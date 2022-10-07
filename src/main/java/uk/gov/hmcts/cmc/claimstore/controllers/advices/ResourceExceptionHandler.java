c5177c5455be502a5885e146d544e0858db9d238
Maciej Modzelewski <18092228+maciejmodzelewski@users.noreply.github.com>
2022-10-07 12:38:16 +0100

    Fix/civ 4990 post callbackcontroller callback http 500 (#2649)
    * New exception added
    
    * Additional exception handlers added
    
    * Try-catch block added
    
    The original exception caused by key duplication caught
    and rethrown as a custom exception to handle in
    exception handler.
    
    * Additional constructor added
    
    * Test for the duplicate key exception in email method
    
    Since the email method is private, the test checks if catch block works indirectly via handle method.
    
    * Additional exception handlers tested
    
    * Reverted result of automatic import optimisation in IntelliJ
    
    * Reverted result of automatic import optimisation in IntelliJ
    
    * Wrong lexicographical order in imports fixed

--- src/main/java/uk/gov/hmcts/cmc/claimstore/controllers/advices/ResourceExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/controllers/advices/ResourceExceptionHandler.java
@@ -25,12 +25,14 @@ import uk.gov.hmcts.cmc.claimstore.exceptions.ClaimantLinkException;
 import uk.gov.hmcts.cmc.claimstore.exceptions.ConflictException;
 import uk.gov.hmcts.cmc.claimstore.exceptions.DefendantLinkingException;
 import uk.gov.hmcts.cmc.claimstore.exceptions.DocumentDownloadForbiddenException;
+import uk.gov.hmcts.cmc.claimstore.exceptions.DuplicateKeyException;
 import uk.gov.hmcts.cmc.claimstore.exceptions.ForbiddenActionException;
 import uk.gov.hmcts.cmc.claimstore.exceptions.InvalidApplicationException;
 import uk.gov.hmcts.cmc.claimstore.exceptions.NotFoundException;
 import uk.gov.hmcts.cmc.claimstore.exceptions.OnHoldClaimAccessAttemptException;
 import uk.gov.hmcts.cmc.domain.exceptions.BadRequestException;
 import uk.gov.hmcts.cmc.domain.exceptions.IllegalSettlementStatementException;
+import uk.gov.hmcts.cmc.domain.exceptions.NotificationException;
 
 import java.util.List;
 import java.util.Optional;
@@ -197,5 +199,17 @@ public class ResourceExceptionHandler {
         return ResponseEntity
             .status(exc.status())
             .body(new ExceptionForClient(exc.status(), errorMessage));
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<String> duplicateKeyException(DuplicateKeyException exception) {
+        logger.debug(exception);
+        return new ResponseEntity<>(exception.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(NotificationException.class)
+    public ResponseEntity<String> notificationException(NotificationException exception) {
+        logger.debug(exception);
+        return new ResponseEntity<>(exception.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 }
