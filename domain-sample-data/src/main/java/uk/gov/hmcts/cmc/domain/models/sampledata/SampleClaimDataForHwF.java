7aa5dc28039bcfad4a3524ded7223cbdad19e14c
zikrurR <zikrur.rahman@hmcts.net>
2022-10-20 11:58:41 +0100

    Feat/civ 818 (#2657)
    * update to claim mapper
    
    * add hwfMoreInfoNeededDocuments
    
    * modify test data
    
    * fix checkstyle

--- domain-sample-data/src/main/java/uk/gov/hmcts/cmc/domain/models/sampledata/SampleClaimDataForHwF.java
+++ domain-sample-data/src/main/java/uk/gov/hmcts/cmc/domain/models/sampledata/SampleClaimDataForHwF.java
@@ -53,6 +53,7 @@ public class SampleClaimDataForHwF {
     private String hwfFeeDetailsSummary = "Summary";
     private String hwfMandatoryDetails = "Details";
     private List<String> hwfMoreInfoNeededDocuments = asList("BANK_STATEMENTS", "PRISONERS_INCOME");
+    private String hwfProvideDocumentName = "Sample document name";
     private LocalDate hwfDocumentsToBeSentBefore;
     private HousingDisrepair housingDisrepair = new HousingDisrepair(
         DamagesExpectation.MORE_THAN_THOUSAND_POUNDS,
@@ -236,6 +237,11 @@ public class SampleClaimDataForHwF {
         return this;
     }
 
+    public SampleClaimDataForHwF withHwfProvideDocumentName(String hwfProvideDocumentName) {
+        this.hwfProvideDocumentName = hwfProvideDocumentName;
+        return this;
+    }
+
     public SampleClaimDataForHwF withHwfDocumentsToBeSentBefore(LocalDate hwfDocumentsToBeSentBefore) {
         this.hwfDocumentsToBeSentBefore = hwfDocumentsToBeSentBefore;
         return this;
@@ -269,6 +275,7 @@ public class SampleClaimDataForHwF {
             hwfFeeDetailsSummary,
             hwfMandatoryDetails,
             hwfMoreInfoNeededDocuments,
+            hwfProvideDocumentName,
             hwfDocumentsToBeSentBefore,
             breathingSpace);
     }
