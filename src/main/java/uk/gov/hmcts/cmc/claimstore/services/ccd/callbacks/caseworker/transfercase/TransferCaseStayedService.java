ae66bf8ffdcdf03d6c876d0dcc2bd3851bf7a1bb
mfallonhmcts <114912573+mfallonhmcts@users.noreply.github.com>
2022-12-05 17:32:12 +0000

    Update TransferCaseStayedService.java

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/transfercase/TransferCaseStayedService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/transfercase/TransferCaseStayedService.java
@@ -58,7 +58,7 @@ public class TransferCaseStayedService {
                 ? pageNumber : 1
         );
 
-        LocalDate currentDate = LocalDate.parse("2023-01-09");
+        LocalDate currentDate = LocalDate.parse("2023-01-11");
 
         JSONArray listOfCasesJson = !listOfCases.isEmpty()
             ? new JSONArray(listOfCases) : null;
