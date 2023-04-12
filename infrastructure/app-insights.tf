4ebece5ca10a067ec376761a91b921f9086a2d27
Sabah Irfan <sabah.irfan@gmail.com>
2023-04-12 14:31:14 +0100

    CIV-7814: Upgrade to Java 17

---
+++ infrastructure/app-insights.tf
@@ -0,0 +1,5 @@
+resource "azurerm_key_vault_secret" "app_insights_connection_string" {
+  name         = "app-insights-connection-string"
+  value        = azurerm_application_insights.appinsights.connection_string
+  key_vault_id = module.key-vault.key_vault_id
+}
