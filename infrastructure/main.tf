1a53ada5cebcf233722463999fc9b016577eb53d
v1ferrarij <94917970+v1ferrarij@users.noreply.github.com>
2023-04-26 11:58:16 +0100

    add ai connection string

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -176,3 +176,13 @@ resource "azurerm_key_vault_secret" "cmc-db-password-v11" {
   key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
+data "azurerm_application_insights" "cmc" {
+  name                = "${var.product}-${var.env}"
+  resource_group_name = "${var.product}-${var.env}"
+}
+
+resource "azurerm_key_vault_secret" "appinsights_connection_string" {
+  name         = "appinsights-connection-string"
+  value        = data.azurerm_application_insights.cmc.connection_string
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
+}
