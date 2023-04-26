efe0606fc85ef55056c7ef7b436fbd3bd25ba00e
v1ferrarij <94917970+v1ferrarij@users.noreply.github.com>
2023-04-26 10:42:20 +0100

    Update app-insights.tf

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -6,5 +6,5 @@ data "azurerm_application_insights" "cmc" {
 resource "azurerm_key_vault_secret" "appinsights_connection_string" {
   name         = "appinsights-connection-string"
   value        = data.azurerm_application_insights.cmc.connection_string
-  key_vault_id = data.azurerm_key_Vault.cmc_key_vault.id
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
