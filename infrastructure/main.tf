fee7b9e201e318fac8f72c70c955a397e770d2b8
v1ferrarij <94917970+v1ferrarij@users.noreply.github.com>
2023-04-26 10:43:45 +0100

    remove ai and kv

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -137,26 +137,7 @@ resource "azurerm_key_vault_secret" "sendgrid_api_key-2" {
   name         = "sendgrid-api-key-2"
   value        = data.azurerm_key_vault_secret.send_grid_api_key.value
 }
-
-data "azurerm_user_assigned_identity" "rpe-shared-identity" {
-  name                = "rpe-shared-${var.env}-mi"
-  resource_group_name = "managed-identities-${var.env}-rg"
-}
-
-module "key-vault" {
-  source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  product             = var.product
-  env                 = var.env
-  tenant_id           = var.tenant_id
-  object_id           = var.jenkins_AAD_objectId
-  resource_group_name = azurerm_resource_group.rg.name
-
-  # dcd_cc-dev group object ID
-  product_group_object_id     = "38f9dea6-e861-4a50-9e73-21e64f563537"
-  common_tags                 = var.common_tags
-  managed_identity_object_ids = ["${data.azurerm_user_assigned_identity.rpe-shared-identity.principal_id}"]
-}
-
+  
 module "database" {
   source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
   product = var.product
@@ -193,27 +174,4 @@ resource "azurerm_key_vault_secret" "cmc-db-password-v11" {
   name      = "cmc-db-password-v11"
   value     = module.database-v11.postgresql_password
   key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.component}-${var.env}"
-  location = var.location
-  tags = var.common_tags
-}
-
-resource "azurerm_application_insights" "appinsights" {
-  name                = "${var.product}-${var.component}-appinsights-${var.env}"
-  location            = var.appinsights_location
-  resource_group_name = azurerm_resource_group.rg.name
-  application_type    = "web"
-
-  tags = var.common_tags
-
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
-      # destroys and re-creates this appinsights instance
-      application_type,
-    ]
-  }
 }
