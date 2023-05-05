172b7f6e0f12c0e961d98def4ff0ad9fb49a45f0
JackDevN <94063348+JackDevN@users.noreply.github.com>
2023-05-05 17:06:04 +0100

    CIV-7830: Fix WebSecurityConfigurerAdapter Deprecated (#2735)
    * CIV-7830: Fix WebSecurityConfigurerAdapter Deprecated
    
    * CIV-7830: remove unused imports
    
    * Update src/main/java/uk/gov/hmcts/cmc/claimstore/config/SecurityConfiguration.java
    
    * CIV-7830: fix deprecated method containsClaim
    
    ---------
    
    Co-authored-by: Sabah u Din Irfan <sabah.irfan@gmail.com>
    Co-authored-by: mfallonhmcts <114912573+mfallonhmcts@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/cmc/claimstore/security/JwtGrantedAuthoritiesConverterTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/security/JwtGrantedAuthoritiesConverterTest.java
@@ -48,7 +48,7 @@ class JwtGrantedAuthoritiesConverterTest {
     @DisplayName("No Claims should return empty authorities")
     void shouldReturnEmptyAuthoritiesWhenClaimNotAvailable() {
         Jwt jwt = Mockito.mock(Jwt.class);
-        when(jwt.containsClaim(anyString())).thenReturn(false);
+        when(jwt.hasClaim(anyString())).thenReturn(false);
         Collection<GrantedAuthority> authorities = converter.convert(jwt);
         assertNotNull(authorities);
         assertEquals(0, authorities.size());
@@ -58,7 +58,7 @@ class JwtGrantedAuthoritiesConverterTest {
     @DisplayName("Should return empty authorities when token value is not matching with expected")
     void shouldReturnEmptyAuthoritiesWhenClaimValueNotEquals() {
         Jwt jwt = Mockito.mock(Jwt.class);
-        when(jwt.containsClaim(anyString())).thenReturn(true);
+        when(jwt.hasClaim(anyString())).thenReturn(true);
         when(jwt.getClaim(anyString())).thenReturn("Test");
         Collection<GrantedAuthority> authorities = converter.convert(jwt);
         assertNotNull(authorities);
@@ -69,7 +69,7 @@ class JwtGrantedAuthoritiesConverterTest {
     @DisplayName("Should return empty authorities when token value is not matching with expected")
     void shouldReturnEmptyAuthoritiesWhenIdamReturnsNoUsers() {
         Jwt jwt = Mockito.mock(Jwt.class);
-        when(jwt.containsClaim(anyString())).thenReturn(true);
+        when(jwt.hasClaim(anyString())).thenReturn(true);
         when(jwt.getClaim(anyString())).thenReturn("access_token");
         when(jwt.getTokenValue()).thenReturn("access_token");
         UserInfo userInfo = mock(UserInfo.class);
@@ -85,7 +85,7 @@ class JwtGrantedAuthoritiesConverterTest {
     @DisplayName("Should return empty authorities when token value is not matching with expected")
     void shouldReturnAuthoritiesWhenIdamReturnsUserRoles() {
         Jwt jwt = Mockito.mock(Jwt.class);
-        when(jwt.containsClaim(anyString())).thenReturn(true);
+        when(jwt.hasClaim(anyString())).thenReturn(true);
         when(jwt.getClaim(anyString())).thenReturn("access_token");
         when(jwt.getTokenValue()).thenReturn("access_token");
         UserInfo userInfo = mock(UserInfo.class);
