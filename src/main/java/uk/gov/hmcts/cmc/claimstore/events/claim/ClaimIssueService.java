80b69e2630334007207550e50ffb82709ba56f8c
Sabah u Din Irfan <sabah.irfan@gmail.com>
2023-06-13 16:22:38 +0100

    CIV-8537: automate unstuck claims (#2757)

--- src/main/java/uk/gov/hmcts/cmc/claimstore/events/claim/ClaimIssueService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/events/claim/ClaimIssueService.java
@@ -4,20 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.cmc.claimstore.events.solicitor.RepresentedClaimCreatedEvent;
 import uk.gov.hmcts.cmc.claimstore.models.idam.User;
 import uk.gov.hmcts.cmc.claimstore.services.ClaimService;
 import uk.gov.hmcts.cmc.claimstore.services.UserService;
 import uk.gov.hmcts.cmc.claimstore.stereotypes.LogExecutionTime;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 import uk.gov.hmcts.cmc.domain.models.ClaimState;
-import uk.gov.hmcts.cmc.domain.models.legalrep.Representative;
-import uk.gov.hmcts.cmc.domain.models.party.Party;
 
 import java.util.List;
-import java.util.Optional;
-
-import static uk.gov.hmcts.cmc.claimstore.utils.CommonErrors.MISSING_REPRESENTATIVE;
 
 @Service
 public class ClaimIssueService {
@@ -41,25 +35,23 @@ public class ClaimIssueService {
         User user = userService.authenticateAnonymousCaseWorker();
         String authorisation = user.getAuthorisation();
         List<Claim> createdClaims = claimService.getClaimsByState(ClaimState.CREATE, user);
-        logger.info("Automated Claim Issue - Total claims in Create state: {}",
-            createdClaims.stream().filter(claim -> (claim.getReferenceNumber() != null)).count());
-        createdClaims.stream().filter(claim -> (claim.getReferenceNumber() != null))
+        logger.info("Automated Claim Issue - Total claims in Create state (Stuck Claims): {}",
+            createdClaims
+                .stream()
+                .filter(claim -> (claim.getReferenceNumber() != null))
+                .count());
+        createdClaims
+            .stream()
+            .filter(claim -> (claim.getReferenceNumber() != null))
             .forEach(claim -> issueClaimsPendingInCreatedState(claim, authorisation));
     }
 
     private void issueClaimsPendingInCreatedState(Claim claim, String authorisation) {
-        logger.info("Automated Claim Issue - {}", claim.getReferenceNumber());
         if (claim.getClaimData().isClaimantRepresented()) {
-            String submitterName = claim.getClaimData().getClaimants().stream()
-                .findFirst()
-                .map(Party::getRepresentative)
-                .map(Optional::get)
-                .map(Representative::getOrganisationName)
-                .orElseThrow(() -> new IllegalArgumentException(MISSING_REPRESENTATIVE));
-
-            this.postClaimOrchestrationHandler
-                .representativeIssueHandler(new RepresentedClaimCreatedEvent(claim, submitterName, authorisation));
+            logger.info("Automated Claim Issue should not been triggered "
+                + "as this is Legal Rep case which is deprecated. {}", claim.getReferenceNumber());
         } else {
+            logger.info("Automated Claim Issue for citizen claim ref no: {}", claim.getReferenceNumber());
             String submitterName = claim.getClaimData().getClaimant().getName();
             this.postClaimOrchestrationHandler
                 .citizenIssueHandler(new CitizenClaimCreatedEvent(claim, submitterName, authorisation));
