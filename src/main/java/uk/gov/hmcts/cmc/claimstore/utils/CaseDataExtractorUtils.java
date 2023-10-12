105ab635ab5b22c8e63f22f7bc976d8baf1196e2
zikrurR <workzikr@gmail.com>
2023-10-12 09:16:39 +0100

    initial changes

--- src/main/java/uk/gov/hmcts/cmc/claimstore/utils/CaseDataExtractorUtils.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/utils/CaseDataExtractorUtils.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.cmc.claimstore.utils;
 
+import uk.gov.hmcts.cmc.claimstore.documents.BulkPrintRequestType;
 import uk.gov.hmcts.cmc.domain.models.Claim;
 
 import java.util.List;
@@ -12,6 +13,13 @@ public class CaseDataExtractorUtils {
         // NO-OP
     }
 
+    public static List<String> getRecipient(BulkPrintRequestType bulkPrintRequestType, Claim claim){
+        switch (bulkPrintRequestType) {
+            case DIRECTION_ORDER_LETTER_TYPE, GENERAL_LETTER_TYPE -> getClaimant(claim);
+        }
+        return List.of();
+    }
+
     public static List<String> getDefendant(Claim claim) {
         return List
             .of(requireNonNull(claim
@@ -20,4 +28,14 @@ public class CaseDataExtractorUtils {
                 .getName()
             ));
     }
+
+    private static List<String> getClaimant(Claim claim) {
+        return List
+            .of(requireNonNull(claim
+                .getClaimData()
+                .getClaimant()
+                .getName()
+            ));
+    }
+
 }
