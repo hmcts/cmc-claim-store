5cdba2d131badfc4e5198dc66dfda054335e5369
Astha Malviya <astha.malviya@HMCTS.NET>
2023-11-01 13:12:14 +0000

    adding host too

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -241,12 +241,6 @@ resource "azurerm_key_vault_secret" "cmc-db-username-v15" {
   key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
-resource "azurerm_key_vault_secret" "cmc-db-username-v15" {
-  name         = "cmc-db-username-v15"
-  value        = module.db-v15.username
-  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
-}
-
 resource "azurerm_key_vault_secret" "cmc-db-host-v15" {
   name         = "cmc-db-host-v15"
   value        = module.db-v15.fqdn
