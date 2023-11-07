f17d24d758d9ec2bf1c5f98da6ed4d70f0e3c714
zikrurR <workzikr@gmail.com>
2023-11-07 15:31:46 +0000

    code cleanup

--- src/test/java/uk/gov/hmcts/cmc/claimstore/documents/BulkPrintHandlerTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/documents/BulkPrintHandlerTest.java
@@ -25,6 +25,7 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.cmc.claimstore.documents.BulkPrintRequestType.BULK_PRINT_TRANSFER_TYPE;
@@ -32,6 +33,7 @@ import static uk.gov.hmcts.cmc.claimstore.documents.BulkPrintRequestType.CLAIMAN
 import static uk.gov.hmcts.cmc.claimstore.documents.BulkPrintRequestType.DIRECTION_ORDER_LETTER_TYPE;
 import static uk.gov.hmcts.cmc.claimstore.documents.BulkPrintRequestType.FIRST_CONTACT_LETTER_TYPE;
 import static uk.gov.hmcts.cmc.claimstore.documents.BulkPrintRequestType.GENERAL_LETTER_TYPE;
+import static uk.gov.hmcts.cmc.claimstore.documents.BulkPrintRequestType.PAPER_DEFENCE_TYPE;
 import static uk.gov.hmcts.cmc.claimstore.utils.DocumentNameUtils.buildDefendantLetterClaimantMediationRefusedFileBaseName;
 import static uk.gov.hmcts.cmc.claimstore.utils.DocumentNameUtils.buildOcon9FormFileBaseName;
 import static uk.gov.hmcts.cmc.claimstore.utils.DocumentNameUtils.buildOconFormFileBaseName;
@@ -209,6 +211,51 @@ public class BulkPrintHandlerTest {
     }
 
     @Test
+    public void shouldNotifyForBulkPrintTransferEventWhenLetterTypeNotBulkPrintTransferType() {
+        //given
+        BulkPrintHandler bulkPrintHandler = new BulkPrintHandler(bulkPrintService, launchDarklyClient);
+        Claim claim = mock(Claim.class);
+        when(claim.getReferenceNumber()).thenReturn("AAA");
+        when(claim.getClaimData())
+            .thenReturn(
+                SampleClaimData
+                    .builder()
+                    .withDefendant(
+                        SampleTheirDetails
+                            .builder()
+                            .withName("Dr. John Smith")
+                            .partyDetails())
+                    .build());
+
+        Document coverLetter = new Document("letter", new HashMap<>());
+        Document caseDocument = mock(Document.class);
+        String caseDocumentFileName = "caseDoc.pdf";
+
+        List<BulkPrintTransferEvent.PrintableDocument> caseDocuments = List.of(
+            new BulkPrintTransferEvent.PrintableDocument(caseDocument, caseDocumentFileName)
+        );
+
+        //when
+        bulkPrintHandler.printBulkTransferDocs(claim, coverLetter, caseDocuments, AUTHORISATION);
+
+        //verify
+        verify(bulkPrintService, never()).printPdf(
+            claim,
+            List.of(
+                new PrintablePdf(
+                    coverLetter,
+                    claim.getReferenceNumber() + "-directions-order-cover-sheet"),
+                new PrintablePdf(
+                    caseDocument,
+                    caseDocumentFileName
+                )
+            ),
+            GENERAL_LETTER_TYPE,
+            AUTHORISATION,
+            USER_LIST);
+    }
+
+    @Test
     public void notifyPaperDefenceLetter() {
         //given
         BulkPrintHandler bulkPrintHandler = new BulkPrintHandler(bulkPrintService, launchDarklyClient);
@@ -220,6 +267,33 @@ public class BulkPrintHandlerTest {
 
         //verify
         verify(bulkPrintService).printPdf(
+            claim,
+            ImmutableList.<Printable>builder()
+                .add(new PrintablePdf(
+                    letter,
+                    buildPaperDefenceCoverLetterFileBaseName(claim.getReferenceNumber())))
+                .add(new PrintablePdf(
+                    letter,
+                    buildOconFormFileBaseName(claim.getReferenceNumber())))
+                .build(),
+            PAPER_DEFENCE_TYPE,
+            AUTHORISATION,
+            USER_LIST
+        );
+    }
+
+    @Test
+    public void shouldNotNotifyPaperDefenceLetterWhenGeneralLetterType() {
+        //given
+        BulkPrintHandler bulkPrintHandler = new BulkPrintHandler(bulkPrintService, launchDarklyClient);
+        Claim claim = SampleClaim.getDefault();
+        Document letter = new Document("letter", new HashMap<>());
+
+        //when
+        bulkPrintHandler.printPaperDefence(claim, letter, letter, letter, AUTHORISATION, true);
+
+        //verify
+        verify(bulkPrintService, never()).printPdf(
             claim,
             ImmutableList.<Printable>builder()
                 .add(new PrintablePdf(
@@ -256,7 +330,7 @@ public class BulkPrintHandlerTest {
                     letter,
                     buildOconFormFileBaseName(claim.getReferenceNumber())))
                 .build(),
-            GENERAL_LETTER_TYPE,
+            PAPER_DEFENCE_TYPE,
             AUTHORISATION,
             USER_LIST
         );
@@ -286,7 +360,7 @@ public class BulkPrintHandlerTest {
                     letter,
                     buildOconFormFileBaseName(claim.getReferenceNumber())))
                 .build(),
-            GENERAL_LETTER_TYPE,
+            PAPER_DEFENCE_TYPE,
             AUTHORISATION,
             USER_LIST
         );
