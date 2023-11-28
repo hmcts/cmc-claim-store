923ec2ec132123f794876bbae738173969ffa4f1
zikrurR <workzikr@gmail.com>
2023-11-28 14:56:20 +0000

    CIV-10929

--- src/main/java/uk/gov/hmcts/cmc/claimstore/documents/BulkPrintHandler.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/documents/BulkPrintHandler.java
@@ -183,7 +183,7 @@ public class BulkPrintHandler {
                     oconForm,
                     buildOconFormFileBaseName(claim.getReferenceNumber())))
                 .build(),
-            BulkPrintRequestType.GENERAL_LETTER_TYPE,
+            BulkPrintRequestType.PAPER_DEFENCE_TYPE,
             authorisation,
             CaseDataExtractorUtils.getDefendant(claim));
     }
@@ -198,7 +198,7 @@ public class BulkPrintHandler {
         documents = getPrintables(claim, coverLetter, oconForm, ocon9Form, disableN9Form);
         return bulkPrintService.printPdf(claim,
             documents,
-            BulkPrintRequestType.GENERAL_LETTER_TYPE,
+            BulkPrintRequestType.PAPER_DEFENCE_TYPE,
             authorisation,
             CaseDataExtractorUtils.getDefendant(claim));
     }
