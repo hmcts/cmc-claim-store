a95ffe57dbfe036bef3cb6338a1b3214709e02ff
JackDevN <jack.nicholson@version1.com>
2024-03-04 10:50:30 +0000

    Fixed SonarQube quality failing

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/pilotcourt/PilotCourt.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/pilotcourt/PilotCourt.java
@@ -22,7 +22,7 @@ public class PilotCourt {
 
     public Optional<HearingCourt> getHearingCourt() {
         log.trace("Got HearingCourt: %s", hearingCourt);
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAa " + hearingCourt);
+        log.trace("AAAAAAAAAAAAAAAAAAAAAAAAa " + hearingCourt);
         return Optional.ofNullable(hearingCourt);
     }
 
