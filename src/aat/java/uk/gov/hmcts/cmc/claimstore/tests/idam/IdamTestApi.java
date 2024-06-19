f846cbe7ac85caa83747df044f3419253d055fc8
Jefex7 <duartepireslopes@gmail.com>
2024-06-19 16:01:32 +0100

    Fix error and add suppress CVE

--- src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/idam/IdamTestApi.java
+++ src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/idam/IdamTestApi.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import uk.gov.hmcts.cmc.claimstore.tests.config.FeignConfig;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
