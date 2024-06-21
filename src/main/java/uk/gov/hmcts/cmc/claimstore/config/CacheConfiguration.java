9be6358b0415712c6d8e292628ed2a4c1f34188c
Jefex7 <duartepireslopes@gmail.com>
2024-06-21 09:47:44 +0100

    Add scope to CacheConfiguration

--- src/main/java/uk/gov/hmcts/cmc/claimstore/config/CacheConfiguration.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/config/CacheConfiguration.java
@@ -5,12 +5,15 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.context.WebApplicationContext;
 
 @Configuration
 @EnableCaching
 public class CacheConfiguration {
 
     @Bean
+    @Scope(value = WebApplicationContext.SCOPE_APPLICATION)
     public CacheManagerCustomizer<CaffeineCacheManager> cacheManagerCustomizer() {
         return cacheManager -> cacheManager.setAllowNullValues(false);
     }
