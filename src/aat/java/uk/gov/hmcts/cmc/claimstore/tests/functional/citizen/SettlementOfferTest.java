d9badf0d677a757fa5e29dc832e31b6a47306afb
Jefex7 <duartepireslopes@gmail.com>
2024-06-24 13:57:14 +0100

    @Ignore functional test causing too many request.

--- src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/functional/citizen/SettlementOfferTest.java
+++ src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/functional/citizen/SettlementOfferTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.cmc.claimstore.tests.functional.citizen;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
@@ -18,6 +19,7 @@ import uk.gov.hmcts.cmc.domain.models.sampledata.offers.SampleOffer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Ignore
 public class SettlementOfferTest extends BaseTest {
 
     private User claimant;
