ce2d71a74380f091d0611f25aadd567c99bf5fbd
Jefex7 <duartepireslopes@gmail.com>
2024-06-24 07:03:01 +0100

    Upgrade caffeine and revert Scope config

--- src/main/java/uk/gov/hmcts/cmc/claimstore/config/CacheConfiguration.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/config/CacheConfiguration.java
@@ -5,16 +5,12 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.web.context.WebApplicationContext;
 
 @Configuration
 @EnableCaching
 public class CacheConfiguration {
 
     @Bean
-    @Scope(value = WebApplicationContext.SCOPE_APPLICATION, proxyMode = ScopedProxyMode.TARGET_CLASS)
     public CacheManagerCustomizer<CaffeineCacheManager> cacheManagerCustomizer() {
         return cacheManager -> cacheManager.setAllowNullValues(false);
     }
