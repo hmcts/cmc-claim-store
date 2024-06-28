a5780d7714d971d1801e72d16965df43ad952458
Mike Lemos <gobii@me.com>
2024-06-28 15:37:39 +0100

    DTSCCI-508 adding provider contract test

--- src/contractTest/java/uk/gov/hmcts/cmc/claimstore/api/provider/CmcDefendantApiProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/cmc/claimstore/api/provider/CmcDefendantApiProviderTest.java
@@ -1,20 +1,14 @@
 package uk.gov.hmcts.cmc.claimstore.api.provider;
 
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
-import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
 import au.com.dius.pact.provider.junitsupport.Provider;
-import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
 import au.com.dius.pact.provider.junitsupport.loader.VersionSelector;
-import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.hmcts.cmc.claimstore.controllers.ClaimController;
 
 @ExtendWith(SpringExtension.class)
 @Provider("cmc_defendant")
