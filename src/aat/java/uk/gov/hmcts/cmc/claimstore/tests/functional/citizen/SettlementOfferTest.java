76c911ef905c94ac2cf3010012fcc90c64104c01
Jefex7 <duartepireslopes@gmail.com>
2024-06-28 09:19:06 +0100

    Fix Checkstyle.

--- src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/functional/citizen/SettlementOfferTest.java
+++ src/aat/java/uk/gov/hmcts/cmc/claimstore/tests/functional/citizen/SettlementOfferTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.cmc.claimstore.tests.functional.citizen;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
@@ -18,7 +17,6 @@ import uk.gov.hmcts.cmc.domain.models.sampledata.SampleResponse;
 import uk.gov.hmcts.cmc.domain.models.sampledata.offers.SampleOffer;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 
 public class SettlementOfferTest extends BaseTest  {
 
