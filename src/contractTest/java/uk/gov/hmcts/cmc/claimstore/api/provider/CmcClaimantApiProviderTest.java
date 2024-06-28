ace1027b7a91d304fd59f3255d9a480e2918f27c
Mike Lemos <gobii@me.com>
2024-06-28 14:54:25 +0100

    DTSCCI-508 adding provider contract test

---
+++ src/contractTest/java/uk/gov/hmcts/cmc/claimstore/api/provider/CmcClaimantApiProviderTest.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.cmc.claimstore.api.provider;
+
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.loader.VersionSelector;
+import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
+import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.cmc.claimstore.controllers.ClaimController;
+
+@ExtendWith(SpringExtension.class)
+@Provider("cmc_claimant")
+@PactBroker(scheme = "${PACT_BROKER_SCHEME:http}",
+    host = "${PACT_BROKER_URL:localhost}",
+    port = "${PACT_BROKER_PORT:80}",
+    consumerVersionSelectors = {@VersionSelector(tag = "master")})
+@IgnoreNoPactsToVerify
+public class CmcClaimantApiProviderTest {
+
+    @TestTemplate
+    @ExtendWith(PactVerificationSpringProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+}
