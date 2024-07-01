a94ef80a93fc193d0ced2f9c993c064f2ad47e67
Mike Lemos <gobii@me.com>
2024-07-01 13:53:47 +0100

    DTSCCI-508 adding provider contract test (#2914)
    * DTSCCI-508 adding provider contract test
    
    * DTSCCI-508 adding provider contract test
    
    * DTSCCI-508 adding provider contract test

---
+++ src/contractTest/java/uk/gov/hmcts/cmc/claimstore/api/provider/CmcDefendantLinkedApiProviderTest.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.cmc.claimstore.api.provider;
+
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.loader.VersionSelector;
+import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@Provider("cmc_defendantLinked")
+@PactBroker(scheme = "${PACT_BROKER_SCHEME:http}",
+    host = "${PACT_BROKER_URL:localhost}",
+    port = "${PACT_BROKER_PORT:80}",
+    consumerVersionSelectors = {@VersionSelector(tag = "master")})
+@IgnoreNoPactsToVerify
+public class CmcDefendantLinkedApiProviderTest {
+
+    @TestTemplate
+    @ExtendWith(PactVerificationSpringProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+}
