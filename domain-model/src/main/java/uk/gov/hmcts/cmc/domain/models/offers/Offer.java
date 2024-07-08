da7154a5d432368c55c6567b40557e4a7fe2b77b
EllisD-B <ellis.domengerboyce@hmcts.net>
2024-07-08 09:26:29 +0100

    Checkstyle + resolve conflicts

--- domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/offers/Offer.java
+++ domain-model/src/main/java/uk/gov/hmcts/cmc/domain/models/offers/Offer.java
@@ -1,10 +1,10 @@
 package uk.gov.hmcts.cmc.domain.models.offers;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
