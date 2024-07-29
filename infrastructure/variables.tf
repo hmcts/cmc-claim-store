2b32638bb1e05800d6fa8a1d7924b42996b39e46
58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
2024-07-29 08:35:53 +0000

    Updating Terraform Formatting

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -49,13 +49,13 @@ variable "subscription" {}
 variable "aks_subscription_id" {}
 
 variable "jenkins_AAD_objectId" {
-  type                        = string
-  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+  type        = string
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
 variable "appinsights_instrumentation_key" {
   description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
-  default = ""
+  default     = ""
 }
 
 variable "send_grid_subscription" {
