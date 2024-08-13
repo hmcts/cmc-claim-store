c323ed821e4ef8b770ea1448a628ad1a474dc20b
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2024-08-13 14:04:39 +0100

    Add cache configuration

--- src/main/java/uk/gov/hmcts/cmc/claimstore/config/CacheConfiguration.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/config/CacheConfiguration.java
@@ -1,6 +1,10 @@
 package uk.gov.hmcts.cmc.claimstore.config;
 
+import java.util.concurrent.TimeUnit;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
@@ -13,5 +17,17 @@ public class CacheConfiguration {
     @Bean
     public CacheManagerCustomizer<CaffeineCacheManager> cacheManagerCustomizer() {
         return cacheManager -> cacheManager.setAllowNullValues(false);
+    }
+
+    @Bean
+    public Caffeine caffeineConfig() {
+        return Caffeine.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
     }
 }
