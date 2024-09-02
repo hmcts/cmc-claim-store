ec9026c92c24cb7b5d0f56a9ecf492268f2ad4c8
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2024-09-02 16:10:08 +0100

    DTSCCI-242: Remove @Cacheable annotation from interface

--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/user/IUserAuthorisationTokenService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/user/IUserAuthorisationTokenService.java
@@ -1,10 +1,5 @@
 package uk.gov.hmcts.cmc.claimstore.services.user;
 
-import org.springframework.cache.annotation.Cacheable;
-import uk.gov.hmcts.cmc.claimstore.stereotypes.LogExecutionTime;
-
 public interface IUserAuthorisationTokenService {
-    @LogExecutionTime
-    @Cacheable(value = "userOIDTokenCache")
     String getAuthorisationToken(String username, String password);
 }
--- src/main/java/uk/gov/hmcts/cmc/claimstore/services/user/IUserInfoService.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/user/IUserInfoService.java
@@ -1,11 +1,7 @@
 package uk.gov.hmcts.cmc.claimstore.services.user;
 
-import org.springframework.cache.annotation.Cacheable;
 import uk.gov.hmcts.cmc.claimstore.models.idam.UserInfo;
-import uk.gov.hmcts.cmc.claimstore.stereotypes.LogExecutionTime;
 
 public interface IUserInfoService {
-    @LogExecutionTime
-    @Cacheable(value = "userInfoCache")
     UserInfo getUserInfo(String bearerToken);
 }
