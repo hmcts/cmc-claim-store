ff3f44b9cb293a7ec120ce571991fda81972f33b
AbuSalamHMCTS <138011124+AbuSalamHMCTS@users.noreply.github.com>
2024-09-06 13:13:09 +0100

    DTSCCI-242 CMC Claim Store functional test 429 errors (#2976)
    * DTSCCI-242: Fix @cacheable self invocation
    
    * DTSCCI-242: Fix contract test config
    
    * Re-enable pact tests on PR
    
    * DTSCCI-242: Fix @Cacheable self invocation
    
    * DTSCCI-242: Create cacheable beans for user authorisations
    
    * DTSCCI-242: Create cacheable beans for user authorisations
    
    * DTSCCI-242 Caching fix and Jcenter fix
    
    * DTSCCI-242 Checkstyle fix
    
    * DTSCCI-242 Code clean up
    
    * DTSCCI-242: Remove @Cacheable annotation from interface
    
    * DTSCCI-242: Enable cacheable tokens in preview and aat
    
    * DTSCCI-242: update hmcts java to resolve build error
    
    * Revert "DTSCCI-242: update hmcts java to resolve build error"
    
    This reverts commit 18b62e45434503725f02c99e57caddbd4f334155.
    
    * DTSCCI-242: Correct property name
    
    ---------
    
    Co-authored-by: Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
    Co-authored-by: AbuSalam22 <136581242+AbuSalam22@users.noreply.github.com>
    Co-authored-by: Ruban <ruban72@hotmail.com>

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/user/UserAuthorisationTokenService.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.cmc.claimstore.services.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.cmc.claimstore.models.idam.Oauth2;
+import uk.gov.hmcts.cmc.claimstore.models.idam.TokenExchangeResponse;
+import uk.gov.hmcts.cmc.claimstore.requests.idam.IdamApi;
+import uk.gov.hmcts.cmc.claimstore.stereotypes.LogExecutionTime;
+
+@Component
+@ConditionalOnProperty(prefix = "idam.user.token.cache", name = "enabled", havingValue = "false", matchIfMissing = true)
+public class UserAuthorisationTokenService implements IUserAuthorisationTokenService {
+    public static final String GRANT_TYPE_PASSWORD = "password";
+    public static final String DEFAULT_SCOPE = "openid profile roles";
+    public static final String BEARER = "Bearer ";
+
+    private final IdamApi idamApi;
+    private final Oauth2 oauth2;
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public UserAuthorisationTokenService(IdamApi idamApi, Oauth2 oauth2) {
+        this.idamApi = idamApi;
+        this.oauth2 = oauth2;
+    }
+
+    @Override
+    @LogExecutionTime
+    public String getAuthorisationToken(String username, String password) {
+        logger.info("IDAM /o/token invoked.");
+        TokenExchangeResponse tokenExchangeResponse = idamApi.exchangeToken(
+            oauth2.getClientId(),
+            oauth2.getClientSecret(),
+            oauth2.getRedirectUrl(),
+            GRANT_TYPE_PASSWORD,
+            username,
+            password,
+            DEFAULT_SCOPE);
+        return BEARER + tokenExchangeResponse.getAccessToken();
+    }
+}
