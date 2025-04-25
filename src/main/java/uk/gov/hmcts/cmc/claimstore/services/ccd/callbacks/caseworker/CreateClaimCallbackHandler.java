715659ca03b4fc5041d34e8f365f4aa861716e4e
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-04-25 15:57:54 +0100

    DTSCCI-1530 changes

---
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/CreateClaimCallbackHandler.java
@@ -0,0 +1,83 @@
+package uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.caseworker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.cmc.ccd.domain.CaseEvent;
+import uk.gov.hmcts.cmc.ccd.mapper.CaseMapper;
+import uk.gov.hmcts.cmc.claimstore.exceptions.ForbiddenActionException;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.Role;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.Callback;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackHandler;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackParams;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackType;
+import uk.gov.hmcts.cmc.claimstore.stereotypes.LogExecutionTime;
+import uk.gov.hmcts.cmc.claimstore.utils.CaseDetailsConverter;
+import uk.gov.hmcts.cmc.domain.models.Claim;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.CREATE_CLAIM;
+import static uk.gov.hmcts.cmc.claimstore.services.ccd.Role.CASEWORKER;
+
+@Service
+public class CreateClaimCallbackHandler extends CallbackHandler {
+
+    private static final List<CaseEvent> EVENTS = Arrays.asList(CREATE_CLAIM);
+    private static final List<Role> ROLES = Collections.singletonList(CASEWORKER);
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final CaseDetailsConverter caseDetailsConverter;
+    private final CaseMapper caseMapper;
+    private final boolean featureCreateClaimEnabled;
+
+    @Autowired
+    public CreateClaimCallbackHandler(
+        CaseDetailsConverter caseDetailsConverter,
+        CaseMapper caseMapper,
+        @Value("${feature_toggles.create_claim_enabled:false}") boolean featureCreateClaimEnabled
+    ) {
+        this.caseDetailsConverter = caseDetailsConverter;
+        this.caseMapper = caseMapper;
+        this.featureCreateClaimEnabled = featureCreateClaimEnabled;
+    }
+
+    @Override
+    protected Map<CallbackType, Callback> callbacks() {
+        return Map.of(CallbackType.ABOUT_TO_SUBMIT, this::createClaim);
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    @Override
+    public List<Role> getSupportedRoles() {
+        return ROLES;
+    }
+
+    @LogExecutionTime
+    private CallbackResponse createClaim(CallbackParams callbackParams) {
+        logger.info("Create claim feature is: {}", featureCreateClaimEnabled ? "enabled" : "disabled");
+        if (!featureCreateClaimEnabled) {
+            throw new ForbiddenActionException("Create claim is not permitted.");
+        }
+        Claim claim = caseDetailsConverter.extractClaim(callbackParams.getRequest().getCaseDetails());
+        logger.info("Creating case worker case for callback of type {}, claim with external id {}",
+            callbackParams.getType(),
+            claim.getExternalId());
+
+        return AboutToStartOrSubmitCallbackResponse
+            .builder()
+            .data(caseDetailsConverter.convertToMap(caseMapper.to(claim)))
+            .build();
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/ccd/callbacks/caseworker/CreateClaimCallbackHandlerTest.java
@@ -0,0 +1,100 @@
+package uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.caseworker;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.cmc.ccd.mapper.CaseMapper;
+import uk.gov.hmcts.cmc.claimstore.exceptions.ForbiddenActionException;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackParams;
+import uk.gov.hmcts.cmc.claimstore.services.ccd.callbacks.CallbackType;
+import uk.gov.hmcts.cmc.claimstore.utils.CaseDetailsConverter;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+import static java.time.LocalDate.now;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.cmc.ccd.domain.CaseEvent.CREATE_LEGAL_REP_CLAIM;
+import static uk.gov.hmcts.cmc.claimstore.services.ccd.Role.SOLICITOR;
+import static uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaim.getLegalDataWithReps;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CreateClaimCallbackHandlerTest {
+
+    public static final String REFERENCE_NO = "000LR001";
+    public static final LocalDate ISSUE_DATE = now();
+    public static final LocalDate RESPONSE_DEADLINE = ISSUE_DATE.plusDays(14);
+    private static final String BEARER_TOKEN = "Bearer let me in";
+
+    @Mock
+    private CaseDetailsConverter caseDetailsConverter;
+    @Mock
+    private CaseMapper caseMapper;
+    private CallbackRequest callbackRequest;
+    private CreateClaimCallbackHandler createClaimCallbackHandler;
+
+    private final CaseDetails caseDetails = CaseDetails.builder().id(3L).data(Collections.emptyMap()).build();
+
+    @Before
+    public void setUp() {
+        createClaimCallbackHandler = new CreateClaimCallbackHandler(
+            caseDetailsConverter,
+            caseMapper,
+            true
+        );
+
+        callbackRequest = CallbackRequest
+            .builder()
+            .eventId(CREATE_LEGAL_REP_CLAIM.getValue())
+            .caseDetails(caseDetails)
+            .build();
+
+        when(caseDetailsConverter.extractClaim(any(CaseDetails.class))).thenReturn(getLegalDataWithReps());
+    }
+
+    @Test
+    public void shouldSuccessfullyReturnCallBackResponse() {
+
+        CallbackParams callbackParams = CallbackParams.builder()
+            .type(CallbackType.ABOUT_TO_SUBMIT)
+            .request(callbackRequest)
+            .params(ImmutableMap.of(CallbackParams.Params.BEARER_TOKEN, BEARER_TOKEN))
+            .build();
+
+        AboutToStartOrSubmitCallbackResponse response = (AboutToStartOrSubmitCallbackResponse)
+            createClaimCallbackHandler.handle(callbackParams);
+
+        assertThat(response.getErrors()).isNull();
+        assertThat(response.getWarnings()).isNull();
+    }
+
+    @Test
+    public void shouldHaveCorrectLegalRepSupportingRole() {
+        assertThat(createClaimCallbackHandler.getSupportedRoles().size()).isEqualTo(1);
+        assertThat(createClaimCallbackHandler.getSupportedRoles()).contains(SOLICITOR);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenFeatureCreateClaimIsDisabled() {
+        CallbackParams callbackParams = CallbackParams.builder()
+            .type(CallbackType.ABOUT_TO_SUBMIT)
+            .request(callbackRequest)
+            .params(ImmutableMap.of(CallbackParams.Params.BEARER_TOKEN, BEARER_TOKEN))
+            .build();
+        createClaimCallbackHandler = new CreateClaimCallbackHandler(
+            caseDetailsConverter,
+            caseMapper,
+            false
+        );
+        assertThrows(ForbiddenActionException.class, () -> createClaimCallbackHandler.handle(callbackParams));
+    }
+}
