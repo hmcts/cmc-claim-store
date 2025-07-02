b878e62aceae31467f22a8b68d367202f3071df1
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-07-02 09:21:49 +0100

    DTSCCI-2344 Update teraform version and fix pipieline (#3249)
    * Create ado artifacts build
    
    * DTSCCI-2344 changes
    
    * DTSCCI-2344 update terraform/azuread version
    
    * DTSCCI-2344 update terraform/azurerm version

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,14 +4,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0.0"
     }
     random = {
       source = "hashicorp/random"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "1.6.0"
+      version = "2.33.0"
     }
   }
 }
