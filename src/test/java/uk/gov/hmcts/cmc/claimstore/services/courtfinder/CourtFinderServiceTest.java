a7bfab9b0cc34f60c380b87d3c1fc4c61277b19a
Ruban <ruban72@hotmail.com>
2025-10-20 11:54:08 +0100

    DTSCC-3095: Java/Spring upgrade

--- src/test/java/uk/gov/hmcts/cmc/claimstore/services/courtfinder/CourtFinderServiceTest.java
+++ src/test/java/uk/gov/hmcts/cmc/claimstore/services/courtfinder/CourtFinderServiceTest.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.cmc.claimstore.services.courtfinder;
 
 import com.google.common.collect.ImmutableList;
+import feign.FeignException;
+import feign.Request;
+import feign.Response;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,12 +12,21 @@ import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.cmc.claimstore.controllers.CourtFinderController;
 import uk.gov.hmcts.cmc.claimstore.models.courtfinder.AreaOfLaw;
 import uk.gov.hmcts.cmc.claimstore.models.courtfinder.Court;
+import uk.gov.hmcts.cmc.claimstore.models.factapi.courtfinder.search.postcode.SearchCourtByPostcodeResponse;
 import uk.gov.hmcts.cmc.claimstore.requests.courtfinder.CourtFinderApi;
+import uk.gov.hmcts.cmc.claimstore.test.utils.DataFactory;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -34,6 +46,19 @@ public class CourtFinderServiceTest {
 
     @Mock
     private CourtFinderService courtFinderService;
+
+    private FeignException createFeignException() {
+        Map<String, Collection<String>> headers = new HashMap<>();
+        Request request = Request.create(Request.HttpMethod.GET, "/test", headers, new byte[0], StandardCharsets.UTF_8);
+        Response response = Response.builder()
+            .status(500)
+            .reason("failure")
+            .request(request)
+            .headers(headers)
+            .body(new byte[0])
+            .build();
+        return FeignException.errorStatus("method", response);
+    }
 
     @Before
     public void setup() {
@@ -55,6 +80,38 @@ public class CourtFinderServiceTest {
     public void shouldGetNoCourtsByName() {
         List<Court> courtList = courtFinderService.getCourtsByName("Newcastle Civil & Family Courts and Tribunals Centre");
         assertThat(courtList.size()).isZero();
+    }
+
+    @Test
+    public void shouldReturnEmptyListWhenPostcodeLookupFailsGracefully() {
+        CourtFinderService service = new CourtFinderService(courtFinderApi);
+        when(courtFinderApi.findMoneyClaimCourtByPostcode("NE1"))
+            .thenThrow(createFeignException());
+
+        List<Court> courts = service.getCourtDetailsListFromPostcode("NE1");
+
+        assertThat(courts).isEmpty();
+        verify(courtFinderApi, never()).getCourtDetailsFromNameSlug(anyString());
+    }
+
+    @Test
+    public void shouldReturnSlugOnlyWhenCourtDetailsLookupFails() {
+        CourtFinderService service = new CourtFinderService(courtFinderApi);
+        SearchCourtByPostcodeResponse searchResponse =
+            DataFactory.createSearchCourtByPostcodeResponseFromJson(SEARCH_BY_POSTCODE_NEWCASTLE_RESPONSE);
+        String expectedSlug = searchResponse.getCourts().get(0).getSlug();
+
+        when(courtFinderApi.findMoneyClaimCourtByPostcode("NE1"))
+            .thenReturn(searchResponse);
+        when(courtFinderApi.getCourtDetailsFromNameSlug(expectedSlug))
+            .thenThrow(createFeignException());
+
+        List<Court> courts = service.getCourtDetailsListFromPostcode("NE1");
+
+        assertThat(courts).hasSize(1);
+        Court fallbackCourt = courts.get(0);
+        assertThat(fallbackCourt.getSlug()).isEqualTo(expectedSlug);
+        assertThat(fallbackCourt.getName()).isNull();
     }
 
 }
