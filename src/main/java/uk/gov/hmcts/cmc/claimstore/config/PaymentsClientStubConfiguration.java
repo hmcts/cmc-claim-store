888353881f6409a6eba136e4c1459e197762fa8f
Ruban <ruban72@hotmail.com>
2026-02-03 14:59:00 +0000

    DTSCC-3203: Fix payments issue

--- src/main/java/uk/gov/hmcts/cmc/claimstore/config/PaymentsClientStubConfiguration.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/config/PaymentsClientStubConfiguration.java
@@ -1,9 +1,9 @@
 package uk.gov.hmcts.cmc.claimstore.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import uk.gov.hmcts.cmc.claimstore.services.payments.PaymentsClientStub;
 import uk.gov.hmcts.reform.payments.client.PaymentsClient;
 
